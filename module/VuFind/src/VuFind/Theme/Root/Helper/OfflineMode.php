16ef392b5b599301fdd8cbd0e48224ee2490678a
Demian Katz <demian.katz@villanova.edu>
2012-06-25 15:49:23 -0400

    Introduced "offline mode" view helper.

---
+++ module/VuFind/src/VuFind/Theme/Root/Helper/OfflineMode.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * "Offline mode" view helper
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/building_a_recommendations_module Wiki
+ */
+namespace VuFind\Theme\Root\Helper;
+use VuFind\Search\Options, Zend\View\Helper\AbstractHelper;
+
+/**
+ * "Offline mode" view helper
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/building_a_recommendations_module Wiki
+ */
+class OfflineMode extends AbstractHelper
+{
+    /**
+     * Wrapper function to the VF_Search_Options getInstance function
+     *
+     * @param string $type The search type of the object to retrieve
+     *
+     * @return SearchOptions
+     */
+    public function __invoke()
+    {
+        /* TODO:
+        return VF_Connection_Manager::connectToCatalog()->getOfflineMode();
+         */
+        return '';
+    }
+}
