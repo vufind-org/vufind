5549221739e6f82ffe87fb8f58bdc9c58310111f
Demian Katz <demian.katz@villanova.edu>
2012-06-27 10:24:47 -0400

    Began setting up unit testing infrastructure; ported over ISBN code as a first test case.

---
+++ module/VuFind/tests/Code/ISBNTest.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * ISBN Test Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/unit_tests Wiki
+ */
+namespace VuFind\Tests;
+use VuFind\Code\ISBN;
+
+/**
+ * ISBN Test Class
+ *
+ * @category VuFind2
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Chris Hallberg <challber@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/unit_tests Wiki
+ */
+class ISBNTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test Valid ISBN-10.
+     *
+     * @return void
+     */
+    public function testValidISBN10()
+    {
+        $isbn = new ISBN('0123456789');
+        $this->assertEquals($isbn->get10(), '0123456789');
+        $this->assertEquals($isbn->get13(), '9780123456786');
+        $this->assertTrue($isbn->isValid());
+    }
+
+    /**
+     * Test Valid ISBN-13.
+     *
+     * @return void
+     */
+    public function testValidISBN13()
+    {
+        $isbn = new ISBN('9780123456786');
+        $this->assertEquals($isbn->get10(), '0123456789');
+        $this->assertEquals($isbn->get13(), '9780123456786');
+        $this->assertTrue($isbn->isValid());
+    }
+
+    /**
+     * Test Valid ISBN-10 with dashes.
+     *
+     * @return void
+     */
+    public function testValidISBN10WithDashes()
+    {
+        $isbn = new ISBN('0-12-345678-9');
+        $this->assertEquals($isbn->get10(), '0123456789');
+        $this->assertEquals($isbn->get13(), '9780123456786');
+        $this->assertTrue($isbn->isValid());
+    }
+
+    /**
+     * Test Valid ISBN-13 with dashes.
+     *
+     * @return void
+     */
+    public function testValidISBN13WithDashes()
+    {
+        // Valid ISBN-13 with dashes:
+        $isbn = new ISBN('978-0-12-345678-6');
+        $this->assertEquals($isbn->get10(), '0123456789');
+        $this->assertEquals($isbn->get13(), '9780123456786');
+        $this->assertTrue($isbn->isValid());
+    }
+
+    /**
+     * Test Valid ISBN-13 that is not part of the Bookland EAN.
+     *
+     * @return void
+     */
+    public function testValidISBN13OutsideOfBooklandEAN()
+    {
+        // Valid ISBN-13 outside of Bookland EAN:
+        $isbn = new ISBN('9790123456785');
+        $this->assertEquals($isbn->get10(), false);
+        $this->assertEquals($isbn->get13(), '9790123456785');
+        $this->assertTrue($isbn->isValid());
+    }
+
+    /**
+     * Test Invalid ISBN-10.
+     *
+     * @return void
+     */
+    public function testInvalidISBN10()
+    {
+        // Invalid ISBN-10:
+        $isbn = new ISBN('2314346323');
+        $this->assertEquals($isbn->get10(), false);
+        $this->assertEquals($isbn->get13(), false);
+        $this->assertFalse($isbn->isValid());
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+use Zend\Loader\AutoloaderFactory;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\Service\ServiceManagerConfiguration;
+
+// Set flag that we're in test mode
+define('VUFIND_PHPUNIT_RUNNING', 1);
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', dirname(__DIR__));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('VUFIND_ENV') ? getenv('VUFIND_ENV') : 'testing'));
+
+// Define path to local override directory
+defined('LOCAL_OVERRIDE_DIR')
+    || define('LOCAL_OVERRIDE_DIR', (getenv('VUFIND_LOCAL_DIR') ? getenv('VUFIND_LOCAL_DIR') : ''));
+
+chdir(APPLICATION_PATH);
+
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    $loader = include 'vendor/autoload.php';
+}
+
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2.');
+}
+
+// Get application stack configuration
+$configuration = include 'config/application.config.php';
+
+// Setup service manager
+$serviceManager = new ServiceManager(new ServiceManagerConfiguration($configuration['service_manager']));
+$serviceManager->setService('ApplicationConfiguration', $configuration);
+$serviceManager->get('ModuleManager')->loadModules();
