841e8a6ed08d8daa9440e4e4897684e558e83196
Demian Katz <demian.katz@villanova.edu>
2012-07-02 16:06:20 -0400

    Fixed naming conflict.

--- module/VuFind/src/VuFind/Search/EmptySet/Results.php
+++ module/VuFind/src/VuFind/Search/EmptySet/Results.php
@@ -25,7 +25,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://www.vufind.org  Main Page
  */
-namespace VuFind\Search\Empty;
+namespace VuFind\Search\EmptySet;
 use VuFind\Search\Base\Results as BaseResults;
 
 /**
