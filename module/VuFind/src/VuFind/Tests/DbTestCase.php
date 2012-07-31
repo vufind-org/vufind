86339ce6679488c23e26a3af42dcf65d6f2c9576
Demian Katz <demian.katz@villanova.edu>
2012-07-31 10:30:57 -0400

    Corrected comments.

--- module/VuFind/src/VuFind/Tests/DbTestCase.php
+++ module/VuFind/src/VuFind/Tests/DbTestCase.php
@@ -22,7 +22,7 @@
  *
  * @category VuFind2
  * @package  Tests
- * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://vufind.org/wiki/unit_tests Wiki
  */
@@ -35,7 +35,7 @@ use VuFind\Db\AdapterFactory as DbAdapterFactory,
  *
  * @category VuFind2
  * @package  Tests
- * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://vufind.org/wiki/unit_tests Wiki
  */
