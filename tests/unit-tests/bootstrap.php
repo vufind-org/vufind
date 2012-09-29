8cc23f0fd2e85e25cf77271d0f3faa6ab004d727
David Maus <maus@hab.de>
2012-09-29 14:54:48 +0200

    Remove hard-coded dependency on ZF2 vendor/ path

--- tests/unit-tests/bootstrap.php
+++ tests/unit-tests/bootstrap.php
@@ -1,30 +1,23 @@
 <?php
 
 /**
- * Proxy service PHPUnit bootstrap.
+ * Proxy servce PHPUnit bootstrap.
  *
  * @author    David Maus <maus@hab.de>
  * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @copyright Copyright (C) Villanova University 2011
+ *
  */
 
-$zf2 = realpath(__DIR__ . '/../../vendor/zf2/library/');
-
-if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-    require_once(
-        realpath("{$zf2}/Zend/Loader/AutoloaderFactory.php")
-    );
-    \Zend\Loader\AutoloaderFactory::factory(
-        array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    'Zend' => "{$zf2}/Zend"
-                )
-            )
+require_once('Zend/Loader/AutoloaderFactory.php');
+\Zend\Loader\AutoloaderFactory::factory(
+    array(
+        'Zend\Loader\StandardAutoloader' => array(
+            'namespaces' => array(
+                'VuFindProxy' => realpath(__DIR__ . '/../../src/VuFindProxy'),
+                'VuFindTest' => realpath(__DIR__ . '/src/VuFindTest'),
+            ),
+            'autoregister_zf' => true
         )
-    );
-}
-
-$loader = \Zend\Loader\AutoloaderFactory::getRegisteredAutoloader('Zend\Loader\StandardAutoloader');
-$loader->registerNamespace('VuFindProxy', realpath(__DIR__ . '/../../src/VuFindProxy'));
-$loader->registerNamespace('VuFindTest', realpath(__DIR__ . '/src/VuFindTest'));
+    )
+);
