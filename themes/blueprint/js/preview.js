75e38eadfd9785b034a0b39b8c0d0cea0fca5f3b
Demian Katz <demian.katz@villanova.edu>
2012-10-10 08:59:10 -0400

    Improved preview compatibility with IE versions prior to 9 (see VUFIND-318).

--- themes/blueprint/js/preview.js
+++ themes/blueprint/js/preview.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    if (!Array.prototype.indexOf) {
+        setIndexOf();
+    }
     getBookPreviews();
 });
 
@@ -116,5 +119,43 @@ function processBookInfo(booksInfo, previewClass) {
                 $link.attr('href', bookInfo.preview_url).show();
             }
         }
+    }
+}
+/**
+ * Array.indexOf is not universally supported
+ * We need to set it for users who don't have it.
+ *
+ * developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/indexOf
+ */
+function setIndexOf() {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 1) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
     }
 }
