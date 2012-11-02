fa5287011d2305f00d18e1a6ab791da18cf8050d
Demian Katz <demian.katz@villanova.edu>
2012-11-02 14:16:27 -0400

    Style fix.

--- module/VuFind/src/VuFind/Controller/BrowseController.php
+++ module/VuFind/src/VuFind/Controller/BrowseController.php
@@ -583,7 +583,8 @@ class BrowseController extends AbstractBase
         if (isset($result[$facet])) {
             // Sort facets alphabetically if configured to do so:
             if (isset($this->config->Browse->alphabetical_order)
-                && $this->config->Browse->alphabetical_order) {
+                && $this->config->Browse->alphabetical_order
+            ) {
                 if (isset($this->config->Site->locale)) {
                     setlocale(LC_ALL, $this->config->Site->locale . ".utf8");
                     $callback = function ($a, $b) {
