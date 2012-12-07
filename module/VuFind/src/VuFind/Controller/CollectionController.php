b8aa7e713ee14f5e3b14535e6c28bd155f0a591f
Demian Katz <demian.katz@villanova.edu>
2012-12-07 10:00:57 -0500

    Created collection controller; for starters, just an extension of AbstractRecord with a few minor changes and some cloned Solr Record templates.

---
+++ module/VuFind/src/VuFind/Controller/CollectionController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Collection Controller
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Controller
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+namespace VuFind\Controller;
+
+/**
+ * Collection Controller
+ *
+ * @category VuFind2
+ * @package  Controller
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class CollectionController extends AbstractRecord
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        // Call standard record controller initialization:
+        parent::__construct();
+
+        // TODO: set appropriate default tab
+    }
+
+    /**
+     * Get the tab configuration for this controller.
+     *
+     * @return array
+     */
+    protected function getTabConfiguration()
+    {
+        // TODO: fill in
+        return array();
+    }
+
+    /**
+     * Display a particular tab.
+     *
+     * @param string $tab  Name of tab to display
+     * @param bool   $ajax Are we in AJAX mode?
+     *
+     * @return mixed
+     */
+    protected function showTab($tab, $ajax = false)
+    {
+        $result = parent::showTab($tab, $ajax);
+        if (!$ajax && $result instanceof \Zend\View\Model\ViewModel) {
+            $result->setTemplate('collection/view');
+        }
+        return $result;
+    }
+}
