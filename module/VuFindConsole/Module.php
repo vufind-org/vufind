2b83f0b001e0a2cc898a598a592b9c744ee9fc61
Demian Katz <demian.katz@villanova.edu>
2013-01-10 13:50:19 -0500

    Added missing module comments.

--- module/VuFindConsole/Module.php
+++ module/VuFindConsole/Module.php
@@ -1,16 +1,60 @@
 <?php
-
+/**
+ * ZF2 module definition for the VuFind console module
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Module
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/dmj/vf2-proxy
+ */
 namespace VuFindConsole;
 use VuFindConsole\Mvc\Router\ConsoleRouter,
     Zend\ModuleManager\ModuleManager, Zend\Mvc\MvcEvent;
 
+/**
+ * ZF2 module definition for the VuFind console module
+ *
+ * @category VuFind2
+ * @package  Module
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/dmj/vf2-proxy
+ */
 class Module
 {
+    /**
+     * Get module configuration
+     *
+     * @return array
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * Get autoloader configuration
+     *
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
         return array(
@@ -22,10 +66,20 @@ class Module
         );
     }
 
+    /**
+     * Initialize the module
+     *
+     * @return void
+     */
     public function init(ModuleManager $m)
     {
     }
 
+    /**
+     * Bootstrap the module
+     *
+     * @return void
+     */
     public function onBootstrap(MvcEvent $e)
     {
         $callback = function ($e) {
