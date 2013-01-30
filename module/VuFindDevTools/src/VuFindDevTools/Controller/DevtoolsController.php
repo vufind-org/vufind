cd6984e65795c0b9822eade198e28fd44ee9fdf8
Demian Katz <demian.katz@villanova.edu>
2013-01-30 09:31:16 -0500

    Style fix.

--- module/VuFindDevTools/src/VuFindDevTools/Controller/DevtoolsController.php
+++ module/VuFindDevTools/src/VuFindDevTools/Controller/DevtoolsController.php
@@ -116,7 +116,7 @@ class DevtoolsController extends \VuFind\Controller\AbstractBase
         // Find strings missing from language 2:
         $notInL2 = array();
         $l2Keys = array_keys((array)$lang2);
-        foreach($lang1 as $key => $junk) {
+        foreach ($lang1 as $key => $junk) {
             if (!in_array($key, $l2Keys)) {
                 $notInL2[] = $key;
             }
