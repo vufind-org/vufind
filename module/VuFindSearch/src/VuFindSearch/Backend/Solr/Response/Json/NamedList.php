b77576e1c2100147cf2c304f04ca7d5a5c4eb05b
David Maus <maus@hab.de>
2013-02-15 08:30:25 +0100

    New class: Iteratable NamedList `arrarr'
    * VuFindSearch/Backend/Solr/Response/Json/NamedList.php: New
    class. Iteratable NamedList `arrarr'.

---
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/Response/Json/NamedList.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * SOLR NamedList with parameter json.nl=arrarr.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindSearch\Backend\Solr\Response\Json;
+
+use Iterator;
+
+/**
+ * SOLR NamedList with parameter json.nl=arrarr.
+ *
+ * A NamedList arrarr represent a NamedList as an array of two element arrays
+ * [[name1,val1], [name2, val2], [name3,val3]].
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ * @see      http://wiki.apache.org/solr/SolJSON
+ *
+ */
+class NamedList implements Iterator
+{
+    /**
+     * The named list.
+     *
+     * @var array
+     */
+    protected $list;
+
+    /**
+     * Constructor.
+     *
+     * @param array $list Named list
+     *
+     * @return void
+     */
+    public function __construct (array $list)
+    {
+        $this->list = $list;
+    }
+
+    /// Iterator
+
+    /**
+     * Return current element value.
+     *
+     * @return mixed
+     */
+    public function current ()
+    {
+        return $this->valid() ? $this->current[1] : null;
+    }
+
+    /**
+     * Return current element name.
+     *
+     * @return string
+     */
+    public function key ()
+    {
+        return $this->valid() ? $this->current[0] : null;
+    }
+
+    /**
+     * Move to next element.
+     *
+     * @return void
+     */
+    public function next ()
+    {
+        $this->current = next($this->list);
+    }
+
+    /**
+     * Return true if the iterator is at a valid position.
+     *
+     * @return boolean
+     */
+    public function valid ()
+    {
+        return is_array($this->current);
+    }
+
+    /**
+     * Rewind iterator.
+     *
+     * @return void
+     */
+    public function rewind ()
+    {
+        reset($this->list);
+        $this->current = current($this->list);
+    }
+}
---
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Solr/Response/Json/NamedListTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Unit tests for SOLR NamedList.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindTest\Backend\Solr\Json\Response;
+
+use VuFindSearch\Backend\Solr\Response\Json\NamedList;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Unit tests for SOLR NamedList.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class NamedListTest extends TestCase
+{
+    /**
+     * Test iterating over the list.
+     *
+     * @return void
+     */
+    public function testIterate ()
+    {
+        $list = new NamedList(array(array('first term', 'info'), array('second term', 'info')));
+        $keys = array();
+        foreach ($list as $key => $value) {
+            $keys []= $key;
+        }
+        $this->assertEquals(array('first term', 'second term'), $keys);
+    }
+}
