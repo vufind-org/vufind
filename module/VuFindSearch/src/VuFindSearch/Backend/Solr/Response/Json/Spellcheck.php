1e89e720fa89f2fe7af8b73649b89cfa9e2287cc
David Maus <maus@hab.de>
2013-02-15 09:03:31 +0100

    New class: SOLR Spellcheck information
    * VuFindSearch/Backend/Solr/Response/Json/Spellcheck.php: New
    class. SOLR Spellcheck information.

---
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/Response/Json/Spellcheck.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * SOLR spellcheck information.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindSearch\Backend\Solr\Response\Json;
+
+use IteratorAggregate;
+use ArrayObject;
+use Countable;
+
+/**
+ * SOLR spellcheck information.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class Spellcheck implements IteratorAggregate, Countable
+{
+    /**
+     * Spellcheck terms mapped to term information.
+     *
+     * @var ArrayObject
+     */
+    protected $terms;
+
+    /**
+     * Constructor.
+     *
+     * @param array $spellcheck SOLR spellcheck information
+     *
+     * @return void
+     */
+    public function __construct (array $spellcheck)
+    {
+        $this->terms = new ArrayObject();
+        $list = new NamedList($spellcheck);
+        foreach ($list as $term => $info) {
+            if (is_array($info)) {
+                $this->terms->offsetSet($term, $info);
+            }
+        }
+    }
+
+    /**
+     * Merge in other spellcheck information.
+     *
+     * @param Spellcheck $spellcheck Other spellcheck information
+     *
+     * @return void
+     */
+    public function mergeWith (Spellcheck $spellcheck)
+    {
+        $this->terms->uksort(array($this, 'compareTermLength'));
+        foreach ($spellcheck as $term => $info) {
+            if (!$this->contains($term)) {
+                $this->terms->offsetSet($term, $info);
+            }
+        }
+    }
+
+    /// IteratorAggregate
+
+    /**
+     * Return aggregated iterator.
+     *
+     * @return ArrayIterator
+     */
+    public function getIterator ()
+    {
+        return $this->terms->getIterator();
+    }
+
+    /// Countable
+
+    /**
+     * Return number of terms.
+     *
+     * @return integer
+     */
+    public function count ()
+    {
+        return $this->terms->count();
+    }
+
+    /// Internal API
+
+    /**
+     * Return true if we already have information for the term.
+     *
+     * @param string $term Term to check
+     *
+     * @return boolean
+     */
+    protected function contains ($term)
+    {
+        if ($this->terms->offsetExists($term)) {
+            return true;
+        }
+
+        $qTerm = preg_quote($term, '/');
+        $length = strlen($term);
+        foreach ($this->terms as $key => $value) {
+            if ($length > strlen($key)) {
+                return false;
+            }
+            if (strstr($key, $term) && preg_match("/\b$qTerm\b/u", $key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Compare length of two terms such that terms are sorted by descending
+     * length.
+     *
+     * This method belongs to the internal API but must be declared public in
+     * order to be used for ArrayObject::uksort().
+     *
+     * @param string $a First term
+     * @param string $b Second term
+     *
+     * @return integer
+     *
+     * @see    http://www.php.net/manual/en/arrayobject.uksort.php
+     */
+    public function compareTermLength ($a, $b)
+    {
+        return (strlen($b) - strlen($a));
+    }
+
+}
---
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Solr/Response/Json/SpellcheckTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Unit tests for spellcheck information.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindTest\Backend\Solr\Json\Response;
+
+use VuFindSearch\Backend\Solr\Response\Json\Spellcheck;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Unit tests for spellcheck information.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class SpellcheckTest extends TestCase
+{
+    /**
+     * Test merge.
+     *
+     * @return void
+     */
+    public function testMerge ()
+    {
+        $s1 = new Spellcheck(
+            array(
+                array('this is a phrase', array()),
+                array('foo', array()),
+                array('foobar', array())
+            )
+        );
+        $s2 = new Spellcheck(
+            array(
+                array('is a', array()),
+                array('bar', array()),
+                array('foo bar', array())
+            )
+        );
+        $s1->mergeWith($s2);
+        $this->assertCount(5, $s1);
+    }
+}
