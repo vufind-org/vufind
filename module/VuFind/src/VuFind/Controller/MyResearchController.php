7e7aa8141004eba970a0770af0c183a9d25de124
David Maus <maus@hab.de>
2013-02-18 08:36:01 +0100

    Revert "Update module/VuFind/src/VuFind/Controller/MyResearchController.php"
    This reverts commit f462d4f616b64bcec050def2c5857ed547b6ef23.
    
    As an afterthought cherry-pick seems to be the wrong way to
    incorporate this change. Merge is better because the commit id is
    preserved. Sorry for the VCS log noise ;)

--- module/VuFind/src/VuFind/Controller/MyResearchController.php
+++ module/VuFind/src/VuFind/Controller/MyResearchController.php
@@ -82,8 +82,8 @@ class MyResearchController extends AbstractBase
         }
 
         $config = ConfigReader::getConfig();
-        $page = isset($config->Site->defaultAccountPage)
-            ? $config->Site->defaultAccountPage : 'Favorites';
+        $page = isset($configArray->Site->defaultAccountPage)
+            ? $configArray->Site->defaultAccountPage : 'Favorites';
         return $this->forwardTo('MyResearch', $page);
     }
 
