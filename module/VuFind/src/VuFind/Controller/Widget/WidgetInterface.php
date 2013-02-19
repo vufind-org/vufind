2ea829befbb0a80efb2cbec34c13a4161d6baaa8
David Maus <maus@hab.de>
2013-02-19 14:39:05 +0100

    Revert "New function: Render widget in specified format"
    This reverts commit 87bb7a40d3b2e03d3fd536eb303656115fde2b6c.

--- module/VuFind/src/VuFind/Controller/Widget/WidgetInterface.php
+++ module/VuFind/src/VuFind/Controller/Widget/WidgetInterface.php
@@ -29,7 +29,6 @@
 
 namespace VuFind\Controller\Widget;
 
-use Zend\View\Renderer\RendererInterface;
 use Zend\Stdlib\Parameters;
 
 /**
@@ -66,15 +65,5 @@ interface WidgetInterface
      * @return array
      */
     public function getState ();
-
-    /**
-     * Render the widget.
-     *
-     * @param RendererInterface $renderer Responsible renderer
-     * @param string            $type     Media type or format
-     *
-     * @return void
-     */
-    public function render (RendererInterface $renderer, $type = 'text/html');
 
 }
