7792d91aa0cbdd11a7ddf3bb60953e10e74ebae8
David Maus <maus@hab.de>
2013-02-21 14:36:03 +0100

    Add temporary getter for raw SOLR response
    * VuFindSearch/Backend/Solr/Response/Json/RecordCollection.php
    (getRawResponse): Add temporary getter for raw SOLR response.

--- module/VuFindSearch/src/VuFindSearch/Backend/Solr/Response/Json/RecordCollection.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/Response/Json/RecordCollection.php
@@ -96,6 +96,18 @@ class RecordCollection implements RecordCollectionInterface
     }
 
     /**
+     * Return raw deserialized response.
+     *
+     * @return array
+     *
+     * @todo Remove once we don't need it anymore (02/2013)
+     */
+    public function getRawResponse ()
+    {
+        return $this->response;
+    }
+
+    /**
      * Return total number of records found.
      *
      * @return int
