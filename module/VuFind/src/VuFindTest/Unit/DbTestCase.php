82ef2a70496b67531787e8523752d2775483f6b1
Demian Katz <demian.katz@villanova.edu>
2013-03-01 10:42:38 -0500

    Fixed bug in unit test.

--- module/VuFind/src/VuFindTest/Unit/DbTestCase.php
+++ module/VuFind/src/VuFindTest/Unit/DbTestCase.php
@@ -52,7 +52,9 @@ abstract class DbTestCase extends TestCase
 
         // Add database service:
         if (!$sm->has('VuFind\DbTablePluginManager')) {
-            $dbFactory = new \VuFind\Db\AdapterFactory($sm->get('VuFind\Config'));
+            $dbFactory = new \VuFind\Db\AdapterFactory(
+                $sm->get('VuFind\Config')->get('config')
+            );
             $sm->setService('VuFind\DbAdapter', $dbFactory->getAdapter());
             $factory = new \VuFind\Db\Table\PluginManager(
                 new \Zend\ServiceManager\Config(
