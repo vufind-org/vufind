ca8ca123f46cb75e88de05b52627a8531c5b7071
David Maus <maus@hab.de>
2013-03-15 11:01:25 +0100

    Add MultiIndexListener

---
+++ module/VuFind/src/VuFind/Search/Listener/MultiIndexListener.php
@@ -0,0 +1,220 @@
+<?php
+
+/**
+ * MultiIndex listener class file.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2013.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+
+namespace VuFind\Search\Listener;
+
+use VuFindSearch\Backend\BackendInterface;
+use Zend\EventManager\EventInterface;
+
+/**
+ * MultiIndex listener class file.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class MultiIndexListener
+{
+    /**
+     * Backend.
+     *
+     * @var BackendInterface
+     */
+    protected $backend;
+
+    /**
+     * Available shards, indexed by name.
+     *
+     * @var array
+     */
+    protected $available;
+
+    /**
+     * Active shards.
+     *
+     * @var array
+     */
+    protected $active;
+
+    /**
+     * Fields to strip, indexed by shard name.
+     *
+     * @var array
+     */
+    protected $stripfields;
+
+    /**
+     * Base search specs.
+     *
+     * @var array
+     */
+    protected $specs;
+
+    /**
+     * Constructor.
+     *
+     * @param BackendInterface $backend     Backend
+     * @param array            $shards      Available shards, indexed by name
+     * @param array            $stripfields Fields to strip, indexed by shard name
+     * @param array            $specs       Base search specs
+     *
+     * @return void
+     */
+    public function __construct (BackendInterface $backend, array $shards, array $stripfields, array $specs)
+    {
+        $this->specs       = $specs;
+        $this->active      = array();
+        $this->backend     = $backend;
+        $this->available   = $shards;
+        $this->stripfields = $stripfields;
+    }
+
+    /**
+     * Set active shards.
+     *
+     * @param array $active Active shards
+     *
+     * @return void
+     */
+    public function setActiveShards (array $active)
+    {
+        $this->active = array_combine($active, $active);
+    }
+
+    /**
+     * VuFindSearch.pre()
+     *
+     * @param EventInterface $event Event
+     *
+     * @return EventInterface
+     */
+    public function onSearchPre (EventInterface $event)
+    {
+        $backend = $event->getTarget();
+        if ($backend === $this->backend) {
+            $params = $event->getParam('params');
+            $fields = $this->getFields();
+            $specs  = $this->getSearchSpecs($fields);
+            $backend->getQueryBuilder()->setSpecs($specs);
+            $facets = $params->get('facet.field') ?: array();
+            $params->set('facet.field', array_diff($facets, $fields));
+            $shards = array_intersect_key($this->available, $this->active);
+            $params->set('shards', implode(',', $shards));
+        }
+        return $event;
+    }
+
+    /// Internal API
+
+    /**
+     * Return array of fields to strip.
+     *
+     * @return array
+     */
+    protected function getFields ()
+    {
+        $fields = array();
+        foreach ($this->stripfields as $shard => $fields) {
+            if (isset($this->active[$shard])) {
+                $fields = array_merge($fields, $fields);
+            }
+        }
+        return array_unique($fields);
+    }
+
+    /**
+     * Strip fields from base search specs.
+     *
+     * @param array $fields Fields to strip
+     *
+     * @return array
+     */
+    protected function getSearchSpecs (array $fields)
+    {
+        $specs  = array();
+        $fields = array_merge(
+            $fields,
+            array_map(
+                function ($field) {
+                    return "-{$field}";
+                },
+                $fields
+            )
+        );
+        foreach ($this->specs as $handler => $spec) {
+            $specs[$handler] = array();
+            foreach ($spec as $component => $settings) {
+                switch ($component) {
+                case 'QueryFields':
+                    $specs[$handler][$component] = $this->stripSpecsQueryFields($settings, $fields);
+                    break;
+                default:
+                    $specs[$handler][$component] = $settings;
+                    break;
+                }
+            }
+        }
+        return $specs;
+    }
+
+    /**
+     * Strip fields from a search specs QueryFields section.
+     *
+     * @param array $settings QueryField section
+     * @param array $fields   Fields to strip
+     *
+     * @return array
+     */
+    protected function stripSpecsQueryFields (array $settings, array $fields)
+    {
+        $stripped = array();
+        foreach ($settings as $field => $rule) {
+            if (is_numeric($field)) {
+                $group = array();
+                $type  = reset($rule);
+                while (next($rule) !== false) {
+                    if (!in_array(key($rule), $fields)) {
+                        $group[key($rule)] = current($rule);
+                    }
+                }
+                if ($group) {
+                    array_unshift($group, $type);
+                    $stripped[$field] = $group;
+                }
+            } else {
+                if (!in_array($field, $fields, true)) {
+                    $stripped[$field] = $rule;
+                }
+            }
+        }
+        return $stripped;
+    }
+}
---
+++ module/VuFind/tests/unit-tests/src/Search/Listener/MultiIndexListenerTest.php
@@ -0,0 +1,228 @@
+<?php
+
+/**
+ * Unit tests for multiindex listener.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2013.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+
+namespace VuFindTest\Search\Listener;
+
+use VuFindSearch\ParamBag;
+use VuFindSearch\Backend\Solr\Backend;
+use VuFindSearch\Backend\Solr\Connector;
+
+use VuFind\Search\Listener\MultiIndexListener;
+use VuFindTest\Unit\TestCase;
+use Zend\EventManager\Event;
+
+/**
+ * Unit tests for multiindex listener.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class MultiIndexListenerTest extends TestCase
+{
+    /**
+     * Specs used for stripping tests.
+     *
+     * @var array
+     */
+    protected static $specs = array(
+        'test' => array(
+            'QueryFields' => array(
+                'A' => array(
+                    array('onephrase', 500),
+                    array('and', 200)
+                ),
+                'B' => array(
+                    array('and', 100),
+                    array('or', 50),
+                ),
+                0 => array(
+                    0 => array('AND', 50),
+                    'C' => array(
+                        array('onephrase', 200),
+                    ),
+                    'D' => array(
+                        array('onephrase', 300),
+                    ),
+                    '-E' => array(
+                        array('or', '~')
+                    )
+                )
+            )
+        )
+    );
+
+    /**
+     * Available shards used for stripping tests.
+     *
+     * @var array
+     */
+    protected static $shards = array(
+        'a' => 'example.org/a',
+        'b' => 'example.org/b',
+        'c' => 'example.org/c',
+    );
+
+    /**
+     * Shard fields used for stripping tests.
+     *
+     * @var array
+     */
+    protected static $fields = array(
+        'a' => array('field_1', 'field_3'),
+        'b' => array('field_3')
+    );
+
+    /**
+     * Backend.
+     *
+     * @var BackendInterface
+     */
+    protected $backend;
+
+    /**
+     * Prepare listener.
+     *
+     * @var MultiIndexListener
+     */
+    protected $listener;
+
+    /**
+     * Setup.
+     *
+     * @return void
+     */
+    protected function setup ()
+    {
+        $connector      = new Connector('http://example.org/');
+        $this->backend  = new Backend($connector);
+        $this->listener = new MultiIndexListener($this->backend, self::$shards, self::$fields, self::$specs);
+    }
+
+    /**
+     * Strip fields from a field facet.
+     *
+     * @return void
+     */
+    public function testStripFacetFields ()
+    {
+        $params   = new ParamBag(array('facet.field' => array('field_1', 'field_2', 'field_3')));
+        $event    = new Event('pre', $this->backend, array('params' => $params));
+        $this->listener->setActiveShards(array('b', 'c'));
+        $this->listener->onSearchPre($event);
+
+        $facets   = $params->get('facet.field');
+        sort($facets);
+        $this->assertEquals(array('field_1', 'field_2'), $facets);
+    }
+
+    /**
+     * `shards' parameter is set according to active shards.
+     *
+     * @return void
+     */
+    public function testSetShardsParameter ()
+    {
+        $params   = new ParamBag(array('facet.field' => array('field_1', 'field_2', 'field_3')));
+        $event    = new Event('pre', $this->backend, array('params' => $params));
+        $this->listener->setActiveShards(array('b', 'c'));
+        $this->listener->onSearchPre($event);
+
+        $shards   = $params->get('shards');
+        $shards   = explode(',', reset($shards));
+        sort($shards);
+        $this->assertEquals(array('example.org/b', 'example.org/c'), $shards);
+    }
+
+    /**
+     * Apply strip to empty specs.
+     *
+     * @return void
+     */
+    public function testStripSpecsEmptySpecs ()
+    {
+        $this->setProperty($this->listener, 'specs', array());
+        $specs = $this->callMethod($this->listener, 'getSearchSpecs', array(array('A', 'B', 'E')));
+        $this->assertEmpty($specs);
+    }
+
+    /**
+     * Don't strip anything.
+     *
+     * @return void
+     */
+    public function testStripSpecsNoFieldsToStrip ()
+    {
+        $specs = $this->callMethod($this->listener, 'getSearchSpecs', array(array('F', 'G', 'H')));
+        $this->assertEquals($specs, self::$specs);
+    }
+
+    /**
+     * Strip specs.
+     *
+     * @return void
+     */
+    public function testStripSpecsStrip ()
+    {
+        $specs = $this->callMethod($this->listener, 'getSearchSpecs', array(array('A', 'B', 'E')));
+        $this->assertEquals(
+            array('test' => array(
+                      'QueryFields' => array(
+                          0 => array(
+                              0 => array('AND', 50),
+                              'C' => array(
+                                  array('onephrase', 200)
+                              ),
+                              'D' => array(
+                                  array('onephrase', 300)
+                              )
+                          )
+                      )
+                  )
+            ),
+            $specs
+        );
+    }
+
+    /**
+     * Strip an entire QueryFields section.
+     *
+     * @return void
+     */
+    public function testStripSpecsAllQueryFields ()
+    {
+        $specs = $this->callMethod($this->listener, 'getSearchSpecs', array(array('A', 'B', 'C', 'D', 'E')));
+        $this->assertEquals(
+            array('test' => array('QueryFields' => array())),
+            $specs
+        );
+    }
+}
