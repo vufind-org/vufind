579722efcc62896eec679af3399056caa7a33a6b
Demian Katz <demian.katz@villanova.edu>
2013-03-26 08:24:05 -0400

    Resolving VUFIND-770 (string "0" is not translated). Condition for ignoring rows of translation file was too general. Thanks to Josef Moravec for the fix.

--- module/VuFind/src/VuFind/I18n/Translator/Loader/ExtendedIni.php
+++ module/VuFind/src/VuFind/I18n/Translator/Loader/ExtendedIni.php
@@ -92,7 +92,7 @@ class ExtendedIni implements FileLoaderInterface
                 // Split the string on the equals sign, keeping a max of two chunks:
                 $parts = explode('=', $current, 2);
                 $key = trim($parts[0]);
-                if (!empty($key) && substr($key, 0, 1) != ';') {
+                if ($key != "" && substr($key, 0, 1) != ';') {
                     // Trim outermost double quotes off the value if present:
                     if (isset($parts[1])) {
                         $value = preg_replace(
