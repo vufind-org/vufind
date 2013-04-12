489baed4cdad1fdebaa06cda9a7538b56a3977da
Demian Katz <demian.katz@villanova.edu>
2013-04-12 08:21:13 -0400

    Added wrapper for raw XML Solr submissions.

---
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/Document/RawXMLDocument.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * SOLR "raw XML" document class for manual overrides.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindSearch\Backend\Solr\Document;
+
+/**
+ * SOLR "raw XML" document class for manual overrides.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class RawXMLDocument extends AbstractDocument
+{
+    /**
+     * Raw XML
+     *
+     * @var string
+     */
+    protected $xml;
+
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
+    public function __construct($xml)
+    {
+        $this->xml = $xml;
+    }
+
+    /**
+     * Return serialized JSON representation.
+     *
+     * @return string
+     */
+    public function asJSON()
+    {
+        // @todo Implement XML to JSON conversion
+        throw new \Exception('JSON not supported here.');
+    }
+
+    /**
+     * Return serialized XML representation.
+     *
+     * @return string
+     */
+    public function asXML()
+    {
+        return $this->xml;
+    }
+}
