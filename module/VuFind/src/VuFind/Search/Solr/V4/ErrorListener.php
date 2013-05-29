d02e850c2e14f32ebd2d632d2167a36759454e4b
Demian Katz <demian.katz@villanova.edu>
2013-05-29 11:52:02 -0400

    Added Solr 4 error listener.

---
+++ module/VuFind/src/VuFind/Search/Solr/V4/ErrorListener.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * SOLR 4.x error listener.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2013.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+
+namespace VuFind\Search\Solr\V4;
+
+use VuFindSearch\Backend\Exception\HttpErrorException;
+
+use Zend\EventManager\EventInterface;
+
+/**
+ * SOLR 3.x error listener.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class ErrorListener
+{
+
+    /**
+     * Backends ot listen on.
+     *
+     * @var array
+     */
+    protected $backends;
+
+    /**
+     * Constructor.
+     *
+     * @param array $backends Name of backends to listen on
+     *
+     * @return void
+     */
+    public function __construct(array $backends)
+    {
+        $this->backends = $backends;
+    }
+
+    /**
+     * VuFindSearch.error
+     *
+     * @param EventInterface $event Event
+     *
+     * @return EventInterface
+     */
+    public function onSearchError(EventInterface $event)
+    {
+        $backend = $event->getParam('backend');
+        if (in_array($backend, $this->backends)) {
+            $error  = $event->getTarget();
+            if ($error instanceOf HttpErrorException) {
+                $body = $error->getResponse()->getBody();
+                $type = $error->getResponse()->getHeaders()->get('content-type')
+                    ->toString();
+                if (stristr($type, 'json')) {
+                    $body = json_decode($body);
+                    $reason = isset($body->error->msg) ? $body->error->msg : '';
+                } else if (stristr($type, 'xml')) {
+                    // TODO -- parse XML response
+                    $reason = '';
+                } else {
+                    $reason = '';
+                }
+                if (stristr($reason, 'org.apache.solr.search.SyntaxError')
+                    || stristr($reason, 'undefined field')
+                ) {
+                    $error->addTag('VuFind\Search\ParserError');
+                }
+            }
+        }
+        return $event;
+    }
+}
---
+++ module/VuFind/tests/unit-tests/src/Search/Solr/V4/ErrorListenerTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Unit tests for SOLR 3.x error listener.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2013.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+
+namespace VuFindTest\Search\Solr\V4;
+
+use VuFind\Search\Solr\V4\ErrorListener;
+
+use VuFindSearch\Backend\Exception\HttpErrorException;
+
+use Zend\EventManager\Event;
+use Zend\Http\Response;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+use RuntimeException;
+
+/**
+ * Unit tests for SOLR 3.x error listener.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   David Maus <maus@hab.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class ErrorListenerTest extends TestCase
+{
+    /**
+     * Detect parser error response.
+     *
+     * @return void
+     */
+    public function testDetectParseError()
+    {
+        $response  = $this->createResponse('solr4-parse-error');
+
+        $exception = HttpErrorException::createFromResponse($response);
+        $params    = array('backend' => 'test');
+        $event     = new Event(null, $exception, $params);
+        $listener  = new ErrorListener(array('test'));
+        $listener->onSearchError($event);
+        $this->assertTrue($exception->hasTag('VuFind\Search\ParserError'));
+    }
+
+    /**
+     * Detect parser error response.
+     *
+     * @return void
+     */
+    public function testDetectUndefinedFieldError()
+    {
+        $response = $this->createResponse('solr4-undefined-field-error');
+
+        $exception = HttpErrorException::createFromResponse($response);
+        $params    = array('backend' => 'test');
+        $event     = new Event(null, $exception, $params);
+        $listener  = new ErrorListener(array('test'));
+        $listener->onSearchError($event);
+        $this->assertTrue($exception->hasTag('VuFind\Search\ParserError'));
+    }
+
+    /// Internal API
+
+    /**
+     * Return response fixture
+     *
+     * @param string $name Name of fixture
+     *
+     * @return Response Response
+     */
+    protected function createResponse($name)
+    {
+        $file = realpath(
+            \VUFIND_PHPUNIT_MODULE_PATH . '/fixtures/response/solr/' . $name
+        );
+        if (!$file) {
+            throw new RuntimeException(
+                sprintf('Unable to resolve fixture to fixture file: %s', $name)
+            );
+        }
+        $response = Response::fromString(file_get_contents($file));
+        return $response;
+    }
+}
