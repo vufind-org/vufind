c93008182f5dcbe8b2bdb830de95c08f4abcf1a7
Demian Katz <demian.katz@villanova.edu>
2013-06-25 09:20:32 -0400

    Resolving VUFIND-829 (authority core broken). Includes integration test.

---
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Connection/SolrAuthTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Solr Auth Connection Test Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:unit_tests Wiki
+ */
+namespace VuFindTest\Integration\Connection;
+use VuFindSearch\Query\Query;
+
+/**
+ * Solr Auth Test Class
+ *
+ * @category VuFind2
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:unit_tests Wiki
+ */
+class SolrAuthTest extends \VuFindTest\Unit\TestCase
+{
+    /**
+     * Standard setup method.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        // Give up if we're not running in CI:
+        if (!$this->continuousIntegrationRunning()) {
+            return $this->markTestSkipped('Continuous integration not running.');
+        }
+    }
+
+    /**
+     * Check that we can find a record in the authority core.
+     *
+     * @return void
+     */
+    public function testSimpleSearch()
+    {
+        $solr = $this->getServiceManager()->get('VuFind\Search\BackendManager')
+            ->get('SolrAuth');
+
+        // Search for a term known to exist in the sample data; request just one
+        // record -- we should get a single record back.
+        $result = $solr->search(new Query('Dublin Society', 'AllFields'), 0, 1);
+        $this->assertEquals(1, count($result));
+    }
+}
