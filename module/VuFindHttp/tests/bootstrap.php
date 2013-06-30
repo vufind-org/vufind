8690e7306afb2e5b15cdb04fb472bd85a863ddcd
David Maus <dmaus@ictsoc.de>
2013-06-30 10:41:18 +0200

    Add phpunit.xml and bootstrap.php for standalone unit tests

---
+++ module/VuFindHttp/tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Proxy service PHPUnit bootstrap.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @copyright Copyright (C) Villanova University 2011
+ */
+
+if (file_exists('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+}
