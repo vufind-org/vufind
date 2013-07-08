be4a934174b899d96450dbb0ffc2ff3f05873c70
Demian Katz <demian.katz@villanova.edu>
2013-07-08 11:43:52 -0400

    Proof of concept: combined search module hard-coded to show Catalog + Summon.

---
+++ module/VuFind/src/VuFind/Controller/CombinedController.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Combined Search Controller
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Controller
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+namespace VuFind\Controller;
+use Zend\Stdlib\Parameters;
+
+/**
+ * Redirects the user to the appropriate default VuFind action.
+ *
+ * @category VuFind2
+ * @package  Controller
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org   Main Site
+ */
+class CombinedController extends AbstractSearch
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->searchClassId = 'Combined';
+        parent::__construct();
+    }
+
+    /**
+     * Home action
+     *
+     * @return mixed
+     */
+    public function homeAction()
+    {
+        return $this->createViewModel();
+    }
+
+    /**
+     * Results action
+     *
+     * @return mixed
+     */
+    public function resultsAction()
+    {
+        // Set up current request context:
+        $results = $this->getResultsManager()->get('Combined');
+        $params = $results->getParams();
+        $params->initFromRequest(
+            new Parameters(
+                $this->getRequest()->getQuery()->toArray()
+                + $this->getRequest()->getPost()->toArray()
+            )
+        );
+
+        // Gather combined results:
+        $combinedResults = array();
+        $options = $this->getServiceLocator()
+            ->get('VuFind\SearchOptionsPluginManager');
+        foreach (array('Solr', 'Summon') as $current) {
+            $currentOptions = $options->get($current);
+            list($controller, $action)
+                = explode('-', $currentOptions->getSearchAction());
+            $combinedResults[$current] = $this->forwardTo($controller, $action);
+        }
+
+        // Build view model:
+        return $this->createViewModel(
+            array(
+                'results' => $results,
+                'params' => $params,
+                'combinedResults' => $combinedResults
+            )
+        );
+    }
+}
---
+++ module/VuFind/src/VuFind/Search/Combined/Results.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Combined results search model.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search_Base
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
+namespace VuFind\Search\Combined;
+
+/**
+ * Combined results search model.
+ *
+ * @category VuFind2
+ * @package  Search_Base
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
+class Results extends \VuFind\Search\Base\Results
+{
+    /**
+     * Returns the stored list of facets for the last search
+     *
+     * @param array $filter Array of field => on-screen description listing
+     * all of the desired facet fields; set to null to get all configured values.
+     *
+     * @return array        Facets data arrays
+     */
+    public function getFacetList($filter = null)
+    {
+        // Not relevant:
+        return array();
+    }
+
+    /**
+     * Abstract support method for performAndProcessSearch -- perform a search based
+     * on the parameters passed to the object.  This method is responsible for
+     * filling in all of the key class properties: results, resultTotal, etc.
+     *
+     * @return void
+     */
+    protected function performSearch()
+    {
+        throw new \Exception(
+            'This is a placeholder class and is not meant to be called!'
+        );
+    }
+}
