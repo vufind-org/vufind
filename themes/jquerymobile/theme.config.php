7f38c58363096e19603fc04f9f7197c1365d8b2a
Thomas Misilo <misilot@fit.edu>
2013-08-15 09:37:16 -0400

    Inital work to update jquery mobile

--- themes/jquerymobile/theme.config.php
+++ themes/jquerymobile/theme.config.php
@@ -2,14 +2,14 @@
 return array(
     'extends' => 'root',
     'css' => array(
-        'jquery.mobile-1.0rc2.min.css',
+        'jquery.mobile-1.3.2.min.css',
         'styles.css',
         'formats.css',
     ),
     'js' => array(
-        'jquery-1.6.4.min.js',
+        'jquery-1.9.1.min.js',
         'common.js',
-        'jquery.mobile-1.0rc2.min.js',
+        'jquery.mobile-1.3.2.min.js',
         'jquery.cookie.js',
         'scripts.js',
     ),
