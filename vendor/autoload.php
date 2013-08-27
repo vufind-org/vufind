fab5fe20eb49138af46a73edf63ac9c96a63c8ee
Demian Katz <demian.katz@villanova.edu>
2013-08-27 13:58:54 -0400

    Updated Symfony/Yaml component.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9c2a88ea43224869fc72b5250d94e473::getLoader();
+return ComposerAutoloaderInitead8bc6e07874aa3993992df7474c87b::getLoader();
