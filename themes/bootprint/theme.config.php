e73939f9176db77fc59dba052de85a52b296534a
Chris Hallberg <crhallberg@gmail.com>
2013-09-17 11:21:47 -0400

    Bootprint theme.

---
+++ themes/bootprint/theme.config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'extends' => 'bootstrap',
+    'css' => array(
+        'style.css',
+        'icons.css'
+    ),
+    'helpers' => array(
+        'factories' => array(
+            'layoutclass' => function ($sm) {
+                $config = $sm->getServiceLocator()->get('VuFind\Config')->get('config');
+                $left = !isset($config->Site->sidebarOnLeft) ? false : $config->Site->sidebarOnLeft;
+                return new \VuFind\View\Helper\Bootprint\LayoutClass($left);
+            }
+        )
+    )
+);
