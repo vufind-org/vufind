f62dc060160e7ec24bb55a1c7a47e6ab56fa073e
Chris Hallberg <crhallberg@gmail.com>
2013-09-19 11:51:26 -0400

    VUFIND-898. Bootstrap savedList relocation.

---
+++ themes/bootstrap/js/collection_record.js
@@ -0,0 +1,16 @@
+function toggleCollectionInfo() {
+  $("#collectionInfo").toggle();
+}
+
+function showMoreInfoToggle() {
+  toggleCollectionInfo();
+  $("#moreInfoToggle").show();
+  $("#moreInfoToggle").click(function(e) {
+    e.preventDefault();
+    toggleCollectionInfo();
+  });
+}
+
+$(document).ready(function() {
+  showMoreInfoToggle();
+});
