0d369ecc984a7c270c38e72af1ee175f4df5222f
Demian Katz <demian.katz@villanova.edu>
2013-10-10 14:19:53 -0400

    Added PP2 search test.

--- module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Pazpar2/BackendTest.php
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Pazpar2/BackendTest.php
@@ -29,8 +29,10 @@
 
 namespace VuFindTest\Backend\Pazpar2;
 
+use VuFindSearch\Query\Query;
 use VuFindSearch\Backend\Pazpar2\Backend;
 use PHPUnit_Framework_TestCase;
+use InvalidArgumentException;
 
 /**
  * Unit tests for Pazpar2 backend.
@@ -59,5 +61,69 @@ class BackendTest extends PHPUnit_Framework_TestCase
             $this->getMock('VuFindSearch\Response\RecordCollectionFactoryInterface')
         );
         $this->assertEquals($connector, $back->getConnector());
+    }
+
+    /**
+     * Test performing a search.
+     *
+     * @return void
+     */
+    public function testSearch()
+    {
+        $conn = $this->getConnectorMock(array('search', 'show'));
+        $conn->expects($this->once())
+            ->method('search')
+            ->will($this->returnValue($this->loadResponse('pp2search')));
+        $conn->expects($this->once())
+            ->method('show')
+            ->will($this->returnValue($this->loadResponse('pp2show')));
+
+        $back = new Backend($conn);
+        $back->setIdentifier('test');
+        $coll = $back->search(new Query('foobar'), 0, 20);
+        $this->assertCount(20, $coll);
+        $this->assertEquals('test', $coll->getSourceIdentifier());
+        $rec  = $coll->first();
+        $this->assertEquals('test', $rec->getSourceIdentifier());
+        $this->assertEquals('content: author test title test medium book', (string)$rec->getXML()->recid);
+        $recs = $coll->getRecords();
+        $this->assertEquals('test', $recs[19]->getSourceIdentifier());
+        $this->assertEquals('content: author navalani k author gidwani n n title a practical guide to colon classification medium book', (string)$recs[19]->getXML()->recid);
+        $this->assertEquals(54, $coll->getTotal());
+    }
+
+    /// Internal API
+
+    /**
+     * Load a response as fixture.
+     *
+     * @param string $fixture Fixture file
+     *
+     * @return mixed
+     *
+     * @throws InvalidArgumentException Fixture files does not exist
+     */
+    protected function loadResponse($fixture)
+    {
+        $file = realpath(sprintf('%s/pazpar2/response/%s', PHPUNIT_SEARCH_FIXTURES, $fixture));
+        if (!is_string($file) || !file_exists($file) || !is_readable($file)) {
+            throw new InvalidArgumentException(sprintf('Unable to load fixture file: %s', $fixture));
+        }
+        return simplexml_load_file($file);
+    }
+
+    /**
+     * Return connector mock.
+     *
+     * @param array $mock Functions to mock
+     *
+     * @return array
+     */
+    protected function getConnectorMock(array $mock = array())
+    {
+        $client = $this->getMock('Zend\Http\Client');
+        return $this->getMock(
+            'VuFindSearch\Backend\Pazpar2\Connector', $mock, array('fake', $client)
+        );
     }
 }
