35a32289817e43e5a343f8e349a6a61f3f301031
Ere Maijala <ere.maijala@helsinki.fi>
2014-03-05 14:02:00 +0200

    Voyager: Oops. Fixed patron login broken in previous commit.

--- module/VuFind/src/VuFind/ILS/Driver/Voyager.php
+++ module/VuFind/src/VuFind/ILS/Driver/Voyager.php
@@ -1141,7 +1141,7 @@ class Voyager extends AbstractBase
         $sql = "SELECT PATRON.PATRON_ID, PATRON.FIRST_NAME, PATRON.LAST_NAME " .
                "FROM $this->dbName.PATRON, $this->dbName.PATRON_BARCODE " .
                "WHERE PATRON.PATRON_ID = PATRON_BARCODE.PATRON_ID AND " .
-               "lower(PATRON.{$login_field}) = :login AND ";
+               "lower(PATRON_BARCODE.PATRON_BARCODE) = :barcode AND ";
         if (isset($this->config['Catalog']['fallback_login_field'])) {
             $fallback_login_field = preg_replace(
                 '/[^\w]/', '', $this->config['Catalog']['fallback_login_field']
