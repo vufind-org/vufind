a666ad01a5b140bbdd49edd5e79c78c23aa0b994
Demian Katz <demian.katz@villanova.edu>
2014-03-26 12:39:37 -0400

    Allow toggle of comments via config.ini. - Resolves VUFIND-688.

--- module/VuFind/src/VuFind/RecordTab/UserComments.php
+++ module/VuFind/src/VuFind/RecordTab/UserComments.php
@@ -39,6 +39,33 @@ namespace VuFind\RecordTab;
 class UserComments extends AbstractBase
 {
     /**
+     * Is this tab enabled?
+     *
+     * @var bool
+     */
+    protected $enabled;
+
+    /**
+     * Constructor
+     *
+     * @param bool $enabled is this tab enabled?
+     */
+    public function __construct($enabled = true)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * Is this tab active?
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->enabled;
+    }
+
+    /**
      * Get the on-screen description for this tab.
      *
      * @return string
