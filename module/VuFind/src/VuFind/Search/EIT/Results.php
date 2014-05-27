3ce636a727399957e7510c22366d74c8fd0b0d9d
Demian Katz <demian.katz@villanova.edu>
2014-05-27 14:37:46 -0400

    Fixed bad namespace.

--- module/VuFind/src/VuFind/Search/EIT/Results.php
+++ module/VuFind/src/VuFind/Search/EIT/Results.php
@@ -26,7 +26,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://www.vufind.org  Main Page
  */
-namespace VuFind\Search\EBSCO;
+namespace VuFind\Search\EIT;
 
 /**
  * EBSCO Search Parameters
