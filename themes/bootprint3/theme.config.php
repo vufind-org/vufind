e902c9aeb47ed85869fca3650d5b70df01b2b3c4
Chris Hallberg <crhallberg@gmail.com>
2014-07-03 15:27:07 -0400

    Back to CSS for default. Sorry.

--- themes/bootprint3/theme.config.php
+++ themes/bootprint3/theme.config.php
@@ -4,16 +4,16 @@ return array(
     'css' => array(
         //'bootstrap.min.css',
         //'bootprint-custom.css',
-        //'compiled.css',
-        //'icons.css'
+        'compiled.css',
+        'icons.css'
     ),
     'less' => array(
         //'compiled.less',
         //'icons.less'
     ),
     'scss' => array(
-        'compiled.scss',
-        'icons.scss'
+        //'compiled.scss',
+        //'icons.scss'
     ),
     'js' => array(
         'vendor/bootstrap.min.js'
