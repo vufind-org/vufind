3c428aaf75879ba267f792c70fae1c28f27bf325
Chris Hallberg <crhallberg@gmail.com>
2014-07-14 15:34:58 -0400

    Bootprint theme.config super simplification. Frighteningly elegant.

--- themes/bootprint3/theme.config.php
+++ themes/bootprint3/theme.config.php
@@ -1,23 +1,4 @@
 <?php
 return array(
-    'extends' => 'bootstrap3',
-    'css' => array(
-        //'vendor/bootstrap.min.css',
-        //'bootprint-custom.css',
-        'compiled.css',
-        'icons.css'
-    ),
-    'less' => array(
-        'active' => false,
-        'compiled.less',
-        'icons.less'
-    ),
-    'scss' => array(
-        'active' => false,
-        'compiled.scss',
-        'icons.scss'
-    ),
-    'js' => array(
-        'vendor/bootstrap.min.js'
-    )
+    'extends' => 'bootstrap3'
 );
