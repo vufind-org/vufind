e0d164f5f9aebd555934e19d7a0b26393d40f94e
Chris Hallberg <crhallberg@gmail.com>
2014-07-16 10:56:50 -0400

    Improved responding at smaller screens for search results.

--- module/VuFind/src/VuFind/View/Helper/Bootstrap3/LayoutClass.php
+++ module/VuFind/src/VuFind/View/Helper/Bootstrap3/LayoutClass.php
@@ -53,11 +53,11 @@ class LayoutClass extends \VuFind\View\Helper\AbstractLayoutClass
     {
         switch ($class) {
         case 'mainbody':
-            return $this->left ? 'col-md-9 col-md-push-3' : 'col-md-9';
+            return $this->left ? 'col-sm-9 col-sm-push-3' : 'col-sm-9';
         case 'sidebar':
             return $this->left
-                ? 'sidebar col-md-3 col-md-pull-9 hidden-print'
-                : 'sidebar col-md-3 hidden-print';
+                ? 'sidebar col-sm-3 col-sm-pull-9 hidden-print'
+                : 'sidebar col-sm-3 hidden-print';
         }
     }
 }
