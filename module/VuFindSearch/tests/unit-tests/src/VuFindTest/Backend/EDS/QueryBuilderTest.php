3a0165cfe3b8e85baf0e905d9743f229515ea843
Demian Katz <demian.katz@villanova.edu>
2014-08-05 15:30:52 -0400

    Added more tests.

---
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/EDS/QueryBuilderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Unit tests for EDS query builder
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2013.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindTest\Backend\EDS;
+
+use VuFindSearch\Backend\EDS\QueryBuilder;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Unit tests for EDS query builder
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class QueryBuilderTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test query parsing.
+     *
+     * @return void
+     */
+    public function testParsing()
+    {
+        // Set up an array of expected inputs (serialized objects) and outputs
+        // (queries):
+        // @codingStandardsIgnoreStart
+        $tests = array(
+            array('advanced', array('AND,cheese', 'AND,SU:test'))
+        );
+        // @codingStandardsIgnoreEnd
+
+        $qb = new QueryBuilder();
+        foreach ($tests as $test) {
+            list($input, $output) = $test;
+            $q = unserialize(
+                file_get_contents(
+                    PHPUNIT_SEARCH_FIXTURES . '/eds/query/' . $input
+                )
+            );
+            $response = $qb->build($q);
+            $this->assertEquals($output, $response->get('query'));
+        }
+    }
+}
