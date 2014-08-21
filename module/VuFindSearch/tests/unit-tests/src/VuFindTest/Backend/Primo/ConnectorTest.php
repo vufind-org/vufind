6e4a42dd72cd1ce0f86a1272cc045a77030c109e
Demian Katz <demian.katz@villanova.edu>
2014-08-21 12:44:03 -0400

    Began framing out Primo connector test.

---
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Primo/ConnectorTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Unit tests for Primo connector.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+
+namespace VuFindTest\Backend\Primo;
+
+use VuFindSearch\Backend\Primo\Connector;
+
+use Zend\Http\Client\Adapter\Test as TestAdapter;
+use Zend\Http\Client as HttpClient;
+
+use PHPUnit_Framework_TestCase;
+use InvalidArgumentException;
+
+/**
+ * Unit tests for Primo connector.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class ConnectorTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test default timeout value
+     *
+     * @return void
+     */
+    public function testInstitutionCode()
+    {
+        $this->assertEquals('fakeinst', $this->createConnector()->getInstitutionCode());
+    }
+
+    /**
+     * Create connector with fixture file.
+     *
+     * @param string $fixture Fixture file
+     *
+     * @return Connector
+     *
+     * @throws InvalidArgumentException Fixture file does not exist
+     */
+    protected function createConnector($fixture = null)
+    {
+        $adapter = new TestAdapter();
+        if ($fixture) {
+            $file = realpath(sprintf('%s/primo/response/%s', PHPUNIT_SEARCH_FIXTURES, $fixture));
+            if (!is_string($file) || !file_exists($file) || !is_readable($file)) {
+                throw new InvalidArgumentException(sprintf('Unable to load fixture file: %s', $file));
+            }
+            $response = file_get_contents($file);
+            $adapter->setResponse($response);
+        }
+        $client = new HttpClient();
+        $client->setAdapter($adapter);
+        $conn = new Connector('fakeid', 'fakeinst', $client);
+        return $conn;
+    }
+}
