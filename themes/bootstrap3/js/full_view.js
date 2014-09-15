fcf7df1858d0f34b9c8d6179136b345ea5ced0df
Chris Hallberg <crhallberg@gmail.com>
2014-09-15 10:50:45 -0400

    ID encoding changes to get accordions and AJAX to work with non word characters in IDs.

--- themes/bootstrap3/js/full_view.js
+++ themes/bootstrap3/js/full_view.js
@@ -2,41 +2,39 @@
 
 $(document).ready(function() {
   $('.getFull').click(function() {
-	var div_id = $(this).parent().parent().find(".hiddenId")[0].value;
+    var div_id = $(this).parent().parent().find(".hiddenId")[0].value;
+    var div_html_id = div_id.replace(/\W/g, "_");
 
-	var shortNode = jQuery('#short_'+div_id);
-	var mainNode = shortNode.parent();
-        var longNode = jQuery('#long_'+div_id);
-	if ( !longNode.is(":visible") ) {
-	console.debug("unsichtbar");
+    var shortNode = jQuery('#short_'+div_html_id);
+    var mainNode = shortNode.parent();
+    var longNode = jQuery('#long_'+div_html_id);
+    if ( !longNode.is(":visible") ) {
+      console.debug("unsichtbar");
 
-	console.debug(div_id);
-         var url = path + '/AJAX/JSON?' + $.param({method:'getRecordDetails',id:div_id});
-         $.ajax({
-                dataType: 'json',
-                url: url,
-                success: function(response) {
-                        if (response.status == 'OK') {
-					shortNode.hide();
-					//$('#id_'+div_id).addClass("col-xs-11");
-				 			
-        				longNode.html(response.data);
-					longNode.addClass("ajaxItem");
-					longNode.show();
-					console.debug(response);
-                                                                                                                                          }
-		}
-         });
-	return false;
-	} else {
-        console.debug("sichtbar");
-		longNode.hide();
-                     
-   shortNode.show();
-	return false;
+      console.debug(div_id);
+      var url = path + '/AJAX/JSON?' + $.param({method:'getRecordDetails',id:div_id});
+      $.ajax({
+        dataType: 'json',
+        url: url,
+        success: function(response) {
+          if (response.status == 'OK') {
+            shortNode.addClass('hidden');
 
-	}
-	return false;
+            longNode.html(response.data);
+            longNode.addClass("ajaxItem");
+            longNode.removeClass("hidden");
+            //console.debug(response.data);
+          }
+        }
+      });
+      return false;
+    } else {
+      console.debug("sichtbar");
+      longNode.addClass("hidden");
+
+      shortNode.removeClass("hidden");
+      return false;
+    }
+    return false;
   });
-
 });
