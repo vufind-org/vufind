1ff60a68cc5266ea83422a975e65ff03dd6cf43e
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2014-10-31 12:27:03 +0100

    RDS: first files for rdsproxy

---
+++ module/VuFind/src/VuFind/Search/RDSProxy/Params.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * RDSProxy aspect of the Search Multi-class (Params)
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2011.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search_RDSProxy
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Jochen Lienhard <lienhard@ub.uni-freiburg.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
+namespace VuFind\Search\RDSProxy;
+use VuFindSearch\ParamBag;
+
+/**
+ * Solr Search Parameters
+ *
+ * @category VuFind2
+ * @package  Search_Solr
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Jochen Lienhard <lienhard@ub.uni-freiburg.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
+class Params extends \VuFind\Search\Solr\Params
+{
+    /**
+     * Constructor
+     *
+     * @param \VuFind\Search\Base\Options  $options      Options to use
+     * @param \VuFind\Config\PluginManager $configLoader Config loader
+     */
+    public function __construct($options, \VuFind\Config\PluginManager $configLoader)
+    {
+        parent::__construct($options, $configLoader);
+        // Use basic facet limit by default, if set:
+
+        $config = $configLoader->get('RDSProxy_facets');
+        if (isset($config->Results_Settings->facet_limit)
+            && is_numeric($config->Results_Settings->facet_limit)
+        ) {
+            $this->setFacetLimit($config->Results_Settings->facet_limit);
+        }
+
+    }
+
+    /**
+     * Initialize facet settings for the specified configuration sections.
+     *
+     * @param string $facetList     Config section containing fields to activate
+     * @param string $facetSettings Config section containing related settings
+     *
+     * @return bool                 True if facets set, false if no settings found
+     */
+    protected function initFacetList($facetList, $facetSettings, $cfgFile = 'RDSProxy_facets')
+    {
+	// $this->resetFacetConfig(); // facet Liste zurücksetzen
+        $config = $this->getServiceLocator()->get('VuFind\Config')->get('RDSProxy_facets');
+        if (!isset($config->$facetList)) {
+            return false;
+        }
+        if (isset($config->$facetSettings->orFacets)) {
+            $orFields
+                = array_map('trim', explode(',', $config->$facetSettings->orFacets));
+        } else {
+            $orFields = array();
+        }
+        foreach ($config->$facetList as $key => $value) {
+            $useOr = (isset($orFields[0]) && $orFields[0] == '*')
+                || in_array($key, $orFields);
+            $this->addFacet($key, $value, $useOr);
+        }
+        if (isset($config->$facetSettings->facet_limit)
+            && is_numeric($config->$facetSettings->facet_limit)
+        ) {
+            $this->setFacetLimit($config->$facetSettings->facet_limit);
+        }
+        return true;
+    }
+
+    /**
+     * Initialize facet settings for the standard search screen.
+     *
+     * @return void
+     */
+    public function initBasicFacets()
+    {
+	$this->resetFacetConfig();
+        $this->initFacetList('ResultsTop', 'Results_Settings');
+        $this->initFacetList('Results', 'Results_Settings');
+    }
+
+    /**
+     * Create search backend parameters for advanced features.
+     *
+     * @return ParamBag
+     */
+    public function getBackendParameters()
+    {
+        $backendParams = new ParamBag();
+
+        // Spellcheck
+        $backendParams->set(
+            'spellcheck', $this->getOptions()->spellcheckEnabled() ? 'true' : 'false'
+        );
+
+        // Facets
+	$this->initBasicFacets();
+        $facets = $this->getFacetSettings();
+        if (!empty($facets)) {
+            $backendParams->add('facet', 'true');
+            foreach ($facets as $key => $value) {
+                $backendParams->add("facet.{$key}", $value);
+            }
+            $backendParams->add('facet.mincount', 1);
+        }
+
+        // Filters
+        $filters = $this->getFilterSettings();
+        foreach ($filters as $filter) {
+            $backendParams->add('fq', $filter);
+        }
+
+        // Shards
+        $allShards = $this->getOptions()->getShards();
+        $shards = $this->getSelectedShards();
+        if (is_null($shards)) {
+            $shards = array_keys($allShards);
+        }
+
+        // If we have selected shards, we need to format them:
+        if (!empty($shards)) {
+            $selectedShards = array();
+            foreach ($shards as $current) {
+                $selectedShards[$current] = $allShards[$current];
+            }
+            $shards = $selectedShards;
+            $backendParams->add('shards', implode(',', $selectedShards));
+        }
+
+        // Sort
+        $sort = $this->getSort();
+        if ($sort) {
+            $backendParams->add('sort', $this->normalizeSort($sort));
+        }
+
+        // Highlighting -- on by default, but we should disable if necessary:
+        if (!$this->getOptions()->highlightEnabled()) {
+            $backendParams->add('hl', 'false');
+        }
+
+        return $backendParams;
+    }
+
+}
