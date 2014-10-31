54ced30675dd846c99dd30ed168fe2555c6fa434
Demian Katz <demian.katz@villanova.edu>
2014-10-31 10:32:00 -0400

    Added "fullDateRange" facets for dealing with granular dates.

--- module/VuFind/src/VuFind/Recommend/SideFacets.php
+++ module/VuFind/src/VuFind/Recommend/SideFacets.php
@@ -42,11 +42,18 @@ use VuFind\Solr\Utils as SolrUtils;
 class SideFacets extends AbstractFacets
 {
     /**
-     * Date facet configuration
+     * Year-only date facet configuration
      *
      * @var array
      */
     protected $dateFacets = array();
+
+    /**
+     * Day/month/year date facet configuration
+     *
+     * @var array
+     */
+    protected $fullDateFacets = array();
 
     /**
      * Generic range facet configuration
@@ -115,6 +122,9 @@ class SideFacets extends AbstractFacets
         if (isset($config->SpecialFacets->dateRange)) {
             $this->dateFacets = $config->SpecialFacets->dateRange->toArray();
         }
+        if (isset($config->SpecialFacets->fullDateRange)) {
+            $this->fullDateFacets = $config->SpecialFacets->fullDateRange->toArray();
+        }
         if (isset($config->SpecialFacets->genericRange)) {
             $this->genericRangeFacets
                 = $config->SpecialFacets->genericRange->toArray();
@@ -182,13 +192,27 @@ class SideFacets extends AbstractFacets
     /**
      * getDateFacets
      *
-     * Return date facet information in a format processed for use in the view.
+     * Return year-based date facet information in a format processed for use in the
+     * view.
      *
      * @return array Array of from/to value arrays keyed by field.
      */
     public function getDateFacets()
     {
         return $this->getRangeFacets('dateFacets');
+    }
+
+    /**
+     * getFullDateFacets
+     *
+     * Return year/month/day-based date facet information in a format processed for
+     * use in the view.
+     *
+     * @return array Array of from/to value arrays keyed by field.
+     */
+    public function getFullDateFacets()
+    {
+        return $this->getRangeFacets('fullDateFacets');
     }
 
     /**
@@ -226,6 +250,7 @@ class SideFacets extends AbstractFacets
     {
         $raw = array(
             'date' => $this->getDateFacets(),
+            'fulldate' => $this->getFullDateFacets(),
             'generic' => $this->getGenericRangeFacets(),
             'numeric' => $this->getNumericRangeFacets()
         );
