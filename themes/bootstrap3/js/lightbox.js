2cc1dab09c4e6e39dd3efedb6eea8d6f4719b798
Chris Hallberg <crhallberg@gmail.com>
2014-11-12 14:31:22 -0500

    Make sure validator is called first, not removed, and that prevented events don't go through on the Lightbox.

--- themes/bootstrap3/js/lightbox.js
+++ themes/bootstrap3/js/lightbox.js
@@ -329,21 +329,23 @@ var Lightbox = {
    * is called and the 'shown' lightbox event is triggered
    */
   registerForms: function() {
-    var form = $("#modal").find('form');
-    form.validator();
-    var name = $(form).attr('name');
+    var $form = $("#modal").find('form');
+    $form.validator();
+    var name = $form.attr('name');
     // Assign form handler based on name
     if(typeof name !== "undefined" && typeof Lightbox.formHandlers[name] !== "undefined") {
-      $(form).unbind('submit').submit(Lightbox.formHandlers[name]);
+      $form.submit(Lightbox.formHandlers[name]);
     // Default action, with custom callback
     } else if(typeof Lightbox.formCallbacks[name] !== "undefined") {
-      $(form).unbind('submit').submit(function(evt){
+      $form.submit(function(evt){
+        if(evt.isDefaultPrevented()) return false;
         Lightbox.submit($(evt.target), Lightbox.formCallbacks[name]);
         return false;
       });
     // Default
     } else {
-      $(form).unbind('submit').submit(function(evt){
+      $form.unbind('submit').submit(function(evt){
+        if(evt.isDefaultPrevented()) return false;
         Lightbox.submit($(evt.target), function(html){
           Lightbox.checkForError(html, Lightbox.close);
         });
