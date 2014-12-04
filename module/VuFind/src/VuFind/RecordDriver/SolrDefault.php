94eba42f415e778af94e3b05e91af751b906d396
Stefan Weil <sw@weilnetz.de>
2014-12-04 22:17:17 +0100

    Fix typos in comments.

--- module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
+++ module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
@@ -1451,7 +1451,7 @@ class SolrDefault extends AbstractBase
      *
      * @param string $hierarchyID The hierarchy to get the tree for
      *
-     * @return mixed An associative array of hierachy trees on success (id => title),
+     * @return mixed An associative array of hierarchy trees on success (id => title),
      * false if no hierarchies found
      */
     public function getHierarchyTrees($hierarchyID = false)
