a0f4899de59eeb07f21ba3f1b8407c625c64549e
Ere Maijala <ere.maijala@helsinki.fi>
2015-01-15 16:48:52 +0200

    Updated Copyright message.

--- module/Finna/src/Finna/Search/Solr/SolrExtensionsListener.php
+++ module/Finna/src/Finna/Search/Solr/SolrExtensionsListener.php
@@ -5,7 +5,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) The National Library of Finland 2013.
+ * Copyright (C) The National Library of Finland 2013-2015.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
