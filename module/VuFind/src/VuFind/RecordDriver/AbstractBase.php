d4792bf22d110297a0903e971f68ab492662789a
Markus Beh <markus.beh@ub.uni-freiburg.de>
2015-01-21 10:36:42 +0100

    fix bug due to renamed constant for cache policy (CACHE::FAVORITE)

--- module/VuFind/src/VuFind/RecordDriver/AbstractBase.php
+++ module/VuFind/src/VuFind/RecordDriver/AbstractBase.php
@@ -257,7 +257,7 @@ abstract class AbstractBase implements \VuFind\Db\Table\DbTableAwareInterface,
         );
         
         // Persist record in the database for "offline" use
-        $this->recordCache->setPolicy(Cache::FAVORITES);
+        $this->recordCache->setPolicy(Cache::FAVORITE);
         $this->recordCache->createOrUpdate($resource->record_id, $user->id, $resource->source, $this->getRawData(), null, $resource->id);
         
         // Add the information to the user's account:
