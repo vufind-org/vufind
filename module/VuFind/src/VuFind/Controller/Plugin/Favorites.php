054a752c036ae7d3b96788a9d40e54e3d02ff682
Markus Beh <markus.beh@ub.uni-freiburg.de>
2015-01-28 11:32:29 +0100

    use record cache when adding favorites from bookbag

--- module/VuFind/src/VuFind/Controller/Plugin/Favorites.php
+++ module/VuFind/src/VuFind/Controller/Plugin/Favorites.php
@@ -86,7 +86,13 @@ class Favorites extends AbstractPlugin
             $tags = isset($params['mytags'])
                 ? $tagParser->parse($params['mytags']) : array();
             $user->saveResource($resource, $list, $tags, '', false);
+
+            $recordCache = $this->getController()->getServiceLocator()->get('VuFind\Record\Cache');
+            $recordLoader = $this->getController()->getServiceLocator()->get('VuFind\Record\Loader');
+            $record = $recordLoader->load($resource->record_id, $resource->source);
+            $recordCache->createOrUpdate($resource->record_id, $user->id, $resource->source, $record->getRawData(), null, $resource->id);
         }
+    
     }
 
     /**
