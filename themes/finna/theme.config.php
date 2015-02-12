992fd2a8f8c4870f9104c2e037166906b0cb2332
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2015-02-12 09:09:09 +0200

    [ALLI-3178] Remove Page-helper and unused JavaScript variables from layout.

--- themes/finna/theme.config.php
+++ themes/finna/theme.config.php
@@ -5,7 +5,6 @@ return array(
         'factories' => array(
             'content' => 'Finna\View\Helper\Root\Factory::getContent',
             'header' => 'Finna\View\Helper\Root\Factory::getHeader',
-            'page' => 'Finna\View\Helper\Root\Factory::getPage',
             'record' => 'Finna\View\Helper\Root\Factory::getRecord',
             'recordImage' => 'Finna\View\Helper\Root\Factory::getRecordImage'
         )
