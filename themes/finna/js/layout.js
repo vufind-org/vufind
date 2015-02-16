03761731f2bf7826eddf40e19ee6c1418e35a69c
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2015-02-16 14:30:31 +0200

    Add module for layout related JavaScript. Adjust footer based on screen height. Override VuFind jumoMenu selector.

---
+++ themes/finna/js/layout.js
@@ -0,0 +1,41 @@
+finna.layout = (function() {
+
+    var isTouchDevice = function() {
+        return !!('ontouchstart' in window)
+            || !!('onmsgesturechange' in window); // IE10
+    };
+
+    // Append current anchor (location.hash) to selected links
+    // in order to preserve the anchor when the link is clicked.
+    // This is used in top header language links.
+    var initAnchorNavigationLinks = function() {
+        $('a.preserve-anchor').each(function() {
+            var hash = location.hash;
+            if (hash.length == 0) {
+                return;
+            }
+            $(this).attr('href', $(this).attr('href') + hash);
+        });
+    };
+
+    var initFixFooter = function() {
+        var detectHeight = $(window).height() - $('body').height();
+        if (detectHeight > 0) {
+            var expandedFooter = $('footer').height() + detectHeight;
+            $('footer').height(expandedFooter);
+        }
+    };
+
+    var my = {
+        isTouchDevice: isTouchDevice,
+        init: function() {
+            $('select.jumpMenu').unbind('change').change(function(){ $(this).closest('form').submit(); });
+
+            initAnchorNavigationLinks();
+            initFixFooter();
+        },
+    };
+
+    return my;
+})(finna);
+
