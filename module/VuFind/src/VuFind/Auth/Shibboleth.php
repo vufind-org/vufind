a16792149bd866b6c2cb17441fb5d6aa2d5a365b
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2015-02-23 09:45:16 +0100

    modified config, permissions and new implementation of shibboleth

--- module/VuFind/src/VuFind/Auth/Shibboleth.php
+++ module/VuFind/src/VuFind/Auth/Shibboleth.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) Villanova University 2010.
+ * Copyright (C) Villanova University 2014.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,7 +21,7 @@
  *
  * @category VuFind2
  * @package  Authentication
- * @author   Franck Borel <franck.borel@gbv.de>
+ * @author   Jochen Lienhard <lienhard@ub.uni-freiburg.de>
  * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://www.vufind.org  Main Page
@@ -34,12 +34,12 @@ use VuFind\Exception\Auth as AuthException;
  *
  * @category VuFind2
  * @package  Authentication
- * @author   Franck Borel <franck.borel@gbv.de>
+ * @author   Jochen Lienhard <lienhard@ub.uni-freiburg.de>
  * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://www.vufind.org  Main Page
  */
-class Shibboleth extends AbstractBase
+class Shibboleth extends  AbstractBase
 {
     /**
      * Validate configuration parameters.  This is a support method for getConfig(),
@@ -51,19 +51,14 @@ class Shibboleth extends AbstractBase
      */
     protected function validateConfig()
     {
-        // Throw an exception if the required username setting is missing.
+        // Throw an exception if no login endpoint is available.
         $shib = $this->config->Shibboleth;
-        if (!isset($shib->username) || empty($shib->username)) {
-            throw new AuthException(
-                "Shibboleth username is missing in your configuration file."
-            );
-        }
-
         if (!isset($shib->login)) {
             throw new AuthException(
                 'Shibboleth login configuration parameter is not set.'
             );
         }
+
     }
 
     /**
@@ -79,83 +74,28 @@ class Shibboleth extends AbstractBase
     {
         // Check if username is set.
         $shib = $this->getConfig()->Shibboleth;
-        $username = $request->getServer()->get($shib->username);
+        $username = $request->getServer()->get('REMOTE_USER');
         if (empty($username)) {
             throw new AuthException('authentication_error_admin');
         }
 
-        // Check if required attributes match up:
-        foreach ($this->getRequiredAttributes() as $key => $value) {
-            if (!preg_match('/' . $value . '/', $request->getServer()->get($key))) {
-                throw new AuthException('authentication_error_denied');
+        $entityId = $request->getServer()->get('Shib-Identity-Provider');
+        if (is_array($shib->idpentityid)) {
+            if (!in_array($entityId, $shib->idpentityid)) {
+                  throw new AuthException('authentication_error_denied');
             }
+        } elseif (is_string($shib->idpentityid)) {
+            if ($shib->idpentityid!=$entityId) {
+                 throw new AuthException('authentication_error_denied');
+            }
+        } else { 
+            throw new AuthException('authentication_error_admin'); 
         }
 
         // If we made it this far, we should log in the user!
         $user = $this->getUserTable()->getByUsername($username);
 
-        // Variable to hold catalog password (handled separately from other
-        // attributes since we need to use saveCredentials method to store it):
-        $catPassword = null;
-
-        // Has the user configured attributes to use for populating the user table?
-        $attribsToCheck = [
-            'cat_username', 'cat_password', 'email', 'lastname', 'firstname',
-            'college', 'major', 'home_library'
-        ];
-        foreach ($attribsToCheck as $attribute) {
-            if (isset($shib->$attribute)) {
-                $value = $request->getServer()->get($shib->$attribute);
-                if ($attribute != 'cat_password') {
-                    $user->$attribute = $value;
-                } else {
-                    $catPassword = $value;
-                }
-            }
-        }
-
-        // Save credentials if applicable:
-        if (!empty($catPassword) && !empty($user->cat_username)) {
-            $user->saveCredentials($user->cat_username, $catPassword);
-        }
-
-        // Save and return the user object:
-        $user->save();
         return $user;
-    }
-
-    /**
-     * Get the URL to establish a session (needed when the internal VuFind login
-     * form is inadequate).  Returns false when no session initiator is needed.
-     *
-     * @param string $target Full URL where external authentication method should
-     * send user after login (some drivers may override this).
-     *
-     * @return bool|string
-     */
-    public function getSessionInitiator($target)
-    {
-        $config = $this->getConfig();
-        if (isset($config->Shibboleth->target)) {
-            $shibTarget = $config->Shibboleth->target;
-        } else {
-            $shibTarget = $target;
-        }
-        $append = (strpos($shibTarget, '?') !== false) ? '&' : '?';
-        $sessionInitiator = $config->Shibboleth->login
-            . '?target=' . urlencode($shibTarget)
-            . urlencode($append . 'auth_method=Shibboleth');
-                                                    // makes it possible to
-                                                    // handle logins when using
-                                                    // an auth method that
-                                                    // proxies others
-
-        if (isset($config->Shibboleth->provider_id)) {
-            $sessionInitiator = $sessionInitiator . '&providerId=' .
-                urlencode($config->Shibboleth->provider_id);
-        }
-
-        return $sessionInitiator;
     }
 
     /**
@@ -166,68 +106,7 @@ class Shibboleth extends AbstractBase
     public function isExpired()
     {
         $config = $this->getConfig();
-        if (isset($config->Shibboleth->username)
-            && isset($config->Shibboleth->logout)
-        ) {
-            // It would be more proper to call getServer on a Zend request
-            // object... except that the request object doesn't exist yet when
-            // this routine gets called.
-            $username = isset($_SERVER[$config->Shibboleth->username])
-                ? $_SERVER[$config->Shibboleth->username] : null;
-            return empty($username);
-        }
         return false;
     }
-
-    /**
-     * Perform cleanup at logout time.
-     *
-     * @param string $url URL to redirect user to after logging out.
-     *
-     * @return string     Redirect URL (usually same as $url, but modified in
-     * some authentication modules).
-     */
-    public function logout($url)
-    {
-        // If single log-out is enabled, use a special URL:
-        $config = $this->getConfig();
-        if (isset($config->Shibboleth->logout)
-            && !empty($config->Shibboleth->logout)
-        ) {
-            $url = $config->Shibboleth->logout . '?return=' . urlencode($url);
-        }
-
-        // Send back the redirect URL (possibly modified):
-        return $url;
-    }
-
-    /**
-     * Extract required user attributes from the configuration.
-     *
-     * @return array      Only username and attribute-related values
-     */
-    protected function getRequiredAttributes()
-    {
-        // Special case -- store username as-is to establish return array:
-        $sortedUserAttributes = [];
-
-        // Now extract user attribute values:
-        $shib = $this->getConfig()->Shibboleth;
-        foreach ($shib as $key => $value) {
-            if (preg_match("/userattribute_[0-9]{1,}/", $key)) {
-                $valueKey = 'userattribute_value_' . substr($key, 14);
-                $sortedUserAttributes[$value] = isset($shib->$valueKey)
-                    ? $shib->$valueKey : null;
-
-                // Throw an exception if attributes are missing/empty.
-                if (empty($sortedUserAttributes[$value])) {
-                    throw new AuthException(
-                        "User attribute value of " . $value . " is missing!"
-                    );
-                }
-            }
-        }
-
-        return $sortedUserAttributes;
-    }
 }
+
