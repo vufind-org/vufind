c7caf440d11389252d366c9f8da597b94cff7c7a
Chris Hallberg <crhallberg@gmail.com>
2015-03-11 14:44:49 -0400

    Comments typo.

--- module/VuFind/src/VuFind/Db/Table/ResourceTags.php
+++ module/VuFind/src/VuFind/Db/Table/ResourceTags.php
@@ -203,7 +203,7 @@ class ResourceTags extends Gateway
      *                                  unlink (null for ALL matching resources)
      * @param string       $user     ID of user removing links
      * @param string       $list     ID of list to unlink (null for ALL matching
-     *                                  lists, 'none' for tags not in a list,
+     *                                  tags, 'none' for tags not in a list,
      *                                  true for tags only found in a list)
      * @param string       $tag      ID of tag to unlink (null for ALL matching
      *                                  tags)
