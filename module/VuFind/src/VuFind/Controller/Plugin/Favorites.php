30e910150865d053c766fdb8acdcdf2959d9f464
Markus Beh <markus.beh@ub.uni-freiburg.de>
2015-03-19 10:07:38 +0100

    avoid loading record if the record source won't be cached anyway

--- module/VuFind/src/VuFind/Controller/Plugin/Favorites.php
+++ module/VuFind/src/VuFind/Controller/Plugin/Favorites.php
@@ -27,7 +27,8 @@
  */
 namespace VuFind\Controller\Plugin;
 use VuFind\Exception\LoginRequired as LoginRequiredException,
-    Zend\Mvc\Controller\Plugin\AbstractPlugin;
+    Zend\Mvc\Controller\Plugin\AbstractPlugin,
+    VuFind\Record\Cache;
 
 /**
  * Zend action helper to perform favorites-related actions
@@ -87,10 +88,16 @@ class Favorites extends AbstractPlugin
                 ? $tagParser->parse($params['mytags']) : [];
             $user->saveResource($resource, $list, $tags, '', false);
 
+            // Get and configure the record cache for the use case: Favorite 
             $recordCache = $this->getController()->getServiceLocator()->get('VuFind\RecordCache');
-            $recordLoader = $this->getController()->getServiceLocator()->get('VuFind\RecordLoader');
-            $record = $recordLoader->load($resource->record_id, $resource->source);
-            $recordCache->createOrUpdate($resource->record_id, $user->id, $resource->source, $record->getRawData(), null, $resource->id);
+			$recordCache->setPolicy(Cache::POLICY_FAVORITE);
+                  
+			// Load and persist record only if the source is cachable
+            if ($recordCache->isCacheable($resource->source)) {
+	            $recordLoader = $this->getController()->getServiceLocator()->get('VuFind\RecordLoader');
+            	$record = $recordLoader->load($resource->record_id, $resource->source);
+            	$recordCache->createOrUpdate($resource->record_id, $user->id, $resource->source, $record->getRawData(), null, $resource->id);
+            }
         }
     
     }
