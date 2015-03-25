b400701537ed23581ca14fae5ca22d474b8a49ad
Ere Maijala <ere.maijala@helsinki.fi>
2015-03-25 15:47:01 +0200

    Tweaked ajax availability a bit more and added aborting of previous request when the source is changed.

--- themes/finna/js/check_item_statuses.js
+++ themes/finna/js/check_item_statuses.js
@@ -4,19 +4,26 @@ function checkItemStatus(id) {
   var safeId = jqEscape(id);
   var item = $('.hiddenId[value="' + safeId + '"]').closest('.ajaxItem');
   item.find(".ajax-availability").removeClass('hidden');
-  item.find('.callnumAndLocation').removeClass('hidden');
-  item.find('.no-holdings').addClass('hidden');
+  item.find(".availability-load-indicator").removeClass('hidden');
 
-  $.ajax({
+  // Hide all holdings fields by default:
+  item.find('.locationDetails').addClass('hidden');
+  item.find('.no-holdings').addClass('hidden');
+  item.find('.callnumber').addClass('hidden');
+  item.find('.location').addClass('hidden');
+  item.find('.hideIfDetailed').addClass('hidden');
+  item.find('.status').addClass('hidden');
+  
+  if (typeof item.data('xhr') !== 'undefined') {
+    item.data('xhr').abort();
+  }
+  var xhr = $.ajax({
     dataType: 'json',
     url: path + '/AJAX/JSON?method=getItemStatuses',
     data: {id:[id]},
     success: function(response) {
       if(response.status == 'OK') {
         $.each(response.data, function(i, result) {
-          item.find(".ajax-availability").removeClass('hidden');
-          item.find('.no-holdings').addClass('hidden');
-          
           item.find('.status').empty().append(result.availability_message);
           item.find('.dedup-select').removeAttr('selected').
             find('option[value="' + jqEscape(result.record_number) + '"]').attr('selected', '1');
@@ -25,24 +32,16 @@ function checkItemStatus(id) {
             && result.full_status.length > 0
             && item.find('.callnumAndLocation').length > 0
           ) {
-            // Full status mode is on -- display the HTML and hide extraneous junk:
+            // Full status mode is on -- display the HTML:
             item.find('.locationDetails').empty().append(result.full_status);
-            item.find('.callnumber').addClass('hidden');
-            item.find('.location').addClass('hidden');
-            item.find('.hideIfDetailed').addClass('hidden');
-            item.find('.status').addClass('hidden');
+            item.find('.locationDetails').removeClass('hidden');
           } else if (typeof(result.missing_data) != 'undefined'
             && result.missing_data
           ) {
-            // No data is available -- hide the entire status area:
-            item.find('.callnumAndLocation').addClass('hidden');
-            item.find('.status').addClass('hidden');
+            // No data is available:
             item.find('.no-holdings').removeClass('hidden');
           } else if (result.locationList) {
-            // We have multiple locations -- build appropriate HTML and hide unwanted labels:
-            item.find('.callnumber').addClass('hidden');
-            item.find('.hideIfDetailed').addClass('hidden');
-            item.find('.location').addClass('hidden');
+            // We have multiple locations -- build appropriate HTML:
             var locationListHTML = "";
             for (var x=0; x<result.locationList.length; x++) {
               locationListHTML += '<div class="groupLocation">';
@@ -59,8 +58,8 @@ function checkItemStatus(id) {
                    ?  result.locationList[x].callnumbers : '';
               locationListHTML += '</div>';
             }
-            item.find('.locationDetails').removeClass('hidden');
             item.find('.locationDetails').empty().append(locationListHTML);
+            item.find('.locationDetails').removeClass('hidden');
           } else {
             // Default case -- load call number and location into appropriate containers:
             item.find('.callnumber').empty().append(result.callnumber+'<br/>');
@@ -69,15 +68,19 @@ function checkItemStatus(id) {
               ? result.reserve_message
               : result.location
             );
+            item.find('.callnumber').removeClass('hidden');
+            item.find('.location').removeClass('hidden');
           }
         });
       } else {
         // display the error message on each of the ajax status place holder
-        $(".ajax-availability").empty().append(response.data);
-      }
-      item.find(".ajax-availability").removeClass('ajax-availability');
+        item.find('.locationDetails').empty().append(response.data);
+        item.find('.locationDetails').removeClass('hidden');
+      } 
+      item.find(".availability-load-indicator").addClass('hidden');
     }
   });
+  item.data('xhr', xhr);
 }
 
 function initDedupRecordSelection() 
