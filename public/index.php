85c9ca0fb47249aaffd485125efbaf7c92bee960
Demian Katz <demian.katz@villanova.edu>
2015-04-08 15:13:29 -0400

    Update index.php
    Rolling back accidental commit.

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ defined('APPLICATION_PATH')
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('VUFIND_ENV') ? getenv('VUFIND_ENV') : 'development'));
+    || define('APPLICATION_ENV', (getenv('VUFIND_ENV') ? getenv('VUFIND_ENV') : 'production'));
 
 // Define path to local override directory
 defined('LOCAL_OVERRIDE_DIR')
