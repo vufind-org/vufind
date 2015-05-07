290e09e42ca7687bd326b6ce4001057f10cfc616
Markus Mächler <markus.maechler@bithost.ch>
2015-05-07 18:09:07 +0200

    Fixes piwik limitation to track custom variables 4 and 5 together with trackSiteSearch

--- module/VuFind/src/VuFind/View/Helper/Root/Piwik.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Piwik.php
@@ -329,6 +329,10 @@ EOT;
         $i = 0;
         foreach ($customVars as $key => $value) {
             ++$i;
+
+            //workaround to prevent that custom variables 4 and 5 get overwritten by trackSiteSearch, see http://forum.piwik.org/read.php?2,115537,115538
+            if ($i === 4) $i = 6;
+
             $value = $escape($value);
             $code .= <<<EOT
 _paq.push(['setCustomVariable', $i, '$key', '$value', 'page']);
