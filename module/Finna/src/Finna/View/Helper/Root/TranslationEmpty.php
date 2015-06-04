f24d9c3e4850c67c385b642a7095e34531d3c332
Ere Maijala <ere.maijala@helsinki.fi>
2015-06-04 13:08:28 +0300

    Added a TranslationEmpty() view helper to clean up empty translation string checking.

---
+++ module/Finna/src/Finna/View/Helper/Root/TranslationEmpty.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Helper to check if a translation is empty
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+namespace Finna\View\Helper\Root;
+
+/**
+ * Helper to check if a translation is empty
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+class TranslationEmpty extends \Zend\View\Helper\AbstractHelper
+    implements \VuFind\I18n\Translator\TranslatorAwareInterface
+{
+    use \VuFind\I18n\Translator\TranslatorAwareTrait;
+
+    /**
+     * Check if a translation is empty
+     *
+     * @param string|object $str String to translate
+     *
+     * @return bool
+     */
+    public function __invoke($str)
+    {
+        return $this->translate($str, [], null)
+            == html_entity_decode('&#x200C;', ENT_NOQUOTES, 'UTF-8');
+    }
+}
