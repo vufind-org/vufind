1af8da1e1ffb6520e937faf8e432a2c2803b44c3
Markus Beh <markus.beh@ub.uni-freiburg.de>
2015-08-25 10:29:49 +0200

    fix: re-add getBulkUrl

--- module/VuFind/src/VuFind/Export.php
+++ module/VuFind/src/VuFind/Export.php
@@ -85,6 +85,33 @@ class Export
         return $this->bulkOptions;
     }
 
+    /** 
+     * Get the URL for bulk export.
+     *
+     * @param \Zend\View\Renderer\RendererInterface $view   View object (needed for
+     * URL generation)
+     * @param string                                $format Export format being used
+     * @param array                                 $ids    Array of IDs to export
+     * (in source|id format)
+     *
+     * @return string
+     */
+    public function getBulkUrl($view, $format, $ids)
+    {
+    $params = [];
+    $params[] = 'f=' . urlencode($format);
+    foreach ($ids as $id) {
+           $params[] = urlencode('i[]') . '=' . urlencode($id);
+       }
+       $serverUrlHelper = $view->plugin('serverurl');
+       $urlHelper = $view->plugin('url');
+       $url = $serverUrlHelper($urlHelper('cart-doexport'))
+           . '?' . implode('&', $params);
+    
+       return $this->needsRedirect($format)
+           ? $this->getRedirectUrl($format, $url) : $url;
+    }
+    
     /**
      * Build callback URL for export.
      *
