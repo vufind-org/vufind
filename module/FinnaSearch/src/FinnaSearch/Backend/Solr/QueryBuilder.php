20a3e5b500a1c267b88c32f61867e3d05ec38bf5
Ere Maijala <ere.maijala@helsinki.fi>
2015-09-08 11:52:55 +0300

    [ALLI-3363] Added support for UNICODE normalization of search terms. Made - and !- work like in VuFind 1 (ALLI-3099).

---
+++ module/FinnaSearch/src/FinnaSearch/Backend/Solr/QueryBuilder.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * SOLR QueryBuilder.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ * Copyright (C) The National Library of Finland 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Andrew S. Nagy <vufind-tech@lists.sourceforge.net>
+ * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+namespace FinnaSearch\Backend\Solr;
+
+use VuFindSearch\Query\AbstractQuery;
+use VuFindSearch\Query\QueryGroup;
+use VuFindSearch\Query\Query;
+
+use VuFindSearch\ParamBag;
+
+/**
+ * SOLR QueryBuilder.
+ *
+ * @category VuFind2
+ * @package  Search
+ * @author   Andrew S. Nagy <vufind-tech@lists.sourceforge.net>
+ * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org
+ */
+class QueryBuilder extends \VuFindSearch\Backend\Solr\QueryBuilder
+{
+    /**
+     * Return SOLR search parameters based on a user query and params.
+     *
+     * @param AbstractQuery $query User query
+     *
+     * @return ParamBag
+     */
+    public function build(AbstractQuery $query)
+    {
+        $params = parent::build($query);
+
+        if (!($query instanceof QueryGroup)) {
+            $q = $params->get('q');
+            foreach ($q as &$value) {
+                $value = $this->getLuceneHelper()->finalizeSearchString($value);
+            }
+            $params->set('q', $q);
+        }
+        return $params;
+    }
+
+    /**
+     * Reduce components of query group to a search string of a simple query.
+     *
+     * This function implements the recursive reduction of a query group.
+     *
+     * @param AbstractQuery $component Component
+     *
+     * @return string
+     *
+     * @see self::reduceQueryGroup()
+     */
+    protected function reduceQueryGroupComponents(AbstractQuery $component)
+    {
+        if ($component instanceof QueryGroup) {
+            $reduced = array_map(
+                [$this, 'reduceQueryGroupComponents'], $component->getQueries()
+            );
+            $searchString = $component->isNegated() ? 'NOT ' : '';
+            $searchString .= sprintf(
+                '(%s)', implode(" {$component->getOperator()} ", $reduced)
+            );
+        } else {
+            $searchString  = $this->getLuceneHelper()
+                ->normalizeSearchString($component->getString());
+            $searchString  = $this->getLuceneHelper()
+                ->finalizeSearchString($searchString);
+            $searchHandler = $this->getSearchHandler(
+                $component->getHandler(),
+                $searchString
+            );
+            if ($searchHandler) {
+                $searchString
+                    = $this->createSearchString($searchString, $searchHandler);
+            }
+        }
+        return $searchString;
+    }
+
+}
