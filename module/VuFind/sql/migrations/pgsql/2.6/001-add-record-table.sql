3694c8f16e76fe5aa55d9bf7003fa54242a7e177
Demian Katz <demian.katz@villanova.edu>
2015-09-29 12:40:24 -0400

    Update 001-add-record-table.sql
    Adjusted for consistency with MySQL.

--- module/VuFind/sql/migrations/pgsql/2.6/001-add-record-table.sql
+++ module/VuFind/sql/migrations/pgsql/2.6/001-add-record-table.sql
@@ -8,7 +8,7 @@ CREATE TABLE record
   cache_id varchar(100),
   record_id varchar(120),
   user_id integer,
-  source varchar,
+  source varchar(50),
   version varchar(20) NOT NULL,
   data text,
   resource_id integer,
