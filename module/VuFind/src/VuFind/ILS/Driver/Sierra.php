dfe983de3debd03cfbf583fa7b00ed5ec869a48e
joemontibello <josephmontibello@gmail.com>
2015-11-02 14:18:25 -0500

    more spacing fixes

--- module/VuFind/src/VuFind/ILS/Driver/Sierra.php
+++ module/VuFind/src/VuFind/ILS/Driver/Sierra.php
@@ -172,14 +172,13 @@ class Sierra extends AbstractBase implements TranslatorAwareInterface
                 $this->db, $query, [$this->idStrip($id)]
             );
             if (pg_num_rows($results)>0) {
-               $callnumberarray = pg_fetch_array($results, 0, PGSQL_NUM);
-               $callnumber = $callnumberarray[0];
-               // stripping subfield codes from call numbers
-               $callnumber = preg_replace('/\|(a|b)/', ' ', $callnumber);
-               }
-            else { 
+                $callnumberarray = pg_fetch_array($results, 0, PGSQL_NUM);
+                $callnumber = $callnumberarray[0];
+                // stripping subfield codes from call numbers
+                $callnumber = preg_replace('/\|(a|b)/', ' ', $callnumber);
+            } else { 
                  $callnumber = "";
-                 }
+            }
         }
         return $callnumber;
     }
