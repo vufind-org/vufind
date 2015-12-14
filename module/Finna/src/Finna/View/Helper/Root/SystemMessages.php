e4f178af3dde603a5adde55b8cf2ec87b8e3c594
Ere Maijala <ere.maijala@helsinki.fi>
2015-12-14 11:01:29 +0200

    Added SystemMessages.php missing from a previous commit.

---
+++ module/Finna/src/Finna/View/Helper/Root/SystemMessages.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Helper class for system messages
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+namespace Finna\View\Helper\Root;
+
+/**
+ * Helper class for system messages
+ *
+ * @category VuFind2
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+class SystemMessages extends \Zend\View\Helper\AbstractHelper
+{
+    /**
+     * Configuration
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * Constructor
+     *
+     * @param array $config Configuration
+     */
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Return any system messages (translatable).
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return !empty($this->config->Site->system_messages)
+            ? $this->config->Site->system_messages : [];
+    }
+}
