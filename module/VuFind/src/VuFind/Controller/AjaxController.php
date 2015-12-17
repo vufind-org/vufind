a7c37da535bc5668f20fb83b3b16b35c6ee80a42
Ere Maijala <ere.maijala@helsinki.fi>
2015-12-17 14:39:44 +0200

    Expose the "unknown" status in the item status JSON so that it can be displayed as something else than unavailable when show_full_status = false. Return the field from getStatus() call in Voyager like is done in getHoldings().

--- module/VuFind/src/VuFind/Controller/AjaxController.php
+++ module/VuFind/src/VuFind/Controller/AjaxController.php
@@ -352,6 +352,7 @@ class AjaxController extends AbstractBase
                 && $info['use_unknown_message'] == true
             ) {
                 $use_unknown_status = true;
+                $locations[$info['location']]['status_unknown'] = true;
             }
             // Store call number/location info:
             $callNumbers[] = $info['callnumber'];
@@ -384,7 +385,9 @@ class AjaxController extends AbstractBase
             'reserve_message' => $record[0]['reserve'] == 'Y'
                 ? $this->translate('on_reserve')
                 : $this->translate('Not On Reserve'),
-            'callnumber' => htmlentities($callNumber, ENT_COMPAT, 'UTF-8')
+            'callnumber' => htmlentities($callNumber, ENT_COMPAT, 'UTF-8'),
+            'status_unknown' => isset($details['status_unknown'])
+                ? $details['status_unknown'] : false
         ];
     }
 
@@ -416,6 +419,7 @@ class AjaxController extends AbstractBase
                 && $info['use_unknown_message'] == true
             ) {
                 $use_unknown_status = true;
+                $locations[$info['location']]['status_unknown'] = true;
             }
             // Store call number/location info:
             $locations[$info['location']]['callnumbers'][] = $info['callnumber'];
@@ -437,7 +441,9 @@ class AjaxController extends AbstractBase
                     ENT_COMPAT, 'UTF-8'
                 ),
                 'callnumbers' =>
-                    htmlentities($locationCallnumbers, ENT_COMPAT, 'UTF-8')
+                    htmlentities($locationCallnumbers, ENT_COMPAT, 'UTF-8'),
+                'status_unknown' => isset($details['status_unknown'])
+                    ? $details['status_unknown'] : false
             ];
             $locationList[] = $locationInfo;
         }
