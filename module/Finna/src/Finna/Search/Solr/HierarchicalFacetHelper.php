c2674b83ef2d2ac31c5dffa87846c3ee2dedc67c
Ere Maijala <ere.maijala@helsinki.fi>
2016-02-10 15:50:52 +0200

    [ALLI-3538] Added logic to exclude ancestor or child facets when a facet item from a different level is selected.

--- module/Finna/src/Finna/Search/Solr/HierarchicalFacetHelper.php
+++ module/Finna/src/Finna/Search/Solr/HierarchicalFacetHelper.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) The National Library of Finland 2014-2015.
+ * Copyright (C) The National Library of Finland 2014-2016.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -38,6 +38,29 @@ namespace Finna\Search\Solr;
  */
 class HierarchicalFacetHelper extends \VuFind\Search\Solr\HierarchicalFacetHelper
 {
+    /**
+     * Helper method for building hierarchical facets:
+     * Convert facet list to a hierarchical array
+     *
+     * @param string    $facet     Facet name
+     * @param array     $facetList Facet list
+     * @param UrlHelper $urlHelper Query URL helper for building facet URLs
+     *
+     * @return array Facet hierarchy
+     */
+    public function buildFacetArray($facet, $facetList, $urlHelper = false)
+    {
+        $result = parent::buildFacetArray($facet, $facetList, $urlHelper);
+
+        if ($urlHelper !== false) {
+            // Recreate href's for any facet items that have children/ancestors
+            // applied to remove them from the filters
+            $result = $this->removeAncestorAndChildFilters($facet, $result);
+        }
+
+        return $result;
+    }
+
     /**
      * Filter hierarchical facets
      *
@@ -93,5 +116,123 @@ class HierarchicalFacetHelper extends \VuFind\Search\Solr\HierarchicalFacetHelpe
         }
 
         return array_values($facets);
+    }
+
+    /**
+     * Check if a filter value is an ancestor of the given facet item
+     *
+     * @param array  $item   Facet item
+     * @param string $filter Filter value
+     *
+     * @return bool
+     */
+    protected function isAncestor($item, $filter)
+    {
+        if (!preg_match('/^"(\d+)\/(.+)"$/', $filter, $matches)) {
+            return false;
+        }
+        $level = $matches[1];
+        if ($level >= $item['level']) {
+            return false;
+        }
+        // Extract the ancestor level part from the item value
+        $part = implode(
+            '/', array_slice(explode('/', $item['value']), 1, $level + 1)
+        ) . '/';
+
+        // Extract value part from filter and compare with ancestor level part
+        list(, $filterValue) = explode('/', substr($filter, 1, -1), 2);
+        return $part == $filterValue;
+    }
+
+    /**
+     * Check if a filter value is a child of the given facet item
+     *
+     * @param array  $item   Facet item
+     * @param string $filter Filter value
+     *
+     * @return bool
+     */
+    protected function isChild($item, $filter)
+    {
+        if (!preg_match('/^"(\d+)\/.+"$/', $filter, $matches)) {
+            return false;
+        }
+        $level = $matches[1];
+        if ($level <= $item['level']) {
+            return false;
+        }
+
+        // Compare item value and filter value for the length of the item value
+        list(, $itemValue) = explode('/', $item['value'], 2);
+        list(, $filterValue) = explode('/', substr($filter, 1, -1), 2);
+        return strncmp($filterValue, $itemValue, strlen($itemValue)) == 0;
+    }
+
+    /**
+     * Check all facets for applied ancestors/children and change the href to remove
+     * those filters
+     *
+     * @param string    $facetName       Facet name
+     * @param array     $facets          Hierarchical facet array
+     * @param bool      $ancestorApplied Boolean indicating whether any ancestor is
+     * applied
+     *
+     * @return array Modified facet array
+     */
+    protected function removeAncestorAndChildFilters(
+        $facetName, $facets, $ancestorApplied = false
+    ) {
+        foreach ($facets as &$item) {
+            if (!$item['isApplied'] && $item['hasAppliedChildren']) {
+                $item['href'] = $this->removeFilters($facetName, $item, true);
+            }
+            if ($item['level'] > 0 && $ancestorApplied) {
+                $item['href'] = $this->removeFilters($facetName, $item, false);
+            }
+            if ($item['children']) {
+                $item['children'] = $this->removeAncestorAndChildFilters(
+                    $facetName, $item['children'],
+                    $ancestorApplied || $item['isApplied']
+                );
+            }
+        }
+        return $facets;
+    }
+
+    /**
+     * Remove any ancestor/child filters from the facet href. Would rather use
+     * urlQueryHelper, but it doesn't allow removing multiple filters and adding a
+     * new one.
+     *
+     * @param string $facet    Facet name
+     * @param array  $item     Facet item
+     * @param bool   $children Whether to remove children or ancestors
+     */
+    protected function removeFilters($facet, $item, $children)
+    {
+        $urlParts = explode('?', $item['href'], 2);
+        if (!isset($urlParts[1])) {
+            return $item['href'];
+        }
+        parse_str(htmlspecialchars_decode($urlParts[1]), $params);
+
+        if (!isset($params['filter'])) {
+            return $item['href'];
+        }
+        $newFilters = [];
+        foreach ($params['filter'] as $filter) {
+             list($filterField, $filterValue) = explode(':', $filter, 2);
+             if ($filterField == $facet || $filterField == "~$facet") {
+                 if ((!$children && $this->isAncestor($item, $filterValue))
+                     || ($children && $this->isChild($item, $filterValue))
+                 ) {
+                     continue;
+                 }
+             }
+             $newFilters[] = $filter;
+        }
+        $params['filter'] = $newFilters;
+        return $urlParts[0] . '?' . htmlspecialchars(http_build_query($params));
     }
 }
