9527154b0d4e3a6f98f935ac9f837d097ad3585d
Chris Hallberg <crhallberg@gmail.com>
2016-02-22 15:11:42 -0500

    File cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,12 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          paths: ["themes/bootstrap3/less", "themes/bootprint3/less"] /* ,
-          compress: true //*/
+          paths: ["themes/bootprint3/less", "themes/bootstrap3/less"],
+          compress: true
         },
         files: {
-          "themes/bootstrap3/css/compiled.css": "themes/bootstrap3/less/compiled.less",
-          "themes/bootprint3/css/compiled.css": "themes/bootprint3/less/compiled.less",
+          "themes/bootstrap3/css/compiled.css": "themes/bootstrap3/less/bootstrap.less",
+          "themes/bootprint3/css/compiled.css": "themes/bootprint3/less/bootprint.less",
         }
       }
     },
@@ -23,9 +23,11 @@ module.exports = function(grunt) {
       vendor_min: {
         files: {
           'themes/bootstrap3/js/vendor.min.js': [
+            'themes/bootstrap3/js/vendor/jquery.min.js',
+            'themes/bootstrap3/js/vendor/bootstrap.min.js',
             'themes/bootstrap3/js/vendor/*.js',
-            'themes/bootstrap3/js/autocomplete.js',
-            'themes/bootstrap3/js/vendor/bootlint.min.js'
+            '!themes/bootstrap3/js/vendor/bootlint.min.js',
+            'themes/bootstrap3/js/autocomplete.js'
           ]
         }
       }
