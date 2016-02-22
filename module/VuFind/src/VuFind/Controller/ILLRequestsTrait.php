d815cdc76b7762ae9d123a385bdd90c5facad3ea
Demian Katz <demian.katz@villanova.edu>
2016-02-22 15:07:58 -0500

    Wrap long line.

--- module/VuFind/src/VuFind/Controller/ILLRequestsTrait.php
+++ module/VuFind/src/VuFind/Controller/ILLRequestsTrait.php
@@ -115,7 +115,8 @@ trait ILLRequestsTrait
                     'html' => true,
                     'msg' => 'ill_request_place_success',
                     'tokens' => [
-                        '%%url%%' => $this->url()->fromRoute('myresearch-illrequests')
+                        '%%url%%' => $this->url()
+                            ->fromRoute('myresearch-illrequests')
                     ],
                 ];
                 $this->flashMessenger()->addMessage($msg, 'success');
