9718c87b2ab0ecf375b93ee10c25cb42bb6c54c1
Ere Maijala <ere.maijala@helsinki.fi>
2016-02-29 17:14:21 +0200

    Cleaned up the combined results JS and fixed save statuses and links in combined results. Also fixed item status check when the same record was displayed in multiple columns.

--- themes/bootstrap3/js/check_save_statuses.js
+++ themes/bootstrap3/js/check_save_statuses.js
@@ -1,13 +1,21 @@
 /*global VuFind */
 
-function checkSaveStatuses() {
-  var elements = {}
-  var data = $.map($('.result,.record'), function(record) {
-    if($(record).find('.hiddenId').length == 0 || $(record).find('.hiddenSource').length == 0) {
+function checkSaveStatuses(container) {
+  if (typeof(container) == 'undefined') {
+    container = $('body');
+  }
+    
+  var elements = {};
+  var data = $.map(container.find('.result,.record'), function(record) {
+    if ($(record).find('.hiddenId').length == 0 || $(record).find('.hiddenSource').length == 0) {
       return false;
     }
     var datum = {'id':$(record).find('.hiddenId').val(), 'source':$(record).find('.hiddenSource')[0].value};
-    elements[datum.source+"|"+datum.id] = $(record).find('.savedLists');
+    var key = datum.source+'|'+datum.id;
+    if (typeof elements[key] === 'undefined') {
+      elements[key] = $();
+    }
+    elements[key] = elements[key].add($(record).find('.savedLists'));
     return datum;
   });
   if (data.length) {
@@ -21,7 +29,7 @@ function checkSaveStatuses() {
       dataType: 'json',
       method: 'POST',
       url: VuFind.path + '/AJAX/JSON?method=getSaveStatuses',
-      data: {id:ids, 'source':srcs}
+      data: {'id':ids, 'source':srcs}
     })
     .done(function(response) {
       for (var sel in response.data) {
@@ -32,7 +40,7 @@ function checkSaveStatuses() {
         var html = list.find('strong')[0].outerHTML+'<ul>';
         for (var i=0; i<response.data[sel].length; i++) {
           html += '<li><a href="' + response.data[sel][i].list_url + '">'
-                   + htmlEncode(response.data[sel][i].list_title) + '</a></li>';
+            + htmlEncode(response.data[sel][i].list_title) + '</a></li>';
         }
         html += '</ul>';
         list.html(html).removeClass('hidden');
