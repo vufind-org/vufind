6dc11cc5b00df35880b6df71353a23a155d3a812
Ere Maijala <ere.maijala@helsinki.fi>
2016-03-03 09:02:50 -0500

    ILS driver cache improvements. - Create generic (optional) caching mechanism in base class. - Replace VoyagerRestful session-based caching with new generic mechanism.

--- module/VuFind/src/VuFind/ILS/Driver/Factory.php
+++ module/VuFind/src/VuFind/ILS/Driver/Factory.php
@@ -172,9 +172,13 @@ class Factory
     public static function getVoyagerRestful(ServiceManager $sm)
     {
         $ils = $sm->getServiceLocator()->get('VuFind\ILSHoldSettings');
-        return new VoyagerRestful(
+        $vr = new VoyagerRestful(
             $sm->getServiceLocator()->get('VuFind\DateConverter'),
             $ils->getHoldsMode(), $ils->getTitleHoldsMode()
         );
+        $vr->setCacheStorage(
+            $sm->getServiceLocator()->get('VuFind\CacheManager')->getCache('object')
+        );
+        return $vr;
     }
 }
