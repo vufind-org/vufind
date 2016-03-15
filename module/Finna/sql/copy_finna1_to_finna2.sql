f0a2aaa06d0c526313bad8038b393ea598b769d0
Ere Maijala <ere.maijala@helsinki.fi>
2016-03-15 16:22:57 +0200

    Fixes to database copy script based on production data.

--- module/Finna/sql/copy_finna1_to_finna2.sql
+++ module/Finna/sql/copy_finna1_to_finna2.sql
@@ -28,23 +28,23 @@ DELETE FROM change_tracker;
 
 INSERT INTO change_tracker (core, id, first_indexed, last_indexed, last_record_change, deleted)
   SELECT core, id, first_indexed, last_indexed, last_record_change, deleted 
-    FROM vufind.change_tracker;
+    FROM `vufind`.change_tracker;
 
 INSERT INTO user (id, username, password, firstname, lastname, email, cat_username, cat_password, college, major, home_library, created, finna_language, finna_due_date_reminder, finna_last_login, finna_auth_method)
   SELECT id, username, password, firstname, lastname, email, cat_username, cat_password, college, major, home_library, created, language, due_date_reminder, last_login, authMethod
-    FROM vufind.user;
+    FROM `vufind`.user;
 
 INSERT INTO user_card (id, user_id, card_name, cat_username, cat_password, home_library, created, saved)
   SELECT id, user_id, account_name, cat_username, cat_password, home_library, created, saved
-    FROM vufind.user_account;
+    FROM `vufind`.user_account;
 
 INSERT INTO user_list (id, user_id, title, description, created, public, finna_updated)
   SELECT id, user_id, title, description, created, public, modified
-    FROM vufind.user_list;    
+    FROM `vufind`.user_list;    
     
 INSERT INTO search (id, user_id, session_id, folder_id, created, title, saved, search_object, finna_schedule, finna_last_executed, finna_schedule_base_url)
   SELECT id, user_id, session_id, folder_id, created, title, saved, search_object, schedule, last_executed, schedule_base_url
-    FROM vufind.search
+    FROM `vufind`.search
     WHERE saved=1;
 
 -- Add class to Solr searches     
@@ -58,50 +58,84 @@ UPDATE search SET search_object=CONCAT('O:5:"minSO":9', SUBSTRING(search_object,
 -- Add class to MetaLib searches     
 UPDATE search SET search_object=CONCAT('O:5:"minSO":9', SUBSTRING(search_object, 14, INSTR(search_object, 's:2:"ty";s:7:"MetaLib";')-14), 's:2:"ty";s:5:"basic";s:2:"cl";s:7:"MetaLib";', SUBSTRING(search_object, INSTR(search_object, 's:2:"ty";s:7:"MetaLib";')+23))
   WHERE search_object like '%s:2:"ty";s:7:"MetaLib"%';    
+                
+-- Convert advanced searches that don't have groups to basic searches
+UPDATE search SET search_object=CONCAT(
+    SUBSTRING(search_object, 1, INSTR(search_object, 's:2:"ty";s:8:"advanced";')-1), 
+    's:2:"ty";s:5:"basic";', 
+    SUBSTRING(search_object, INSTR(search_object, 's:2:"ty";s:8:"advanced";')+24)
+  )
+  WHERE search_object like '%advanced%' and search_object not like '%s:1:"g"%';
+
+-- Fix join
+UPDATE search SET search_object=CONCAT(
+    SUBSTRING(search_object, 1, INSTR(search_object, 's:1:"b";N;')-1), 
+    's:1:"b";s:3:"AND";', 
+    SUBSTRING(search_object, INSTR(search_object, 's:1:"b";N;')+10)
+  )
+  WHERE search_object like '%advanced%' and search_object like '%s:1:"b";N;%';
+
+-- Fix bool
+UPDATE search SET search_object=CONCAT(
+    SUBSTRING(search_object, 1, INSTR(search_object, 's:1:"j";N;')-1), 
+    's:1:"j";s:3:"AND";', 
+    SUBSTRING(search_object, INSTR(search_object, 's:1:"j";N;')+10)
+  )
+  WHERE search_object like '%advanced%' and search_object like '%s:1:"j";N;%';
+UPDATE search SET search_object=CONCAT(
+    SUBSTRING(search_object, 1, INSTR(search_object, 's:1:"j";s:0:"";')-1), 
+    's:1:"j";s:3:"AND";', 
+    SUBSTRING(search_object, INSTR(search_object, 's:1:"j";s:0:"";')+15)
+  )
+  WHERE search_object like '%advanced%' and search_object like '%s:1:"j";s:0:"";%';
                   
 INSERT INTO resource (id, record_id, title, author, source)
   SELECT id, record_id, title, author_sort, CASE WHEN source='VuFind' THEN 'Solr' WHEN source='PCI' THEN 'Primo' ELSE source END
-    FROM vufind.resource;
+    FROM `vufind`.resource;
 
 INSERT INTO tags (id, tag)
   SELECT id, tag
-    FROM vufind.tags;  
+    FROM `vufind`.tags;  
       
 INSERT INTO resource_tags (id, resource_id, tag_id, list_id, user_id, posted)
   SELECT id, resource_id, tag_id, list_id, user_id, posted
-    FROM vufind.resource_tags;
+    FROM `vufind`.resource_tags;
     
 INSERT INTO user_resource (id, user_id, resource_id, list_id, notes, saved)
   SELECT id, user_id, resource_id, list_id, notes, saved
-    FROM vufind.user_resource; 
+    FROM `vufind`.user_resource; 
 
+INSERT INTO resource (id, record_id, title, author, source)
+  VALUES (0, 0, '', '', '');
+
+SET foreign_key_checks = 0; -- comments table may have resource_id references that don't exist
 INSERT INTO comments (id, user_id, resource_id, comment, created, finna_visible, finna_rating, finna_type, finna_updated)
   SELECT id, user_id, resource_id, comment, created, visible, rating, type, updated 
-    FROM vufind.comments
-    WHERE EXISTS (SELECT * FROM vufind.resource WHERE resource.id = comments.resource_id);
-  
+    FROM `vufind`.comments;
+SET foreign_key_checks = 1;
+    
 INSERT INTO finna_comments_inappropriate (id, user_id, comment_id, created, reason)
   SELECT id, user_id, comment_id, created, reason 
-    FROM vufind.comments_inappropriate;
+    FROM `vufind`.comments_inappropriate;
 
 INSERT INTO finna_comments_record (id, record_id, comment_id)
   SELECT id, record_id, comment_id 
-    FROM vufind.comments_record
-    WHERE EXISTS (SELECT * FROM vufind.comments WHERE EXISTS (SELECT * FROM vufind.resource WHERE resource.id = comments.resource_id));
+    FROM `vufind`.comments_record;
   
 INSERT INTO finna_transaction (id, transaction_id, user_id, driver, amount, currency, transaction_fee, created, paid, registered, complete, status, cat_username, reported)
   SELECT id, transaction_id, user_id, driver, amount, currency, transaction_fee, created, paid, registered, complete, status, cat_username, reported 
-    FROM vufind.transaction;  
+    FROM `vufind`.transaction;  
   
 INSERT INTO finna_fee (id, user_id, transaction_id, title, type, amount, currency) 
-  SELECT id, user_id, (SELECT transaction_id FROM vufind.transaction_fees WHERE transaction_fees.fee_id=fee.id), title, type, amount, currency 
-    FROM vufind.fee
-    WHERE EXISTS (SELECT transaction_id FROM vufind.transaction_fees WHERE transaction_fees.fee_id=fee.id);
+  SELECT id, user_id, (SELECT transaction_id FROM `vufind`.transaction_fees WHERE transaction_fees.fee_id=fee.id), title, type, amount, currency 
+    FROM `vufind`.fee
+    WHERE EXISTS (SELECT transaction_id FROM `vufind`.transaction_fees WHERE transaction_fees.fee_id=fee.id);
 
 INSERT INTO oai_resumption (id, params, expires)
   SELECT id, params, expires 
-    FROM vufind.oai_resumption;
+    FROM `vufind`.oai_resumption;
 
 INSERT INTO record (record_id, source, version, data, updated)
   SELECT record_id, CASE WHEN source='VuFind' THEN 'Solr' WHEN source='PCI' THEN 'Primo' ELSE source END, '2.5.2', data, NOW() 
-    FROM vufind.resource;
+  FROM `vufind`.resource
+  GROUP BY record_id;
