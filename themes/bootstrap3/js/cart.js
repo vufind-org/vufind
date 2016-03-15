19ee2561c1af1ffd2d7b02d389867380884d5208
Chris Hallberg <crhallberg@gmail.com>
2016-03-15 15:52:52 -0400

    Convert Cart to a namespaced object.

--- themes/bootstrap3/js/cart.js
+++ themes/bootstrap3/js/cart.js
@@ -1,165 +1,199 @@
 /*global bulkActionSubmit, cartCookieDomain, Cookies, Lightbox, refreshPageForLogin, VuFind */
 
-var _CART_COOKIE = 'vufind_cart';
-var _CART_COOKIE_SOURCES = 'vufind_cart_src';
-var _CART_COOKIE_DELIM = "\t";
+VuFind.cart = (function() {
+  var _COOKIE = 'vufind_cart';
+  var _COOKIE_SOURCES = 'vufind_cart_src';
+  var _COOKIE_DELIM = "\t";
 
-var currentId,currentSource;
-var lastCartSubmit = false;
+  var currentId, currentSource;
 
-function getCartItems() {
-  var items = Cookies.getItem(_CART_COOKIE);
-  if(items) {
-    return items.split(_CART_COOKIE_DELIM);
-  }
-  return [];
-}
-function getCartSources() {
-  var items = Cookies.getItem(_CART_COOKIE_SOURCES);
-  if(items) {
-    return items.split(_CART_COOKIE_DELIM);
-  }
-  return [];
-}
-function getFullCartItems() {
-  var items = getCartItems();
-  var sources = getCartSources();
-  var full = [];
-  if(items.length == 0) {
+  function getItems() {
+    var items = Cookies.getItem(_COOKIE);
+    if(items) {
+      return items.split(_COOKIE_DELIM);
+    }
     return [];
   }
-  for(var i=items.length;i--;) {
-    full[full.length] = sources[items[i].charCodeAt(0)-65]+'|'+items[i].substr(1);
+  function getSources() {
+    var items = Cookies.getItem(_COOKIE_SOURCES);
+    if(items) {
+      return items.split(_COOKIE_DELIM);
+    }
+    return [];
   }
-  return full;
-}
+  function getFullItems() {
+    var items = getItems();
+    var sources = getSources();
+    var full = [];
+    if(items.length == 0) {
+      return [];
+    }
+    for(var i=items.length;i--;) {
+      full[full.length] = sources[items[i].charCodeAt(0)-65]+'|'+items[i].substr(1);
+    }
+    return full;
+  }
 
-function updateCartCount() {
-  var items = getCartItems();
-  $('#cartItems strong').html(items.length);
-}
+  function updateCount() {
+    var items = getItems();
+    $('#cartItems strong').html(items.length);
+  }
 
-function addItemToCart(id,source) {
-  if(!source) {
-    source = VuFind.defaultSearchBackend;
-  }
-  var cartItems = getCartItems();
-  var cartSources = getCartSources();
-  var sIndex = cartSources.indexOf(source);
-  if(sIndex < 0) {
-    // Add source to source cookie
-    cartItems[cartItems.length] = String.fromCharCode(65+cartSources.length) + id;
-    cartSources[cartSources.length] = source;
-    Cookies.setItem(_CART_COOKIE_SOURCES, cartSources.join(_CART_COOKIE_DELIM), false, '/', cartCookieDomain);
-  } else {
-    cartItems[cartItems.length] = String.fromCharCode(65+sIndex) + id;
-  }
-  Cookies.setItem(_CART_COOKIE, $.unique(cartItems).join(_CART_COOKIE_DELIM), false, '/', cartCookieDomain);
-  updateCartCount();
-  return true;
-}
-function uniqueArray(op) {
-  var ret = [];
-  for(var i=0;i<op.length;i++) {
-    if(ret.indexOf(op[i]) < 0) {
-      ret.push(op[i]);
+  function addItemTo(id,source) {
+    if(!source) {
+      source = VuFind.defaultSearchBackend;
     }
-  }
-  return ret;
-}
-function removeItemFromCart(id,source) {
-  var cartItems = getCartItems();
-  var cartSources = getCartSources();
-  // Find
-  var cartIndex = cartItems.indexOf(String.fromCharCode(65+cartSources.indexOf(source))+id);
-  if(cartIndex > -1) {
-    var sourceIndex = cartItems[cartIndex].charCodeAt(0)-65;
-    var cartItem = cartItems[cartIndex];
-    var saveSource = false;
-    for(var i=cartItems.length;i--;) {
-      if(i==cartIndex) {
-        continue;
-      }
-      // If this source is shared by another, keep it
-      if(cartItems[i].charCodeAt(0)-65 == sourceIndex) {
-        saveSource = true;
-        break;
-      }
-    }
-    cartItems.splice(cartIndex,1);
-    // Remove unused sources
-    if(!saveSource) {
-      var oldSources = cartSources.slice(0);
-      cartSources.splice(sourceIndex,1);
-      // Adjust source index characters
-      for(var j=cartItems.length;j--;) {
-        var si = cartItems[j].charCodeAt(0)-65;
-        var ni = cartSources.indexOf(oldSources[si]);
-        cartItems[j] = String.fromCharCode(65+ni)+cartItems[j].substring(1);
-      }
-    }
-    if(cartItems.length > 0) {
-      Cookies.setItem(_CART_COOKIE, uniqueArray(cartItems).join(_CART_COOKIE_DELIM), false, '/', cartCookieDomain);
-      Cookies.setItem(_CART_COOKIE_SOURCES, uniqueArray(cartSources).join(_CART_COOKIE_DELIM), false, '/', cartCookieDomain);
+    var cartItems = getItems();
+    var cartSources = getSources();
+    var sIndex = cartSources.indexOf(source);
+    if(sIndex < 0) {
+      // Add source to source cookie
+      cartItems[cartItems.length] = String.fromCharCode(65+cartSources.length) + id;
+      cartSources[cartSources.length] = source;
+      Cookies.setItem(_COOKIE_SOURCES, cartSources.join(_COOKIE_DELIM), false, '/', cartCookieDomain);
     } else {
-      Cookies.removeItem(_CART_COOKIE, '/', cartCookieDomain);
-      Cookies.removeItem(_CART_COOKIE_SOURCES, '/', cartCookieDomain);
+      cartItems[cartItems.length] = String.fromCharCode(65+sIndex) + id;
     }
-    updateCartCount();
+    Cookies.setItem(_COOKIE, $.unique(cartItems).join(_COOKIE_DELIM), false, '/', cartCookieDomain);
+    updateCount();
     return true;
   }
-  return false;
-}
-
-var cartNotificationTimeout = false;
-function registerUpdateCart($form) {
-  if($form) {
-    $("#updateCart, #bottom_updateCart").unbind('click').click(function(){
-      var elId = this.id;
-      var selectedBoxes = $("input[name='ids[]']:checked", $form);
-      var selected = [];
-      $(selectedBoxes).each(function(i) {
-        selected[i] = this.value;
-      });
-      if (selected.length > 0) {
-        var inCart = 0;
-        var msg = "";
-        var orig = getFullCartItems();
-        $(selected).each(function(i) {
-          for (var x in orig) {
-            if (this == orig[x]) {
-              inCart++;
-              return;
-            }
-          }
-          var data = this.split('|');
-          addItemToCart(data[1], data[0]);
-        });
-        var updated = getFullCartItems();
-        var added = updated.length - orig.length;
-        msg += added + " " + VuFind.translate('itemsAddBag');
-        if (inCart > 0 && orig.length > 0) {
-          msg += "<br/>" + inCart + " " + VuFind.translate('itemsInBag');
-        }
-        if (updated.length >= VuFind.translate('bookbagMax')) {
-          msg += "<br/>" + VuFind.translate('bookbagFull');
-        }
-        $('#'+elId).data('bs.popover').options.content = msg;
-        $('#cartItems strong').html(updated.length);
-      } else {
-        $('#'+elId).data('bs.popover').options.content = VuFind.translate('bulk_noitems_advice');
+  function uniqueArray(op) {
+    var ret = [];
+    for(var i=0;i<op.length;i++) {
+      if(ret.indexOf(op[i]) < 0) {
+        ret.push(op[i]);
       }
-      $('#'+elId).popover('show');
-      if (cartNotificationTimeout !== false) {
-        clearTimeout(cartNotificationTimeout);
-      }
-      cartNotificationTimeout = setTimeout(function() {
-        $('#'+elId).popover('hide');
-      }, 5000);
-      return false;
-    });
+    }
+    return ret;
   }
-}
+  function removeItemFrom(id,source) {
+    var cartItems = getItems();
+    var cartSources = getSources();
+    // Find
+    var cartIndex = cartItems.indexOf(String.fromCharCode(65+cartSources.indexOf(source))+id);
+    if(cartIndex > -1) {
+      var sourceIndex = cartItems[cartIndex].charCodeAt(0)-65;
+      var cartItem = cartItems[cartIndex];
+      var saveSource = false;
+      for(var i=cartItems.length;i--;) {
+        if(i==cartIndex) {
+          continue;
+        }
+        // If this source is shared by another, keep it
+        if(cartItems[i].charCodeAt(0)-65 == sourceIndex) {
+          saveSource = true;
+          break;
+        }
+      }
+      cartItems.splice(cartIndex,1);
+      // Remove unused sources
+      if(!saveSource) {
+        var oldSources = cartSources.slice(0);
+        cartSources.splice(sourceIndex,1);
+        // Adjust source index characters
+        for(var j=cartItems.length;j--;) {
+          var si = cartItems[j].charCodeAt(0)-65;
+          var ni = cartSources.indexOf(oldSources[si]);
+          cartItems[j] = String.fromCharCode(65+ni)+cartItems[j].substring(1);
+        }
+      }
+      if(cartItems.length > 0) {
+        Cookies.setItem(_COOKIE, uniqueArray(cartItems).join(_COOKIE_DELIM), false, '/', cartCookieDomain);
+        Cookies.setItem(_COOKIE_SOURCES, uniqueArray(cartSources).join(_COOKIE_DELIM), false, '/', cartCookieDomain);
+      } else {
+        Cookies.removeItem(_COOKIE, '/', cartCookieDomain);
+        Cookies.removeItem(_COOKIE_SOURCES, '/', cartCookieDomain);
+      }
+      updateCount();
+      return true;
+    }
+    return false;
+  }
+
+  var cartNotificationTimeout = false;
+  function registerUpdate($form) {
+    if($form) {
+      $("#updateCart, #bottom_updateCart").unbind('click').click(function(){
+        var elId = this.id;
+        var selectedBoxes = $("input[name='ids[]']:checked", $form);
+        var selected = [];
+        $(selectedBoxes).each(function(i) {
+          selected[i] = this.value;
+        });
+        if (selected.length > 0) {
+          var inCart = 0;
+          var msg = "";
+          var orig = getFullItems();
+          $(selected).each(function(i) {
+            for (var x in orig) {
+              if (this == orig[x]) {
+                inCart++;
+                return;
+              }
+            }
+            var data = this.split('|');
+            addItemTo(data[1], data[0]);
+          });
+          var updated = getFullItems();
+          var added = updated.length - orig.length;
+          msg += added + " " + VuFind.translate('itemsAddBag');
+          if (inCart > 0 && orig.length > 0) {
+            msg += "<br/>" + inCart + " " + VuFind.translate('itemsInBag');
+          }
+          if (updated.length >= VuFind.translate('bookbagMax')) {
+            msg += "<br/>" + VuFind.translate('bookbagFull');
+          }
+          $('#'+elId).data('bs.popover').options.content = msg;
+          $('#cartItems strong').html(updated.length);
+        } else {
+          $('#'+elId).data('bs.popover').options.content = VuFind.translate('bulk_noitems_advice');
+        }
+        $('#'+elId).popover('show');
+        if (cartNotificationTimeout !== false) {
+          clearTimeout(cartNotificationTimeout);
+        }
+        cartNotificationTimeout = setTimeout(function() {
+          $('#'+elId).popover('hide');
+        }, 5000);
+        return false;
+      });
+    }
+  }
+
+  // Reveal
+  return {
+    // Methods
+    updateCount: updateCount,
+    // Setup
+    ready: function() {
+      // Record buttons
+      var $cartId = $('.cartId');
+      if($cartId.length > 0) {
+        $cartId.each(function() {
+          var cartId = this.value.split('|');
+          currentId = cartId[1];
+          currentSource = cartId[0];
+          var $parent = $(this).parent();
+          $parent.find('.cart-add.correct,.cart-remove.correct').removeClass('correct hidden');
+          $parent.find('.cart-add').click(function() {
+            addItemTo(currentId,currentSource);
+            $parent.find('.cart-add,.cart-remove').toggleClass('hidden');
+          });
+          $parent.find('.cart-remove').click(function() {
+            removeItemFrom(currentId,currentSource);
+            $parent.find('.cart-add,.cart-remove').toggleClass('hidden');
+          });
+        });
+      } else {
+        // Search results
+        var $form = $('form[name="bulkActionForm"]');
+        registerUpdate($form);
+      }
+      $("#updateCart, #bottom_updateCart").popover({content:'', html:true, trigger:'manual'});
+    }
+  };
+})();
 
 // Building an array and checking indexes prevents a race situation
 // We want to prioritize empty over printing
@@ -176,31 +210,6 @@ function cartFormHandler(event, data) {
   }
 }
 
-document.addEventListener('VuFind.lightbox.closed', updateCartCount, false);
+document.addEventListener('VuFind.lightbox.closed', VuFind.cart.updateCount, false);
 
-$(document).ready(function() {
-  // Record buttons
-  var $cartId = $('.cartId');
-  if($cartId.length > 0) {
-    $cartId.each(function() {
-      var cartId = this.value.split('|');
-      currentId = cartId[1];
-      currentSource = cartId[0];
-      var $parent = $(this).parent();
-      $parent.find('.cart-add.correct,.cart-remove.correct').removeClass('correct hidden');
-      $parent.find('.cart-add').click(function() {
-        addItemToCart(currentId,currentSource);
-        $parent.find('.cart-add,.cart-remove').toggleClass('hidden');
-      });
-      $parent.find('.cart-remove').click(function() {
-        removeItemFromCart(currentId,currentSource);
-        $parent.find('.cart-add,.cart-remove').toggleClass('hidden');
-      });
-    });
-  } else {
-    // Search results
-    var $form = $('form[name="bulkActionForm"]');
-    registerUpdateCart($form);
-  }
-  $("#updateCart, #bottom_updateCart").popover({content:'', html:true, trigger:'manual'});
-});
+$(document).ready(VuFind.cart.ready);
