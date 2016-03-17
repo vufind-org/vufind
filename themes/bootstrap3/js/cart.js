13d5a99ac2604b7e42601890965e9e8807cf5509
Demian Katz <demian.katz@villanova.edu>
2016-03-17 13:58:14 -0400

    Remove obsolete global declarations.

--- themes/bootstrap3/js/cart.js
+++ themes/bootstrap3/js/cart.js
@@ -1,4 +1,4 @@
-/*global bulkActionSubmit, cartCookieDomain, Cookies, Lightbox, refreshPageForLogin, VuFind */
+/*global cartCookieDomain, Cookies, VuFind */
 
 VuFind.cart = (function() {
   var _COOKIE = 'vufind_cart';
