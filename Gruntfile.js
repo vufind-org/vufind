4394063049d17fd65c71513e4dd4b4b75b402860
Chris Hallberg <crhallberg@gmail.com>
2016-03-18 11:07:52 -0400

    Automated SASS detection.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,16 +15,22 @@ module.exports = function(grunt) {
         }
       }
     },
-    sass: {
-      compile: {
+    css: {
+      sass: {
         options: {
-          loadPath: ["themes/bootprint3/sass", "themes/bootstrap3/sass"],
-          sourcemap: 'none',
-          style: 'compressed'
+          themeFolder: 'themes'
         },
-        files: {
-          'themes/bootstrap3/css/compiled.css': 'themes/bootstrap3/sass/bootstrap.scss',
-          'themes/bootprint3/css/compiled.css': 'themes/bootprint3/sass/bootprint.scss'
+        dist: {
+          options: {
+            style: 'compressed',
+            sourcemap: 'none'
+          }
+        },
+        dev: {
+          options: {
+            style: 'expanded',
+            sourcemap: 'none'
+          }
         }
       }
     },
@@ -39,7 +45,7 @@ module.exports = function(grunt) {
             'themes/bootstrap3/js/vendor/jquery.min.js',
             'themes/bootstrap3/js/vendor/bootstrap.min.js',
             'themes/bootstrap3/js/vendor/*.js',
-            '!themes/bootstrap3/js/vendor/bootlint.min.js',     // un-comment to lint bootstrap syntax
+            '!themes/bootstrap3/js/vendor/bootlint.min.js',     // comment to lint bootstrap syntax
             '!themes/bootstrap3/js/vendor/bootstrap-slider.js', // not "use strict" compatible
             'themes/bootstrap3/js/autocomplete.js'
           ]
@@ -49,8 +55,61 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['less', 'uglify']);
   grunt.registerTask('js', ['uglify']);
+
+  grunt.registerMultiTask('css', function (arg1, arg2) {
+    var fs = require('fs')
+      , path = require('path')
+      , options = (arguments.length > 0 && this.data[arg1] && this.data[arg1].options)
+        ? this.data[arg1].options
+        : this.data.dist.options
+      , theme = (arguments.length > 1) ? arg2 : null
+      , themeFolder = this.data.options.themeFolder || 'themes'
+      , themeList = fs.readdirSync(path.resolve(themeFolder))
+      , sassConfig = {}
+      ;
+
+    var inheritance = {};
+    for (var i in themeList) {
+      if (theme && themeList[i] !== theme) {
+        continue;
+      }
+      var config = fs.readFileSync(path.join(themeFolder, themeList[i], 'theme.config.php'), 'UTF-8');
+      inheritance[themeList[i]] = config.toLowerCase().replace(/[\s']/g, '').match(/extends=>(\w+)/)[1];
+    }
+
+    for (var i in themeList) {
+      var sassDir = path.join(themeFolder, themeList[i], 'sass');
+      var cssDir = path.join(themeFolder, themeList[i], 'css');
+      try {
+        fs.statSync(sassDir);
+        // Build load path
+        var loadPath = [];
+        var curr = themeList[i];
+        while (inheritance[curr] != 'root') {
+          loadPath.unshift(path.join(themeFolder, inheritance[curr], 'sass'));
+          curr = inheritance[curr];
+        }
+        if (loadPath.length > 0) {
+          options.loadPath = loadPath;
+        }
+        // Compile
+        var files = {};
+        files[path.join(cssDir, 'compiled.css')] = path.join(sassDir, themeList[i].replace(/\d/g, '')+'.scss');
+        sassConfig[themeList[i]] = {
+          options: options,
+          files: files
+        };
+      } catch (err) {
+        // silently suppress thrown errors when no sass sources exist in a theme
+      }
+    }
+
+    grunt.config.set('sass', sassConfig);
+    grunt.task.run('sass');
+  });
 };
