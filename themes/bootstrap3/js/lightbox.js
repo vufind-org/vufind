7faafe0c93a9ddc2e2ab8855fcf9ca8075de7750
Ere Maijala <ere.maijala@helsinki.fi>
2016-04-04 08:56:04 +0300

    Revert "Added back support for specifying lightbox title"
    This reverts commit 5e27543095633311678aa3f7c73d45d2289e4a68.

--- themes/bootstrap3/js/lightbox.js
+++ themes/bootstrap3/js/lightbox.js
@@ -3,7 +3,6 @@ VuFind.lightbox = (function() {
   // State
   var _originalUrl = false;
   var _currentUrl = false;
-  var _lightboxTitle = '';
   var refreshOnClose = false;
   // Elements
   var _modal, _modalBody, _clickedButton = null;
@@ -13,15 +12,6 @@ VuFind.lightbox = (function() {
   };
   var _html = function(html) {
     _modalBody.html(html);
-    // Set or update title if we have one
-    if (_lightboxTitle != '') {
-      var h2 = _modalBody.find('h2:first-child');
-      if (h2.length == 0) {
-        h2 = $('<h2/>').prependTo(_modalBody);
-      }
-      h2.text(_lightboxTitle);
-      _lightboxTitle = '';
-    }
     _modal.modal('handleUpdate');
   };
   var _emit = function(msg, details) {
@@ -189,7 +179,6 @@ VuFind.lightbox = (function() {
    * data-lightbox-href = go to this url instead
    * data-lightbox-ignore = do not open this link in lightbox
    * data-lightbox-post = post data
-   * data-lightbox-title = Lightbox title (overrides any title the page provides)
    */
   var _constrainLink = function(event) {
     if (typeof $(this).data('lightboxIgnore') != 'undefined') {
@@ -202,7 +191,6 @@ VuFind.lightbox = (function() {
         obj.type = 'POST';
         obj.data = $(this).data('lightboxPost');
       }
-      _lightboxTitle = $(this).data('lightboxTitle');
       ajax(obj);
       _currentUrl = this.href;
       VuFind.modal('show');
@@ -217,7 +205,6 @@ VuFind.lightbox = (function() {
    *
    * data-lightbox-onsubmit = on submit, run named function
    * data-lightbox-onclose  = on close, run named function
-   * data-lightbox-title = Lightbox title (overrides any title the page provides)
    *
    * Submit button data options:
    *
@@ -262,8 +249,6 @@ VuFind.lightbox = (function() {
     if (submit.closest(_modal).length > 0) {
       submit.attr('disabled', 'disabled');
     }
-    // Store custom title
-    _lightboxTitle = submit.data('lightboxTitle') || $(form).data('lightboxTitle') || '';
     // Get Lightbox content
     ajax({
       url: form.action || _currentUrl,
@@ -308,7 +293,6 @@ VuFind.lightbox = (function() {
       _html(VuFind.translate('loading') + '...');
       _originalUrl = false;
       _currentUrl = false;
-      _lightboxTitle = '';
     },
 
     // Ready
