595cacf11732093ce1b2e08182aabad175652f0c
Chris Hallberg <crhallberg@gmail.com>
2016-05-06 16:26:50 -0400

    Whitespace replacement

--- install.php
+++ install.php
@@ -43,17 +43,17 @@ $basePath = '/vufind';
 try {
     $opts = new Getopt(
         array(
-        'use-defaults' =>
+        'use-defaults' => 
            'Use VuFind Defaults to Configure (ignores any other arguments passed)',
-        'overridedir=s' =>
+        'overridedir=s' => 
            "Where would you like to store your local settings? [{$baseDir}/local]",
-        'module-name=s' =>
+        'module-name=s' => 
            'What module name would you like to use? Use disabled, to not use',
-        'basepath=s' =>
+        'basepath=s' => 
            'What base path should be used in VuFind\'s URL? [/vufind]',
-        'multisite-w' =>
+        'multisite-w' => 
            'Specify we are going to setup a multisite. Options: directory and host',
-        'hostname=s' =>
+        'hostname=s' => 
             'Specify the hostname for the VuFind Site, When multisite=host',
         'non-interactive' =>
             'Use settings if provided via arguments, otherwise use defaults',
