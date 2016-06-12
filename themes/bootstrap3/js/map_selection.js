bd6185581a9f4f6c8912e4f58eeb3f7914f3f333
Leila Gonzales <lmg@agiweb.org>
2016-06-11 17:19:34 -0700

    Geographic search and display js files

---
+++ themes/bootstrap3/js/map_selection.js
@@ -0,0 +1,79 @@
+//Coordinate order:  Storage and Query: WENS ; Display: WSEN
+
+function loadMapSelection(geoField, boundingBox, baseURL, searchParams, showSelection) {
+    var init = true;
+    var srcProj = 'EPSG:4326';
+    var dstProj = 'EPSG:900913';
+    var osm = new ol.layer.Tile({source: new ol.source.OSM()});
+    var vectorSource = new ol.source.Vector();
+    var vectorLayer = new ol.layer.Vector({ source: vectorSource });
+    var draw, map;
+    var geometryFunction = function(coordinates, geometry) {
+       if (!geometry) {
+          geometry = new ol.geom.Polygon(null);
+       }
+       var start = coordinates[0];
+       var end = coordinates[1];
+       geometry.setCoordinates([
+         [start, [start[0], end[1]], end, [end[0], start[1]], start]
+       ]);
+       return geometry;
+     };
+
+    $('#geo_search').show();
+    var init = function(){
+      map = new ol.Map({
+          interactions: ol.interaction.defaults({
+            shiftDragZoom: false
+          }),
+          target: 'geo_search_map',
+          projection: dstProj,
+          layers: [osm, vectorLayer],
+          view: new ol.View({
+          center: [0, 0],
+          zoom: 1
+          })
+       });
+
+       if (showSelection == true) {
+          vectorSource.clear();
+          var newBbox = new ol.geom.Polygon([[
+             ol.proj.transform([boundingBox[0],boundingBox[3]], srcProj, dstProj),
+             ol.proj.transform([boundingBox[0],boundingBox[1]], srcProj, dstProj),
+             ol.proj.transform([boundingBox[2],boundingBox[1]], srcProj, dstProj),
+             ol.proj.transform([boundingBox[2],boundingBox[3]], srcProj, dstProj)
+          ]]); 
+          var featureBbox = new ol.Feature({ 
+             name: "bbox",
+             geometry: newBbox
+           });
+          vectorSource.addFeature(featureBbox);
+          map.getView().fit(vectorSource.getExtent(), map.getSize());
+        }
+    } 
+    function addInteraction() {
+      draw = new ol.interaction.Draw ({
+        source: vectorSource,
+        type: 'LineString',
+        maxPoints: 2,
+        geometryFunction: geometryFunction
+      });
+      draw.on('drawend', function(evt) {
+        var geometry = evt.feature.getGeometry();
+        var coordinates = geometry.getCoordinates();
+        var westnorth = ol.proj.transform(coordinates[0][0], dstProj, srcProj);
+        var eastsouth = ol.proj.transform(coordinates[0][2], dstProj, srcProj);
+        var rawFilter = geoField + ':Intersects(ENVELOPE(' + westnorth[0] + ', ' + eastsouth[0] + ', ' + westnorth[1] + ', ' + eastsouth[1] + '))';
+        location.href = baseURL + searchParams + "&filter[]=" + rawFilter;
+      }, this);
+      map.addInteraction(draw);
+    }   
+   init();
+   $('button').on('click', function () {
+     vectorSource.clear();
+     map.removeInteraction(draw);
+     addInteraction();
+   });
+
+  init = false;
+}
---
+++ themes/bootstrap3/js/map_tab.js
@@ -0,0 +1,156 @@
+//Coordinate order:  Storage and Query: WENS ; Display: WSEN
+function loadMapTab(mapData) {
+    var init=true;
+    var srcProj = 'EPSG:4326';
+    var dstProj = 'EPSG:900913';
+    var osm = new ol.layer.Tile({source: new ol.source.OSM()});
+    var vectorSource = new ol.source.Vector();
+    var map;
+    var iconStyle = new ol.style.Style({
+          image: new ol.style.Circle({
+                radius: 5,
+                fill: new ol.style.Fill({
+                  color: 'red'
+                })
+          })
+    });
+    var polyStyle = new ol.style.Style({
+       fill: new ol.style.Fill({
+         color: [200,0,0,.1]
+       }),
+       stroke: new ol.style.Stroke({
+         color: 'red',
+         width: 2 
+       })
+    });
+
+    $('#map-canvas').show();
+    var init = function() {
+    var featureCount = mapData.length;
+    for (i=0; i<featureCount; i++){
+      //Construct the coordinate labels
+        if ((mapData[i][5] == 'cl') || (mapData[i][5] == 'c')) { 
+          var label_coord1 = mapData[i][6].substring(0,16); 
+          var label_coord2 = mapData[i][6].substring(16); 
+          if (label_coord2) {
+            var label_coord = label_coord1+'<br/>'+label_coord2;
+          } else {
+            var label_coord = label_coord1;
+          }
+         }
+       // Construct the label names
+        if (mapData[i][5] == 'l') {
+          var label_name = mapData[i][6];
+        }
+        if (mapData[i][5] == 'cl') {
+          var label_name = mapData[i][7];
+        }
+       // Construct the entire label string
+        if (mapData[i][5] == 'cl') {
+          var label = label_coord + '<br/>' + label_name;
+          var labelon = true;
+        }
+        if (mapData[i][5] == 'c') {
+          var label = label_coord;
+          var labelon = true;
+        }
+        if (mapData[i][5] == 'l') {
+          var label = label_name;
+          var labelon = true;
+        }
+        if (mapData[i][5] == 'n') {
+          var label = '';
+          var labelon = false;
+        }
+
+       // Determine if entry is point or polygon - Does W=E & N=S? //
+       if (mapData[i][4]==2) {
+       //It's a point feature //
+         var lonlat = ol.proj.transform([mapData[i][0], mapData[i][1]], srcProj, dstProj);
+         var iconFeature = new ol.Feature({
+           geometry: new ol.geom.Point(lonlat),
+           name: label
+         });
+         iconFeature.setStyle(iconStyle);
+         vectorSource.addFeature(iconFeature);
+       } else if (mapData[i][4]==4) { // It's a polygon feature //
+          var point1 = ol.proj.transform([mapData[i][0], mapData[i][3]], srcProj, dstProj);
+          var point2 = ol.proj.transform([mapData[i][0], mapData[i][1]], srcProj, dstProj);
+          var point3 = ol.proj.transform([mapData[i][2], mapData[i][1]], srcProj, dstProj);
+          var point4 = ol.proj.transform([mapData[i][2], mapData[i][3]], srcProj, dstProj);
+          var polyFeature = new ol.Feature({
+            geometry: new ol.geom.Polygon([
+                 [point1, point2, point3, point4, point1]
+            ]),
+            name: label
+         });
+         polyFeature.setStyle(polyStyle);
+         vectorSource.addFeature(polyFeature);
+       }   
+    }
+    var vectorLayer = new ol.layer.Vector({ 
+        source: vectorSource,
+        renderBuffer: 500
+    });
+    map = new ol.Map({
+        renderer: 'canvas',
+        projection: dstProj,
+        layers: [osm, vectorLayer],
+        target: 'map-canvas',
+        view: new ol.View({
+          center: [0,0],
+          zoom: 1
+        })
+    });
+   var extent = vectorLayer.getSource().getExtent();
+   map.getView().fit(extent,map.getSize());
+
+  // Turn on popup tool tips if labels or coordinates are enabled.
+  if (labelon == true) {
+   var element = document.getElementById('popup');
+   var popup = new ol.Overlay({
+     element: element
+   });
+   map.addOverlay(popup);
+
+  // display popup on click
+  map.on('click', function(evt) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature, layer) {
+        return feature;
+    });
+    if (feature) {
+     var element = popup.getElement();
+     var coordinate = evt.coordinate;
+     $(element).popover('destroy');
+     popup.setPosition(coordinate);
+     $(element).popover({
+       'placement': 'top',
+       'animation': false,
+       'html': true,
+       'content': feature.get('name')
+     });
+     $(element).popover('show');
+    }
+  });
+
+  // change mouse cursor when over marker
+  map.on('pointermove', function(e) {
+    if (e.dragging) {
+      $(element).popover('destroy');
+      return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    var target = map.getTarget();
+    if (hit == true) {
+      document.getElementById(target).style.cursor = "pointer";
+    } else {
+      document.getElementById(target).style.cursor = "default";
+    }
+   });
+  }
+ }
+ init();
+ init=false;
+}
