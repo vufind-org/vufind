48faababc3beb3275de881a0c4db2f593b6166a8
Demian Katz <demian.katz@villanova.edu>
2016-06-14 09:36:57 -0400

    Moved bulk of OAI harvest logic outside of VuFind code.

--- module/VuFindConsole/src/VuFindConsole/Controller/HarvestController.php
+++ module/VuFindConsole/src/VuFindConsole/Controller/HarvestController.php
@@ -26,7 +26,7 @@
  * @link     https://vufind.org/wiki/development:plugins:controllers Wiki
  */
 namespace VuFindConsole\Controller;
-use VuFindHarvest\OaiHarvester, Zend\Console\Console;
+use VuFindHarvest\OaiHarvesterConsoleRunner, Zend\Console\Console;
 
 /**
  * This controller handles various command-line tools
@@ -73,59 +73,22 @@ class HarvestController extends AbstractBase
     {
         $this->checkLocalSetting();
 
-        // Parse switches:
-        $this->consoleOpts->addRules(
-            ['from-s' => 'Harvest start date', 'until-s' => 'Harvest end date']
+        // Get default options, add the default --ini setting if missing:
+        $opts = OaiHarvesterConsoleRunner::getDefaultOptions();
+        if (!$opts->getOption('ini')) {
+            $ini = \VuFind\Config\Locator::getConfigPath('oai.ini', 'harvest');
+            $opts->addArguments(['--ini=' . $ini]);
+        }
+
+        // Get the default VuFind HTTP client:
+        $client = $this->getServiceLocator()->get('VuFind\Http')->createClient();
+
+        // Run the job!
+        $runner = new OaiHarvesterConsoleRunner(
+            $opts, $client, $this->getHarvestRoot()
         );
-        $from = $this->consoleOpts->getOption('from');
-        $until = $this->consoleOpts->getOption('until');
-
-        // Read Config files
-        $configFile = \VuFind\Config\Locator::getConfigPath('oai.ini', 'harvest');
-        $oaiSettings = @parse_ini_file($configFile, true);
-        if (empty($oaiSettings)) {
-            Console::writeLine("Please add OAI-PMH settings to oai.ini.");
-            return $this->getFailureResponse();
-        }
-
-        // If first command line parameter is set, see if we can limit to just the
-        // specified OAI harvester:
-        $argv = $this->consoleOpts->getRemainingArgs();
-        if (isset($argv[0])) {
-            if (isset($oaiSettings[$argv[0]])) {
-                $oaiSettings = [$argv[0] => $oaiSettings[$argv[0]]];
-            } else {
-                Console::writeLine("Could not load settings for {$argv[0]}.");
-                return $this->getFailureResponse();
-            }
-        }
-
-        // Loop through all the settings and perform harvests:
-        $processed = 0;
-        foreach ($oaiSettings as $target => $settings) {
-            if (!empty($target) && !empty($settings)) {
-                Console::writeLine("Processing {$target}...");
-                try {
-                    $client = $this->getServiceLocator()->get('VuFind\Http')
-                        ->createClient();
-                    $harvest = new OaiHarvester(
-                        $target, $this->getHarvestRoot(), $settings, $client,
-                        $from, $until, false
-                    );
-                    $harvest->launch();
-                } catch (\Exception $e) {
-                    Console::writeLine($e->getMessage());
-                    return $this->getFailureResponse();
-                }
-                $processed++;
-            }
-        }
-
-        // All done.
-        Console::writeLine(
-            "Completed without errors -- {$processed} source(s) processed."
-        );
-        return $this->getSuccessResponse();
+        return $runner->run()
+            ? $this->getSuccessResponse() : $this->getFailureResponse();
     }
 
     /**
