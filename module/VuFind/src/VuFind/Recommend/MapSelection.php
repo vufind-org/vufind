6415f3ed784926527fd960b2744e88ec7979dcb2
Leila Gonzales <lmg@agiweb.org>
2016-06-30 08:50:31 -0700

    Update MapSelection.php

--- module/VuFind/src/VuFind/Recommend/MapSelection.php
+++ module/VuFind/src/VuFind/Recommend/MapSelection.php
@@ -153,8 +153,10 @@ class MapSelection implements \VuFind\Recommend\RecommendInterface
         foreach ($filters as $key => $value) {
             if ($key == $this->geoField) {
                 $match = [];
-                if (preg_match('/Intersects\(ENVELOPE\((.*), (.*), (.*), (.*)\)\)/',
-                    $value[0], $match)) {
+                if (
+                    preg_match('/Intersects\(ENVELOPE\((.*), (.*), (.*), (.*)\)\)/',
+                    $value[0], $match)
+                    ) {
                     // Need to reorder coords from WENS to WSEN
                     array_push(
                         $coords, (float)$match[1], 
@@ -184,8 +186,10 @@ class MapSelection implements \VuFind\Recommend\RecommendInterface
         foreach ($filters as $key => $value) {
             if ($key == $this->geoField) {
                 $match = [];
-                if (preg_match('/Intersects\(ENVELOPE\((.*), (.*), (.*), (.*)\)\)/',
-                        $value[0], $match)) {
+                if (
+                    preg_match('/Intersects\(ENVELOPE\((.*), (.*), (.*), (.*)\)\)/',
+                    $value[0], $match)
+                    ) {
                     // Need to reorder coords from WENS to WSEN
                     array_push(
                         $reorder_coords, (float)$match[1], 
