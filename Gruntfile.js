4e6d12316e575fc5e56520115e8706d8a74afc53
Chris Hallberg <crhallberg@gmail.com>
2016-07-05 12:17:38 -0400

    Adjust eslint for .js not .json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function(grunt) {
     },
     eslint: {
       options: {
-        configFile: '.eslintrc.json'
+        configFile: '.eslintrc.js'
       },
       bootstrap3: ['themes/bootstrap3/js/*.js']
     },
