5b094eef9a32c50b5140f6d9eaa1136fc1449103
Leila Gonzales <lmg@agiweb.org>
2016-07-19 16:14:29 -0700

    ESLint fixes

--- themes/bootstrap3/js/map_tab_google.js
+++ themes/bootstrap3/js/map_tab_google.js
@@ -6,7 +6,7 @@ var latlng;
 var myOptions;
 var map;
 var infowindow = new google.maps.InfoWindow({maxWidth: 480, minWidth: 480});
-  function initialize(mapData) {
+function initialize(mapData) {
     markersData = mapData;
     latlng = new google.maps.LatLng(0, 0);
     myOptions = {
@@ -14,27 +14,27 @@ var infowindow = new google.maps.InfoWindow({maxWidth: 480, minWidth: 480});
       center: latlng,
       mapTypeControl: true,
       mapTypeControlOptions: {
-          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-        },
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      },
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map_canvas"),
       myOptions);
     showMarkers();
   }
-  function showMarkers(){
+function showMarkers(){
     deleteOverlays();
     markers = [];
 
-    for (var i = 0; i<markersData.length; i++){
+    for (var i = 0; i < markersData.length; i++){
       var disTitle = markersData[i].title;
       var iconTitle = disTitle;
-      if (disTitle.length>25){
-          iconTitle = disTitle.substring(0,25) + "...";
+      if (disTitle.length > 25){
+        iconTitle = disTitle.substring(0, 25) + "...";
       }
-      var markerImg = "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=edge_bc|" + iconTitle +"|EEEAE3|";
+      var markerImg = "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=edge_bc|" + iconTitle + "|EEEAE3|";
       var labelXoffset = 1 + disTitle.length * 4;
-      var latLng = new google.maps.LatLng(markersData[i].lat , markersData[i].lon)
+      var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lon)
       var marker = new google.maps.Marker({
         position: latLng,
         map: map,
@@ -43,17 +43,18 @@ var infowindow = new google.maps.InfoWindow({maxWidth: 480, minWidth: 480});
       });
       markers.push(marker);
     }
-  }
-  function deleteOverlays() {
-      if (markers) {
+}
+function deleteOverlays() {
+    if (markers) {
+        var i;
         for (i in markers) {
           markers[i].setMap(null);
         }
         markers.length = 0;
       }
-  }
-  function refreshMap() {
+}
+function refreshMap() {
     showMarkers();
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
                                                                                  
