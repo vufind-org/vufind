a9e2894e08d2f3ccc4959fcc58204510388b2031
Leila Gonzales <lmg@agiweb.org>
2016-07-19 11:07:52 -0700

    Eslint fixes

--- themes/bootstrap3/js/map_selection.js
+++ themes/bootstrap3/js/map_selection.js
@@ -18,8 +18,9 @@ function loadMapSelection(geoField, boundingBox, baseURL, searchParams, showSele
     })
   });
   var vectorLayer = new ol.layer.Vector({ source: vectorSource, style: vectorStyle });
-  var draw, map, geometry;
+  var draw, map;
   function rectangleFunction(coordinates, geometry) {
+    var geometry;
     if (!geometry) {
       geometry = new ol.geom.Polygon(null);
     }
