dee9daa75feebaca9b9c8cb7aae4ae596644cce3
Josef Moravec <josef.moravec@gmail.com>
2016-08-04 23:03:29 +0200

    Initial implementation of API request method

--- module/VuFind/src/VuFind/ILS/Driver/KohaRESTful.php
+++ module/VuFind/src/VuFind/ILS/Driver/KohaRESTful.php
@@ -28,19 +28,157 @@
  * @link     https://vufind.org/wiki/development:plugins:ils_drivers Wiki
  */
 namespace VuFind\ILS\Driver;
+
 use PDO, PDOException;
-use VuFind\Exception\ILS as ILSException;
-use VuFindHttp\HttpServiceInterface; //todo: use Trait
-use Zend\Log\LoggerInterface; //todo: use Trait
-use VuFind\Exception\Date as DateException;
+use VuFind\Exception\ILS as ILSException,
+    VuFindHttp\HttpServiceAwareInterface as HttpServiceAwareInterface,
+    Zend\Log\LoggerAwareInterface as LoggerAwareInterface,
+    VuFind\Exception\Date as DateException;
 
 //todo: will extend \VuFind\ILS\Driver\AbstractBase, this is just for testing and developing purposes
 class KohaRESTful extends \VuFind\ILS\Driver\KohaILSDI implements
-    \VuFindHttp\HttpServiceAwareInterface, \Zend\Log\LoggerAwareInterface
+    HttpServiceAwareInterface, LoggerAwareInterface
 {
+    use \VuFindHttp\HttpServiceAwareTrait;
+    use \VuFind\Log\LoggerAwareTrait;
+
+    /**
+     * REST API base URL
+     *
+     * @var string
+     */
+    protected $apiUrl;
+
+    /**
+     * Location codes
+     *
+     * @var array
+     */
+    protected $locations;
+
+    /**
+     * Codes of locations available for pickup
+     *
+     * @var array
+     */
+    protected $pickupEnableBranchcodes;
+
+    /**
+     * Codes of locations always should be available
+     *   - For example reference material or material
+     *     not for loan
+     *
+     * @var array
+     */
+    protected $availableLocationsDefault;
+
+    /**
+     * Default location code
+     *
+     * @var string
+     */
+    protected $defaultLocation;
+
+    /**
+     * Date converter object
+     *
+     * @var \VuFind\Date\Converter
+     */
+    //protected $dateConverter;
 
 
+    /**
+     * Initialize the driver.
+     *
+     * Validate configuration and perform all resource-intensive tasks needed to
+     * make the driver active.
+     *
+     * @throws ILSException
+     * @return void
+     */
+    public function init()
+    {
+        if (empty($this->config)) {
+            throw new ILSException('Configuration needs to be set.');
+        }
 
+        // Storing the base URL of ILS
+        $this->apiUrl = isset($this->config['Catalog']['apiurl'])
+            ? $this->config['Catalog']['url'] : "";
+
+        // Default location defined in 'KohaRESTful.ini'
+        $this->defaultLocation
+            = isset($this->config['Holds']['defaultPickUpLocation'])
+            ? $this->config['Holds']['defaultPickUpLocation'] : null;
+
+        $this->pickupEnableBranchcodes
+            = isset($this->config['Holds']['pickupLocations'])
+            ? $this->config['Holds']['pickupLocations'] : [];
+
+        // Locations that should default to available, defined in 'KohaRESTful.ini'
+        $this->availableLocationsDefault
+            = isset($this->config['Other']['availableLocations'])
+            ? $this->config['Other']['availableLocations'] : [];
+
+        // Create a dateConverter
+        //$this->dateConverter = new \VuFind\Date\Converter;
+    }
+
+   /**
+     * Make Request
+     *
+     * Makes a request to the Koha ILSDI API
+     *
+     * @param string $api_query   Query string for request (starts with "/")
+     * @param string $http_method HTTP method (default = GET)
+     * @param array  $data        If method is PUT or POST, provide needed data i this paramater (default = null)
+     *
+     * @throws ILSException
+     * @return array
+     */
+    protected function makeRequest($api_query, $http_method = "GET", $data = null)
+    {
+        $http_headers = [
+        ];
+
+        $client = $this->httpService->createClient($this->apiurl . $api_query, $http_method);
+        if($data !== null) {
+            $client->setRawBody(http_build_query($data));
+        }
+
+        try {
+            $response = $client->send();
+        } catch (\Exception $e) {
+            throw new ILSEException($e->getMessage());
+        }
+
+        if (!$response->isSuccess()) {
+            $this->debug(
+                'HTTP status ' . $response->getStatusCode() .
+                ' received, accessing Koha RESTful API: ' . $api_query
+            );
+            return false;
+        }
+
+        return json_decode($response->getBody(), true);
+    }
+
+   /**
+    * https://vufind.org/wiki/development:plugins:ils_drivers#getpickuplocations
+    */
+    public function getPickupLocations($patron = false, $hodDetails = null)
+    {
+        $libraries = $this->makeRequest("/libraries");
+        $locations = [];
+        foreach($libraries => $library)
+        {
+            $locations[] = [
+                "locationID" => $library["branchcode"],
+                "locationDisplay" => $library["branchname"]
+            ];
+        }
+        return $locations;
+    }
 
 }
 
