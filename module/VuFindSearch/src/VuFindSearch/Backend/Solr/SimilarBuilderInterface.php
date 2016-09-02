4ddf4320d095414296c7e7ef5f61c0ead212e22d
Ere Maijala <ere.maijala@helsinki.fi>
2016-09-02 17:33:01 +0300

    Added support for Solr's MoreLikeThis query handler (#730)
    - When turned on, this adds MoreLikeThis support for sharded indexes in SolrCloud.
    - Note that the MoreLikeThis component remains the default setting, as it provides more nuanced results as of this writing.
    - Resolves VUFIND-652.

---
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/SimilarBuilderInterface.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * SOLR SimilarBuilder interface definition.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ * Copyright (C) The National Library of Finland 2016.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Andrew S. Nagy <vufind-tech@lists.sourceforge.net>
+ * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+namespace VuFindSearch\Backend\Solr;
+
+use VuFindSearch\ParamBag;
+
+/**
+ * SOLR SimilarBuilder interface definition.
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Andrew S. Nagy <vufind-tech@lists.sourceforge.net>
+ * @author   David Maus <maus@hab.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+interface SimilarBuilderInterface
+{
+    /**
+     * Build SOLR query based on VuFind query object.
+     *
+     * @param string $id Record id
+     *
+     * @return ParamBag
+     */
+    public function build($id);
+}
