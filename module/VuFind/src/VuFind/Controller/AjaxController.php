11fd09d8a258afba8698ad209d8d4c67bb64074c
André Lahmann <lahmann@ub.uni-leipzig.de>
2016-09-14 07:59:24 +0200

    * refactored code to allow context specific ils offline messages

--- module/VuFind/src/VuFind/Controller/AjaxController.php
+++ module/VuFind/src/VuFind/Controller/AjaxController.php
@@ -1514,9 +1514,16 @@ class AjaxController extends AbstractBase
     protected function getIlsStatusAjax()
     {
         $this->disableSessionWrites();  // avoid session write timing bug
+        $offlineModeMsg = $this->params()->fromPost(
+            'offlineModeMsg',
+            $this->params()->fromQuery('offlineModeMsg')
+        );
         if ($this->getILS()->getOfflineMode() == 'ils-offline') {
             return $this->output(
-                $this->getViewRenderer()->render('Helpers/ils-offline.phtml'),
+                $this->getViewRenderer()->render(
+                    'Helpers/ils-offline.phtml',
+                    $offlineModeMsg
+                ),
                 self::STATUS_OK
             );
         }
