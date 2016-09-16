2234839fecb63367534843db70c771f8a1492829
Demian Katz <demian.katz@villanova.edu>
2016-09-16 11:48:12 -0400

    Code simplification and div ID removal.

--- module/VuFind/src/VuFind/Service/ReCaptcha.php
+++ module/VuFind/src/VuFind/Service/ReCaptcha.php
@@ -51,54 +51,16 @@ class ReCaptcha extends \LosReCaptcha\Service\ReCaptcha
      */
     public function getHtml($name = null)
     {
-        if ($this->siteKey === null) {
-            throw new Exception('Missing public key');
-        }
+        // Get standard HTML
+        $html = parent::getHtml($name);
 
-        $host = self::API_SERVER;
-
-        if ((bool) $this->params['ssl'] === true) {
-            $host = self::API_SECURE_SERVER;
-        }
-
-        $htmlBreak = '<br>';
-        $htmlInputClosing = '>';
-
-        if ((bool) $this->params['xhtml'] === true) {
-            $htmlBreak = '<br />';
-            $htmlInputClosing = '/>';
-        }
-
-        $langOption = '';
-
-        if (isset($this->options['lang']) && !empty($this->options['lang'])) {
-            $langOption = "?hl={$this->options['lang']}";
-        }
-
-        error_log(print_r($this->options, true));
-        $return = '<div id="recaptcha_widget" class="g-recaptcha" data-sitekey="'
-            . $this->siteKey . '"';
+        // Override placeholder div with richer version:
+        $div = '<div class="g-recaptcha" data-sitekey="' . $this->siteKey . '"';
         foreach ($this->options as $key => $option) {
-            $return .= ' data-' . $key . '="' . $option . '"';
+            $div .= ' data-' . $key . '="' . $option . '"';
         }
-        // @codingStandardsIgnoreStart
-        $return .= <<<HTML
-></div>
-<noscript>
-    <div style="width: 302px; height: 352px;">
-        <div style="width: 302px; height: 352px; position: relative;">
-            <div style="width: 302px; height: 352px; position: absolute;">
-                <iframe src="{$host}/fallback?k={$this->siteKey}" frameborder="0" scrolling="no" style="width: 302px; height:352px; border-style: none;"></iframe>
-            </div>
-            <div style="width: 250px; height: 80px; position: absolute; border-style: none; bottom: 21px; left: 25px; margin: 0px; padding: 0px; right: 25px;">
-                <textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 80px; border: 1px solid #c1c1c1; margin: 0px; padding: 0px; resize: none;" value=""></textarea>
-            </div>
-        </div>
-    </div>
-</noscript>
-<script type="text/javascript" src="{$host}.js{$langOption}" async defer></script>
-HTML;
-        // @codingStandardsIgnoreEnd
-        return $return;
+        $div .= '>';
+        $regex = '/<div[^>]*id=[\'"]recaptcha_widget[\'"][^>]*>/';
+        return preg_replace($regex, $div, $html);
     }
 }
