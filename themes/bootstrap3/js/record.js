bf8a705cd63e99fa33f2bb53eb09622b4dbe05bb
Demian Katz <demian.katz@villanova.edu>
2016-09-21 15:54:41 -0400

    Update record.js

--- themes/bootstrap3/js/record.js
+++ themes/bootstrap3/js/record.js
@@ -1,4 +1,4 @@
-/*global deparam, grecaptcha, syn_get_widget, userIsLoggedIn, VuFind */
+/*global deparam, grecaptcha, recaptchaOnLoad, syn_get_widget, userIsLoggedIn, VuFind */
 /*exported ajaxTagUpdate, recordDocReady */
 
 /**
