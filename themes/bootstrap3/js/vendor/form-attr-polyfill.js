ceeb2a624edac373ce981b9ea282d0ab0da15d86
Chris Hallberg <crhallberg@gmail.com>
2016-09-22 12:22:00 -0400

    Polyfill fixed to run on document ready.

--- themes/bootstrap3/js/vendor/form-attr-polyfill.js
+++ themes/bootstrap3/js/vendor/form-attr-polyfill.js
@@ -14,63 +14,64 @@
     return;
   }
 
-  /**
-   * Append a field to a form
-   *
-   */
-  $.fn.appendField = function appendField(_data) {
-    var data = (!$.isArray(data) && data.name && data.value)
-      ? [_data] // wrap data
-      : _data;
-
-    // for form only
-    if (!this.is('form')) {
-      return;
-    }
-
-    var $form = this;
-
-    // attach new params
-    $.each(data, function appendFieldEach(i, item) {
-      $('<input/>')
-        .attr('type', 'hidden')
-        .attr('name', item.name)
-        .val(item.value).appendTo($form);
-    });
-
-    return $form;
-  };
-
-  /**
-   * Find all input fields with form attribute point to jQuery object
-   *
-   */
-  $('form[id]').submit(function locateFormAttr(/*e*/) {
-    // serialize data
-    var data = $('[form=' + this.id + ']').serializeArray();
-    // append data to form
-    $(this).appendField(data);
-  }).each(function locateFormAttrEach() {
-    var form = this,
-      $fields = $('[form=' + this.id + ']');
-
-    $fields.filter('button, input').filter('[type=reset],[type=submit]').click(function formAttrButtonSubmit() {
-      var type = this.type.toLowerCase();
-      if (type === 'reset') {
-        // reset form
-        form.reset();
-        // for elements outside form
-        $fields.each(function formAttrGatherFieldData() {
-          this.value = this.defaultValue;
-          this.checked = this.defaultChecked;
-        }).filter('select').each(function formAttrGatherSelectData() {
-          $(this).find('option').each(function formAttrGatherOptionData() {
-            this.selected = this.defaultSelected;
-          });
-        });
-      } else if (type.match(/^submit|image$/i)) {
-        $(form).appendField({name: this.name, value: this.value}).submit();
+  $(document).ready(function formAttrReady() {
+    /**
+     * Append a field to a form
+     *
+     */
+    $.fn.appendField = function appendField(_data) {
+      var data = (!$.isArray(_data) && _data.name && _data.value)
+        ? [_data] // wrap data
+        : _data;
+      // for form only
+      if (!this.is('form')) {
+        return;
       }
+
+      var $form = this;
+
+      // attach new params
+      $.each(data, function appendFieldEach(i, item) {
+        $('<input/>')
+          .attr('type', 'hidden')
+          .attr('name', item.name)
+          .val(item.value).appendTo($form);
+      });
+
+      return $form;
+    };
+
+    /**
+     * Find all input fields with form attribute point to jQuery object
+     *
+     */
+    $('form[id]').submit(function locateFormAttr(/*e*/) {
+      // serialize data
+      var data = $('[form=' + this.id + ']').serializeArray();
+      // append data to form
+      $(this).appendField(data);
+    }).each(function locateFormAttrEach() {
+      var form = this,
+        $fields = $('[form=' + this.id + ']');
+
+      $fields.filter('button, input').filter('[type=reset],[type=submit]').click(function formAttrButtonSubmit() {
+        var type = this.type.toLowerCase();
+        if (type === 'reset') {
+          // reset form
+          form.reset();
+          // for elements outside form
+          $fields.each(function formAttrGatherFieldData() {
+            this.value = this.defaultValue;
+            this.checked = this.defaultChecked;
+          }).filter('select').each(function formAttrGatherSelectData() {
+            $(this).find('option').each(function formAttrGatherOptionData() {
+              this.selected = this.defaultSelected;
+            });
+          });
+        } else if (type.match(/^submit|image$/i)) {
+          $(form).appendField({name: this.name, value: this.value}).submit();
+        }
+      });
     });
   });
 
