640e00375f648b0c3e34c31f97e0ff863b782a5f
Chris Hallberg <crhallberg@gmail.com>
2016-09-23 12:17:34 -0400

    Apply ids instead of classes. Simplify logic.

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -205,46 +205,43 @@ function setupOffcanvas() {
 
 function setupAutocomplete() {
   // Search autocomplete
-  $('.autocomplete').each(function autocompleteSetup(i, op) {
-    $(op).autocomplete({
-      maxResults: 10,
-      loadingString: VuFind.translate('loading') + '...',
-      handler: function vufindACHandler(input, cb) {
-        var query = input.val();
-        var searcher = extractClassParams(input);
-        var hiddenFilters = [];
-        $(input).closest('.searchForm').find('input[name="hiddenFilters[]"]').each(function hiddenFiltersEach() {
-          hiddenFilters.push($(this).val());
-        });
-        $.fn.autocomplete.ajax({
-          url: VuFind.path + '/AJAX/JSON',
-          data: {
-            q: query,
-            method: 'getACSuggestions',
-            searcher: searcher.searcher,
-            type: searcher.type ? searcher.type : $(input).closest('.searchForm').find('.searchForm_type').val(),
-            hiddenFilters: hiddenFilters
-          },
-          dataType: 'json',
-          success: function autocompleteJSON(json) {
-            if (json.data.length > 0) {
-              var datums = [];
-              for (var j = 0; j < json.data.length; j++) {
-                datums.push(json.data[j]);
-              }
-              cb(datums);
-            } else {
-              cb([]);
+  $('#searchForm_lookfor').autocomplete({
+    maxResults: 10,
+    loadingString: VuFind.translate('loading') + '...',
+    handler: function vufindACHandler(input, cb) {
+      var query = input.val();
+      var searcher = extractClassParams(input);
+      var hiddenFilters = [];
+      $('#searchForm').find('input[name="hiddenFilters[]"]').each(function hiddenFiltersEach() {
+        hiddenFilters.push($(this).val());
+      });
+      $.fn.autocomplete.ajax({
+        url: VuFind.path + '/AJAX/JSON',
+        data: {
+          q: query,
+          method: 'getACSuggestions',
+          searcher: searcher.searcher,
+          type: searcher.type ? searcher.type : $('#searchForm_type').val(),
+          hiddenFilters: hiddenFilters
+        },
+        dataType: 'json',
+        success: function autocompleteJSON(json) {
+          if (json.data.length > 0) {
+            var datums = [];
+            for (var j = 0; j < json.data.length; j++) {
+              datums.push(json.data[j]);
             }
+            cb(datums);
+          } else {
+            cb([]);
           }
-        });
-      }
-    });
+        }
+      });
+    }
   });
   // Update autocomplete on type change
-  $('.searchForm_type').change(function searchTypeChange() {
-    var $lookfor = $(this).closest('.searchForm').find('.searchForm_lookfor[name]');
-    $lookfor.autocomplete('clear cache');
+  $('#searchForm_type').change(function searchTypeChange() {
+    $('#searchForm_lookfor').autocomplete('clear cache');
   });
 }
 
@@ -253,7 +250,7 @@ function setupAutocomplete() {
  * @returns {undefined}
  */
 function keyboardShortcuts() {
-  var $searchform = $('.searchForm_lookfor');
+  var $searchform = $('#searchForm_lookfor');
   if ($('.pager').length > 0) {
     $(window).keydown(function shortcutKeyDown(e) {
       if (!$searchform.is(':focus')) {
