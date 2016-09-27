a4c02704cbc484b3fbf1022578cd198f3b16d641
Ere Maijala <ere.maijala@helsinki.fi>
2016-09-27 15:54:48 +0300

    Fixed some HTML and JS problems from library card selection and hold pickup location change function.

--- themes/finna/js/finna-holds.js
+++ themes/finna/js/finna-holds.js
@@ -3,41 +3,44 @@ finna.changeHolds = (function() {
     var setupChangeHolds = function () {
         var holds = $('.changeHolds');
         var errorOccured = $('<div></div>').attr('class', 'alert alert-danger').text(VuFind.translate('error_occurred'));
-        var dropdownMenu = $('ul#dropdown-menu');
         
         holds.click(function() {   
             var hold = $(this);
-            var spinnerLoad = $(this).find('.pickup-location-load-indicator');
-            spinnerLoad.removeClass('hidden');
-            var pickupLocations = $(this).find('#pickupLocations');         
-            var recordId = $(this).attr('recordId');
-            var requestId = $(this).attr('requestId');
-            var params = {
-                method: 'getRequestGroupPickupLocations',
-                id: recordId,
-                requestGroupId: '1'
-            };
-            $.ajax({
-                data: params,
-                dataType: 'json',
-                cache: false,
-                url: VuFind.path + '/AJAX/JSON'
-            })
-            .done(function(response) {
-                pickupLocations.empty();
-                $.each(response.data.locations, function() {
-                    var item = $('<li id="pickupLocationItem" class="pickupLocationItem" role="menuitem"></li>')
-                        .data('locationId', this.locationID).data('locationDisplay', this.locationDisplay).data('requestId', requestId).data('hold', hold).click(submitHandler);
-                    var text = $('<a href="#" ></a>').text(this.locationDisplay);
-                    var list = item.append(text);
-                    pickupLocations.append(list);
+            hold.find('.hold-change-success').remove();
+            var pickupLocations = $(this).find('.pickup-locations');
+            if (!pickupLocations.data('populated')) {
+                pickupLocations.data('populated', 1);
+                var spinnerLoad = $(this).find('.pickup-location-load-indicator');
+                spinnerLoad.removeClass('hidden');
+                var recordId = $(this).data('record-id');
+                var requestId = $(this).data('request-id');
+                var params = {
+                    method: 'getRequestGroupPickupLocations',
+                    id: recordId,
+                    requestGroupId: '0'
+                };
+                $.ajax({
+                    data: params,
+                    dataType: 'json',
+                    cache: false,
+                    url: VuFind.path + '/AJAX/JSON'
+                })
+                .done(function(response) {
+                    $.each(response.data.locations, function() {
+                        var item = $('<li id="pickupLocationItem" class="pickupLocationItem" role="menuitem"></li>')
+                            .data('locationId', this.locationID).data('locationDisplay', this.locationDisplay).data('requestId', requestId).data('hold', hold).click(submitHandler);
+                        var text = $('<a href="#" ></a>').text(this.locationDisplay);
+                        item.append(text);
+                        pickupLocations.append(item);
+                    });
+                    spinnerLoad.addClass('hidden');
+                })
+                .fail(function() {
+                    spinnerLoad.addClass('hidden');
+                    holds.append(errorOccured);
+                    pickupLocations.data('populated', 0);
                 });
-                spinnerLoad.addClass('hidden');     
-            })
-            .fail(function() {
-                spinnerLoad.addClass('hidden');
-                holds.append(errorOccured);
-            });
+            }
         });
         
         var submitHandler = function() {  
@@ -67,7 +70,7 @@ finna.changeHolds = (function() {
             .done(function(response) {
                 spinnerChange.addClass('hidden');
                 if (response.data['success']){
-                    var success = $('<div></div>').attr('class', 'alert alert-success').text(VuFind.translate('change_hold_success'));
+                    var success = $('<div></div>').attr('class', 'alert alert-success hold-change-success').text(VuFind.translate('change_hold_success'));
                     hold.append(success);
                 } else {
                     hold.append(errorOccured);
