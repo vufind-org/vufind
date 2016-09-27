a5ab6ed292e3e140a2cae995b9f91141cfe32cb0
Ere Maijala <ere.maijala@helsinki.fi>
2016-09-27 15:44:00 +0300

    Added a comment about the weak comparison.

--- themes/bootstrap3/js/hold.js
+++ themes/bootstrap3/js/hold.js
@@ -24,6 +24,7 @@ function setUpHoldRequestForm(recordId) {
       var defaultValue = $('#pickUpLocation').data('default');
       $.each(response.data.locations, function holdPickupLocationEach() {
         var option = $('<option></option>').attr('value', this.locationID).text(this.locationDisplay);
+        // Weak comparison between locationID and defaultValue since locationID may be an integer 
         if (this.locationID == defaultValue || (defaultValue === '' && this.isDefault && $emptyOption.length === 0)) {
           option.attr('selected', 'selected');
         }
