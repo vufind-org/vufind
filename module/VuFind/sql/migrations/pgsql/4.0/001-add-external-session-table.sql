0ceaaf3a5da1581bf233d1dee765d17f8a23f448
Demian Katz <demian.katz@villanova.edu>
2016-09-29 08:27:59 -0400

    Fixed null inconsistencies.

--- module/VuFind/sql/migrations/pgsql/4.0/001-add-external-session-table.sql
+++ module/VuFind/sql/migrations/pgsql/4.0/001-add-external-session-table.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE external_session (
 id SERIAL,
-session_id varchar(128),
-external_session_id varchar(255),
+session_id varchar(128) NOT NULL,
+external_session_id varchar(255) NOT NULL,
 created timestamp NOT NULL default '1970-01-01 00:00:00',
 PRIMARY KEY (id),
 UNIQUE (session_id)
