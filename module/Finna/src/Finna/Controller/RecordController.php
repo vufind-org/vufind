ba62df8ac0be2b3b639f18d69e71c4ec48d8af59
Ere Maijala <ere.maijala@helsinki.fi>
2016-12-28 15:23:40 +0200

    Improved decoding of Voyager MARC record preview to handle double-encoded UTF-8 better and fixed similar records to hide the spinner also on failure so that preview records don’t display an eternal spinner.

--- module/Finna/src/Finna/Controller/RecordController.php
+++ module/Finna/src/Finna/Controller/RecordController.php
@@ -214,10 +214,18 @@ class RecordController extends \VuFind\Controller\RecordController
             }
             // Voyager doesn't tell the proper encoding, so it's up to the browser to
             // decide. Try to handle both UTF-8 and ISO-8859-1.
-            $len = substr($data, 0, 5);
+            $len = (int)substr($data, 0, 5);
             if (strlen($data) != $len) {
-                $data = utf8_decode($data);
+                $data = $this->decodeVoyagerUTF8($data);
             }
+            $marc = new \File_MARC($data, \File_MARC::SOURCE_STRING);
+            $record = $marc->next();
+            if (false === $record) {
+                throw new \Exception('Missing record data');
+            }
+            $data = $record->toXML();
+            $data = preg_replace('/[\x00-\x1f]/', '', $data);
+            $data = iconv('UTF-8', 'UTF-8//IGNORE', $data);
         }
         if (!$data || !$format || !$source) {
             throw new \Exception('Missing parameters');
@@ -247,5 +255,57 @@ class RecordController extends \VuFind\Controller\RecordController
             return $response;
         }
         return parent::showTab($tab, $ajax);
+    }
+
+    /**
+     * Decode double-encoded UTF-8 received from Voyager
+     *
+     * @param string $str String to decode
+     *
+     * @return string
+     */
+    protected function decodeVoyagerUTF8($str)
+    {
+        $result = '';
+        $len = strlen($str);
+        for ($i = 0; $i < $len; $i++) {
+            $c1 = ord($str[$i]);
+            $c2 = $i + 1 < $len ? ord($str[$i + 1]) : 0;
+            $c3 = $i + 2 < $len ? ord($str[$i + 2]) : 0;
+            $c4 = $i + 3 < $len ? ord($str[$i + 3]) : 0;
+            $c5 = $i + 4 < $len ? ord($str[$i + 4]) : 0;
+            $c6 = $i + 5 < $len ? ord($str[$i + 5]) : 0;
+
+            if ($c1 < 0x80) {
+                $result .= chr($c1);
+            } elseif ($c1 < 0xE0) {
+                $c = (($c1 & 0x1F) << 6) + ($c2 & 0x3F);
+                $result .= chr($c);
+                $i += 1;
+            } elseif ($c1 < 0xF0) {
+                $c = (($c1 & 0x0F) << 12) + (($c2 & 0x3F) << 6) + ($c3 & 0x3F);
+                $result .= chr($c);
+                $i += 2;
+            } elseif ($c1 < 0xF8) {
+                $c = (($c1 & 0x07) << 18) + (($c2 & 0x3F) << 12)
+                    + (($c3 & 0x3F) << 6) + ($c4 & 0x3F);
+                $result .= chr($c);
+                $i += 3;
+            } elseif ($c1 < 0xFC) {
+                $c = (($c1 & 0x03) << 24) + (($c2 & 0x3F) << 18)
+                    + (($c3 & 0x3F) << 12) + (($c4 & 0x3F) << 6) + ($c5 & 0x3F);
+                $result .= chr($c);
+                $i += 4;
+            } elseif ($c1 < 0xFE) {
+                $c = (($c1 & 0x01) << 30) + (($c2 & 0x3F) << 24)
+                    + (($c3 & 0x3F) << 18) + (($c4 & 0x3F) << 12)
+                    + (($c5 & 0x3F) << 6) + ($c6 & 0x3F);
+                $result .= chr($c);
+                $i += 5;
+            } else {
+                $c = chr(0xFF);
+            }
+        }
+        return $result;
     }
 }
