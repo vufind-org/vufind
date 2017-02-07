6573f7c383d33b3c7dfbbe2ee04fb86851d2d292
Demian Katz <demian.katz@villanova.edu>
2017-02-07 09:06:35 -0500

    Remove unused use.

--- module/VuFind/src/VuFind/Resolver/Driver/AbstractBase.php
+++ module/VuFind/src/VuFind/Resolver/Driver/AbstractBase.php
@@ -28,7 +28,6 @@
  * @link     https://vufind.org/wiki/development:plugins:link_resolver_drivers Wiki
  */
 namespace VuFind\Resolver\Driver;
-use DOMDocument, DOMXpath;
 
 /**
  * AbstractBase for Resolver Driver
