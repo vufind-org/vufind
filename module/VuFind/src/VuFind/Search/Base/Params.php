283840817ecc0fd1a8379286715c92491eb26bee
Demian Katz <demian.katz@villanova.edu>
2017-02-07 12:41:07 -0500

    Simplify facet caching to eliminate crazy sleep/wake logic.

--- module/VuFind/src/VuFind/Search/Base/Params.php
+++ module/VuFind/src/VuFind/Search/Base/Params.php
@@ -1654,21 +1654,6 @@ class Params implements ServiceLocatorAwareInterface
     }
 
     /**
-     * Sleep magic method -- the service locator can't be serialized, so we need to
-     * exclude it from serialization.  Since we can't obtain a new locator in the
-     * __wakeup() method, it needs to be re-injected from outside.
-     *
-     * @return array
-     */
-    public function __sleep()
-    {
-        $vars = get_object_vars($this);
-        unset($vars['serviceLocator']);
-        $vars = array_keys($vars);
-        return $vars;
-    }
-
-    /**
      * Set the service locator.
      *
      * @param ServiceLocatorInterface $serviceLocator Locator to register
