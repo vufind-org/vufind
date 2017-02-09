92442052381a4f4d8e5d0adbc8d6a010b06c5b41
Ere Maijala <ere.maijala@helsinki.fi>
2017-02-09 14:16:17 +0200

    Increased timeout for RSS feed HTTP client.

--- module/Finna/src/Finna/Feed/Feed.php
+++ module/Finna/src/Finna/Feed/Feed.php
@@ -253,7 +253,9 @@ class Feed implements \Zend\Log\LoggerAwareInterface
         $maxAge = isset($this->mainConfig->Content->feedcachetime)
             ? $this->mainConfig->Content->feedcachetime : 10;
 
-        Reader::setHttpClient($this->http->createClient());
+        $httpClient = $this->http->createClient();
+        $httpClient->setOptions(['timeout' => 30]);
+        Reader::setHttpClient($httpClient);
 
         if ($maxAge) {
             if (is_readable($localFile)
