e2243b45ba2723c24158fcb1836f706e64a77924
Demian Katz <demian.katz@villanova.edu>
2017-02-10 13:55:14 -0500

    Improve language tool interface. (#906)

--- themes/bootstrap3/js/lightbox.js
+++ themes/bootstrap3/js/lightbox.js
@@ -65,7 +65,7 @@ VuFind.register('lightbox', function Lightbox() {
    */
   var _constrainLink; // function declarations to avoid style warnings
   var _formSubmit;    // about circular references
-  function _update(content) {
+  function render(content) {
     if (!content.match) {
       return;
     }
@@ -173,7 +173,7 @@ VuFind.register('lightbox', function Lightbox() {
           }
           _currentUrl = _originalUrl; // Now that we're logged in, where were we?
         }
-        _update(content);
+        render(content);
       })
       .fail(function lbAjaxFail(deferred, errorType, msg) {
         showAlert(VuFind.translate('error_occurred') + '<br/>' + msg, 'danger');
@@ -376,6 +376,7 @@ VuFind.register('lightbox', function Lightbox() {
     bind: bind,
     flashMessage: flashMessage,
     reload: reload,
+    render: render,
     // Reset
     reset: reset,
     // Init
