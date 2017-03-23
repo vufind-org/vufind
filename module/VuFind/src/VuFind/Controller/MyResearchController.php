2a1fa27e48043a210743799c0635c4489edc6b07
Demian Katz <demian.katz@villanova.edu>
2017-03-23 09:55:17 -0400

    Added comment and development mode details.

--- module/VuFind/src/VuFind/Controller/MyResearchController.php
+++ module/VuFind/src/VuFind/Controller/MyResearchController.php
@@ -90,10 +90,22 @@ class MyResearchController extends AbstractBase
      */
     public function onDispatch(\Zend\Mvc\MvcEvent $event)
     {
+        // Catch any ILSExceptions thrown during processing and display a generic
+        // failure message to the user (instead of going to the fatal exception
+        // screen). This offers a slightly more forgiving experience when there is
+        // an unexpected ILS issue. Note that this is mainly significant when using
+        // the MultiBackend driver, or in other scenarios where loadNoILSOnFailure
+        // cannot be used in config.ini. In other situations, the exception will
+        // be handled at a lower level in the code (see \VuFind\ILS\Connection).
         try {
             return parent::onDispatch($event);
         } catch (ILSException $exception) {
+            // Always display generic message:
             $this->flashMessenger()->addErrorMessage('ils_connection_failed');
+            // In development mode, also show technical failure message:
+            if ('development' == APPLICATION_ENV) {
+                $this->flashMessenger()->addErrorMessage($exception->getMessage());
+            }
             return $this->createViewModel();
         }
     }
