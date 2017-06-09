6cdc19aafc4e47aa31e226efb0c184f7917f0d2d
Chris Hallberg <crhallberg@gmail.com>
2017-06-09 09:54:01 -0400

    Decolumnize Advanced Search (BS3 Reduce) (#976)
    - Includes bug fix to extended range displays (when activating multiple ranges, the alignment and formatting of some controls could become incorrect).

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/AdvancedSearchTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/AdvancedSearchTest.php
@@ -115,12 +115,12 @@ class AdvancedSearchTest extends \VuFindTest\Unit\MinkTestCase
         $this->snooze();
         $this->findCss($page, '#search0_3');
         // No visible x next to lonely search term
-        $this->findCss($page, '#search1_0 .close.hidden');
+        $this->findCss($page, '#search1_0 .adv-term-remove.hidden');
         // Add a search term in another group
         $session->executeScript("addSearch(1)"); // add_search_link_1 click
         $this->findCss($page, '#search1_1');
         // Visible x next to lonely search term
-        $this->findCss($page, '#search1_0 .close:not(.hidden)');
+        $this->findCss($page, '#search1_0 .adv-term-remove:not(.hidden)');
 
         // Enter search for bride of the tomb
         $this->findCss($page, '#search_lookfor0_0')->setValue('bride');
