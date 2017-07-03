cc404e44f4d99d5a3733a1ec2a42bfbf4d7c6a3a
Ere Maijala <ere.maijala@helsinki.fi>
2017-07-03 16:01:12 +0300

    [ALLI-4448] Display multiple host record links properly.

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -425,7 +425,7 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
             ];
         }
 
-            // Try field 700 if 979 is empty
+        // Try field 700 if 979 is empty
         if (!$componentParts) {
             foreach ($this->getMarcRecord()->getFields('700') as $field) {
                 if (!$field->getSubfield('t')) {
@@ -534,6 +534,51 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
                 $this->getFormats(),
                 $this->mainConfig->Catalog->disable_driver_hold_actions->toArray()
             );
+    }
+
+    /**
+     * Get an array of host records
+     *
+     * Return an array of arrays with the following keys:
+     *   id
+     *   title
+     *   reference
+     *
+     * @return array
+     */
+    public function getHostRecords()
+    {
+        $result = [];
+        foreach ($this->getMarcRecord()->getFields('773') as $field) {
+            $id = '';
+            $title = '';
+            $reference = '';
+            $subfields = $field->getSubfields();
+            foreach ($subfields as $subfield) {
+                $subfieldCode = $subfield->getCode();
+                switch ($subfieldCode) {
+                case 'w':
+                    $id = $subfield->getData();
+                    break;
+                case 't':
+                    $title = $this->stripTrailingPunctuation(
+                        $subfield->getData(),
+                        '.-'
+                    );
+                    break;
+                case 'g':
+                    $reference = $subfield->getData();
+                    break;
+                }
+            }
+
+            $result[] = [
+                'id' => $id,
+                'title' => $title,
+                'reference' => $reference
+            ];
+        }
+        return $result;
     }
 
     /**
