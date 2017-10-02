5726f044498192de7f7d7404cbcb6293e97f1098
Sebastian Böttger <seboettg@users.noreply.github.com>
2017-10-02 18:22:39 +0200

    Move search history from the SearchController to a helper Service (#1018)

--- module/VuFind/src/VuFind/Db/Table/Search.php
+++ module/VuFind/src/VuFind/Db/Table/Search.php
@@ -109,15 +109,23 @@ class Search extends Gateway
     }
 
     /**
-     * Delete unsaved searches for a particular session.
+     * Destroy unsaved searches belonging to the specified session/user.
      *
      * @param string $sid Session ID of current user.
+     * @param int    $uid User ID of current user (optional).
      *
      * @return void
      */
-    public function destroySession($sid)
+    public function destroySession($sid, $uid = null)
     {
-        $this->delete(['session_id' => $sid, 'saved' => 0]);
+        $callback = function ($select) use ($sid, $uid) {
+            $select->where->equalTo('session_id', $sid)->and->equalTo('saved', 0);
+            if ($uid !== null) {
+                $select->where->OR
+                    ->equalTo('user_id', $uid)->and->equalTo('saved', 0);
+            }
+        };
+        return $this->delete($callback);
     }
 
     /**
@@ -132,7 +140,7 @@ class Search extends Gateway
     {
         $callback = function ($select) use ($sid, $uid) {
             $select->where->equalTo('session_id', $sid)->and->equalTo('saved', 0);
-            if ($uid != null) {
+            if ($uid !== null) {
                 $select->where->OR->equalTo('user_id', $uid);
             }
             $select->order('created');
