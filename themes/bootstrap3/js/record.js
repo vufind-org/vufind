a1f23e2fbeb39973789e99fccf3a5fff66e15e4a
Ere Maijala <ere.maijala@helsinki.fi>
2017-10-03 17:01:06 +0300

    Oops, fixed location of global definition.

--- themes/bootstrap3/js/record.js
+++ themes/bootstrap3/js/record.js
@@ -1,5 +1,5 @@
-/*global deparam, getUrlRoot, grecaptcha, recaptchaOnLoad, resetCaptcha, syn_get_widget, userIsLoggedIn, VuFind */
-/*exported ajaxTagUpdate, recordDocReady, setupJumpMenus */
+/*global deparam, getUrlRoot, grecaptcha, recaptchaOnLoad, resetCaptcha, syn_get_widget, userIsLoggedIn, VuFind, setupJumpMenus */
+/*exported ajaxTagUpdate, recordDocReady */
 
 /**
  * Functions and event handlers specific to record pages.
