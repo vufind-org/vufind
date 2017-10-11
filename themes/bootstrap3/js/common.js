afe0767cb49e80f6dd3780bb69c3a35a3ceb45a6
Chris Hallberg <crhallberg@gmail.com>
2017-10-11 17:01:55 -0400

    Add centralized event methods to VuFind object. Useful for JS objectification.

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -2,7 +2,7 @@
 /*exported VuFind, htmlEncode, deparam, moreFacets, lessFacets, getUrlRoot, phoneNumberFormHandler, recaptchaOnLoad, resetCaptcha, bulkFormHandler */
 
 // IE 9< console polyfill
-window.console = window.console || {log: function polyfillLog() {}};
+window.console = window.console || { log: function polyfillLog() {} };
 
 var VuFind = (function VuFind() {
   var defaultSearchBackend = null;
@@ -10,6 +10,22 @@ var VuFind = (function VuFind() {
   var _initialized = false;
   var _submodules = [];
   var _translations = {};
+
+  // Emit a custom event
+  // Recommendation: prefix with vf-
+  var emit = function emit(name, detail) {
+    if (typeof detail === 'undefined') {
+      document.dispatchEvent(new Event(name));
+    } else {
+      var event = document.createEvent('CustomEvent');
+      event.initCustomEvent(name, true, true, detail); // name, canBubble, cancelable, detail
+      document.dispatchEvent(event);
+    }
+  };
+  // Listen shortcut to put everyone on the same element
+  var listen = function listen(name, func) {
+    document.addEventListener(name, func, false);
+  };
 
   var register = function register(name, module) {
     if (_submodules.indexOf(name) === -1) {
@@ -73,6 +89,8 @@ var VuFind = (function VuFind() {
 
     addTranslations: addTranslations,
     init: init,
+    emit: emit,
+    listen: listen,
     refreshPage: refreshPage,
     register: register,
     translate: translate
