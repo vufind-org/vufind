29933364005ec430bd9a8ce9b739ac016da6c1e6
Ere Maijala <ere.maijala@helsinki.fi>
2017-10-30 08:36:19 +0200

    Memcache.php changes from upstream somehow missed in merges.

--- module/VuFind/src/VuFind/Session/Memcache.php
+++ module/VuFind/src/VuFind/Session/Memcache.php
@@ -104,23 +104,6 @@ class Memcache extends AbstractBase
 
         // Perform Memcache-specific cleanup:
         return $this->getConnection()->delete("vufind_sessions/{$sess_id}");
-
-        return true;
-    }
-
-    /**
-     * A function that is called internally when session data is to be saved.
-     *
-     * @param string $sess_id The current session ID
-     * @param string $data    The session data to write
-     *
-     * @return bool
-     */
-    protected function saveSession($sess_id, $data)
-    {
-        return $this->getConnection()->set(
-            "vufind_sessions/{$sess_id}", $data, 0, $this->lifetime
-        );
     }
 
     /**
