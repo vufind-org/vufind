6d1e13924cfa74c714a2133054389574e382ef61
Demian Katz <demian.katz@villanova.edu>
2017-11-15 09:47:46 -0500

    Use variable instead of hard-coded value for clarity.

--- themes/bootstrap3/js/pubdate_vis.js
+++ themes/bootstrap3/js/pubdate_vis.js
@@ -51,6 +51,8 @@ function loadVis(facetFields, searchParams, baseURL, zooming) {
   };
 
   // AJAX call
+  var graphMargin = 5;
+
   var url = baseURL + '/AJAX/json?method=getVisData&facetFields=' + encodeURIComponent(facetFields) + '&' + searchParams;
   $.getJSON(url, function getVisDataJSON(data) {
     $.each(data.data, function getVisDataEach(key, val) {
@@ -73,24 +75,24 @@ function loadVis(facetFields, searchParams, baseURL, zooming) {
 
       //check if the min and max value have been set otherwise set them to the ends of the graph
       if (val.min === 0) {
-        val.min = val.data[0][0] - 5;
+        val.min = val.data[0][0] - graphMargin;
       }
       if (val.max === 0) {
-        val.max = parseInt(val.data[val.data.length - 1][0], 10) + 5;
+        val.max = parseInt(val.data[val.data.length - 1][0], 10) + graphMargin;
       }
 
       if (zooming && hasFilter) {
         //check the first and last elements of the data array against min and max value (+padding)
         //if the element exists leave it, otherwise create a new marker with a minus one value
-        if (val.data[val.data.length - 1][0] !== parseInt(val.max, 10) + 5) {
-          val.data.push([parseInt(val.max, 10) + 5, -1]);
+        if (val.data[val.data.length - 1][0] !== parseInt(val.max, 10) + graphMargin) {
+          val.data.push([parseInt(val.max, 10) + graphMargin, -1]);
         }
-        if (val.data[0][0] !== val.min - 5) {
-          val.data.push([val.min - 5, -1]);
+        if (val.data[0][0] !== val.min - graphMargin) {
+          val.data.push([val.min - graphMargin, -1]);
         }
         //check for values outside the selected range and remove them by setting them to null
         for (var i = 0; i < val.data.length; i++) {
-          if (val.data[i][0] < val.min - 5 || val.data[i][0] > parseInt(val.max, 10) + 5) {
+          if (val.data[i][0] < val.min - graphMargin || val.data[i][0] > parseInt(val.max, 10) + graphMargin) {
             //remove this
             val.data.splice(i, 1);
             i--;
@@ -100,9 +102,9 @@ function loadVis(facetFields, searchParams, baseURL, zooming) {
       } else {
         //no zooming means that we need to specifically set the margins
         //do the last one first to avoid getting the new last element
-        val.data.push([parseInt(val.data[val.data.length - 1][0], 10) + 5, -1]);
+        val.data.push([parseInt(val.data[val.data.length - 1][0], 10) + graphMargin, -1]);
         //now get the first element
-        val.data.push([val.data[0][0] - 5, -1]);
+        val.data.push([val.data[0][0] - graphMargin, -1]);
       }
 
 
