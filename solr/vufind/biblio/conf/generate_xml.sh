9d114f6c593a7611f1d3bf24567efaaf07f06cb9
jriedl <johannes.riedl@uni-tuebingen.de>
2017-12-14 09:43:06 +0100

    Update generate_xml.sh

--- solr/vufind/biblio/conf/generate_xml.sh
+++ solr/vufind/biblio/conf/generate_xml.sh
@@ -6,9 +6,9 @@ if [[ ( $# != 1 ) || ( $1 != "krimdok" && $1 != "ixtheo" ) ]]; then
 fi
 
 if [[ $1 == ixtheo ]]; then
-    xmllint --xinclude --format schema_ixtheo_fields.xml > schema_local_fields.xml 2> /dev/null
-    xmllint --xinclude --format schema_ixtheo_types.xml  > schema_local_types.xml  2> /dev/null
+    xmllint --xinclude --format schema_ixtheo_fields.xml > schema_local_fields.xml
+    xmllint --xinclude --format schema_ixtheo_types.xml  > schema_local_types.xml 2> /dev/null
 else
-    xmllint --xinclude --format schema_krimdok_fields.xml > schema_local_fields.xml 2> /dev/null
-    xmllint --xinclude --format schema_krimdok_types.xml  > schema_local_types.xml  2> /dev/null
+    xmllint --xinclude --format schema_krimdok_fields.xml > schema_local_fields.xml
+    xmllint --xinclude --format schema_krimdok_types.xml  > schema_local_types.xml 2> /dev/null
 fi
