68f4b733d8d684e755acb765daa525a40a90eefd
Mario Trojan <mario.trojan@googlemail.com>
2018-01-17 13:35:21 +0100

    ixtheo2 inherits from tuefind instead of bootstrap3 (tuefind also inherits bootstrap3, and adds certain helper functions)

--- themes/ixtheo2/theme.config.php
+++ themes/ixtheo2/theme.config.php
@@ -1,17 +1,17 @@
 <?php
-return array(
-    'extends' => 'bootstrap3',
-    'css' => array(
+return [
+    'extends' => 'tuefind',
+    'css' => [
         'compiled.css',
         //'bootstrap.css',
         //'bootstrap-reboot.css',
         //'bootstrap-grid.css',
-        'font-awesome.css'
-    ),
-    'js' => array(
+        'font-awesome.css',
+    ],
+    'js' => [
         'jquery.js',
         'popper.js',
         'bootstrap-min.js',
-        'scripts.js'
-    )
-);
+        'scripts.js',
+    ]
+];
