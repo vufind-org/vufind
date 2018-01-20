a714bf51e679e87b40707e55395b559a01e48f4b
Benjamin Schnabel <Benjamin-777@gmx.de>
2018-01-20 21:13:41 +0100

    FontAwesome removed from theme.config.php

--- themes/ixtheo2/theme.config.php
+++ themes/ixtheo2/theme.config.php
@@ -3,15 +3,11 @@ return array(
     'extends' => 'bootstrap3',
     'css' => array(
         'compiled.css',
-        //'bootstrap.css',
-        //'bootstrap-reboot.css',
-        //'bootstrap-grid.css',
-        'font-awesome.css'
     ),
     'js' => array(
         'jquery.js',
         'popper.js',
         'bootstrap-min.js',
-        'scripts.js'
+        'scripts.js' // special scripts for ixtheo2
     )
 );
