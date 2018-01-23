2c5c36e0e36af82c590dace9d9a4e2aa6d343572
Benjamin Schnabel <Benjamin-777@gmx.de>
2018-01-23 15:06:10 +0100

    Revert "Template files replaced with symlinks to ixtheo theme."
    This reverts commit b96b0743fc54280cb5a98141eac7254996f159ba.

--- themes/ixtheo2/theme.config.php
+++ themes/ixtheo2/theme.config.php
@@ -3,11 +3,15 @@ return array(
     'extends' => 'bootstrap3',
     'css' => array(
         'compiled.css',
+        //'bootstrap.css',
+        //'bootstrap-reboot.css',
+        //'bootstrap-grid.css',
+        'font-awesome.css'
     ),
     'js' => array(
         'jquery.js',
         'popper.js',
         'bootstrap-min.js',
-        'scripts.js' // special scripts for ixtheo2
+        'scripts.js'
     )
 );
