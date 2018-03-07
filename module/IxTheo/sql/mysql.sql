ecce319d76adf45c305810b74e78f3e92b5a9755
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-03-07 11:38:31 +0100

    Reverted pooorly understood change.

--- module/IxTheo/sql/mysql.sql
+++ module/IxTheo/sql/mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE ixtheo_notations (
        id INT(11) NOT NULL,
        ixtheo_notation_list VARCHAR(8192) NOT NULL,
        FOREIGN KEY (id) REFERENCES user(id)
-) CHARSET=utf8;
+);
 
 CREATE TABLE ixtheo_id_result_sets (
        id INT(11) NOT NULL,
