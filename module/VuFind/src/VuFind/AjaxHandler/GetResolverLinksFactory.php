ce7190c00644ebfa51b9913a163b29622da1233b
Demian Katz <demian.katz@villanova.edu>
2018-03-12 16:40:39 -0400

    Style fix.

--- module/VuFind/src/VuFind/AjaxHandler/GetResolverLinksFactory.php
+++ module/VuFind/src/VuFind/AjaxHandler/GetResolverLinksFactory.php
@@ -38,7 +38,8 @@ use Interop\Container\ContainerInterface;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-class GetResolverLinksFactory implements \Zend\ServiceManager\Factory\FactoryInterface
+class GetResolverLinksFactory
+    implements \Zend\ServiceManager\Factory\FactoryInterface
 {
     /**
      * Create an object
