9fc3f99673218911914a98f59ae541e066fd2773
Demian Katz <demian.katz@villanova.edu>
2018-03-15 15:11:37 -0400

    Fixed typo.

--- module/VuFind/src/VuFind/View/Helper/Root/ConfigFactory.php
+++ module/VuFind/src/VuFind/View/Helper/Root/ConfigFactory.php
@@ -25,7 +25,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-namespace VuFind\Cache;
+namespace VuFind\View\Helper\Root;
 
 use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
