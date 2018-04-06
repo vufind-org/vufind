099abdc53eb4c05bec16f16ac279983e97bc45c4
Demian Katz <demian.katz@villanova.edu>
2018-04-06 12:59:12 -0400

    Add "new ILS items" channel provider.

--- module/VuFind/src/VuFind/ChannelProvider/PluginManager.php
+++ module/VuFind/src/VuFind/ChannelProvider/PluginManager.php
@@ -47,6 +47,7 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
         'alphabrowse' => 'VuFind\ChannelProvider\AlphaBrowse',
         'facets' => 'VuFind\ChannelProvider\Facets',
         'listitems' => 'VuFind\ChannelProvider\ListItems',
+        'newilsitems' => 'VuFind\ChannelProvider\NewILSItems',
         'random' => 'VuFind\ChannelProvider\Random',
         'similaritems' => 'VuFind\ChannelProvider\SimilarItems',
     ];
@@ -63,6 +64,8 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
             'VuFind\ChannelProvider\Factory::getFacets',
         'VuFind\ChannelProvider\ListItems' =>
             'VuFind\ChannelProvider\Factory::getListItems',
+        'VuFind\ChannelProvider\NewILSItems' =>
+            'VuFind\ChannelProvider\AbstractILSChannelProviderFactory',
         'VuFind\ChannelProvider\Random' =>
             'VuFind\ChannelProvider\Factory::getRandom',
         'VuFind\ChannelProvider\SimilarItems' =>
