96118b07f92f9365e42ff1cc3db821deced5d9c0
Konsta Raunio <konstara@users.noreply.github.com>
2018-04-19 13:04:34 +0300

    [ALLI-5450] Fixed case where record page 'material provided by' organ… (#995)

--- module/Finna/src/Finna/OrganisationInfo/OrganisationInfo.php
+++ module/Finna/src/Finna/OrganisationInfo/OrganisationInfo.php
@@ -358,35 +358,36 @@ class OrganisationInfo implements \Zend\Log\LoggerAwareInterface
         ];
         $params['with'] = 'finna';
         $response = $this->fetchData('consortium', $params);
-        if (!$response || $response['total' == 0]) {
+
+        if (!$response || $response['total'] == 0) {
             return false;
-        } else {
-            $urlHelper = $this->viewRenderer->plugin('url');
-            $url = $urlHelper('organisationinfo-home');
-            $result = ['success' => true, 'items' => []];
-            foreach ($response['items'] as $item) {
-                $id = $item['finna']['finna_id'];
-                $data = "{$url}?" . http_build_query(['id' => $id]);
-                if ($link) {
-                    $logo = null;
-                    if (isset($response['items'][0]['logo'])) {
-                        $logos = $response['items'][0]['logo'];
-                        foreach (['small', 'medium'] as $size) {
-                            if (isset($logos[$size])) {
-                                $logo = $logos[$size];
-                                break;
-                            }
+        }
+
+        $urlHelper = $this->viewRenderer->plugin('url');
+        $url = $urlHelper('organisationinfo-home');
+        $result = ['success' => true, 'items' => []];
+        foreach ($response['items'] as $item) {
+            $id = $item['finna']['finna_id'];
+            $data = "{$url}?" . http_build_query(['id' => $id]);
+            if ($link) {
+                $logo = null;
+                if (isset($response['items'][0]['logo'])) {
+                    $logos = $response['items'][0]['logo'];
+                    foreach (['small', 'medium'] as $size) {
+                        if (isset($logos[$size])) {
+                            $logo = $logos[$size];
+                            break;
                         }
                     }
-                    $data = $this->viewRenderer->partial(
-                        'Helpers/organisation-page-link.phtml', [
-                           'url' => $data, 'label' => 'organisation_info_link',
-                           'logo' => $logo, 'name' => $parentName
-                        ]
-                    );
                 }
-                $result['items'][$id] = $data;
+                $data = $this->viewRenderer->partial(
+                    'Helpers/organisation-page-link.phtml', [
+                       'url' => $data, 'label' => 'organisation_info_link',
+                       'logo' => $logo, 'name' => $parentName
+                    ]
+                );
             }
+            $result['items'][$id] = $data;
         }
         return $result;
     }
@@ -407,29 +408,30 @@ class OrganisationInfo implements \Zend\Log\LoggerAwareInterface
     ) {
         $params['id'] = $parent;
         $response = $this->fetchData('consortium', $params, true);
-        if (!$response || $response['museot'] == 0) {
+
+        if (!$response || empty($response['museot'])) {
             return false;
-        } else {
-            $urlHelper = $this->viewRenderer->plugin('url');
-            $url = $urlHelper('organisationinfo-home');
-            $json = $response['museot'][0];
-            if ($json['finna_publish'] == 1) {
-                $result = ['success' => true, 'items' => []];
-                $id = $json['finna_org_id'];
-                $data = "{$url}?" . http_build_query(['id' => $id]);
-                if ($link) {
-                    $logo = isset($json['image']) ? $json['image'] : null;
-                }
-                $name = isset($json['name'][$this->language])
-                    ? $json['name'][$this->language]
-                        : $this->translator->translate("source_{$parent}");
-                $data = $this->viewRenderer->partial(
-                    'Helpers/organisation-page-link.phtml', [
-                       'url' => $data, 'label' => 'organisation_info_link',
-                       'logo' => $logo, 'name' => $name
-                    ]
-                );
+        }
+
+        $urlHelper = $this->viewRenderer->plugin('url');
+        $url = $urlHelper('organisationinfo-home');
+        $json = $response['museot'][0];
+        if ($json['finna_publish'] == 1) {
+            $result = ['success' => true, 'items' => []];
+            $id = $json['finna_org_id'];
+            $data = "{$url}?" . http_build_query(['id' => $id]);
+            if ($link) {
+                $logo = isset($json['image']) ? $json['image'] : null;
             }
+            $name = isset($json['name'][$this->language])
+                ? $json['name'][$this->language]
+                    : $this->translator->translate("source_{$parent}");
+            $data = $this->viewRenderer->partial(
+                'Helpers/organisation-page-link.phtml', [
+                   'url' => $data, 'label' => 'organisation_info_link',
+                   'logo' => $logo, 'name' => $name
+                ]
+            );
             $result['items'][$id] = $data;
         }
         return $result;
