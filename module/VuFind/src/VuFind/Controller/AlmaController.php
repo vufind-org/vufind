1dcdc48cf952b3d663586c39243dacf892fd1f34
Michael Birkner <michael.birkner@akwien.at>
2018-05-04 08:39:24 +0200

    Removed setting flash messenger.

--- module/VuFind/src/VuFind/Controller/AlmaController.php
+++ module/VuFind/src/VuFind/Controller/AlmaController.php
@@ -271,7 +271,6 @@ class AlmaController extends AbstractBase {
                     'url' => $this->getServerUrl('myresearch-verify') . '?hash=' . $user->verify_hash . '&auth_method=' . $method
                 ]);
                 $this->serviceLocator->get('VuFind\Mailer\Mailer')->send($user->email, $config->Site->email, $this->translate('recovery_email_subject'), $message);
-                $this->flashMessenger()->addMessage('recovery_email_sent', 'success');
             } catch (\VuFind\Exception\Mail $e) {
                 error_log('Could not send the \'set-password-email\' to user with primary ID \''.$user->cat_id.'\' | username \''.$user->username.'\'. Exception message: '.$e->getMessage());
             }
