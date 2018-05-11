04d9e8a46a4e6f5a79c5064c1610ebd0537b3164
Michael Birkner <michael.birkner@akwien.at>
2018-05-11 18:12:47 +0200

    Added class doc. Using new functions to reduce duplication of code. Using PHP7 "??" operator instead of "isset".

--- module/VuFind/src/VuFind/Auth/AlmaDatabase.php
+++ module/VuFind/src/VuFind/Auth/AlmaDatabase.php
@@ -25,130 +25,105 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:plugins:authentication_handlers Wiki
  */
-
 namespace VuFind\Auth;
 
 use VuFind\Exception\Auth as AuthException;
 use Zend\Crypt\Password\Bcrypt;
 
+/**
+ * Authentication class for Alma. The VuFind database and the Alma API are
+ * combined for authentication by this classe.
+ *
+ * @category VuFind
+ * @package  Authentication
+ * @author   Michael Birkner <michael.birkner@akwien.at>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:authentication_handlers Wiki
+ */
 class AlmaDatabase extends Database
 {
-    
     /**
      * ILS Authenticator
      *
      * @var \VuFind\Auth\ILSAuthenticator
      */
     protected $authenticator;
-    
+
     /**
      * Catalog connection
      *
      * @var \VuFind\ILS\Connection
      */
     protected $catalog = null;
-    
+
     /**
      * Alma driver
-     * 
+     *
      * @var \VuFind\ILS\Driver\Alma
      */
     protected $almaDriver = null;
-    
-    
+
     /**
      * Alma config
-     * 
+     *
      * @var array
      */
     protected $almaConfig = null;
-    
+
     /**
      * Constructor
-     * 
-     * @param \VuFind\ILS\Connection $connection
-     * @param \VuFind\Auth\ILSAuthenticator $authenticator
+     *
+     * @param \VuFind\ILS\Connection        $connection    The ILS connection
+     * @param \VuFind\Auth\ILSAuthenticator $authenticator The ILS authenticator
      */
-    public function __construct(\VuFind\ILS\Connection $connection, \VuFind\Auth\ILSAuthenticator $authenticator)
-    {
+    public function __construct(
+        \VuFind\ILS\Connection $connection,
+        \VuFind\Auth\ILSAuthenticator $authenticator
+    ) {
         $this->catalog = $connection;
         $this->authenticator = $authenticator;
         $this->almaDriver = $connection->getDriver();
         $this->almaConfig = $connection->getDriverConfig();
     }
-    
+
     /**
      * Create a new user account in Alma AND in the VuFind Database.
-     * 
-     * @param \Zend\Http\PhpEnvironment\Request $request Request object containing new account details.
+     *
+     * @param \Zend\Http\PhpEnvironment\Request $request Request object containing
+     *                                                   new account details.
+     *
      * @return NULL|\VuFind\Db\Row\User New user row.
      */
     public function create($request)
-    {    	
-    	// When in privacy mode, don't create an Alma account and delegate
-    	// further code execution to the parent.
-    	if ($this->getConfig()->Authentication->privacy) {
-    		return parent::create($request);
-    	}
-    	
+    {
+        // When in privacy mode, don't create an Alma account and delegate
+        // further code execution to the parent.
+        if ($this->getConfig()->Authentication->privacy) {
+            return parent::create($request);
+        }
+
         // User variable
         $user = null;
-        
-        // Ensure that all expected parameters are populated to avoid notices
-        // in the code below.
-        $params = [
-            'firstname' => '', 'lastname' => '', 'username' => '',
-            'password' => '', 'password2' => '', 'email' => ''
-        ];
-        foreach ($params as $param => $default) {
-            $params[$param] = $request->getPost()->get($param, $default);
-        }
-        
-        // Validate Input
-        $this->validateUsernameAndPassword($params);
-        
-        // Invalid Email Check
-        $validator = new \Zend\Validator\EmailAddress();
-        if (!$validator->isValid($params['email'])) {
-            throw new AuthException('Email address is invalid');
-        }
-        if (!$this->emailAllowed($params['email'])) {
-            throw new AuthException('authentication_error_creation_blocked');
-        }
-        
-        // Make sure we have a unique username
-        $table = $this->getUserTable();
-        if ($table->getByUsername($params['username'], false)) {
-            throw new AuthException('That username is already taken');
-        }
-        // Make sure we have a unique email
-        if ($table->getByEmail($params['email'])) {
-            throw new AuthException('That email address is already used');
-        }
-        
+
+        $params = $this->collectParamsFromRequest($request);
+        $this->validateParams($params);
+
         // Create user account in Alma
         $almaAnswer = $this->almaDriver->createAlmaUser($params);
-        
+
         // Create user account in VuFind user table if Alma gave us an answer
         if ($almaAnswer != null) {
             // If we got this far, we're ready to create the account:
-            $user = $table->createRowForUsername($params['username']);
-            $user->firstname = $params['firstname'];
-            $user->lastname = $params['lastname'];
-            $user->email = $params['email'];
-            if ($this->passwordHashingEnabled()) {
-                $bcrypt = new Bcrypt();
-                $user->pass_hash = $bcrypt->create($params['password']);
-            } else {
-                $user->password = $params['password'];
-            }
-            // Save the Alma primary ID as cat_id to the VuFind user table
-            $user->cat_id = (isset($almaAnswer->primary_id)) ? $almaAnswer->primary_id : null;
+            $user = $this->createUserFromParams($params);
             
+            // Add the Alma primary ID as cat_id to the VuFind user table
+            $user->cat_id = $almaAnswer->primary_id ?? null;
+
             // Save the new user to the user table
             $user->save();
-            
-            // Save the credentials to cat_username and cat_password to bypass the ILS login screen from VuFind
+
+            // Save the credentials to cat_username and cat_password to bypass
+            // the ILS login screen from VuFind
             $user->saveCredentials($params['username'], $params['password']);
         } else {
             throw new AuthException('Error while creating user in Alma');
@@ -157,5 +132,4 @@ class AlmaDatabase extends Database
         return $user;
     }
 }
-
 ?>
