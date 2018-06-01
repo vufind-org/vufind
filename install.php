44a2bc40ad62a7dfde6c8e0a3ce9e43f7e654726
Demian Katz <demian.katz@villanova.edu>
2018-06-01 08:08:21 -0400

    Adjust version comments.

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 /**
  * Command-line tool to begin VuFind installation process
  *
- * PHP version 5
+ * PHP version 7
  *
  * Copyright (C) Villanova University 2012.
  *
--- module/VuFind/src/VuFind/Service/CsrfValidatorFactory.php
+++ module/VuFind/src/VuFind/Service/CsrfValidatorFactory.php
@@ -2,7 +2,7 @@
 /**
  * CSRF Validator factory.
  *
- * PHP version 5
+ * PHP version 7
  *
  * Copyright (C) Villanova University 2014.
  * Copyright (C) The National Library of Finland 2018.
