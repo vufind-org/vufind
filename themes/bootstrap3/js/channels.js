8bd8cc0ce5f468bd6afc0dfb71a7eed3b4590027
Chris Hallberg <crhallberg@gmail.com>
2018-06-04 12:08:34 -0400

    Responsive sliders for channels.

--- themes/bootstrap3/js/channels.js
+++ themes/bootstrap3/js/channels.js
@@ -21,7 +21,23 @@ function setupChannelSlider(i, op) {
     slidesToShow: 6,
     slidesToScroll: 6,
     infinite: false,
-    rtl: $(document.body).hasClass("rtl")
+    rtl: $(document.body).hasClass("rtl"),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
   $(op).on('swipe', function channelDrag() {
     $('[aria-describedby]').popover('hide');
