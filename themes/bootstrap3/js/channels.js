36f19e778ff360f5a17d2c361cf6529291f2f4c5
Chris Hallberg <crhallberg@gmail.com>
2018-06-04 09:23:37 -0400

    Add Slick.JS. Not working yet.

--- themes/bootstrap3/js/channels.js
+++ themes/bootstrap3/js/channels.js
@@ -17,68 +17,70 @@ function channelAddLinkButtons(elem) {
 }
 
 function setupChannelSlider(i, op) {
-  if (ChannelSlider(op)) {
-    $(op).find('.thumb').each(function thumbnailBackgrounds(index, thumb) {
-      var img = $(thumb).find('img');
-      $(thumb).css('background-image', 'url(' + img.attr('src') + ')');
-      img.remove();
-    });
-    // truncate long titles and add hover
-    $(op).find('.channel-record').dotdotdot({
-      callback: function dddcallback(istrunc, orig) {
-        if (istrunc) {
-          $(this).attr('title', $(orig).text());
-        }
+  $(".channel").slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+  });
+  $(op).find('.thumb').each(function thumbnailBackgrounds(index, thumb) {
+    var img = $(thumb).find('img');
+    $(thumb).css('background-image', 'url(' + img.attr('src') + ')');
+    img.remove();
+  });
+  // truncate long titles and add hover
+  $(op).find('.channel-record').dotdotdot({
+    callback: function dddcallback(istrunc, orig) {
+      if (istrunc) {
+        $(this).attr('title', $(orig).text());
       }
-    });
-    $('.channel-record').unbind('click').click(function channelRecord(event) {
-      var record = $(event.delegateTarget);
-      if (record.data('popover')) {
-        if (record.attr('aria-describedby')) {
-          record.popover('hide');
-        } else {
-          $('[aria-describedby]').popover('hide');
-          record.popover('show');
-        }
+    }
+  });
+  $('.channel-record').unbind('click').click(function channelRecord(event) {
+    var record = $(event.delegateTarget);
+    if (record.data('popover')) {
+      if (record.attr('aria-describedby')) {
+        record.popover('hide');
       } else {
-        record.data('popover', true);
-        record.popover({
-          content: VuFind.translate('loading') + '...',
-          html: true,
-          placement: 'bottom',
-          trigger: 'focus',
-          container: '#' + record.closest('.channel').attr('id')
-        });
         $('[aria-describedby]').popover('hide');
         record.popover('show');
-        $.ajax({
-          url: VuFind.path + getUrlRoot(record.attr('href')) + '/AjaxTab',
-          type: 'POST',
-          data: {tab: 'description'}
-        })
-        .done(function channelPopoverDone(data) {
-          record.data('bs.popover').options.content = '<h2>' + htmlEncode(record.text()) + '</h2>'
-            + '<div class="btn-group btn-group-justified">'
-            + '<a href="' + VuFind.path + '/Channels/Record?'
-              + 'id=' + encodeURIComponent(record.attr('data-record-id'))
-              + '&source=' + encodeURIComponent(record.attr('data-record-source'))
-            + '" class="btn btn-default">' + VuFind.translate('channel_expand') + '</a>'
-            + '<a href="' + record.attr('href') + '" class="btn btn-default">' + VuFind.translate('View Record') + '</a>'
-            + '</div>'
-            + data;
-          record.popover('show');
-        });
       }
-      return false;
-    });
-    // Channel add buttons
-    channelAddLinkButtons(op);
-    $('.channel-add-menu[data-group="' + op.dataset.group + '"].hidden')
-      .clone()
-      .addClass('pull-right')
-      .removeClass('hidden')
-      .appendTo($(op).find('.slider-menu'));
-  }
+    } else {
+      record.data('popover', true);
+      record.popover({
+        content: VuFind.translate('loading') + '...',
+        html: true,
+        placement: 'bottom',
+        trigger: 'focus',
+        container: '#' + record.closest('.channel').attr('id')
+      });
+      $('[aria-describedby]').popover('hide');
+      record.popover('show');
+      $.ajax({
+        url: VuFind.path + getUrlRoot(record.attr('href')) + '/AjaxTab',
+        type: 'POST',
+        data: {tab: 'description'}
+      })
+      .done(function channelPopoverDone(data) {
+        record.data('bs.popover').options.content = '<h2>' + htmlEncode(record.text()) + '</h2>'
+          + '<div class="btn-group btn-group-justified">'
+          + '<a href="' + VuFind.path + '/Channels/Record?'
+            + 'id=' + encodeURIComponent(record.attr('data-record-id'))
+            + '&source=' + encodeURIComponent(record.attr('data-record-source'))
+          + '" class="btn btn-default">' + VuFind.translate('channel_expand') + '</a>'
+          + '<a href="' + record.attr('href') + '" class="btn btn-default">' + VuFind.translate('View Record') + '</a>'
+          + '</div>'
+          + data;
+        record.popover('show');
+      });
+    }
+    return false;
+  });
+  // Channel add buttons
+  channelAddLinkButtons(op);
+  $('.channel-add-menu[data-group="' + op.dataset.group + '"].hidden')
+    .clone()
+    .addClass('pull-right')
+    .removeClass('hidden')
+    .appendTo($(op).find('.slider-menu'));
 }
 
 function bindChannelAddMenu(iteration, scope) {
