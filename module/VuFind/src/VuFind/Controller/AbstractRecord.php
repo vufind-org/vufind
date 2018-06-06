42fcb59fc1e5db8892f57763385cb093a99131dc
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2018-06-06 11:21:56 +0200

    change template for ajaxtabActions

--- module/VuFind/src/VuFind/Controller/AbstractRecord.php
+++ module/VuFind/src/VuFind/Controller/AbstractRecord.php
@@ -266,7 +266,7 @@ class AbstractRecord extends AbstractBase
     {
         $this->loadRecord();
         // Set layout to render content only:
-        $this->layout()->setTemplate('layout/lightbox');
+        $this->layout()->setTemplate('layout/inline');
         return $this->showTab(
             $this->params()->fromPost('tab', $this->getDefaultTab()), true
         );
