c80b2fa593001dcd7d62d80be07fa6d7945b97ef
Ere Maijala <ere.maijala@helsinki.fi>
2018-06-06 12:47:50 +0300

    Restore dummy MetaLib view helper for back-compatibility.

---
+++ module/Finna/src/Finna/View/Helper/Root/MetaLib.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Dummy MetaLib view helper (for legacy code template code)
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2016.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+namespace Finna\View\Helper\Root;
+
+/**
+ * Dummy MetaLib view helper (for legacy code template code)
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+class MetaLib extends \Zend\View\Helper\AbstractHelper
+{
+    /**
+     * Return false for all MetaLib view helper calls.
+     *
+     * @param string $name      Method name
+     * @param array  $arguments Method arguments
+     *
+     * @return bool
+     */
+    public function __call($name, $arguments)
+    {
+        return false;
+    }
+}
