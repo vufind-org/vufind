a77c02b41d2c83e1cc5cddbc65e53f5ecb345016
jriedl <johannes.riedl@uni-tuebingen.de>
2018-06-07 10:05:54 +0200

    Addres of problem of 700  is now longer secondary author

--- import/index_java/src/org/vufind/index/CreatorTools.java
+++ import/index_java/src/org/vufind/index/CreatorTools.java
@@ -132,11 +132,7 @@ public class CreatorTools
         // relator allowed" list.
         if (subfieldE.size() == 0 && subfield4.size() == 0) {
             if (Arrays.asList(noRelatorAllowed).contains(tag)) {
-                // 100 contains the first author even if the relator is not given explicitly
-                if (tag.equals("100"))
-                    relators.add("aut");
-                else
-                    relators.add("");
+                relators.add("");
             }
         } else {
             // If we got this far, we need to figure out what type of relation they have
