b33e5550091394a4d52c32f26147b5eca6e39a3e
jriedl <johannes.riedl@uni-tuebingen.de>
2018-06-11 14:58:47 +0200

    Revert apparently unneeded change

--- module/TueFind/src/TueFind/Controller/SearchController.php
+++ module/TueFind/src/TueFind/Controller/SearchController.php
@@ -20,17 +20,4 @@ class SearchController extends \VuFind\Controller\SearchController
             ]
         );
     }
-
-        /**
-     * Results action.
-     *
-     * @return mixed
-     */
-    public function resultsAction()
-    {
-        // overwrite to suppress special Tag handling as provided by standard
-        // VuFind
-        return parent::resultsAction();
-    }
-
 }
