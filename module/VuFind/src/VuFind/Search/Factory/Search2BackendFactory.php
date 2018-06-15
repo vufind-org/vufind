d62cf81fa0c40e31a2173df8ffd2f1826791fb0e
Demian Katz <demian.katz@villanova.edu>
2018-06-15 11:25:47 -0400

    Give Search2 backend its own YAML configuration file.

--- module/VuFind/src/VuFind/Search/Factory/Search2BackendFactory.php
+++ module/VuFind/src/VuFind/Search/Factory/Search2BackendFactory.php
@@ -46,5 +46,6 @@ class Search2BackendFactory extends SolrDefaultBackendFactory
     {
         parent::__construct();
         $this->mainConfig = $this->searchConfig = $this->facetConfig = 'Search2';
+        $this->searchYaml = 'searchspecs2.yaml';
     }
 }
