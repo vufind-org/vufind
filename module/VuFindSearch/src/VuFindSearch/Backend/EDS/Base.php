9e42c0470e786b7b0df570066924442dd4b0e2ac
Cornelius <cornelius.amzar@bsz-bw.de>
2018-06-15 15:13:15 +0200

    Clean up EDS namespacing and exception handling (#1206)

--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Base.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Base.php
@@ -22,12 +22,11 @@
  * @category EBSCOIndustries
  * @package  EBSCO
  * @author   Michelle Milton <mmilton@epnet.com>
+ * @author   Cornelius Amzar <cornelius.amzar@bsz-bw.de>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://edswiki.ebscohost.com/EDS_API_Documentation
  */
 namespace VuFindSearch\Backend\EDS;
-
-require_once dirname(__FILE__) . '/Exception.php';
 
 /**
  * EBSCO Search API abstract base class
@@ -38,7 +37,7 @@ require_once dirname(__FILE__) . '/Exception.php';
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://edswiki.ebscohost.com/EDS_API_Documentation
  */
-abstract class EdsApi_REST_Base
+abstract class Base
 {
     /**
      * A boolean value determining whether to print debug information
@@ -288,7 +287,7 @@ abstract class EdsApi_REST_Base
      * @param string $message       Message to POST if $method is POST
      * @param string $messageFormat Format of request $messageBody and responses
      *
-     * @throws \EbscoEdsApiException
+     * @throws ApiException
      * @return object         EDS API response (or an Error object).
      */
     protected function call($baseUrl, $headerParams, $params = [],
@@ -323,7 +322,7 @@ abstract class EdsApi_REST_Base
      *
      * @param array $input The raw response from Summon
      *
-     * @throws EbscoEdsApiException
+     * @throws ApiException
      * @return array       The processed response from EDS API
      */
     protected function process($input)
@@ -331,14 +330,14 @@ abstract class EdsApi_REST_Base
         //process response.
         try {
             $result = json_decode($input, true);
-        } catch (Exception $e) {
-            throw new EbscoEdsApiException(
+        } catch (\Exception $e) {
+            throw new ApiException(
                 'An error occurred when processing EDS Api response: '
                 . $e->getMessage()
             );
         }
         if (!isset($result)) {
-            throw new EbscoEdsApiException('Unknown error processing response');
+            throw new ApiException('Unknown error processing response');
         }
         return $result;
     }
