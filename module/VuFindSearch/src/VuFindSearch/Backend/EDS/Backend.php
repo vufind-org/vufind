da74a59f3657626e0b5549ee630f58ae2f54b2eb
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2018-06-15 10:16:28 +0200

    Code style fixes

--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Backend.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Backend.php
@@ -28,17 +28,17 @@
  */
 namespace VuFindSearch\Backend\EDS;
 
-use Exception,
-    VuFindSearch\Backend\AbstractBackend,
-    VuFindSearch\Backend\EDS\Zend2 as ApiClient,
-    VuFindSearch\Backend\Exception\BackendException,
-    VuFindSearch\ParamBag,
-    VuFindSearch\Query\AbstractQuery,
-    VuFindSearch\Response\RecordCollectionFactoryInterface,
-    VuFindSearch\Response\RecordCollectionInterface,
-    Zend\Cache\Storage\Adapter\AbstractAdapter as CacheAdapter,
-    Zend\Config\Config,
-    Zend\Session\Container as SessionContainer;
+use Exception;
+use VuFindSearch\Backend\AbstractBackend;
+use    VuFindSearch\Backend\EDS\Zend2 as ApiClient;
+use    VuFindSearch\Backend\Exception\BackendException;
+use    VuFindSearch\ParamBag;
+use    VuFindSearch\Query\AbstractQuery;
+use    VuFindSearch\Response\RecordCollectionFactoryInterface;
+use    VuFindSearch\Response\RecordCollectionInterface;
+use    Zend\Cache\Storage\Adapter\AbstractAdapter as CacheAdapter;
+use    Zend\Config\Config;
+use    Zend\Session\Container as SessionContainer;
 
 /**
  *  EDS API Backend
--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Zend2.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Zend2.php
@@ -28,11 +28,11 @@
  */
 namespace VuFindSearch\Backend\EDS;
 
-use VuFindSearch\Backend\EDS\Base,
-    Zend\Http\Client\Adapter\Curl as CurlAdapter,
-    Zend\Http\Client as Zend2HttpClient,
-    Zend\Log\LoggerAwareInterface,
-    VuFindSearch\Backend\EDS\Exception as EDSException;
+use VuFindSearch\Backend\EDS\Base;
+use Zend\Http\Client\Adapter\Curl as CurlAdapter;
+use Zend\Http\Client as Zend2HttpClient;
+use Zend\Log\LoggerAwareInterface;
+use VuFindSearch\Backend\EDS\Exception as EDSException;
 
 /**
  * EBSCO EDS API Zend2 Framework implementation
