a887dc52a11bd2201777320643621a6ce0a0bde9
Ere Maijala <ere.maijala@helsinki.fi>
2018-06-19 12:19:12 +0300

    Change Shibboleth login to log requests only in debug mode.

--- module/Finna/src/Finna/Auth/Shibboleth.php
+++ module/Finna/src/Finna/Auth/Shibboleth.php
@@ -60,9 +60,9 @@ class Shibboleth extends \VuFind\Auth\Shibboleth
         $shib = $this->getConfig()->Shibboleth;
         $username = $this->getServerParam($request, $shib->username);
         if (empty($username)) {
-            $this->logError(
-                'Shibboleth login failed for request: no username attribute present'
-                . ' in request: ' . print_r($request->getServer()->toArray(), true)
+            $this->debug(
+                "No username attribute ({$shib->username}) present in request: "
+                . print_r($request->getServer()->toArray(), true)
             );
             throw new AuthException('authentication_error_admin');
         }
@@ -71,9 +71,9 @@ class Shibboleth extends \VuFind\Auth\Shibboleth
         foreach ($this->getRequiredAttributes() as $key => $value) {
             $attrValue = $this->getServerParam($request, $key);
             if (!preg_match('/' . $value . '/', $attrValue)) {
-                $this->logError(
-                    "Shibboleth login failed for request: attribute '$key'"
-                    . " did not match regexp /$value/ in request"
+                $this->debug(
+                    "Attribute '$key' does not match required value '$value' in"
+                    . ' request: ' . print_r($request->getServer()->toArray(), true)
                 );
                 throw new AuthException('authentication_error_invalid_attributes');
             }
