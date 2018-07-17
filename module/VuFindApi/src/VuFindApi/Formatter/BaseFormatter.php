371baed58c850fcd159e7dfafe3f1a84bf09cd1d
Demian Katz <demian.katz@villanova.edu>
2018-07-17 11:40:39 -0400

    Add comment.

--- module/VuFindApi/src/VuFindApi/Formatter/BaseFormatter.php
+++ module/VuFindApi/src/VuFindApi/Formatter/BaseFormatter.php
@@ -56,6 +56,9 @@ class BaseFormatter
                 $this->resetArrayIndices($value);
             }
 
+            // We don't want to return empty values -- unless it's an empty array
+            // with a non-numeric key, since the key could be significant (e.g. in
+            // the case of an author name => roles array with no assigned roles).
             if ((is_numeric($key) && is_array($value) && empty($value))
                 || (is_bool($value) && !$value)
                 || $value === null || $value === ''
