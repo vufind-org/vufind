e406f4a259868746cc61ae18186f332d3515883f
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-07-26 12:32:36 +0100

    try passing VUFIND_HOME to java process as runtime arg

--- import-marc.sh
+++ import-marc.sh
@@ -123,7 +123,7 @@ MARC_FILE=`basename $1`
 # Execute Importer
 #####################################################
 
-RUN_CMD="$JAVA $INDEX_OPTIONS -Duser.timezone=UTC $EXTRA_SOLRMARC_SETTINGS -jar $JAR_FILE $PROPERTIES_FILE -solrj $VUFIND_HOME/solr/vendor/dist/solrj-lib $MARC_PATH/$MARC_FILE"
+RUN_CMD="$JAVA $INDEX_OPTIONS -Duser.timezone=UTC -Duser.VUFIND_HOME=$VUFIND_HOME $EXTRA_SOLRMARC_SETTINGS -jar $JAR_FILE $PROPERTIES_FILE -solrj $VUFIND_HOME/solr/vendor/dist/solrj-lib $MARC_PATH/$MARC_FILE"
 echo "Now Importing $1 ..."
 # solrmarc writes log messages to stderr, write RUN_CMD to the same place
 echo "`date '+%h %d, %H:%M:%S'` $RUN_CMD" >&2
--- import/index_java/src/org/vufind/index/ConfigManager.java
+++ import/index_java/src/org/vufind/index/ConfigManager.java
@@ -71,7 +71,7 @@ public class ConfigManager
         // Find VuFind's home directory in the environment; if it's not available,
         // try using a relative path on the assumption that we are currently in
         // VuFind's import subdirectory:
-        String vufindHome = System.getenv("VUFIND_HOME");
+        String vufindHome = System.getProperty("user.VUFIND_HOME");
         if (vufindHome == null) {
             // this shouldn't happen since import-marc.sh and .bat always set VUFIND_HOME
             throw new IllegalStateException("VUFIND_HOME must be set");
