70fe094ea533dd7174a34be3895b9a9ecfb24492
Konsta Raunio <konstara@users.noreply.github.com>
2018-08-21 11:05:48 +0300

    [ALLI-5551] Mikromarc: Fix for holds availability information. (#1053)
    There was possibility for it to show wrong status text.

--- module/Finna/src/Finna/ILS/Driver/Mikromarc.php
+++ module/Finna/src/Finna/ILS/Driver/Mikromarc.php
@@ -1482,8 +1482,7 @@ class Mikromarc extends \VuFind\ILS\Driver\AbstractBase implements
             $available = $item['ItemStatus'] === 'AvailableForLoan';
             $statusCode = $this->getItemStatusCode($item);
             $organisationTotal[$unit['branch']] = [
-               'reservations' => $item['ReservationQueueLength'],
-               'displayText' => $statusCode
+               'reservations' => $item['ReservationQueueLength']
             ];
 
             $unit = $this->getLibraryUnit($unitId);
@@ -1499,7 +1498,6 @@ class Mikromarc extends \VuFind\ILS\Driver\AbstractBase implements
                 'branch_id' => $unit['branch'],
                 'availability' => $available,
                 'status' => $statusCode,
-                'status_array' => [$statusCode],
                 'reserve' => 'N',
                 'callnumber' => $item['Shelf'],
                 'duedate' => null,
