73e1354739e4ee84f6b49997cc663acd49dc9855
Ere Maijala <ere.maijala@helsinki.fi>
2018-09-11 21:22:39 +0300

    [VUFIND-1245] Make MultiBackend use getStatuses of the ILS backend drivers.

--- module/VuFind/src/VuFind/ILS/Driver/MultiBackend.php
+++ module/VuFind/src/VuFind/ILS/Driver/MultiBackend.php
@@ -190,20 +190,53 @@ class MultiBackend extends AbstractBase implements \Zend\Log\LoggerAwareInterfac
      */
     public function getStatuses($ids)
     {
-        $items = [];
+        // Group records by source and request statuses from the drivers
+        $grouped = [];
         foreach ($ids as $id) {
-            try {
-                $items[] = $this->getStatus($id);
-            } catch (ILSException $e) {
-                $items[] = [
-                    [
-                        'id' => $id,
-                        'error' => 'An error has occurred'
-                    ]
+            $source = $this->getSource($id);
+            if (!isset($grouped[$source])) {
+                $driver = $this->getDriver($source);
+                $grouped[$source] = [
+                    'driver' => $driver,
+                    'ids' => []
                 ];
             }
+            $grouped[$source]['ids'][] = $id;
         }
-        return $items;
+
+        // Process each group
+        $results = [];
+        foreach ($grouped as $source => $current) {
+            if ($current['driver']) {
+                $localIds = array_map(
+                    function ($id) {
+                        return $this->getLocalId($id);
+                    },
+                    $current['ids']
+                );
+                try {
+                    $statuses = $current['driver']->getStatuses($localIds);
+                } catch (ILSException $e) {
+                    $statuses = [];
+                    foreach ($localIds as $id) {
+                        $statuses[] = [
+                            [
+                                'id' => $id,
+                                'error' => 'An error has occurred'
+                            ]
+                        ];
+                    }
+                }
+                $statuses = array_map(
+                    function ($status) use ($source) {
+                        return $this->addIdPrefixes($status, $source);
+                    },
+                    $statuses
+                );
+                $results = array_merge($results, $statuses);
+            }
+        }
+        return $results;
     }
 
     /**
