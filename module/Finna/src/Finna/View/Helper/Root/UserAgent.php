993560fcf8f4f648e70ebdfa3447a5cc897f73dd
Ere Maijala <ere.maijala@helsinki.fi>
2018-09-12 01:21:36 +0300

    [ALLI-3398] Improve image handling for bot.
    Display only links to large images for bots and hide images in record lists (e.g. collection contents) to avoid bad attribution to the main record.

---
+++ module/Finna/src/Finna/View/Helper/Root/UserAgent.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * User agent view helper.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) The National Library of Finland 2018.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+namespace Finna\View\Helper\Root;
+
+/**
+ * User agent view helper
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+class UserAgent extends \Zend\View\Helper\AbstractHelper
+{
+    /**
+     * Request object
+     *
+     * @var \Zend\Http\PhpEnvironment\Request
+     */
+    protected $request;
+
+    /**
+     * Cached result for isBot
+     *
+     * @var bool|null
+     */
+    protected $isBot = null;
+
+    /**
+     * Constructor
+     *
+     * @param Zend\Http\PhpEnvironment\Request $request Request
+     */
+    public function __construct(\Zend\Http\PhpEnvironment\Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Check if the request comes from a bot
+     *
+     * @return bool
+     */
+    public function isBot()
+    {
+        if (null === $this->isBot) {
+            $headers = $this->request->getHeaders();
+            if (!$headers->has('User-Agent')) {
+                return;
+            }
+            $agent = $headers->get('User-Agent')->toString();
+            $this->isBot = preg_match('/bot|crawl|slurp|spider/i', $agent);
+        }
+        return $this->isBot;
+    }
+}
--- themes/finna2/theme.config.php
+++ themes/finna2/theme.config.php
@@ -56,6 +56,7 @@ return [
             'Finna\View\Helper\Root\Translation' => 'Finna\View\Helper\Root\TranslationFactory',
             'Finna\View\Helper\Root\TranslationEmpty' => 'Zend\ServiceManager\Factory\InvokableFactory',
             'Finna\View\Helper\Root\TruncateUrl' => 'Zend\ServiceManager\Factory\InvokableFactory',
+            'Finna\View\Helper\Root\UserAgent' => 'Finna\View\Helper\Root\UserAgentFactory',
             'Finna\View\Helper\Root\UserPublicName' => 'Zend\ServiceManager\Factory\InvokableFactory',
         ],
         'aliases' => [
@@ -99,6 +100,7 @@ return [
             'translation' => 'Finna\View\Helper\Root\Translation',
             'translationEmpty' => 'Finna\View\Helper\Root\TranslationEmpty',
             'truncateUrl' => 'Finna\View\Helper\Root\TruncateUrl',
+            'userAgent' => 'Finna\View\Helper\Root\UserAgent',
             'userPublicName' => 'Finna\View\Helper\Root\UserPublicName',
 
             // Overrides
