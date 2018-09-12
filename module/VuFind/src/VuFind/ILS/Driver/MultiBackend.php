5a4dc5119abfc6a3380b48466364d4c279f9ce3d
Ere Maijala <ere.maijala@helsinki.fi>
2018-09-12 19:56:17 +0300

    Address issues found during review.

--- module/VuFind/src/VuFind/ILS/Driver/MultiBackend.php
+++ module/VuFind/src/VuFind/ILS/Driver/MultiBackend.php
@@ -207,6 +207,7 @@ class MultiBackend extends AbstractBase implements \Zend\Log\LoggerAwareInterfac
         // Process each group
         $results = [];
         foreach ($grouped as $source => $current) {
+            // Get statuses only if a driver is configured for this source
             if ($current['driver']) {
                 $localIds = array_map(
                     function ($id) {
@@ -217,15 +218,12 @@ class MultiBackend extends AbstractBase implements \Zend\Log\LoggerAwareInterfac
                 try {
                     $statuses = $current['driver']->getStatuses($localIds);
                 } catch (ILSException $e) {
-                    $statuses = [];
-                    foreach ($localIds as $id) {
-                        $statuses[] = [
-                            [
-                                'id' => $id,
-                                'error' => 'An error has occurred'
-                            ]
-                        ];
-                    }
+                    $statuses = array_map(
+                        function ($id) {
+                            return ['id' => $id, 'error' => 'An error has occurred'];
+                        },
+                        $localIds
+                    );
                 }
                 $statuses = array_map(
                     function ($status) use ($source) {
