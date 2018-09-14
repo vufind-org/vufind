0b3c8df6355869f89cbdd21e0833bf4c52a99cf8
Mario Trojan <mario.trojan@googlemail.com>
2018-09-14 16:41:27 +0200

    automatically add botprotect element to all forms

--- module/TueFind/src/TueFind/BotProtect.php
+++ module/TueFind/src/TueFind/BotProtect.php
@@ -7,14 +7,19 @@ use \Zend\Mvc\MvcEvent;
 /**
  * Bot protection
  *
- * If an input field with a special name contains contents, then it must be filled out by a bot.
+ * If a hidden input field with a special name is not empty, then it must have been filled out by a bot.
  * The field exists in the HTML form, but is hidden by CSS.
  */
 class BotProtect {
 
     const BOT_PROTECT_FIELD = 'botprotect';
 
-    static public function CheckRequest(MvcEvent $event) {
+    /**
+     * Abort & display error page if botprotect field detected
+     *
+     * @param MvcEvent $event
+     */
+    static public function ProcessRequest(MvcEvent $event) {
         if ($event->getRequest()->getQuery(self::BOT_PROTECT_FIELD) != '') {
             $response = $event->getResponse();
             $response->setStatusCode(400);
@@ -22,5 +27,17 @@ class BotProtect {
             $response->send();
             exit;
         }
+    }
+
+    /**
+     * Automatically add botprotect element to all forms
+     *
+     * @param MvcEvent $event
+     */
+    static public function ProcessResponse(MvcEvent $event) {
+        $response = $event->getResponse();
+        $content = $response->getContent();
+        $content = preg_replace('"</form>"i', '<input name="'.self::BOT_PROTECT_FIELD.'" class="'.self::BOT_PROTECT_FIELD.'" type="text"></input></form>', $content);
+        $response->setContent($content);
     }
 }
