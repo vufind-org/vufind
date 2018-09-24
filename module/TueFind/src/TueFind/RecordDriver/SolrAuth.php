3478732258b574e6fefcc279888718692d84a9b3
Mario Trojan <mario.trojan@googlemail.com>
2018-09-24 18:04:50 +0200

    tuefind authority record template example with hyperlinks generated via GND number

--- module/TueFind/src/TueFind/RecordDriver/SolrAuth.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrAuth.php
@@ -4,4 +4,16 @@ namespace TueFind\RecordDriver;
 
 class SolrAuth extends \VuFind\RecordDriver\SolrAuth {
 
+    /**
+     * Get GND Number from 035a (DE-588) or null
+     * @return string
+     */
+    public function getGNDNumber() {
+        $pattern = '"^\(DE-588\)"';
+        $values = $this->getFieldArray('035', 'a');
+        foreach ($values as $value) {
+            if (preg_match($pattern, $value))
+                return preg_replace($pattern, '', $value);
+        }
+    }
 }
