fce88223861fb338698231fcd00800baa4bc3739
Demian Katz <demian.katz@villanova.edu>
2018-10-10 12:36:11 -0400

    Remove long lines.

--- module/VuFind/src/VuFindTest/Container/MockContainer.php
+++ module/VuFind/src/VuFindTest/Container/MockContainer.php
@@ -124,9 +124,6 @@ class MockContainer implements ContainerInterface
      * Returns true if the container can return an entry for the given identifier.
      * Returns false otherwise.
      *
-     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
-     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
-     *
      * @param string $id Identifier of the entry to look for.
      *
      * @return bool
