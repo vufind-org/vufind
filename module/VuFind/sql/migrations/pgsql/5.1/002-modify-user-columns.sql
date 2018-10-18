6a89a75f394a66f2f1bbc9bff7c39bdbeaf377e3
Ere Maijala <ere.maijala@helsinki.fi>
2018-10-18 17:04:53 +0300

    Increase lengths in user_card table too.

--- module/VuFind/sql/migrations/pgsql/5.1/002-modify-user-columns.sql
+++ module/VuFind/sql/migrations/pgsql/5.1/002-modify-user-columns.sql
@@ -4,3 +4,7 @@
 
 ALTER TABLE "user"
   ALTER COLUMN cat_pass_enc TYPE varchar(255);
+
+ALTER TABLE "user_card"
+  ALTER COLUMN cat_password TYPE varchar(70),
+  ALTER COLUMN cat_pass_enc TYPE varchar(255);
