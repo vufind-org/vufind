5991c63ee501935682b8afeafa29c5c3797fcade
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2018-10-18 13:56:00 +0200

    Adapted to the new comman-line paraemters for the bib_ref_to_codes_tool utility.

--- module/IxTheo/src/IxTheo/Search/Backend/Solr/QueryBuilder.php
+++ module/IxTheo/src/IxTheo/Search/Backend/Solr/QueryBuilder.php
@@ -5,7 +5,8 @@ namespace IxTheo\Search\Backend\Solr;
 use VuFindSearch\Query\AbstractQuery;
 
 define("_BIB_REF_MAPS_PATH_", '/usr/local/var/lib/tuelib/bibleRef/'); // Must end with backslash!
-define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_code.map', _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map', _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
+define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'bible_aliases.map', 'books_of_the_bible_to_code.map',
+       _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map', _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
 
 class QueryBuilder extends \VuFindSearch\Backend\Solr\QueryBuilder
 {
