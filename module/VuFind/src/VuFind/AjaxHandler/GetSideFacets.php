72f15196001906f64fdc09fe30ae9e242778bf74
Ere Maijala <ere.maijala@helsinki.fi>
2018-10-25 14:11:42 +0300

    Code cleanup, add missing translation and tweak badge position while at it.

--- module/VuFind/src/VuFind/AjaxHandler/GetSideFacets.php
+++ module/VuFind/src/VuFind/AjaxHandler/GetSideFacets.php
@@ -28,9 +28,12 @@
 namespace VuFind\AjaxHandler;
 
 use VuFind\Recommend\PluginManager as RecommendPluginManager;
+use VuFind\Recommend\SideFacets;
+use VuFind\Search\Base\Results;
 use VuFind\Search\RecommendListener;
 use VuFind\Search\SearchRunner;
 use VuFind\Search\Solr\HierarchicalFacetHelper;
+use VuFind\Search\UrlQueryHelper;
 use VuFind\Session\Settings as SessionSettings;
 use Zend\Mvc\Controller\Plugin\Params;
 use Zend\View\Renderer\RendererInterface;
@@ -117,9 +120,49 @@ class GetSideFacets extends \VuFind\AjaxHandler\AbstractBase
     public function handleRequest(Params $params)
     {
         $this->disableSessionWrites();  // avoid session write timing bug
-        // Send both GET and POST variables to search class:
+
+        // Allow both GET and POST variables:
         $request = $params->fromQuery() + $params->fromPost();
 
+        $results = $this->getFacetResults($request);
+        if ($results instanceof \VuFind\Search\EmptySet\Results) {
+            $this->logError('Faceting request failed');
+            return $this->formatResponse('', self::STATUS_HTTP_ERROR);
+        }
+
+        $recommend = $results->getRecommendations('side');
+        $recommend = reset($recommend);
+
+        $context = [
+            'recommend' => $recommend,
+            'params' => $results->getParams(),
+            'searchClassId' => DEFAULT_SEARCH_BACKEND
+        ];
+        if (isset($request['enabledFacets'])) {
+            // Render requested facets separately
+            $facets = $this->formatFacets(
+                $context, $recommend, $request['enabledFacets'], $results
+            );
+            return $this->formatResponse(compact('facets'));
+        }
+
+        // Render full sidefacets
+        $html = $this->renderer->render(
+            'Recommend/SideFacets.phtml',
+            $context
+        );
+        return $this->formatResponse(compact('html'));
+    }
+
+    /**
+     * Perform search and return the results
+     *
+     * @param array $request Request params
+     *
+     * @return Results
+     */
+    protected function getFacetResults(array $request)
+    {
         $rManager = $this->recommendPluginManager;
         $setupCallback = function ($runner, $params, $searchId) use ($rManager) {
             $listener = new RecommendListener($rManager, $searchId);
@@ -146,118 +189,137 @@ class GetSideFacets extends \VuFind\AjaxHandler\AbstractBase
         };
 
         $runner = $this->searchRunner;
-        $results = $runner->run($request, DEFAULT_SEARCH_BACKEND, $setupCallback);
+        return $runner->run($request, DEFAULT_SEARCH_BACKEND, $setupCallback);
+    }
 
-        if ($results instanceof \VuFind\Search\EmptySet\Results) {
-            $this->logError('Faceting request failed');
-            return $this->formatResponse('', self::STATUS_HTTP_ERROR);
+    /**
+     * Format facets according to their type
+     *
+     * @param array      $context   View rendering context
+     * @param SideFacets $recommend Recommendation module
+     * @param array      $facets    Facets to process
+     * @param Results    $results   Search results
+     *
+     * @return array
+     */
+    protected function formatFacets($context, SideFacets $recommend, $facets,
+        Results $results
+    ) {
+        $response = [];
+        $hierarchicalFacets = [];
+        $options = $results->getOptions();
+        if (is_callable([$options, 'getHierarchicalFacets'])) {
+            $hierarchicalFacets = $options->getHierarchicalFacets();
+            $hierarchicalFacetSortOptions
+                = $recommend->getHierarchicalFacetSortOptions();
         }
-
-        $recommend = $results->getRecommendations('side');
-        $recommend = reset($recommend);
-
-        $context = [
-            'recommend' => $recommend,
-            'params' => $results->getParams(),
-            'searchClassId' => DEFAULT_SEARCH_BACKEND
-        ];
-        if (isset($request['enabledFacets'])) {
-            // Render requested facets separately
-            $response = [];
-            $hierarchicalFacets = [];
-            $options = $results->getOptions();
-            if (is_callable([$options, 'getHierarchicalFacets'])) {
-                $hierarchicalFacets = $options->getHierarchicalFacets();
-                $hierarchicalFacetSortOptions
-                    = $recommend->getHierarchicalFacetSortOptions();
+        $facetSet = $recommend->getFacetSet();
+        $urlHelper = $results->getUrlQuery();
+        foreach ($facets as $facet) {
+            if (strpos($facet, ':')) {
+                $response[$facet]['checkboxCount']
+                    = $this->getCheckboxFacetCount($facet, $results);
+            } elseif (in_array($facet, $hierarchicalFacets)) {
+                $response[$facet]['list'] = $this->getHierarchicalFacetData(
+                    $facet,
+                    $hierarchicalFacetSortOptions,
+                    $facetSet[$facet]['list'] ?? [],
+                    $urlHelper
+                );
+            } else {
+                $context['facet'] = $facet;
+                $context['cluster'] = $facetSet[$facet] ?? [];
+                $response[$facet]['html'] = $this->renderer->render(
+                    'Recommend/SideFacets/facet.phtml',
+                    $context
+                );
             }
-            $checkboxFacets = $results->getParams()->getCheckboxFacets();
-            $sideFacetSet = $recommend->getFacetSet();
-            $results = $recommend->getResults();
-            foreach ($request['enabledFacets'] as $facet) {
-                if (strpos($facet, ':')) {
-                    foreach ($checkboxFacets as $checkboxFacet) {
-                        if ($facet !== $checkboxFacet['filter']) {
-                            continue;
-                        }
-                        list($field, $value) = explode(':', $facet, 2);
-                        $checkboxResults = $results->getFacetList(
-                            [$field => $value]
-                        );
-                        if (!isset($checkboxResults[$field]['list'])) {
-                            $response[$facet]['checkboxCount'] = 0;
-                            continue 2;
-                        }
-                        $count = 0;
-                        $truncate = substr($value, -1) === '*';
-                        if ($truncate) {
-                            $value = substr($value, 0, -1);
-                        }
-                        foreach ($checkboxResults[$field]['list'] as $item) {
-                            if ($item['value'] == $value
-                                || ($truncate
-                                && preg_match('/^' . $value . '/', $item['value']))
-                                || ($item['value'] == 'true' && $value == '1')
-                                || ($item['value'] == 'false' && $value == '0')
-                            ) {
-                                $count += $item['count'];
-                            }
-                        }
-                        $response[$facet]['checkboxCount'] = $count;
-                        continue 2;
-                    }
-                }
-                if (in_array($facet, $hierarchicalFacets)) {
-                    // Return the facet data for hierarchical facets
-                    $facetList = $sideFacetSet[$facet]['list'];
+        }
+        return $response;
+    }
 
-                    if (!empty($hierarchicalFacetSortOptions[$facet])) {
-                        $this->facetHelper->sortFacetList(
-                            $facetList,
-                            'top' === $hierarchicalFacetSortOptions[$facet]
-                        );
-                    }
-
-                    $facetList = $this->facetHelper->buildFacetArray(
-                        $facet, $facetList, $results->getUrlQuery(), false
-                    );
-
-                    if (!empty($this->facetConfig->FacetFilters->$facet)
-                        || !empty($this->facetConfig->ExcludeFilters->$facet)
-                    ) {
-                        $filters = !empty($this->facetConfig->FacetFilters->$facet)
-                            ? $this->facetConfig->FacetFilters->$facet->toArray()
-                            : [];
-                        $excludeFilters
-                            = !empty($this->facetConfig->ExcludeFilters->$facet)
-                            ? $this->facetConfig->ExcludeFilters->$facet->toArray()
-                            : [];
-
-                        $facetList = $this->facetHelper->filterFacets(
-                            $facetList,
-                            $filters,
-                            $excludeFilters
-                        );
-                    }
-
-                    $response[$facet]['list'] = $facetList;
-                } else {
-                    $context['facet'] = $facet;
-                    $context['cluster'] = $sideFacetSet[$facet] ?? [];
-                    $response[$facet]['html'] = $this->renderer->render(
-                        'Recommend/SideFacets/facet.phtml',
-                        $context
-                    );
+    /**
+     * Get the result count for a checkbox facet
+     *
+     * @param string  $facet   Facet
+     * @param Results $results Search results
+     *
+     * @return int
+     */
+    protected function getCheckboxFacetCount($facet, Results $results)
+    {
+        $checkboxFacets = $results->getParams()->getCheckboxFacets();
+        foreach ($checkboxFacets as $checkboxFacet) {
+            if ($facet !== $checkboxFacet['filter']) {
+                continue;
+            }
+            list($field, $value) = explode(':', $facet, 2);
+            $checkboxResults = $results->getFacetList(
+                [$field => $value]
+            );
+            if (!isset($checkboxResults[$field]['list'])) {
+                return 0;
+            }
+            $count = 0;
+            $truncate = substr($value, -1) === '*';
+            if ($truncate) {
+                $value = substr($value, 0, -1);
+            }
+            foreach ($checkboxResults[$field]['list'] as $item) {
+                if ($item['value'] == $value
+                    || ($truncate
+                    && preg_match('/^' . $value . '/', $item['value']))
+                    || ($item['value'] == 'true' && $value == '1')
+                    || ($item['value'] == 'false' && $value == '0')
+                ) {
+                    $count += $item['count'];
                 }
             }
-            return $this->formatResponse(['facets' => $response]);
+            return $count;
         }
 
-        // Render full sidefacets
-        $html = $this->renderer->render(
-            'Recommend/SideFacets.phtml',
-            $context
+        return 0;
+    }
+
+    /**
+     * Get facet data for a hierarchical facet
+     *
+     * @param string         $facet       Facet
+     * @param array          $sortOptions Hierarhical facet sort options
+     * @param array          $facetList   Facet list
+     * @param UrlQueryHelper $urlHelper   UrlQueryHelper for creating facet URLs
+     *
+     * @return array
+     */
+    protected function getHierarchicalFacetData($facet, $sortOptions, $facetList,
+        UrlQueryHelper $urlHelper
+    ) {
+        if (!empty($sortOptions[$facet])) {
+            $this->facetHelper->sortFacetList(
+                $facetList,
+                'top' === $sortOptions[$facet]
+            );
+        }
+
+        $result = $this->facetHelper->buildFacetArray(
+            $facet, $facetList, $urlHelper, false
         );
-        return $this->formatResponse(compact('html'));
+
+        if (!empty($this->facetConfig->FacetFilters->$facet)
+            || !empty($this->facetConfig->ExcludeFilters->$facet)
+        ) {
+            $filters = !empty($this->facetConfig->FacetFilters->$facet)
+                ? $this->facetConfig->FacetFilters->$facet->toArray() : [];
+            $excludeFilters = !empty($this->facetConfig->ExcludeFilters->$facet)
+                ? $this->facetConfig->ExcludeFilters->$facet->toArray() : [];
+
+            $result = $this->facetHelper->filterFacets(
+                $result,
+                $filters,
+                $excludeFilters
+            );
+        }
+
+        return $result;
     }
 }
