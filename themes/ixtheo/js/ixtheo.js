7aed280308e1634100ea9538eda92e7e60006189
jriedl <johannes.riedl@uni-tuebingen.de>
2018-10-30 11:58:12 +0100

    Change name

--- themes/ixtheo/js/ixtheo.js
+++ themes/ixtheo/js/ixtheo.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-   var previous;
+   var previous_handler;
    $("#searchForm_type").on('focus', function () {
-       previous = this.value;
+       previous_handler = this.value;
    }).change(function adjustSearchSort(e) {
-     if (previous == 'BibleRangeSearch') {
+     if (previous_handler == 'BibleRangeSearch') {
          var default_sort = $("#sort_options_1").data('default_sort');
          $("#sort_options_1").off(); // Prevent automatic reloading
          $("#sort_options_1").removeAttr('disabled'); //Handle leftover of forcing relevance search for bibrange
