aaf4b58e405b256f6d0ddf487224bed9ed24b14d
jriedl <johannes.riedl@uni-tuebingen.de>
2018-12-04 08:39:10 +0100

    Add language files and normalize descriptions

--- module/TueFind/src/TueFind/RecordDriver/SolrMarc.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrMarc.php
@@ -161,6 +161,26 @@ class SolrMarc extends SolrDefault
         return $parallel_ppns_and_type;
     }
 
+    protected function normalizeGermanParallelDescriptions($german_term) {
+       $translations = [ "Äquivalent" => "Equivalent",
+                         "Digitale Übertragung von" => "Digital Reproduction of",
+                         "Digitale Übertragung" => "Digital Reproduction of",
+                         "Faksimile von" => "Facsimile of",
+                         "Faksimile" => "Facsimile",
+                         "Bestanderhaltungsfaksimile von" => "Preservation Facsimile of",
+                         "Bestanderhaltungsfaksimile" => "Preservation Facsimile",
+                         "Nachdruck von" => "Reprint of",
+                         "Nachgedruckt als" => "Reprinted as",
+                         "Reproduktion von" => "Reproduction of",
+                         "Reproduziert als" => "Reproduced as",
+                         "Elektronische Reproduktion von" => "Electronic Reproduction of",
+                         "Elektronische Reproduktion" => "Electronic Reproduction"
+                        ];
+        if (array_key_exists($german_term, $translations))
+            return $translations[$german_term];
+        return $german_term;
+    }
+
 
     public function getUnlinkedParallelEditions() {
         $parallel_editions = [];
@@ -182,7 +202,8 @@ class SolrMarc extends SolrDefault
                     $parallel_edition .= $subfield_a->getData() . ": ";
                 $further_subfields = $this->getSubfieldArray($field, ['t','d','g','o','u','z'], false);
                 $parallel_edition .= implode('.- ', $further_subfields);
-                array_push($parallel_editions, [ $subfield_i->getData() => $parallel_edition ]);
+                $description = $this->normalizeGermanParallelDescriptions($subfield_i->getData());
+                array_push($parallel_editions, [ $description => $parallel_edition ]);
             }
         }
         return $parallel_editions;
