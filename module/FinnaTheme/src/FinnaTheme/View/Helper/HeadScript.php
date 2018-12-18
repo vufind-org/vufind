48148c00011b7833149330792088097aea22f2d0
Ere Maijala <ere.maijala@helsinki.fi>
2018-12-18 16:20:26 +0200

    Revert "Defer loading scripts in head."
    This reverts commit b931c2929310cb2f504ec7685794f9130372f59a.

--- module/FinnaTheme/src/FinnaTheme/View/Helper/HeadScript.php
+++ module/FinnaTheme/src/FinnaTheme/View/Helper/HeadScript.php
@@ -111,7 +111,6 @@ class HeadScript extends \VuFindTheme\View\Helper\HeadScript
                 }
             }
         }
-        $item->attributes['defer'] = true;
         return parent::itemToString($item, $indent, $escapeStart, $escapeEnd);
     }
 }
