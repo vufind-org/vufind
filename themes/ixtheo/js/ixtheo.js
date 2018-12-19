633bcc4b77528c39bb66dcb1cac8c1c995e6b0d7
jriedl <johannes.riedl@uni-tuebingen.de>
2018-12-19 10:09:31 +0100

    Improved display logic

--- themes/ixtheo/js/ixtheo.js
+++ themes/ixtheo/js/ixtheo.js
@@ -1,23 +1,32 @@
+function convertToListEntries(bundle_id, items) {
+  var bundle_div = $("#" + bundle_id).find("div");
+  var entry_list = '<ul class="list-group">';
+  $.each(items, function(index, item) {
+    entry_list += ('<li class="list-group-item">' +
+                     '<a href=' + VuFind.path + /Record/ + item.id + '>' + item.title + '</a>' +
+                     '<a class="subscribe-record save-record modal-link" data-lightbox  id="' + item.id + '"' +
+                       'href="' + VuFind.path + '/Record/' + item.id + '/Subscribe" rel="nofollow" title="Subscribe">' +
+                     '<i class="fa fa-fw fa-bell"></i>' +
+                   '</li>');
+  });
+  entry_list += '</ul>';
+  bundle_div.append(entry_list);
+}
+
 function getSubscriptionBundleItems(bundle_id) {
   var query = "bundle_id:" + bundle_id;
-  var datums = [];
   $.ajax({
     url: VuFind.path + '/AJAX/JSON',
     data: {
-       method: 'getSubscriptionBundleEntries',
-       bundle_id: bundle_id
+      method: 'getSubscriptionBundleEntries',
+      bundle_id: bundle_id
     },
     dataType: 'json',
     success: function displaySubscriptionItems(json) {
       $(document).ready(function() {
         var data = $.parseJSON(json.data)
-        if (data.items.length > 0) {
-          datums = [];
-          for (var j = 0; j < data.items.length; j++) {
-            datums.push(data.items[j].title);
-            $("#" + bundle_id).find("div").append(data.items[j].title);
-          }
-        }
+        if (data.items.length > 0)
+          convertToListEntries(bundle_id, data.items);
       });
     },
     error: function(xhr, ajaxOptions, thrownError) {
@@ -29,18 +38,18 @@ function getSubscriptionBundleItems(bundle_id) {
 }
 
 $(document).ready(function() {
-   var previous_handler;
-   $("#searchForm_type").on('focus', function () {
-       previous_handler = this.value;
-   }).change(function adjustSearchSort(e) {
-     if (previous_handler == 'BibleRangeSearch') {
-         var default_sort = $("#sort_options_1").data('default_sort');
-         $("#sort_options_1").off(); // Prevent automatic reloading
-         $("#sort_options_1").removeAttr('disabled'); //Handle leftover of forcing relevance search for bibrange
-         $("#sort_options_1").val(default_sort).change();
-         $(":input[name='sort']").val(default_sort);
-         $("#sort_options_1").on();
-         return false;
-     }
-   });
+  var previous_handler;
+  $("#searchForm_type").on('focus', function () {
+      previous_handler = this.value;
+  }).change(function adjustSearchSort(e) {
+    if (previous_handler == 'BibleRangeSearch') {
+        var default_sort = $("#sort_options_1").data('default_sort');
+        $("#sort_options_1").off(); // Prevent automatic reloading
+        $("#sort_options_1").removeAttr('disabled'); //Handle leftover of forcing relevance search for bibrange
+        $("#sort_options_1").val(default_sort).change();
+        $(":input[name='sort']").val(default_sort);
+        $("#sort_options_1").on();
+        return false;
+    }
+  });
 });
