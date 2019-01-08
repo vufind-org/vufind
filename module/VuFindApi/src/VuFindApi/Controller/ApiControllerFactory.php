454764dfc23b5146f0b74e3bfc74defe8262590a
Demian Katz <demian.katz@villanova.edu>
2019-01-08 12:50:04 -0500

    Eliminate static API controller factories. - Progress on VUFIND-1302.

--- module/VuFindApi/src/VuFindApi/Controller/ApiControllerFactory.php
+++ module/VuFindApi/src/VuFindApi/Controller/ApiControllerFactory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Factory for controllers.
+ * Factory for ApiController.
  *
  * PHP version 7
  *
@@ -27,48 +27,42 @@
  */
 namespace VuFindApi\Controller;
 
-use Zend\ServiceManager\ServiceManager;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 
 /**
- * Factory for controllers.
+ * Factory for ApiController.
  *
  * @category VuFind
  * @package  Controller
  * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:plugins:controllers Wiki
- *
- * @codeCoverageIgnore
  */
-class Factory
+class ApiControllerFactory implements FactoryInterface
 {
     /**
-     * Construct the ApiController.
+     * Create an object
      *
-     * @param ServiceManager $sm Service manager.
+     * @param ContainerInterface $container     Service manager
+     * @param string             $requestedName Service being created
+     * @param null|array         $options       Extra options (optional)
      *
-     * @return ApiController
+     * @return object
+     *
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     * creating a service.
+     * @throws ContainerException if any other error occurs
      */
-    public static function getApiController(ServiceManager $sm)
-    {
-        $controller = new ApiController($sm);
-        $controller->addApi($sm->get('ControllerManager')->get('SearchApi'));
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null
+    ) {
+        if (!empty($options)) {
+            throw new \Exception('Unexpected options sent to factory.');
+        }
+        $controller = new $requestedName($container);
+        $controller->addApi($container->get('ControllerManager')->get('SearchApi'));
         return $controller;
-    }
-
-    /**
-     * Construct the SearchApiController.
-     *
-     * @param ServiceManager $sm Service manager.
-     *
-     * @return SearchApiController
-     */
-    public static function getSearchApiController(ServiceManager $sm)
-    {
-        return new SearchApiController(
-            $sm,
-            $sm->get('VuFindApi\Formatter\RecordFormatter'),
-            $sm->get('VuFindApi\Formatter\FacetFormatter')
-        );
     }
 }
