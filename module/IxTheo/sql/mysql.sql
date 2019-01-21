69363075ec08dd91e707aaee0fa4ba591b5a7402
Mario Trojan <mario.trojan@googlemail.com>
2019-01-21 15:44:42 +0100

    removed ixtheo_notations table

--- module/IxTheo/sql/mysql.sql
+++ module/IxTheo/sql/mysql.sql
@@ -1,9 +1,3 @@
-CREATE TABLE ixtheo_notations (
-    id INT(11) NOT NULL,
-    ixtheo_notation_list VARCHAR(8192) NOT NULL,
-    CONSTRAINT `ixtheo_notations_ibfk_1` FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE
-);
-
 CREATE TABLE ixtheo_id_result_sets (
     id INT(11) NOT NULL,
     ids VARCHAR(128) NOT NULL,
