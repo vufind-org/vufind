dc647743d62b8c0354abe0bf54172682fbc40525
Mario Trojan <mario.trojan@googlemail.com>
2019-01-23 11:30:39 +0100

    Autocomplete\SolrFactory needs to override __invoke function

--- module/IxTheo/src/IxTheo/Autocomplete/SolrFactory.php
+++ module/IxTheo/src/IxTheo/Autocomplete/SolrFactory.php
@@ -5,17 +5,11 @@ use Zend\ServiceManager\ServiceManager;
 
 class SolrFactory extends \VuFind\Autocomplete\SolrFactory
 {
-    /**
-     * Construct the Solr plugin.
-     *
-     * @param ServiceManager $sm Service manager.
-     *
-     * @return Solr
-     */
-    public static function getSolr(ServiceManager $sm)
-    {
-        return new Solr(
-            $sm->getServiceLocator()->get('VuFind\SearchResultsPluginManager')
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null
+    ) {
+        return new $requestedName(
+            $container->get('VuFind\Search\Results\PluginManager')
         );
     }
 }
