nikopartanen/NDL-VuFind2

[ALLI-5384] Initial support for fetching editions/expressions with work keys.

module/FinnaSearch/src/FinnaSearch/Backend/Solr/Backend.php
@@ -29,6 +29,7 @@
  */
 namespace FinnaSearch\Backend\Solr;
 
+use FinnaSearch\Feature\WorkExpressionsInterface;
 use VuFindSearch\ParamBag;
 use VuFindSearch\Response\RecordCollectionInterface;
 
@@ -43,6 +44,7 @@ use VuFindSearch\Response\RecordCollectionInterface;
  * @link     https://vufind.org
  */
 class Backend extends \VuFindSearch\Backend\Solr\Backend
+    implements WorkExpressionsInterface
 {
     /**
      * Return similar records.
@@ -77,6 +79,35 @@ class Backend extends \VuFindSearch\Backend\Solr\Backend
             $params->mergeWith($this->getSimilarBuilder()->build($id, $params));
             $response = $this->connector->similar($id, $params);
         }
+        $collection = $this->createRecordCollection($response);
+        $this->injectSourceIdentifier($collection);
+        return $collection;
+    }
+
+    /**
+     * Return work expressions.
+     *
+     * @param string   $id            Id of record to compare with
+     * @param array    $workKeys      Work identification keys
+     * @param ParamBag $defaultParams Search backend parameters
+     *
+     * @return RecordCollectionInterface
+     */
+    public function workExpressions($id, $workKeys, ParamBag $defaultParams = null)
+    {
+        $params = $defaultParams ? clone $defaultParams
+            : new \VuFindSearch\ParamBag();
+        $this->injectResponseWriter($params);
+        $query = [];
+        foreach ($workKeys as $key) {
+            $key = addcslashes($key, '+-&|!(){}[]^"~*?:\\/');
+            $query[] = "work_keys_str_mv:(\"$key\")";
+        }
+        $params->set('q', implode(' OR ', $query));
+        $params->add('fq', sprintf('-id:"%s"', addcslashes($id, '"')));
+        $params->add('rows', 100);
+        $params->add('sort', 'main_date_str_mv desc, title_sort asc');
+        $response = $this->connector->search($params);
         $collection = $this->createRecordCollection($response);
         $this->injectSourceIdentifier($collection);
         return $collection;

vufind-org/vufind

module/FinnaSearch/src/FinnaSearch/Backend/Solr/Backend.php
@@ -29,6 +29,7 @@
  */
 namespace FinnaSearch\Backend\Solr;
 
+use FinnaSearch\Feature\WorkExpressionsInterface;
 use VuFindSearch\ParamBag;
 use VuFindSearch\Response\RecordCollectionInterface;
 
@@ -43,6 +44,7 @@ use VuFindSearch\Response\RecordCollectionInterface;
  * @link     https://vufind.org
  */
 class Backend extends \VuFindSearch\Backend\Solr\Backend
+    implements WorkExpressionsInterface
 {
     /**
      * Return similar records.
@@ -77,6 +79,35 @@ class Backend extends \VuFindSearch\Backend\Solr\Backend
             $params->mergeWith($this->getSimilarBuilder()->build($id, $params));
             $response = $this->connector->similar($id, $params);
         }
+        $collection = $this->createRecordCollection($response);
+        $this->injectSourceIdentifier($collection);
+        return $collection;
+    }
+
+    /**
+     * Return work expressions.
+     *
+     * @param string   $id            Id of record to compare with
+     * @param array    $workKeys      Work identification keys
+     * @param ParamBag $defaultParams Search backend parameters
+     *
+     * @return RecordCollectionInterface
+     */
+    public function workExpressions($id, $workKeys, ParamBag $defaultParams = null)
+    {
+        $params = $defaultParams ? clone $defaultParams
+            : new \VuFindSearch\ParamBag();
+        $this->injectResponseWriter($params);
+        $query = [];
+        foreach ($workKeys as $key) {
+            $key = addcslashes($key, '+-&|!(){}[]^"~*?:\\/');
+            $query[] = "work_keys_str_mv:(\"$key\")";
+        }
+        $params->set('q', implode(' OR ', $query));
+        $params->add('fq', sprintf('-id:"%s"', addcslashes($id, '"')));
+        $params->add('rows', 100);
+        $params->add('sort', 'main_date_str desc, title_sort asc');
+        $response = $this->connector->search($params);
         $collection = $this->createRecordCollection($response);
         $this->injectSourceIdentifier($collection);
         return $collection;