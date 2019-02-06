5d28e43d5261792b1fb07da58751753b622d7570
jriedl <johannes.riedl@uni-tuebingen.de>
2019-02-06 08:46:19 +0100

    Boilerplate for new TueFindSearch module

---
+++ module/TueFind/src/TueFind/Service/Factory.php
@@ -0,0 +1,23 @@
+<?php
+namespace TueFind\Service;
+
+use Zend\ServiceManager\ServiceManager;
+
+class Factory extends \VuFind\Service\Factory {
+
+    /**
+     * Construct the search service.
+     *
+     * @param ServiceManager $sm Service manager.
+     *
+     * @return \TueFindSearch\Service
+     */
+    public static function getSearchService(ServiceManager $sm)
+    {
+        return new \TueFindSearch\Service(
+            new \Zend\EventManager\EventManager($sm->get('SharedEventManager'))
+        );
+    }
+
+}
+?>
---
+++ module/TueFindSearch/src/TueFindSearch/Service.php
@@ -0,0 +1,20 @@
+<?php
+namespace TueFindSearch;
+
+use VuFindSearch\Backend\BackendInterface;
+use VuFindSearch\Backend\Exception\BackendException;
+use VuFindSearch\Feature\RandomInterface;
+use VuFindSearch\Feature\RetrieveBatchInterface;
+use VuFindSearch\Response\RecordCollectionInterface;
+
+use Zend\EventManager\EventManager;
+use Zend\EventManager\EventManagerInterface;
+
+
+class Service extends \VuFindSearch\Service {
+
+    public function __construct(EventManagerInterface $events = null) {
+        parent::__construct($events);
+    }
+}
+?>
