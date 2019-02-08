98e712a1ef3455986285187894d5d7fb41224ab2
Chris Hallberg <crhallberg@gmail.com>
2019-02-08 13:01:33 -0500

    Initial work on language. Themes needs same parameter keeping treatment.

--- module/VuFind/src/VuFind/View/Helper/Root/DisplayLanguageOption.php
+++ module/VuFind/src/VuFind/View/Helper/Root/DisplayLanguageOption.php
@@ -28,6 +28,7 @@
 namespace VuFind\View\Helper\Root;
 
 use Zend\I18n\Translator\TranslatorInterface;
+use Zend\Http\PhpEnvironment\Request;
 
 /**
  * DisplayLanguageOption view helper
@@ -51,10 +52,12 @@ class DisplayLanguageOption extends \Zend\View\Helper\AbstractHelper
      * Constructor
      *
      * @param TranslatorInterface $translator Main VuFind translator
+     * @param Request             $request    Request object for GET parameters
      */
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(TranslatorInterface $translator, Request $request)
     {
         $this->translator = $translator;
+        $this->request = $request;
         try {
             $this->translator->addTranslationFile(
                 'ExtendedIni', null, 'default', 'native'
@@ -77,8 +80,25 @@ class DisplayLanguageOption extends \Zend\View\Helper\AbstractHelper
      *
      * @return string
      */
-    public function __invoke($str)
+    public function __invoke($str = null)
     {
+        if ($str === null) {
+            return $this;
+        }
         return $this->view->escapeHtml($this->translator->translate($str));
+    }
+
+    /**
+     * Get URL to change the language via a GET parameter
+     *
+     * @param sring          $code Language code
+     *
+     * @return string
+     */
+    public function getUrl($code)
+    {
+        $requestQuery = $this->request->getQuery()->toArray();
+        $options = ['query' => array_merge($requestQuery, ['lng' => $code])];
+        return $this->view->url(null, [], $options);
     }
 }
