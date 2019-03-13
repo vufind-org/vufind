fec60fc3397a6d04b6e2554cb23c8e65d6e4f930
Mario Trojan <mario.trojan@googlemail.com>
2019-03-13 13:59:26 +0100

    redirect page for changed record ids

---
+++ module/TueFind/src/TueFind/Controller/RecordController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TueFind\Controller;
+
+use Zend\View\Model\ViewModel;
+
+class RecordController extends \VuFind\Controller\RecordController {
+    /**
+     * Show redirect page if FallbackLoader was active
+     *
+     * @return mixed
+     */
+    public function homeAction()
+    {
+        $this->loadRecord();
+        if (isset($this->driver->isFallback) && $this->driver->isFallback) {
+            $params = [ 'driver' => $this->driver,
+                        'originalId' => $this->params()->fromRoute('id', $this->params()->fromQuery('id'))];
+
+            $helper = $this->serviceLocator->get('ViewHelperManager')->get('HelpText');
+            $template = $helper->getTemplate('record_id_changed', 'static');
+            if ($template) {
+                $view = $this->createViewModel($params);
+                $view->setTemplate($template[1]);
+                $this->getResponse()->setStatusCode(301);
+                return $view;
+            }
+        }
+        return $this->showTab(
+            $this->params()->fromRoute('tab', $this->getDefaultTab())
+        );
+    }
+}
