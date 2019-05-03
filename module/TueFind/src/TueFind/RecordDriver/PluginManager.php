1338547182bb1d062b2401ef8ffa85b931134e13
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-05-03 13:50:02 +0200

    solved SolrAuth problem with 5.1

--- module/TueFind/src/TueFind/RecordDriver/PluginManager.php
+++ module/TueFind/src/TueFind/RecordDriver/PluginManager.php
@@ -15,12 +15,15 @@ class PluginManager extends \VuFind\RecordDriver\PluginManager {
     public function __construct($configOrContainerInstance = null,
         array $v3config = []
     ) {
-        $this->aliases['solrauth'] = 'TueFind\RecordDriver\SolrAuth';
+        $this->aliases['solrauth'] = 'TueFind\RecordDriver\SolrAuthMarc';
+        $this->aliases['solrauthdefault'] = 'TueFind\RecordDriver\SolrAuthMarc';
+        $this->aliases['solrauthmarc'] = 'TueFind\RecordDriver\SolrAuthMarc';
         $this->aliases['solrdefault'] = 'TueFind\RecordDriver\SolrDefault';
         $this->aliases['solrmarc'] = 'TueFind\RecordDriver\SolrMarc';
 
         $this->delegators['TueFind\RecordDriver\SolrMarc'] = ['VuFind\RecordDriver\IlsAwareDelegatorFactory'];
 
+        $this->factories['TueFind\RecordDriver\SolrAuth'] = 'VuFind\RecordDriver\SolrDefaultWithoutSearchServiceFactory';
         $this->factories['TueFind\RecordDriver\SolrAuthDefault'] = 'VuFind\RecordDriver\SolrDefaultWithoutSearchServiceFactory';
         $this->factories['TueFind\RecordDriver\SolrAuthMarc'] = 'VuFind\RecordDriver\SolrDefaultWithoutSearchServiceFactory';
         $this->factories['TueFind\RecordDriver\SolrDefault'] = 'TueFind\RecordDriver\SolrDefaultFactory';
