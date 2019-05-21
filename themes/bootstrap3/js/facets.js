238820fed9e531a598a32d70040d3b6d0e0bac87
Chris Hallberg <crhallberg@gmail.com>
2019-05-21 12:43:46 -0400

    Re-align filter dropdowns if they go off the edge of the screen.

--- themes/bootstrap3/js/facets.js
+++ themes/bootstrap3/js/facets.js
@@ -266,6 +266,16 @@ VuFind.register('sideFacets', function SideFacets() {
         loadAjaxSideFacets();
       });
     loadAjaxSideFacets();
+
+    // Keep filter dropdowns on screen
+    $(".search-filter-dropdown").on("shown.bs.dropdown", function checkFilterDropdownWidth(e) {
+      var $dropdown = $(e.target).find(".dropdown-menu");
+      if ($(e.target).position().left + $dropdown.width() >= window.innerWidth) {
+        $dropdown.addClass("dropdown-menu-right");
+      } else {
+        $dropdown.removeClass("dropdown-menu-right");
+      }
+    });
   }
 
   return { init: init, showLoadingOverlay: showLoadingOverlay };
