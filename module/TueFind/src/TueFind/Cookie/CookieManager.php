2e3eb7a0d0e933f71dd69bd99e392314fd6f06ca
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-05-24 13:27:06 +0200

    session manager now using cookie lifetime from cookie manager

--- module/TueFind/src/TueFind/Cookie/CookieManager.php
+++ module/TueFind/src/TueFind/Cookie/CookieManager.php
@@ -18,15 +18,31 @@ class CookieManager extends \VuFind\Cookie\CookieManager implements \TueFind\Ser
         // Normal session lifetime only refers to session, but not to cookie.
         // So this way we will keep the session cookie even after browser is closed.
         if ($expire == 0) {
-            $persistentCookies = $this->getConfig('tuefind')->General->persistent_cookies ?? false;
-            if ($persistentCookies) {
-                $lifetime = $this->getConfig('config')->Session->lifetime ?? false;
-                if ($lifetime != false) {
-                    $expire = time() + $lifetime;
-                }
+            $expirationOverride = $this->getLifetimeOverride();
+            if ($expirationOverride != false) {
+                $expire = time() + $expirationOverride;
             }
         }
 
         return parent::setGlobalCookie($key, $value, $expire, $httpOnly);
+    }
+
+    /**
+     * If persistent cookies are enabled in config, return Session->lifetime, else false.
+     *
+     * @return mixed
+     */
+    public function getLifetimeOverride() {
+        $persistentCookies = $this->getConfig('tuefind')->General->persistent_cookies ?? false;
+        if (!$persistentCookies) {
+            return false;
+        }
+
+        $lifetime = $this->getConfig('config')->Session->lifetime ?? false;
+        if (!$lifetime) {
+            return false;
+        }
+
+        return intval($lifetime);
     }
 }
--- module/TueFind/src/TueFind/Session/ManagerFactory.php
+++ module/TueFind/src/TueFind/Session/ManagerFactory.php
@@ -19,13 +19,10 @@ class ManagerFactory extends \VuFind\Session\ManagerFactory {
         // Make cookies persistent.
         // Normal session lifetime only refers to session, but not to cookie.
         // So this way we will keep the session cookie even after browser is closed.
-        $configManager = $container->get('VuFind\Config\PluginManager');
-        $persistentCookies = $configManager->get('tuefind')->General->persistent_cookies ?? false;
-        if ($persistentCookies) {
-            $lifetime = $configManager->get('config')->Session->lifetime ?? false;
-            if ($lifetime != false) {
-                $options['cookie_lifetime'] = $lifetime;
-            }
+        $cookieManager = $container->get('VuFind\Cookie\CookieManager');
+        $lifetimeOverride = $cookieManager->getLifetimeOverride();
+        if ($lifetimeOverride != false) {
+            $options['cookie_lifetime'] = $lifetimeOverride;
         }
 
         return $options;
