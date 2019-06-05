c709d27024c4943775f1dc795f0592453ead551c
Demian Katz <demian.katz@villanova.edu>
2019-06-05 14:15:58 -0400

    Update SolrOverdrive.php

--- module/VuFind/src/VuFind/RecordDriver/SolrOverdrive.php
+++ module/VuFind/src/VuFind/RecordDriver/SolrOverdrive.php
@@ -4,8 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) Villanova University 2010.
- * Copyright (C) The National Library of Finland 2015.
+ * Copyright (C) Villanova University 2019.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
