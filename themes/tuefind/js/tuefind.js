dc93a91d732ca3e33ffaccfcd6ca506b25638cd0
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-06-06 13:56:06 +0200

    tuefind.js: alphabetical method sort order

--- themes/tuefind/js/tuefind.js
+++ themes/tuefind/js/tuefind.js
@@ -1,15 +1,4 @@
 var TueFind = {
-    // helper function to set focus on a specified input field, also sets cursor position to end of field content
-    SetFocus: function(input_selector) {
-        $(input_selector).focus();
-        if ($(input_selector).length) {
-           // now we are sure that element exists
-           // don't assign input_field earlier, JS might crash if element doesnt exist
-           var input_field = $(input_selector);
-           input_field[0].setSelectionRange(input_field.val().length, input_field.val().length);
-        }
-    },
-
     // helper function to add content anchors to content page links
     AddContentAnchors: function() {
         $('a').each(function() {
@@ -20,40 +9,6 @@ var TueFind = {
                 }
             }
         });
-    },
-
-    // function to register onload handler function
-    // multiple handlers can be chained, if necessary
-    RegisterOnLoad: function(functionOnLoad) {
-        if(window.attachEvent) {
-            window.attachEvent('onload', functionOnLoad);
-        } else {
-            if(window.onload) {
-                var currentOnLoad = window.onload;
-                var newOnLoad = function(evt) {
-                    currentOnLoad(evt);
-                    functionOnLoad(evt);
-                };
-                window.onload = newOnLoad;
-            } else {
-                window.onload = functionOnLoad;
-            }
-        }
-    },
-
-    // onload handler for tuefind
-    OnLoad: function() {
-        // advanced search: set focus on first input field of first search group
-        if (window.location.href.match(/\/Search\/Advanced/i)) {
-            TueFind.SetFocus('#search_lookfor0_0');
-        // keywordchainsearch: set focus on 2nd input field
-        } else if (window.location.href.match(/\/Keywordchainsearch\//i)) {
-            TueFind.SetFocus('#kwc_input');
-        // alphabrowse: set focus on "starting from" edit field
-        } else if (window.location.href.match(/\/Alphabrowse\//i)) {
-            TueFind.SetFocus('#alphaBrowseForm_from');
-        }
-        TueFind.AddContentAnchors();
     },
 
     GetFulltextSnippets: function(url, doc_id, query) {
@@ -149,6 +104,51 @@ var TueFind = {
                 }
             }
         }); // end ajax
+    },
+
+    // onload handler for tuefind
+    OnLoad: function() {
+        // advanced search: set focus on first input field of first search group
+        if (window.location.href.match(/\/Search\/Advanced/i)) {
+            TueFind.SetFocus('#search_lookfor0_0');
+        // keywordchainsearch: set focus on 2nd input field
+        } else if (window.location.href.match(/\/Keywordchainsearch\//i)) {
+            TueFind.SetFocus('#kwc_input');
+        // alphabrowse: set focus on "starting from" edit field
+        } else if (window.location.href.match(/\/Alphabrowse\//i)) {
+            TueFind.SetFocus('#alphaBrowseForm_from');
+        }
+        TueFind.AddContentAnchors();
+    },
+
+    // function to register onload handler function
+    // multiple handlers can be chained, if necessary
+    RegisterOnLoad: function(functionOnLoad) {
+        if(window.attachEvent) {
+            window.attachEvent('onload', functionOnLoad);
+        } else {
+            if(window.onload) {
+                var currentOnLoad = window.onload;
+                var newOnLoad = function(evt) {
+                    currentOnLoad(evt);
+                    functionOnLoad(evt);
+                };
+                window.onload = newOnLoad;
+            } else {
+                window.onload = functionOnLoad;
+            }
+        }
+    },
+
+    // helper function to set focus on a specified input field, also sets cursor position to end of field content
+    SetFocus: function(input_selector) {
+        $(input_selector).focus();
+        if ($(input_selector).length) {
+           // now we are sure that element exists
+           // don't assign input_field earlier, JS might crash if element doesnt exist
+           var input_field = $(input_selector);
+           input_field[0].setSelectionRange(input_field.val().length, input_field.val().length);
+        }
     }
 };
 
@@ -156,7 +156,7 @@ var TueFind = {
 TueFind.RegisterOnLoad(TueFind.OnLoad);
 
 
-$(document).ready(function () {
+$(document).ready(function() {
     // Make sure that a selection of the search handler is transparently adjusted for a potential reload
     // i.e. when changing the sort order
     var saved_search_handler = sessionStorage.getItem("tuefind_saved_search_handler");
