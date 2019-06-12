e23a29bf7c36422c863784ca100ea8cf7a1e345d
Demian Katz <demian.katz@villanova.edu>
2019-06-12 14:14:44 -0400

    Fix database upgrader mediumtext support.

--- module/VuFind/sql/mysql.sql
+++ module/VuFind/sql/mysql.sql
@@ -171,7 +171,7 @@ CREATE TABLE `external_session` (
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shortlinks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` MEDIUMTEXT NOT NULL,
+  `path` mediumtext NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
