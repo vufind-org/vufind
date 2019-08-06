182433977d27e760bbac63d69817d6a36356a217
Ere Maijala <ere.maijala@helsinki.fi>
2019-08-06 16:00:08 +0300

    AxiellWebServices: fix getHolding method signature.

--- module/Finna/src/Finna/ILS/Driver/AxiellWebServices.php
+++ module/Finna/src/Finna/ILS/Driver/AxiellWebServices.php
@@ -845,16 +845,18 @@ class AxiellWebServices extends \VuFind\ILS\Driver\AbstractBase
      * This is responsible for retrieving the holding information of a certain
      * record.
      *
-     * @param string $id     The record id to retrieve the holdings for
-     * @param array  $patron Patron data
+     * @param string $id      The record id to retrieve the holdings for
+     * @param array  $patron  Patron data
+     * @param array  $options Extra options
      *
-     * @throws DateException
-     * @throws ILSException
+     * @throws \VuFind\Exception\ILS
      * @return array         On success, an associative array with the following
      * keys: id, availability (boolean), status, location, reserve, callnumber,
      * duedate, number, barcode.
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function getHolding($id, array $patron = null)
+    public function getHolding($id, array $patron = null, array $options = [])
     {
         $function = 'GetHoldings';
         $functionResult = 'GetHoldingResult';
