379a5a81ebb0a5b8352ade85218d3c130b7887ab
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-08-12 15:29:37 +0200

    JOP: minor display optimization

--- themes/tuefind/js/tuefind.js
+++ themes/tuefind/js/tuefind.js
@@ -115,7 +115,7 @@ var TueFind = {
                     $(xml).find('Result').each(function (index, value) {
                         var state = $(this).attr("state");
                         if (state >= 0 && state <= 3) {
-                            $('#' + jop_icons_id).removeAttr("style").append('<span style="float:right";>' + title + '</span>');
+                            $('#' + jop_icons_id).removeAttr("style").append('<span>' + title + '</span>');
                             var accessURL = $(value).find('AccessURL').first().text();
                             if (accessURL) {
                                 if (filter[accessURL] != 1) {
@@ -141,7 +141,7 @@ var TueFind = {
                         } else if (state == 4 || state == 10) {
                             if (replacement == "") {
                                 replacement = '<a href="' + url_html + '" target="_blank">' +
-                                        part_img + check_availability_text + '</a>' + '<span style="float:right";>' + title + '</span>';
+                                        part_img + check_availability_text + '</a>' + '<span>' + title + '</span>';
                                 // We get an 1x1 pixel gif from JOP that can be seen as an empty line
                                 // => remove it
                                 $("#" + jop_icons_id).remove();
