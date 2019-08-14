32e93f10015d6ba2bf2760000b245dc00ef6988f
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-08-14 10:15:26 +0300

    Close lightbox on status code 204 (no content).

--- themes/bootstrap3/js/lightbox.js
+++ themes/bootstrap3/js/lightbox.js
@@ -190,6 +190,10 @@ VuFind.register('lightbox', function Lightbox() {
           VuFind.refreshPage();
           return;
         }
+        if (jq_xhr.status === 204) {
+          // No content, close lightbox
+          close();
+        }
         render(content);
       })
       .fail(function lbAjaxFail(deferred, errorType, msg) {
