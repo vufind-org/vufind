274db073d0373ea38356ab82725217a0e0d4d6e7
Demian Katz <demian.katz@villanova.edu>
2019-08-29 13:20:16 -0400

    Add support for email address changes. (#1401)

--- module/VuFind/src/VuFind/Auth/CAS.php
+++ module/VuFind/src/VuFind/Auth/CAS.php
@@ -142,7 +142,9 @@ class CAS extends AbstractBase
         foreach ($attribsToCheck as $attribute) {
             if (isset($cas->$attribute)) {
                 $value = $casauth->getAttribute($cas->$attribute);
-                if ($attribute != 'cat_password') {
+                if ($attribute == 'email') {
+                    $user->updateEmail($value);
+                } elseif ($attribute != 'cat_password') {
                     $user->$attribute = ($value === null) ? '' : $value;
                 } else {
                     $catPassword = $value;
