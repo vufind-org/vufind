d37117568aa9d274bebefa4dee82090dd264a84b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-09-27 13:15:09 +0200

    support special mail receiver configuration in local_overrides for AcquisitionRequest form (acquisition_request_receivers), see #894

--- module/TueFind/src/TueFind/Form/Form.php
+++ module/TueFind/src/TueFind/Form/Form.php
@@ -7,6 +7,9 @@ use VuFind\Config\YamlReader;
 class Form extends \VuFind\Form\Form {
     public $defaultSiteConfig;
 
+    // to map a form id to its (optional existing) config key in local overrides
+    protected $emailReceiverLocalOverridesConfigKeys = ['AcquisitionRequest' => 'acquisition_request_receivers'];
+
     public function __construct(YamlReader $yamlReader, array $defaultFormConfig = null, array $defaultSiteConfig = null)
     {
         parent::__construct($yamlReader, $defaultFormConfig);
@@ -17,8 +20,18 @@ class Form extends \VuFind\Form\Form {
     {
         $recipient = $this->formConfig['recipient'] ?? null;
 
-        // TueFind: use Site email as fallback (local_overrides)
-        $recipientEmail = $recipient['email']
+        $recipientEmail = $recipient['email'] ?? null;
+
+        // TueFind: local overrides / special forms
+        $formId = $this->formConfig['id'];
+        if (!isset($recipientEmail) && isset($this->emailReceiverLocalOverridesConfigKeys[$formId])) {
+            $configKey = $this->emailReceiverLocalOverridesConfigKeys[$formId];
+            if (isset($this->defaultSiteConfig[$configKey]))
+                $recipientEmail = $this->defaultSiteConfig[$configKey];
+        }
+
+        // TueFind: local overrides / general email address
+        $recipientEmail = $recipientEmail
             ?? $this->defaultFormConfig['recipient_email'] ?? $this->defaultSiteConfig['email'] ?? null;
 
         $recipientName = $recipient['name']
