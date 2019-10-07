e449a9762a3c8732247673e3d8b21005dad99880
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-10-07 13:13:59 +0200

    authority: added ISNI, changed LCCN & changed order of appearance for external links

--- module/TueFind/src/TueFind/RecordDriver/SolrAuthDefault.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrAuthDefault.php
@@ -3,6 +3,20 @@
 namespace TueFind\RecordDriver;
 
 class SolrAuthDefault extends \VuFind\RecordDriver\SolrAuthMarc {
+    public function getISNI() {
+        return $this->fields['isni'] ?? null;
+    }
+
+    /**
+     * Overwrite VuFind's standard lccn mechanism.
+     * => Take return value directly from Solr.
+     * => Also, VuFind defines lccn as a "multiple" field.
+     *    We only return the first value instead of an array.
+     */
+    public function getLCCN() {
+        return $this->fields['lccn'][0] ?? null;
+    }
+
     public function getORCID() {
         return $this->fields['orcid'] ?? null;
     }
