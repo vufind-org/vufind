894eac18cbc4c606ea625cae631723f20941dad3
Ere Maijala <ere.maijala@helsinki.fi>
2019-10-08 09:58:17 +0300

    Fix test.

--- module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Primo/ConnectorTest.php
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Primo/ConnectorTest.php
@@ -81,10 +81,7 @@ class ConnectorTest extends TestCase
         $terms = [];
         $result = $conn->query('dummyinst', $terms);
         $this->assertEquals(0, $result['recordCount']);
-        $this->assertEquals(
-            'An empty query is not allowed with the current search target',
-            $result['error']
-        );
+        $this->assertEquals('empty_search_disallowed', $result['error']);
     }
 
     /**
