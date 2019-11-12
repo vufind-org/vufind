bba03054d933b199fd8eb592b7b8174c2d1a1715
Josef Moravec <josef.moravec@gmail.com>
2019-11-12 09:33:41 +0100

    Fix eslint failures in requests.js

--- themes/bootstrap3/js/requests.js
+++ themes/bootstrap3/js/requests.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-  function confirmCancelRequest(link, action) {
-    $('#cancelConfirm').val(1);
-    $('#submitType').attr('name', action);
-    $(link).parents('form').submit();
-  }
-  $('#confirm_cancel_selected_yes').click(function(e) {
+function confirmCancelRequest(link, action) {
+  $('#cancelConfirm').val(1);
+  $('#submitType').attr('name', action);
+  $(link).parents('form').submit();
+}
+
+$(document).ready(function cancelRequestsHandlers() {
+  $('#confirm_cancel_selected_yes').click(function cancelSelectedRequests(e) {
     e.preventDefault();
     confirmCancelRequest(this, 'cancelSelected');
   });
-  $('#confirm_cancel_all_yes').click(function(e) {
+  $('#confirm_cancel_all_yes').click(function cancelAllRequests(e) {
     e.preventDefault();
     confirmCancelRequest(this, 'cancelAll');
   });
-  $('.confirm_cancel_no').click(function(e) {
+  $('.confirm_cancel_no').click(function doNotCancelRequest(e) {
     e.preventDefault();
   });
 });
