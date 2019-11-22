9e85d66243aa9c728f24bb93f7d7f046b38bd196
Ere Maijala <ere.maijala@helsinki.fi>
2019-11-22 13:01:36 +0200

    Fix optional OpenURL display in holdings.

--- themes/finna2/js/finna-openurl.js
+++ themes/finna2/js/finna-openurl.js
@@ -1,7 +1,8 @@
 /*global VuFind, finna */
 finna.openUrl = (function finnaOpenUrl() {
-  function initLinks() {
-    $('.openUrlEmbed a').each(function initOpenUrlEmbed(ind, e) {
+  function initLinks(_container) {
+    var container = _container || $('body');
+    $(container).find('.openUrlEmbed a').each(function initOpenUrlEmbed(ind, e) {
       $(e).one('inview', function onInViewOpenUrl() {
         VuFind.openurl.embedOpenUrlLinks($(this));
       });
