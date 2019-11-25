e3ccca46cbf4470a7bd95d9cdc479d77b392b5cc
Ere Maijala <ere.maijala@helsinki.fi>
2019-11-25 11:51:53 +0200

    Alma: Handle timestamps with milliseconds properly.

--- module/VuFind/src/VuFind/ILS/Driver/Alma.php
+++ module/VuFind/src/VuFind/ILS/Driver/Alma.php
@@ -1584,6 +1584,8 @@ class Alma extends AbstractBase implements \VuFindHttp\HttpServiceAwareInterface
         $euroPad = "/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}$/"; // e. g. 13/07/2012
         $datestamp = "/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/"; // e. g. 2012-07-13
         $timestamp = "/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$/";
+        $timestampMs
+            = "/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z$/";
         // e. g. 2017-07-09T18:00:00
 
         if ($date == null || $date == '') {
@@ -1602,6 +1604,18 @@ class Alma extends AbstractBase implements \VuFindHttp\HttpServiceAwareInterface
             if ($withTime) {
                 return $this->dateConverter->convertToDisplayDateAndTime(
                     'Y-m-d\TH:i:sT',
+                    $date
+                );
+            } else {
+                return $this->dateConverter->convertToDisplayDate(
+                    'Y-m-d',
+                    substr($date, 0, 10)
+                );
+            }
+        } elseif (preg_match($timestampMs, $date) === 1) {
+            if ($withTime) {
+                return $this->dateConverter->convertToDisplayDateAndTime(
+                    'Y-m-d\TH:i:s#???T',
                     $date
                 );
             } else {
