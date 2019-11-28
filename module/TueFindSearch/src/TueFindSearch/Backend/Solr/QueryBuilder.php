95d2bd8525e99e6d533e9774be9cbae4ce58f1a6
jriedl <johannes.riedl@uni-tuebingen.de>
2019-11-28 10:00:22 +0100

    Add dynymic search field selection depending on the handler

--- module/TueFindSearch/src/TueFindSearch/Backend/Solr/QueryBuilder.php
+++ module/TueFindSearch/src/TueFindSearch/Backend/Solr/QueryBuilder.php
@@ -4,19 +4,56 @@ namespace TueFindSearch\Backend\Solr;
 use VuFindSearch\ParamBag;
 use VuFindSearch\Query\AbstractQuery;
 use VuFindSearch\Query\Query;
-
 use VuFindSearch\Query\QueryGroup;
 
 
 class QueryBuilder extends \VuFindSearch\Backend\Solr\QueryBuilder {
+
+    const FULLTEXT_ONLY = 'FulltextOnly';
+    const FULLTEXT_WITH_SYNONYMS_HANDLER = 'FulltextWithSynonyms';
+    const FULLTEXT_ALL_SYNONYMS_HANDLER = 'FulltextAllSynonyms';
+    const FULLTEXT_ONE_LANGUAGE_SYNONYMS_FIELD = 'fulltext_synonyms'; // Language selection is done by SOLR itself
+    const FULLTEXT_ALL_LANGUAGE_SYNONYMS_FIELD = 'fulltext_synonyms_all';
     protected $createExplainQuery = false;
+
 
     public function setCreateExplainQuery($enable)
     {
         $this->createExplainQuery = $enable;
     }
 
-    public function build(AbstractQuery $query) {
+
+    protected function useSynonyms($handler)
+    {
+        switch ($handler) {
+            case self::FULLTEXT_WITH_SYNONYMS_HANDLER:
+                return true;
+            case self::FULLTEXT_ALL_SYNONYMS_HANDLER:
+                return true;
+         }
+         return false;
+    }
+
+
+    protected function getSynonymQueryField($handler)
+    {
+        switch ($handler) {
+            case self::FULLTEXT_WITH_SYNONYMS_HANDLER:
+                return self::FULLTEXT_ONE_LANGUAGE_SYNONYMS_FIELD;
+            case self::FULLTEXT_ALL_SYNONYMS_HANDLER:
+                return self::FULLTEXT_ALL_LANGUAGE_SYNONYMS_FIELD;
+        }
+        return "";
+    }
+
+
+    protected function getSynonymsPartialExpressionOrEmpty($search_handler, $query_terms) {
+       return $this->useSynonyms($search_handler) ? ' OR ' . $this->getSynonymQueryField($search_handler) . ':' . $query_terms : '';
+    }
+
+
+    public function build(AbstractQuery $query)
+    {
         $params = parent::build($query);
         if ($this->createExplainQuery) {
             $query_terms =  $this->getLuceneHelper()->extractSearchTerms($query->getAllTerms());
@@ -25,12 +62,9 @@ class QueryBuilder extends \VuFindSearch\Backend\Solr\QueryBuilder {
                                                  $query_terms : '(' . $query_terms . ')';
                 $params->set('explainOther', 'fulltext:' . $query_terms_normalized .
                                              ' OR fulltext_unstemmed:' . $query_terms_normalized .
-                                             ' OR fulltext_synonyms:' . $query_terms_normalized);
-
+                                             $this->getSynonymsPartialExpressionOrEmpty($query->getHandler(), $query_terms_normalized));
             }
         }
         return $params;
     }
-
-
 }
