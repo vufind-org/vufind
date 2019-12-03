93ff83d4a27c6de5aaa25ab6f375cfb6a5855999
Ere Maijala <ere.maijala@helsinki.fi>
2019-12-03 12:02:20 +0200

    Add max width to images in result feeds.

--- module/Finna/src/Finna/View/Helper/Root/ResultFeed.php
+++ module/Finna/src/Finna/View/Helper/Root/ResultFeed.php
@@ -5,7 +5,7 @@
  * PHP version 7
  *
  * Copyright (C) Villanova University 2010.
- * Copyright (C) The National Library of Finland 2015.
+ * Copyright (C) The National Library of Finland 2015-2019.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -118,11 +118,10 @@ class ResultFeed extends \VuFind\View\Helper\Root\ResultFeed
         if (!empty($dcDate)) {
             $entry->setDCDate($dcDate);
         }
-        $urlHelper = $this->getView()->plugin('url');
         $recordHelper = $this->getView()->plugin('record');
         $recordImage = $this->getView()->plugin('recordImage');
         $imageUrl = $recordImage($recordHelper($record))->getLargeImage()
-            . '&imgext=.jpeg';
+            . '&w=1024&imgext=.jpeg';
         $entry->setEnclosure(
             [
                 'uri' => $serverUrl($imageUrl),
