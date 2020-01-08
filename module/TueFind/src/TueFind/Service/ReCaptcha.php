58fe5617ff4f154569df4229cff6408ceee2ddb1
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-01-08 15:57:33 +0100

    TueFind: Change CAPTCHA alphabet, see #875

--- module/TueFind/src/TueFind/Service/ReCaptcha.php
+++ module/TueFind/src/TueFind/Service/ReCaptcha.php
@@ -5,6 +5,10 @@ namespace TueFind\Service;
 /* see https://docs.zendframework.com/zend-captcha/adapters/ */
 class ReCaptcha extends \Zend\Captcha\Image
 {
+    // remove "i", so users can't mistake it for an "l" due to noise
+    public static $V  = ["a", "e", "o", "u", "y"];
+    public static $VN = ["a", "e", "o", "u", "y", "2", "3", "4", "5", "6", "7", "8", "9"];
+
     public function getHtml($name = null)
     {
         $this->setFont('/usr/local/var/lib/tuelib/captcha.ttf');
