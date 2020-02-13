627e06afbcdd337be65dda44864c12dc78ddc18a
Cornelius Amzar <cornelius.amzar@bsz-bw.de>
2020-02-13 16:30:26 +0100

    Added column in mysql.sql

--- module/VuFind/sql/mysql.sql
+++ module/VuFind/sql/mysql.sql
@@ -177,10 +177,12 @@ CREATE TABLE `external_session` (
 CREATE TABLE `shortlinks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `path` mediumtext NOT NULL,
+  `hash` varchar(9) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+CREATE UNIQUE INDEX shortlinks_hash_IDX USING HASH ON shortlinks (hash);
 
 --
 -- Table structure for table `tags`
