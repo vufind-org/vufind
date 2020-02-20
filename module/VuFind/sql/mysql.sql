e03a2afa00bc187c0a46239a8093e686648ef251
Demian Katz <demian.katz@villanova.edu>
2020-02-20 12:50:12 -0500

    Factor out CREATE INDEX statement.

--- module/VuFind/sql/mysql.sql
+++ module/VuFind/sql/mysql.sql
@@ -179,10 +179,10 @@ CREATE TABLE `shortlinks` (
   `path` mediumtext NOT NULL,
   `hash` varchar(9),
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `shortlinks_hash_IDX` USING HASH (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
-CREATE UNIQUE INDEX shortlinks_hash_IDX USING HASH ON shortlinks (hash);
 
 --
 -- Table structure for table `tags`
