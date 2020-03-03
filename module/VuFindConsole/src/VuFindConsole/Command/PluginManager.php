a97fda6acebb0fd784a22ed4ddc6998c127e2dcc
Demian Katz <demian.katz@villanova.edu>
2020-03-03 12:21:39 -0500

    Fix line length issues.

--- module/VuFindConsole/src/VuFindConsole/Command/PluginManager.php
+++ module/VuFindConsole/src/VuFindConsole/Command/PluginManager.php
@@ -48,38 +48,61 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     protected $aliases = [
         'util/lint_marc' => Util\LintMarcCommand::class,
         /*
-        'compile/theme' => 'compile theme [--force] [<source>] [<target>]',
-        'generate/dynamicroute' => 'generate dynamicroute [<name>] [<newController>] [<newAction>] [<module>]',
-        'generate/extendclass' => 'generate extendclass [--extendfactory] [<class>] [<target>]',
-        'generate/extendservice' => 'generate extendservice [<source>] [<target>]',
-        'generate/nontabrecordaction' => 'generate nontabrecordaction [<newAction>] [<module>]',
+        'compile/theme' =>
+            'compile theme [--force] [<source>] [<target>]',
+        'generate/dynamicroute' =>
+            'generate dynamicroute [<name>] [<newController>]
+            [<newAction>] [<module>]',
+        'generate/extendclass' =>
+            'generate extendclass [--extendfactory] [<class>] [<target>]',
+        'generate/extendservice' =>
+            'generate extendservice [<source>] [<target>]',
+        'generate/nontabrecordaction' =>
+            'generate nontabrecordaction [<newAction>] [<module>]',
         'generate/plugin' => 'generate plugin [<class>] [<factory>]',
-        'generate/recordroute' => 'generate recordroute [<base>] [<newController>] [<module>]',
-        'generate/staticroute' => 'generate staticroute [<name>] [<module>]',
+        'generate/recordroute' =>
+            'generate recordroute [<base>] [<newController>] [<module>]',
+        'generate/staticroute' =>
+            'generate staticroute [<name>] [<module>]',
         'generate/theme' => 'generate theme [<themename>]',
         'generate/thememixin' => 'generate thememixin [<name>]',
         'harvest/harvest_oai' => 'harvest harvest_oai [...params]',
         'harvest/merge-marc' => 'harvest merge-marc [<dir>]',
-        'import/import-xsl' => 'import import-xsl [--test-only] [--index=] [<xml>] [<properties>]',
+        'import/import-xsl' =>
+            'import import-xsl [--test-only] [--index=] [<xml>] [<properties>]',
         'import/webcrawl' => 'import webcrawl [--test-only] [--index=]',
-        'language/addusingtemplate' => 'language addusingtemplate [<target>] [<template>]',
+        'language/addusingtemplate' =>
+            'language addusingtemplate [<target>] [<template>]',
         'language/copystring' => 'language copystring [<source>] [<target>]',
         'language/delete' => 'language delete [<target>]',
         'language/normalize' => 'language normalize [<target>]',
         'scheduledsearch/notify' => 'scheduledsearch notify',
-        'util/cleanup_record_cache' => 'util (cleanuprecordcache|cleanup_record_cache) [--help|-h]',
+        'util/cleanup_record_cache' =>
+            'util (cleanuprecordcache|cleanup_record_cache) [--help|-h]',
         'util/commit' => 'util commit [<core>]',
-        'util/createHierarchyTrees' => 'util createHierarchyTrees [--skip-xml|-sx] [--skip-json|-sj] [<backend>] [--help|-h]',
+        'util/createHierarchyTrees' =>
+            'util createHierarchyTrees [--skip-xml|-sx]
+            [--skip-json|-sj] [<backend>] [--help|-h]',
         'util/cssBuilder' => 'util cssBuilder [...themes]',
-        'util/deletes' => 'util deletes [--verbose] [<filename>] [<format>] [<index>]',
-        'util/expire_auth_hashes' => 'util expire_auth_hashes [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
-        'util/expire_external_sessions' => 'util expire_external_sessions [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
-        'util/expire_searches' => 'util expire_searches [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
-        'util/expire_sessions' => 'util expire_sessions [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
-        'util/index_reserves' => 'util index_reserves [--help|-h] [-d=s] [-t=s] [-f=s]',
+        'util/deletes' =>
+            'util deletes [--verbose] [<filename>] [<format>] [<index>]',
+        'util/expire_auth_hashes' =>
+            'util expire_auth_hashes [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
+        'util/expire_external_sessions' =>
+            'util expire_external_sessions [--help|-h] [--batch=] [--sleep=]
+             [<daysOld>]',
+        'util/expire_searches' =>
+            'util expire_searches [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
+        'util/expire_sessions' =>
+            'util expire_sessions [--help|-h] [--batch=] [--sleep=] [<daysOld>]',
+        'util/index_reserves' =>
+            'util index_reserves [--help|-h] [-d=s] [-t=s] [-f=s]',
         'util/optimize' => 'util optimize [<core>]',
-        'util/sitemap' => 'util sitemap [--help|-h] [--verbose] [--baseurl=s] [--basesitemapurl=s]',
-        'util/suppressed' => 'util suppressed [--help|-h] [--authorities] [--outfile=s]',
+        'util/sitemap' =>
+            'util sitemap [--help|-h] [--verbose] [--baseurl=s]
+            [--basesitemapurl=s]',
+        'util/suppressed' =>
+            'util suppressed [--help|-h] [--authorities] [--outfile=s]',
         'util/switch_db_hash' => 'util switch_db_hash [<newhash>] [<newkey>]',
         */
     ];
