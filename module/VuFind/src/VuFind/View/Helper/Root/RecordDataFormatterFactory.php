608c623671861f162842d1349c3e9f696c119d0f
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-03-03 16:29:50 +0100

    schema.org: removed language translation as requested by @demiankatz, should be addressed later in pull request vufind-org/vufind#413

--- module/VuFind/src/VuFind/View/Helper/Root/RecordDataFormatterFactory.php
+++ module/VuFind/src/VuFind/View/Helper/Root/RecordDataFormatterFactory.php
@@ -145,7 +145,7 @@ class RecordDataFormatterFactory implements FactoryInterface
             'Language', 'getLanguages', null,
             ['itemPrefix' => '<span property="availableLanguage" typeof="Language">'
                            . '<span property="name">',
-             'itemSuffix' => '</span></span>', 'translate' => true]
+             'itemSuffix' => '</span></span>']
         );
         $spec->setTemplateLine(
             'Published', 'getPublicationDetails', 'data-publicationDetails.phtml'
@@ -192,7 +192,7 @@ class RecordDataFormatterFactory implements FactoryInterface
             'Language', 'getLanguages', null,
             ['itemPrefix' => '<span property="availableLanguage" typeof="Language">'
                            . '<span property="name">',
-             'itemSuffix' => '</span></span>', 'translate' => true]
+             'itemSuffix' => '</span></span>']
         );
         $spec->setLine(
             'Format', 'getFormats', 'RecordHelper',
@@ -231,7 +231,7 @@ class RecordDataFormatterFactory implements FactoryInterface
             'Language', 'getLanguages', null,
             ['itemPrefix' => '<span property="availableLanguage" typeof="Language">'
                            . '<span property="name">',
-             'itemSuffix' => '</span></span>', 'translate' => true]
+             'itemSuffix' => '</span></span>']
         );
         $spec->setTemplateLine(
             'Published', 'getPublicationDetails', 'data-publicationDetails.phtml'
