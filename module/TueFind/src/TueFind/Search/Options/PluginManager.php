c966a3f274c19498fd7b53a096d3feaa575a7d77
jriedl <johannes.riedl@uni-tuebingen.de>
2020-03-04 11:48:11 +0100

    Disable advanced search in Search2 context

---
+++ module/TueFind/src/TueFind/Search/Options/PluginManager.php
@@ -0,0 +1,14 @@
+<?php
+  
+namespace TueFind\Search\Options;
+
+use VuFind\Search\Options\OptionsFactory; 
+
+class PluginManager extends \VuFind\Search\Options\PluginManager {
+    public function __construct($configOrContainerInstance = null,
+        array $v3config = []
+    ) {
+        $this->aliases['search2'] = \TueFind\Search\Search2\Options::class;
+        parent::__construct($configOrContainerInstance, $v3config);
+    }
+}
