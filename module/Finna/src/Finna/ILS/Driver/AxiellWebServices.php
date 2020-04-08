536480ba42c85a3038c5dad705ece52da7397470
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2020-04-08 13:47:44 +0300

    Fix aurora nonobject method check

--- module/Finna/src/Finna/ILS/Driver/AxiellWebServices.php
+++ module/Finna/src/Finna/ILS/Driver/AxiellWebServices.php
@@ -1852,11 +1852,11 @@ class AxiellWebServices extends \VuFind\ILS\Driver\AbstractBase
             $currentMethods = $sendMethods->sendMethods->sendMethod;
             $serviceType = $sendMethods->serviceType;
             foreach ($currentMethods as $key => $value) {
-                $method = $value->value;
+                $method = is_object($value) ? $value->value : $value;
                 if (in_array($method, $this->messagingBlackLists[$serviceType])) {
                     continue;
                 }
-                $current[] = $value->value;
+                $current[] = $method;
             }
             $returnable[$serviceType] = $current;
         }
