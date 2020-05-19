8befe1d167198d5123c82eb80bdc8cb2de899bcf
Josef Moravec <josef.moravec@gmail.com>
2020-05-19 18:55:10 +0200

    GetRecordCover ajax handler style fixes

--- module/VuFind/src/VuFind/AjaxHandler/GetRecordCoverFactory.php
+++ module/VuFind/src/VuFind/AjaxHandler/GetRecordCoverFactory.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Factory for GetRecordCover AJAX handler.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2018.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  AJAX
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
 namespace VuFind\AjaxHandler;
 
 use Interop\Container\ContainerInterface;
@@ -7,6 +33,15 @@ use Laminas\ServiceManager\Exception\ServiceNotCreatedException;
 use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
+/**
+ * Factory for GetRecordCover AJAX handler.
+ *
+ * @category VuFind
+ * @package  AJAX
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
 class GetRecordCoverFactory implements FactoryInterface
 {
     /**
