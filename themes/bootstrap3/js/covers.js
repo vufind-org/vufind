dc2678101bbafbe1dc1432ab0614dc3de8fe3bd4
Josef Moravec <josef.moravec@gmail.com>
2020-06-01 11:17:18 +0200

    Fix covers.js

--- themes/bootstrap3/js/covers.js
+++ themes/bootstrap3/js/covers.js
@@ -1,50 +1,44 @@
-
-function loadCoversForResults(size = "small") {
-    results = $('div.result');
-    ids = [];
-    results.each(function(index, element) {
-        ids.push({
-            elementId: $(element).attr("id"),
-            data: {
-                source: $(this).find(".hiddenSource").val(),
-                recordId: $(this).find(".hiddenId").val(),
-                size: size
-            }
-        });
-    });
-    ids.forEach(function(value) {
-        loadCover(value.data, "#" + value.elementId + " .recordcover");
-    });
-}
-
-function loadCoverForDetail(size = "small") {
-    data = {
-        source: $(".record .hiddenSource").val(),
-        recordId: $(".record .hiddenId").val(),
-        size: size
-    };
-    loadCover(data, ".record .recordcover");
-}
-
+/*global VuFind */
 function loadCover(data, selector) {
-    url = VuFind.path + '/AJAX/JSON?method=' + 'getRecordCover';
-    callback = function(response, status, xhr) {
-        console.log(response);
-        if (response.data.url !== false) {
-            $(this.elementSelector).attr("src", response.data.url);
-        }
-    };
-    $.ajax({
-        dataType: "json",
-        url: url,
-        method: "GET",
-        data: data,
-        elementSelector: selector,
-        success: callback
-    });
+  var url = VuFind.path + '/AJAX/JSON?method=' + 'getRecordCover';
+  function coverCallback(response) {
+    if (response.data.url !== false) {
+      $(this.elementSelector).attr("src", response.data.url);
+    }
+  }
+  $.ajax({
+    dataType: "json",
+    url: url,
+    method: "GET",
+    data: data,
+    elementSelector: selector,
+    success: coverCallback
+  });
 }
 
-//$(document).ready(function() {
-//    loadCoversForResults();
-//    loadCoverForDetail();
-//});
+function loadCoversForResults(size) { // eslint-disable-line no-unused-vars
+  var results = $('div.result');
+  var ids = [];
+  results.each(function addId(index, element) {
+    ids.push({
+      elementId: $(element).attr("id"),
+      data: {
+        source: $(this).find(".hiddenSource").val(),
+        recordId: $(this).find(".hiddenId").val(),
+        size: (typeof size !== 'undefined') ? size : 'small'
+      }
+    });
+  });
+  ids.forEach(function batchLoadCovers(value) {
+    loadCover(value.data, "#" + value.elementId + " .recordcover");
+  });
+}
+
+function loadCoverForDetail(size) { // eslint-disable-line no-unused-vars
+  var data = {
+    source: $(".record .hiddenSource").val(),
+    recordId: $(".record .hiddenId").val(),
+    size: (typeof size !== 'undefined') ? size : 'small'
+  };
+  loadCover(data, ".record .recordcover");
+}
