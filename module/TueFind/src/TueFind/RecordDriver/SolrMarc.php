8eb59fc8644be25d5cbfa8de67c804c787d0c4c7
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2020-06-02 15:06:10 +0200

    Attempt to fix tuefind issue #1158.

--- module/TueFind/src/TueFind/RecordDriver/SolrMarc.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrMarc.php
@@ -297,7 +297,7 @@ class SolrMarc extends SolrDefault
 
     public function getContainsInformation(): array {
         $contains = [];
-        $fields = $this->getMarcRecord()->getFields('772');
+        $fields = $this->getMarcRecord()->getFields('772|773');
         foreach ($fields as $field) {
             $opening = $field->getSubfield('i') ? $field->getSubfield('i')->getData() : '';
             $titles = [];
