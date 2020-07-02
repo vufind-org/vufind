f3292cc24b29d88ead4174112d8d1d08f24538b0
Demian Katz <demian.katz@villanova.edu>
2020-07-02 16:21:27 -0400

    Add validation and test logic to match Java code.

--- module/VuFind/src/VuFind/XSLT/Import/VuFindGeo.php
+++ module/VuFind/src/VuFind/XSLT/Import/VuFindGeo.php
@@ -39,6 +39,25 @@ namespace VuFind\XSLT\Import;
 class VuFindGeo
 {
     /**
+     * Method for logging errors (overrideable for testing purposes)
+     *
+     * @var \Callable
+     */
+    public static $logMethod = 'error_log';
+
+    /**
+     * Log an error message
+     *
+     * @param string $msg Message
+     *
+     * @return void
+     */
+    protected static function logError($msg)
+    {
+        call_user_func(static::$logMethod, $msg);
+    }
+
+    /**
      * Parse a dc:coverage string into a key/value array.
      *
      * @param string $coverage Raw dc:coverage string.
@@ -60,6 +79,145 @@ class VuFindGeo
     }
 
     /**
+     * Return true if the coordinate set is complete and numeric.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateNumericCoordinates($coords)
+    {
+        if (!is_numeric($coords['westlimit'] ?? 'NaN')
+            || !is_numeric($coords['eastlimit'] ?? 'NaN')
+            || !is_numeric($coords['northlimit'] ?? 'NaN')
+            || !is_numeric($coords['southlimit'] ?? 'NaN')
+        ) {
+            static::logError('Missing or non-numeric coordinate value.');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check decimal degree coordinates to make sure they do not form a line at the
+     * poles.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateLines($coords)
+    {
+        if ($coords['westlimit'] != $coords['eastlimit']
+            && $coords['northlimit'] == $coords['southlimit']
+            && abs($coords['northlimit']) == 90
+        ) {
+            static::logError('Coordinates form a line at the pole');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check decimal degree coordinates to make sure they are within map extent.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateExtent($coords)
+    {
+        if (abs($coords['northlimit']) > 90
+            || abs($coords['southlimit']) > 90
+            || abs($coords['eastlimit']) > 180
+            || abs($coords['westlimit']) > 180
+        ) {
+            static::logError('Coordinates exceed map extent.');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check decimal degree coordinates to make sure that north is not less than
+     * south.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateNorthSouth($coords)
+    {
+        if ($coords['northlimit'] < $coords['southlimit']) {
+            static::logError('North < South.');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check decimal degree coordinates to make sure that east is not less than west.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateEastWest($coords)
+    {
+        $east = $coords['eastlimit'];
+        $west = $coords['westlimit'];
+        if ($east < $west) {
+            // Convert to 360 degree grid
+            if ($east <= 0) {
+                $east += 360;
+            }
+            if ($west < 0) {
+                $west += 360;
+            }
+            // Check again
+            if ($east < $west) {
+                static::logError('East < West.');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check decimal degree coordinates to make sure they are not too close.
+     * Coordinates too close will cause Solr to run out of memory during indexing.
+     *
+     * @param array $coords Output of parseCoverage() in need of validation
+     *
+     * @return bool
+     */
+    protected static function validateCoordinateDistance($coords)
+    {
+        $distEW = $coords['eastlimit'] - $coords['westlimit'];
+        $distNS = $coords['northlimit'] - $coords['southlimit'];
+        if (($coords['northlimit'] == -90 || $coords['southlimit'] == -90)
+            && ($distNS > 0 && $distNS < 0.167)
+        ) {
+            static::logError(
+                'Coordinates < 0.167 degrees from South Pole. Coordinate Distance: '
+                . round($distNS, 2)
+            );
+            return false;
+        }
+
+        if (($coords['westlimit'] == 0 || $coords['eastlimit'] == 0)
+            && ($distEW > -2 && $distEW < 0)
+        ) {
+            static::logError(
+                'Coordinates within 2 degrees of Prime Meridian. '
+                . 'Coordinate Distance: ' . round($distEW, 2)
+            );
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * Return true if the coordinate set is valid for inclusion in VuFind's index.
      *
      * @param array $coords Output of parseCoverage() in need of validation
@@ -68,10 +226,12 @@ class VuFindGeo
      */
     protected static function validateCoverageCoordinates($coords)
     {
-        return isset($coords['westlimit'])
-            && isset($coords['eastlimit'])
-            && isset($coords['northlimit'])
-            && isset($coords['southlimit']);
+        return static::validateNumericCoordinates($coords)
+            && static::validateLines($coords)
+            && static::validateExtent($coords)
+            && static::validateNorthSouth($coords)
+            && static::validateEastWest($coords)
+            && static::validateCoordinateDistance($coords);
     }
 
     /**
