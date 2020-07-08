0658c1901777b87224f79a81b75174bdd156431e
Demian Katz <demian.katz@villanova.edu>
2020-07-08 13:53:06 -0400

    Revise whitelist/blacklist terminology.

--- module/VuFind/src/VuFind/Cover/CachingProxyFactory.php
+++ module/VuFind/src/VuFind/Cover/CachingProxyFactory.php
@@ -66,8 +66,8 @@ class CachingProxyFactory implements FactoryInterface
         $client = $container->get(\VuFindHttp\HttpService::class)->createClient();
         $config = $container->get(\VuFind\Config\PluginManager::class)->get('config')
             ->toArray();
-        $whitelist = isset($config['Content']['coverproxyCache'])
+        $allowedHosts = isset($config['Content']['coverproxyCache'])
             ? (array)$config['Content']['coverproxyCache'] : [];
-        return new $requestedName($client, $cacheDir . '/proxy', $whitelist);
+        return new $requestedName($client, $cacheDir . '/proxy', $allowedHosts);
     }
 }
