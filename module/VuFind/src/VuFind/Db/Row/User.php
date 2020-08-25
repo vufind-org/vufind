vufind-org/vufind

commit 2b05cef5c7f3041444ad415935f482e8b3cacfee

Add support for tagging of user lists. - Deprecates VuFind\Db\Row\UserList::getTags() in favor of VuFind\Db\Row\UserList::getResourceTags() - Deprecates VuFind\Db\Table\ResourceTags::destroyLinks() in favor of VuFind\Db\Table\ResourceTags::destroyResourceLinks() - Includes code originally developed as part of pull request #1645.

module/VuFind/src/VuFind/Db/Row/User.php
@@ -275,6 +275,19 @@ class User extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterface,
     }
 
     /**
+     * Get tags assigned by the user to a favorite list.
+     *
+     * @param int $listId List id
+     *
+     * @return \Laminas\Db\ResultSet\AbstractResultSet
+     */
+    public function getListTags($listId)
+    {
+        return $this->getDbTable('Tags')
+            ->getForList($listId, $this->id);
+    }
+
+    /**
      * Same as getTags(), but returns a string for use in edit mode rather than an
      * array of tag objects.
      *
@@ -289,14 +302,25 @@ class User extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterface,
      */
     public function getTagString($resourceId = null, $listId = null, $source = null)
     {
-        $myTagList = $this->getTags($resourceId, $listId, $source);
+        return $this->formatTagString($this->getTags($resourceId, $listId, $source));
+    }
+
+    /**
+     * Same as getTagString(), but operates on a list of tags.
+     *
+     * @param array $tags Tags
+     *
+     * @return string
+     */
+    public function formatTagString($tags)
+    {
         $tagStr = '';
-        if (count($myTagList) > 0) {
-            foreach ($myTagList as $myTag) {
-                if (strstr($myTag->tag, ' ')) {
-                    $tagStr .= "\"$myTag->tag\" ";
+        if (count($tags) > 0) {
+            foreach ($tags as $tag) {
+                if (strstr($tag->tag, ' ')) {
+                    $tagStr .= "\"$tag->tag\" ";
                 } else {
-                    $tagStr .= "$myTag->tag ";
+                    $tagStr .= "$tag->tag ";
                 }
             }
         }
@@ -412,7 +436,7 @@ class User extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterface,
         // Remove Resource (related tags are also removed implicitly)
         $userResourceTable = $this->getDbTable('UserResource');
         // true here makes sure that only tags in lists are deleted
-        $userResourceTable->destroyLinks($resourceIDs, $this->id, true);
+        $userResourceTable->destroyResourceLinks($resourceIDs, $this->id, true);
     }
 
     /**
@@ -656,7 +680,7 @@ class User extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterface,
             $list->delete($this, true);
         }
         $resourceTags = $this->getDbTable('ResourceTags');
-        $resourceTags->destroyLinks(null, $this->id);
+        $resourceTags->destroyResourceLinks(null, $this->id);
         if ($removeComments) {
             $comments = $this->getDbTable('Comments');
             $comments->deleteByUser($this);

module/VuFind/src/VuFind/Db/Row/UserList.php
@@ -30,6 +30,7 @@ namespace VuFind\Db\Row;
 use Laminas\Session\Container;
 use VuFind\Exception\ListPermission as ListPermissionException;
 use VuFind\Exception\MissingField as MissingFieldException;
+use VuFind\Tags;
 
 /**
  * Row Definition for user_list
@@ -52,13 +53,22 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
     protected $session = null;
 
     /**
+     * Tag parser.
+     *
+     * @var Tags
+     */
+    protected $tagParser;
+
+    /**
      * Constructor
      *
-     * @param \Laminas\Db\Adapter\Adapter $adapter Database adapter
-     * @param Container                   $session Session container
+     * @param \Laminas\Db\Adapter\Adapter $adapter   Database adapter
+     * @param Tags                        $tagParser Tag parser
+     * @param Container                   $session   Session container
      */
-    public function __construct($adapter, Container $session = null)
+    public function __construct($adapter, Tags $tagParser, Container $session = null)
     {
+        $this->tagParser = $tagParser;
         $this->session = $session;
         parent::__construct('id', 'user_list', $adapter);
     }
@@ -79,11 +89,11 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
     }
 
     /**
-     * Get an array of tags associated with this list.
+     * Get an array of resource tags associated with this list.
      *
      * @return array
      */
-    public function getTags()
+    public function getResourceTags()
     {
         $table = $this->getDbTable('User');
         $user = $table->select(['id' => $this->user_id])->current();
@@ -91,6 +101,33 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
             return [];
         }
         return $user->getTags(null, $this->id);
+    }
+
+    /**
+     * Get an array of resource tags associated with this list.
+     *
+     * @deprecated Deprecated, use getResourceTags.
+     *
+     * @return array
+     */
+    public function getTags()
+    {
+        return $this->getResourceTags();
+    }
+
+    /**
+     * Get an array of tags assigned to this list.
+     *
+     * @return array
+     */
+    public function getListTags()
+    {
+        $table = $this->getDbTable('User');
+        $user = $table->select(['id' => $this->user_id])->current();
+        if (empty($user)) {
+            return [];
+        }
+        return $user->getListTags($this->id, $this->user_id);
     }
 
     /**
@@ -110,7 +147,37 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
         $this->description = $request->get('desc');
         $this->public = $request->get('public');
         $this->save($user);
+
+        if (null !== ($tags = $request->get('tags'))) {
+            $linker = $this->getDbTable('resourcetags');
+            $linker->destroyListLinks($this->id, $user->id);
+            foreach ($this->tagParser->parse($tags) as $tag) {
+                $this->addListTag($tag, $user);
+            }
+        }
+
         return $this->id;
+    }
+
+    /**
+     * Add a tag to the list.
+     *
+     * @param string              $tagText The tag to save.
+     * @param \VuFind\Db\Row\User $user    The user posting the tag.
+     *
+     * @return void
+     */
+    public function addListTag($tagText, $user)
+    {
+        $tagText = trim($tagText);
+        if (!empty($tagText)) {
+            $tags = $this->getDbTable('tags');
+            $tag = $tags->getByText($tagText);
+            $linker = $this->getDbTable('resourcetags');
+            $linker->createLink(
+                null, $tag->id, $user->id, $this->id
+            );
+        }
     }
 
     /**
@@ -192,7 +259,9 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
 
         // Remove Resource (related tags are also removed implicitly)
         $userResourceTable = $this->getDbTable('UserResource');
-        $userResourceTable->destroyLinks($resourceIDs, $this->user_id, $this->id);
+        $userResourceTable->destroyResourceLinks(
+            $resourceIDs, $this->user_id, $this->id
+        );
     }
 
     /**
@@ -223,6 +292,10 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
         // Remove user_resource and resource_tags rows:
         $userResource = $this->getDbTable('UserResource');
         $userResource->destroyLinks(null, $this->user_id, $this->id);
+
+        // Remove resource_tags rows for list tags:
+        $linker = $this->getDbTable('resourcetags');
+        $linker->destroyListLinks($this->id, $user->id);
 
         // Remove the list itself:
         return parent::delete();

config/vufind/config.ini
@@ -1984,6 +1984,8 @@ lists_view=full
 ; Tags may be "enabled" or "disabled" (default = "enabled")
 ; When disabling tags, don't forget to also turn off tag search in searches.ini.
 tags = enabled
+; User list tags may be "enabled" or "disabled" (default = "disabled")
+listTags = disabled
 ; This controls the maximum length of a single tag; it should correspond with the
 ; field size in the tags database table.
 max_tag_length = 64

module/VuFind/sql/migrations/pgsql/7.1/001-allow-list-tags.sql
@@ -1,0 +1,9 @@
+--
+-- Modifications to table `resource_tags`
+--
+
+ALTER TABLE "resource_tags"
+   ALTER COLUMN resource_id DROP NOT NULL;
+
+ALTER TABLE "resource_tags"
+   ALTER COLUMN resource_id SET DEFAULT NULL;

module/VuFind/sql/mysql.sql
@@ -88,7 +88,7 @@ CREATE TABLE `resource` (
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `resource_tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `resource_id` int(11) NOT NULL DEFAULT '0',
+  `resource_id` int(11) DEFAULT NULL,
   `tag_id` int(11) NOT NULL DEFAULT '0',
   `list_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,

module/VuFind/sql/pgsql.sql
@@ -47,7 +47,7 @@ DROP TABLE IF EXISTS "resource_tags";
 
 CREATE TABLE resource_tags (
 id SERIAL,
-resource_id int NOT NULL DEFAULT '0',
+resource_id int DEFAULT NULL,
 tag_id int NOT NULL DEFAULT '0',
 list_id int DEFAULT NULL,
 user_id int DEFAULT NULL,

module/VuFind/src/VuFind/Config/AccountCapabilities.php
@@ -137,6 +137,19 @@ class AccountCapabilities
     }
 
     /**
+     * Get list tag setting.
+     *
+     * @return string
+     */
+    public function getListTagSetting()
+    {
+        if (!$this->isAccountAvailable()) {
+            return 'disabled';
+        }
+        return $this->config->Social->listTags ?? 'disabled';
+    }
+
+    /**
      * Get SMS setting ('enabled' or 'disabled').
      *
      * @return string

module/VuFind/src/VuFind/Controller/BrowseController.php
@@ -345,9 +345,14 @@ class BrowseController extends AbstractBase
                 }
             } else {
                 // Default case: always display tag list for non-alphabetical modes:
+                $callback = function ($select) {
+                    // Discard user list tags
+                    $select->where->isNotNull('resource_tags.resource_id');
+                };
+
                 $tagList = $tagTable->getTagList(
                     $params['findby'],
-                    $this->config->Browse->result_limit
+                    $this->config->Browse->result_limit, $callback
                 );
                 $resultList = [];
                 foreach ($tagList as $i => $tag) {

commit a539ae34a00290e6d3115281186048aa51c2bfcb

Fix deleting favorite from userlist.
Fixes a bug introduced in 2b05cef5c7f3041444ad415935f482e8b3cacfee

module/VuFind/src/VuFind/Db/Row/User.php
@@ -436,7 +436,7 @@ class User extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterface,
         // Remove Resource (related tags are also removed implicitly)
         $userResourceTable = $this->getDbTable('UserResource');
         // true here makes sure that only tags in lists are deleted
-        $userResourceTable->destroyResourceLinks($resourceIDs, $this->id, true);
+        $userResourceTable->destroyLinks($resourceIDs, $this->id, true);
     }
 
     /**

module/VuFind/src/VuFind/Db/Row/UserList.php
@@ -259,7 +259,7 @@ class UserList extends RowGateway implements \VuFind\Db\Table\DbTableAwareInterf
 
         // Remove Resource (related tags are also removed implicitly)
         $userResourceTable = $this->getDbTable('UserResource');
-        $userResourceTable->destroyResourceLinks(
+        $userResourceTable->destroyLinks(
             $resourceIDs, $this->user_id, $this->id
         );
     }