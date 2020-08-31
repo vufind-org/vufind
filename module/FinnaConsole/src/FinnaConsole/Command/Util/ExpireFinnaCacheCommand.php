bd589925a41a7af608d387758cf6832be469400c
Ere Maijala <ere.maijala@helsinki.fi>
2020-08-31 10:53:55 +0300

    Fix Finna cache record expiration.

---
+++ module/FinnaConsole/src/FinnaConsole/Command/Util/ExpireFinnaCacheCommand.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Console command: expire Finna cache records.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2020.
+ * Copyright (C) The National Library of Finland 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Console
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+namespace FinnaConsole\Command\Util;
+
+/**
+ * Console command: expire Finna cache records.
+ *
+ * @category VuFind
+ * @package  Console
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+class ExpireFinnaCacheCommand
+    extends \VuFindConsole\Command\Util\AbstractExpireCommand
+{
+    /**
+     * Help description for the command.
+     *
+     * @var string
+     */
+    protected $commandDescription = 'Database finna_cache table cleanup';
+
+    /**
+     * Label to use for rows in help messages.
+     *
+     * @var string
+     */
+    protected $rowLabel = 'Finna cache records';
+
+    /**
+     * The name of the command (the part after "public/index.php")
+     *
+     * @var string
+     */
+    protected static $defaultName = 'util/expire_finna_cache';
+}
