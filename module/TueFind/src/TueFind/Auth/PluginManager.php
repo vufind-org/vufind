7d1335242f2d8e0368f935a8ca8785aaa348c154
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-09-02 15:24:36 +0200

    IxTheo: Inherit Auth\PluginManager from TueFind module

--- module/TueFind/src/TueFind/Auth/PluginManager.php
+++ module/TueFind/src/TueFind/Auth/PluginManager.php
@@ -3,12 +3,16 @@
 namespace TueFind\Auth;
 
 class PluginManager extends \VuFind\Auth\PluginManager {
+
+    use \TueFind\PluginManagerExtensionTrait;
+
     public function __construct($configOrContainerInstance = null,
         array $v3config = []
     ) {
-        $this->aliases['database'] = Database::class;
-        $this->aliases['db'] = Database::class;
-        $this->factories[Database::class] = \Zend\ServiceManager\Factory\InvokableFactory::class;
+        $this->addOverride('aliases', 'database', Database::class);
+        $this->addOverride('aliases', 'db', Database::class);
+        $this->addOverride('factories', Database::class, \Zend\ServiceManager\Factory\InvokableFactory::class);
+        $this->applyOverrides();
         parent::__construct($configOrContainerInstance, $v3config);
     }
 }
