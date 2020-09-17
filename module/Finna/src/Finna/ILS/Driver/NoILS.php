4addfbb31d68a3b7374247d90a3ef807d585f293
Ere Maijala <ere.maijala@helsinki.fi>
2020-09-17 14:10:24 +0300

    Add locationhref support to the NoILS and other changes for temporarily unavailable libraries.

---
+++ module/Finna/src/Finna/ILS/Driver/NoILS.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Driver for offline/missing ILS.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) The National Library of Finland 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  ILS_Drivers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:ils_drivers Wiki
+ */
+namespace Finna\ILS\Driver;
+
+use VuFind\Exception\ILS as ILSException;
+
+/**
+ * Driver for offline/missing ILS.
+ *
+ * @category VuFind
+ * @package  ILS_Drivers
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:ils_drivers Wiki
+ */
+class NoILS extends \VuFind\ILS\Driver\NoILS
+{
+    /**
+     * Get Status
+     *
+     * This is responsible for retrieving the status information of a certain
+     * record.
+     *
+     * @param string $id The record id to retrieve the holdings for
+     *
+     * @throws ILSException
+     * @return mixed     On success, an associative array with the following keys:
+     * id, availability (boolean), status, location, reserve, callnumber.
+     */
+    public function getStatus($id)
+    {
+        $result = parent::getStatus($id);
+
+        $useStatus = $this->config['settings']['useStatus'] ?? 'none';
+        if ($useStatus == "custom") {
+            if ($url = $this->config['Status']['locationhref'] ?? '') {
+                $result[0]['locationhref'] = $url;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get Holding
+     *
+     * This is responsible for retrieving the holding information of a certain
+     * record.
+     *
+     * @param string $id      The record id to retrieve the holdings for
+     * @param array  $patron  Patron data
+     * @param array  $options Extra options (not currently used)
+     *
+     * @throws ILSException
+     * @return array         On success, an associative array with the following
+     * keys: id, availability (boolean), status, location, reserve, callnumber,
+     * duedate, number, barcode.
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function getHolding($id, array $patron = null, array $options = [])
+    {
+        $result = parent::getHolding($id, $patron, $options);
+
+        $useHoldings = $this->config['settings']['useHoldings'] ?? 'none';
+
+        if ($useHoldings == "custom") {
+            if ($url = $this->config['Holdings']['locationhref'] ?? '') {
+                $result[0]['locationhref'] = $url;
+            }
+        }
+
+        return $result;
+    }
+}
