9f0122e2a70c25f6d129386715284de0f33dddf4
Ere Maijala <ere.maijala@helsinki.fi>
2020-09-25 20:39:30 +0300

    Move record driver logic into a feature trait.

--- module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
+++ module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
@@ -45,9 +45,10 @@ namespace VuFind\RecordDriver;
  *
  * @SuppressWarnings(PHPMD.ExcessivePublicCount)
  */
-class SolrDefault extends DefaultRecord
+class SolrDefault extends DefaultRecord implements Feature\VersionAwareInterface
 {
     use HierarchyAwareTrait;
+    use Feature\VersionAwareTrait;
 
     /**
      * These Solr fields should be used for snippets if available (listed in order
@@ -319,67 +320,5 @@ class SolrDefault extends DefaultRecord
     public function getWorkKeys()
     {
         return $this->fields['work_keys_str_mv'] ?? [];
-    }
-
-    /**
-     * Return count of other versions available
-     *
-     * @return int
-     */
-    public function getOtherVersionCount()
-    {
-        if (null === $this->searchService) {
-            return false;
-        }
-
-        if (!($workKeys = $this->getWorkKeys())) {
-            return false;
-        }
-
-        if (!isset($this->otherVersionsCount)) {
-            $params = new \VuFindSearch\ParamBag();
-            $params->add('rows', 0);
-            $results = $this->searchService->workExpressions(
-                $this->getSourceIdentifier(),
-                $this->getUniqueID(),
-                $workKeys,
-                $params
-            );
-            $this->otherVersionsCount = $results->getTotal();
-        }
-        return $this->otherVersionsCount;
-    }
-
-    /**
-     * Retrieve versions as a search result
-     *
-     * @param bool $includeSelf Whether to include this record
-     * @param int  $count       Maximum number of records to display
-     * @param int  $offset      Start position (0-based)
-     *
-     * @return \VuFindSearch\Response\RecordCollectionInterface
-     */
-    public function getVersions($includeSelf = false, $count = 20, $offset = 0)
-    {
-        if (null === $this->searchService) {
-            return false;
-        }
-
-        if (!($workKeys = $this->getWorkKeys())) {
-            return false;
-        }
-
-        if (!isset($this->otherVersions)) {
-            $params = new \VuFindSearch\ParamBag();
-            $params->add('rows', $count);
-            $params->add('start', $offset);
-            $this->otherVersions = $this->searchService->workExpressions(
-                $this->getSourceIdentifier(),
-                $includeSelf ? '' : $this->getUniqueID(),
-                $workKeys,
-                $params
-            );
-        }
-        return $this->otherVersions;
     }
 }
