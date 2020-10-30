dc2658e0557aef85f5afbf8b19cd645876920c74
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-10-30 11:18:33 +0200

    R2: Connector updates (#1739)

--- module/FinnaSearch/src/FinnaSearch/Backend/R2/Connector.php
+++ module/FinnaSearch/src/FinnaSearch/Backend/R2/Connector.php
@@ -28,6 +28,7 @@
  */
 namespace FinnaSearch\Backend\R2;
 
+use Laminas\EventManager\EventManager;
 use Laminas\Http\Client as HttpClient;
 use VuFindSearch\Backend\Exception\HttpErrorException;
 
@@ -44,6 +45,27 @@ use VuFindSearch\ParamBag;
  */
 class Connector extends \VuFindSearch\Backend\Solr\Connector
 {
+    /**
+     * R2 daily request limit exceeded.
+     *
+     * @var string
+     */
+    const EVENT_DAILY_REQUEST_LIMIT_EXCEEDED = 'daily-request-limit-exceeded';
+
+    /**
+     * R2 monthly request limit exceeded.
+     *
+     * @var string
+     */
+    const EVENT_MONTHLY_REQUEST_LIMIT_EXCEEDED = 'monthly-request-limit-exceeded';
+
+    /**
+     * REMS session expired.
+     *
+     * @var string
+     */
+    const EVENT_REMS_SESSION_EXPIRED = 'rems-session-expired';
+
     /**
      * Username
      *
@@ -98,6 +120,13 @@ class Connector extends \VuFindSearch\Backend\Solr\Connector
     protected $httpConfig;
 
     /**
+     * Event manager
+     *
+     * @var EventManager
+     */
+    protected $events = null;
+
+    /**
      * Set API user and password for authentication to index.
      *
      * @param string $user     User
@@ -133,6 +162,19 @@ class Connector extends \VuFindSearch\Backend\Solr\Connector
     public function setRems($rems)
     {
         $this->rems = $rems;
+    }
+
+    /**
+     * Set event manager.
+     *
+     * @param EventManager $events Event manager
+     *
+     * @return void
+     */
+    public function setEventManager($events)
+    {
+        $events->setIdentifiers([__CLASS__]);
+        $this->events = $events;
     }
 
     /**
@@ -237,6 +279,19 @@ class Connector extends \VuFindSearch\Backend\Solr\Connector
                     $blocklisted = $blocklistedAt->getFieldValue();
                 }
                 $this->rems->setBlocklistStatusFromConnector($blocklisted);
+            }
+            if ($this->events && $headers->get('x-user-session-expired-closed')) {
+                $this->events->trigger(self::EVENT_REMS_SESSION_EXPIRED);
+            }
+
+            $limits = [
+                'x-user-daily-request-limit-exceeded' => 'daily',
+                'x-user-monthly-request-limit-exceeded' => 'monthly'
+            ];
+            foreach ($limits as $header => $type) {
+                $this->rems->setSearchLimitExceededFromConnector(
+                    $type, (bool)$headers->get($header)
+                );
             }
         }
 
