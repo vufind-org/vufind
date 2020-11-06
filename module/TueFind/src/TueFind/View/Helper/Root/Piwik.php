9e7f9a347be5d7b35bcb443e1d0e449a7d04aff1
hlindeboom <Hjordis.Lindeboom@Student.Reutlingen-University.DE>
2020-11-06 10:25:20 +0100

    Anpassung Piwik Volltextsuche #1342

--- module/TueFind/src/TueFind/View/Helper/Root/Piwik.php
+++ module/TueFind/src/TueFind/View/Helper/Root/Piwik.php
@@ -9,12 +9,41 @@ class Piwik extends \VuFind\View\Helper\Root\Piwik
     public function __construct($url, $options, $customVars, $router, $request, ?\VuFind\Auth\Manager $auth)
     {
         parent::__construct($url, $options, $customVars, $router, $request);
-        $this->auth = $auth;
+	$this->auth = $auth;
     }
 
     protected function getCustomVarsCode($customVars)
     {
         $customVars['isLoggedIn'] = ((isset($this->auth) && $this->auth->isLoggedIn()) ? 'true' : 'false');
-        return parent::getCustomVarsCode($customVars);
+        if ($this->isfulltextsearch()) {
+		$customVars['SearchType'] = 'fulltext';
+	}
+	return parent::getCustomVarsCode($customVars);
+    }
+
+    protected function getTrackSearchCode($results)
+    {
+        $escape = $this->getView()->plugin('escapeHtmlAttr');
+        $params = $results->getParams();
+        $searchTerms = $escape($params->getDisplayQuery());
+	$searchType = $escape($params->getSearchType());
+	if ($this->isfulltextsearch()) {
+		$searchType = $escape('fulltext');
+	}
+        $resultCount = $results->getResultTotal();
+        $backendId = $results->getOptions()->getSearchClassId();
+
+        // Use trackSiteSearch *instead* of trackPageView in searches
+        return <<<EOT
+    VuFindPiwikTracker.trackSiteSearch(
+        '{$this->searchPrefix}$backendId|$searchTerms', '$searchType', $resultCount
+    );
+
+EOT;
+    }
+
+    protected function isfulltextsearch() {
+        return strpos($this->request->getUriString(), 'Search2') !== false;
     }
 }
+?>
