26166b122e851de9db4b618c31224dae3d06280b
Ere Maijala <ere.maijala@helsinki.fi>
2020-11-27 16:54:58 +0200

    KohaRest: Check also for 'errors' element in response.
    Looks like a low-level error such as 500 results in 'errors' element instead of 'error'.

--- module/VuFind/src/VuFind/ILS/Driver/KohaRest.php
+++ module/VuFind/src/VuFind/ILS/Driver/KohaRest.php
@@ -1532,7 +1532,8 @@ class KohaRest extends \VuFind\ILS\Driver\AbstractBase implements
         // valid JSON that the caller can handle
         $decodedResult = json_decode($result, true);
         if (empty($request['errors']) && !$response->isSuccess()
-            && (null === $decodedResult || !empty($decodedResult['error']))
+            && (null === $decodedResult || !empty($decodedResult['error'])
+            || !empty($decodedResult['errors']))
         ) {
             $params = $method == 'GET'
                 ? $client->getRequest()->getQuery()->toString()
