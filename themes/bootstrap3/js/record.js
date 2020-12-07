a8cfc8c45b6790221328d1ebf1edbccefda982b0
Brad Busenius <bbusenius@uchicago.edu>
2020-12-07 16:07:42 -0600

    Fixed function target argument.

--- themes/bootstrap3/js/record.js
+++ themes/bootstrap3/js/record.js
@@ -407,8 +407,8 @@ function recordDocReady() {
     backgroundLoadTab(el.dataset.tab);
   });
 
-  initTruncate('.truncate-subjects', $('.subject-line'));
-  initTruncate('table.truncate-field', $('tr[typeof="Offer"]'), function(m) { return '<td colspan="2">' + m + '</td>'; });
+  initTruncate('.truncate-subjects', '.subject-line');
+  initTruncate('table.truncate-field', 'tr[typeof="Offer"]', function(m) { return '<td colspan="2">' + m + '</td>'; });
   registerTabEvents();
   applyRecordTabHash();
 }
