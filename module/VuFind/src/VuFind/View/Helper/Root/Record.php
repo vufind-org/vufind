e058a48e003ba8f51f434e0d95c68b64e3430984
Ere Maijala <ere.maijala@helsinki.fi>
2020-12-10 14:49:39 +0200

    Make it possible to call renderTemplate for optional templates.
    This allows renderTemplate to be called even if it's not certain that the given template exists.

--- module/VuFind/src/VuFind/View/Helper/Root/Record.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Record.php
@@ -99,15 +99,17 @@ class Record extends \Laminas\View\Helper\AbstractHelper
      * @param array  $context Variables needed for rendering template; these will
      * be temporarily added to the global view context, then reverted after the
      * template is rendered (default = record driver only).
+     * @param bool   $throw   If true (default), an exception is thrown if the
+     * template is not found. Otherwise an empty string is returned.
      *
      * @return string
      */
-    public function renderTemplate($name, $context = null)
+    public function renderTemplate($name, $context = null, $throw = true)
     {
         $template = 'RecordDriver/%s/' . $name;
         $className = get_class($this->driver);
         return $this->renderClassTemplate(
-            $template, $className, $context ?? ['driver' => $this->driver]
+            $template, $className, $context ?? ['driver' => $this->driver], $throw
         );
     }
 
