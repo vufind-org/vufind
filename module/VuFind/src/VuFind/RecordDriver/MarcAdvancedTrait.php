f378e5c2850ee75b212bf8bea7d621eb5232ab7e
Vít Novotný <witiko@mail.muni.cz>
2021-01-15 18:40:25 +0100

    Update undocumented behavior in MarcAdvancedTrait::getConsortialIDs

--- module/VuFind/src/VuFind/RecordDriver/MarcAdvancedTrait.php
+++ module/VuFind/src/VuFind/RecordDriver/MarcAdvancedTrait.php
@@ -901,6 +901,6 @@ trait MarcAdvancedTrait
      */
     public function getConsortialIDs()
     {
-        return $this->getFieldArray('035', 'a', true);
+        return $this->getFieldArray('035');
     }
 }
