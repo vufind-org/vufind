cbf961e86b46321ac346c6c567427d76e8ba7749
Ere Maijala <ere.maijala@helsinki.fi>
2021-02-05 17:43:36 +0200

    Blender: add support for backend-specific mappings files.
    Allows use of e.g. BlenderMappingsPrimo.yaml and BlenderMappingsEDS.yaml. BlenderMappings.yaml is still used if backend-specific mapping is not found.

--- module/Finna/src/Finna/Search/Blender/ParamsFactory.php
+++ module/Finna/src/Finna/Search/Blender/ParamsFactory.php
@@ -74,8 +74,12 @@ class ParamsFactory extends \VuFind\Search\Params\ParamsFactory
         if (!isset($blenderConfig['Secondary']['backend'])) {
             throw new \Exception('Secondary backend not defined in blender.ini');
         }
+        $secondary = $blenderConfig['Secondary']['backend'];
         $yamlReader = $container->get(\VuFind\Config\YamlReader::class);
-        $blenderMappings = $yamlReader->get('BlenderMappings.yaml');
+        $blenderMappings = $yamlReader->get("BlenderMappings$secondary.yaml");
+        if (empty($blenderMappings)) {
+            $blenderMappings = $yamlReader->get("BlenderMappings.yaml");
+        }
         $paramsMgr = $container->get(\VuFind\Search\Params\PluginManager::class);
         $secondaryParams = $paramsMgr->get(
             'VuFind\\Search\\' . $blenderConfig['Secondary']['backend']
