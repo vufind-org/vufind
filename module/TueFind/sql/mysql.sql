29d519854b25fe635059470e35b18dc2028d7ec8
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2021-02-12 10:00:46 +0100

    Fixed various syntax problems.

--- module/TueFind/sql/mysql.sql
+++ module/TueFind/sql/mysql.sql
@@ -9,7 +9,7 @@ ALTER TABLE vufind.user ADD tuefind_subscribed_to_newsletter BOOL NOT NULL DEFAU
 CREATE INDEX IF NOT EXISTS tuefind_subscribed_to_newsletter_index ON vufind.user (tuefind_subscribed_to_newsletter);
 
 
-CREATE TABLE IF NOT EXISTS tuefind_rss_feeds (
+CREATE TABLE IF NOT EXISTS vufind.tuefind_rss_feeds (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     feed_name VARCHAR(200) NOT NULL,
     subsystem_types SET('krimdok', 'ixtheo', 'relbib') NOT NULL,
@@ -18,20 +18,20 @@ CREATE TABLE IF NOT EXISTS tuefind_rss_feeds (
     downloader_time_limit INT NOT NULL DEFAULT 30,
     CONSTRAINT id_constraint UNIQUE (id),
     CONSTRAINT feed_name_constraint UNIQUE (feed_name),
-    CONSTRAINT feed_url_constraint UNIQUE (feed_url),
-    CONSTRAINT website_url_constraint UNIQUE (website_url)
+    CONSTRAINT feed_url_constraint UNIQUE (feed_url(768)),
+    CONSTRAINT website_url_constraint UNIQUE (website_url(768))
 ) DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
 
 
-CREATE TABLE IF NOT EXISTS tuefind_rss_items (
-    rss_feeds_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS vufind.tuefind_rss_items (
+    rss_feeds_id INT UNSIGNED NOT NULL,
     item_id VARCHAR(768) NOT NULL,
     item_url VARCHAR(1000) NOT NULL,
     item_title VARCHAR(1000) NOT NULL,
     item_description MEDIUMTEXT NOT NULL,
     pub_date DATETIME NOT NULL,
     insertion_time TIMESTAMP DEFAULT NOW() NOT NULL,
-    FOREIGN KEY (rss_feeds_id) REFERENCES tuefind_rss_feeds(id) ON DELETE CASCADE,
+    FOREIGN KEY (rss_feeds_id) REFERENCES vufind.tuefind_rss_feeds(id) ON DELETE CASCADE,
     CONSTRAINT rss_aggregator_item_id UNIQUE (item_id),
     INDEX rss_aggregator_item_id_index(item_id(768)),
     INDEX rss_aggregator_item_url_index(item_url(768)),
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS tuefind_rss_items (
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 
-CREATE TABLE IF NOT EXISTS tuefind_rss_subscriptions (
-    rss_feeds_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS vufind.tuefind_rss_subscriptions (
+    rss_feeds_id INT UNSIGNED NOT NULL,
     user_id INT NOT NULL,
     FOREIGN KEY (rss_feeds_id) REFERENCES tuefind_rss_feeds(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES vufind.user(id) ON DELETE CASCADE
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-ALTER TABLE vufind.user ADD tuefind_rss_feed_notification_type('rss', 'email') DEFAULT NULL;
+ALTER TABLE vufind.user ADD tuefind_rss_feed_notification_type ENUM('rss', 'email') DEFAULT NULL;
