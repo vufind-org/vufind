481897b6960c48dddb216f719a59080825a4db6a
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2021-02-12 09:35:14 +0100

    Various fixes or improvements as discussed.

--- module/TueFind/sql/mysql.sql
+++ module/TueFind/sql/mysql.sql
@@ -10,23 +10,25 @@ CREATE INDEX IF NOT EXISTS tuefind_subscribed_to_newsletter_index ON vufind.user
 
 
 CREATE TABLE IF NOT EXISTS tuefind_rss_feeds (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    feed_name VARCHAR(100) NOT NULL UNIQUE,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    feed_name VARCHAR(200) NOT NULL,
     subsystem_types SET('krimdok', 'ixtheo', 'relbib') NOT NULL,
-    feed_url VARCHAR(1000) NOT NULL UNIQUE,
-    blog_url VARCHAR(1000) NOT NULL UNIQUE,
-    default_downloader_time_limit INT NOT NULL DEFAULT 30
+    feed_url VARCHAR(1000) NOT NULL,
+    website_url VARCHAR(1000) NOT NULL,
+    downloader_time_limit INT NOT NULL DEFAULT 30,
+    CONSTRAINT id_constraint UNIQUE (id),
+    CONSTRAINT feed_name_constraint UNIQUE (feed_name),
+    CONSTRAINT feed_url_constraint UNIQUE (feed_url),
+    CONSTRAINT website_url_constraint UNIQUE (website_url)
 ) DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
 
 
-CREATE TABLE IF NOT EXISTS tuefind_rss_aggregator (
+CREATE TABLE IF NOT EXISTS tuefind_rss_items (
     rss_feeds_id INT NOT NULL,
     item_id VARCHAR(768) NOT NULL,
     item_url VARCHAR(1000) NOT NULL,
     item_title VARCHAR(1000) NOT NULL,
     item_description MEDIUMTEXT NOT NULL,
-    serial_name VARCHAR(1000) NOT NULL,
-    feed_url VARCHAR(1000) NOT NULL,
     pub_date DATETIME NOT NULL,
     insertion_time TIMESTAMP DEFAULT NOW() NOT NULL,
     FOREIGN KEY (rss_feeds_id) REFERENCES tuefind_rss_feeds(id) ON DELETE CASCADE,
@@ -44,4 +46,4 @@ CREATE TABLE IF NOT EXISTS tuefind_rss_subscriptions (
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-ALTER TABLE vufind.user ADD tuefind_rss_feed_notification_type('rss', 'email') NOT NULL DEFAULT 'email';
+ALTER TABLE vufind.user ADD tuefind_rss_feed_notification_type('rss', 'email') DEFAULT NULL;
