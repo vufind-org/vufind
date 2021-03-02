4c935e796f692e04aa42996ba8174d0b7649decb
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2021-03-02 15:44:35 +0100

    Renamed column.

--- module/TueFind/sql/mysql.sql
+++ module/TueFind/sql/mysql.sql
@@ -49,5 +49,5 @@ CREATE TABLE IF NOT EXISTS vufind.tuefind_rss_subscriptions (
     FOREIGN KEY (user_id) REFERENCES vufind.user(id) ON DELETE CASCADE
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-ALTER TABLE vufind.user ADD tuefind_rss_feed_notification_type ENUM('rss', 'email') DEFAULT NULL;
+ALTER TABLE vufind.user ADD tuefind_rss_feed_send_emails BOOLEAN DEFAULT FALSE;
 ALTER TABLE vufind.user ADD tuefind_rss_feed_last_notification TIMESTAMP DEFAULT NOW();
