39cda0aba4ed3948d314f4e072d75a97be01f68e
Chris Hallberg <chris.hallberg@hey.com>
2021-03-05 13:06:01 -0500

    Add tests to demonstrate escaping.

--- module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/MakeLinkTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/MakeLinkTest.php
@@ -27,7 +27,10 @@
  */
 namespace VuFindTest\View\Helper\Root;
 
+use Laminas\Config\Config;
+use VuFind\Record\Router;
 use VuFind\View\Helper\Root\MakeLink;
+use VuFind\View\Helper\Root\RecordLink;
 
 /**
  * makeLink view helper Test Class
@@ -135,5 +138,82 @@ class MakeLinkTest extends \PHPUnit\Framework\TestCase
             '<span class="btn">text</span>',
             $helper('text', null, ['class' => 'btn'])
         );
+    }
+
+    /**
+     * Test escaping with other plugins
+     *
+     * @return void
+     */
+    public function testEscaping()
+    {
+        $helper = $this->getHelper();
+
+        $recordLink = $this->getRecordLink();
+        $this->assertEquals(
+            '<a href="/Record/foo">recordLink</a>',
+            $helper('recordLink', $recordLink->getUrl('Solr|foo'))
+        );
+
+        $urlHelper = $this->getUrl();
+        $this->assertEquals(
+            '<a href="/Record/foo%2Fbar?checkRoute=1">recordLink</a>',
+            $helper(
+                'recordLink',
+                $recordLink->getTabUrl('Solr|foo/bar', null, ['checkRoute' => 1])
+            )
+        );
+    }
+
+    /**
+     * Get a RecordLink object ready for testing.
+     *
+     * @return Record
+     */
+    protected function getRecordLink()
+    {
+        $view = new \Laminas\View\Renderer\PhpRenderer();
+        $container = new \VuFindTest\Container\MockViewHelperContainer($this);
+        $container->set('url', $this->getUrl());
+        $view->setHelperPluginManager($container);
+
+        $recordLink = new RecordLink(new Router(new Config([])));
+        $recordLink->setView($view);
+        return $recordLink;
+    }
+
+    /**
+     * Get a URL helper.
+     *
+     * @return \VuFind\View\Helper\Root\Url
+     */
+    protected function getUrl()
+    {
+        $request = $this->getMockBuilder(\Laminas\Http\PhpEnvironment\Request::class)
+            ->setMethods(['getQuery'])->getMock();
+        $request->expects($this->any())->method('getQuery')
+            ->will($this->returnValue(new \Laminas\Stdlib\Parameters()));
+
+        $url = new \VuFind\View\Helper\Root\Url($request);
+
+        // Create router
+        $router = new \Laminas\Router\Http\TreeRouteStack();
+        $router->setRequestUri(new \Laminas\Uri\Http('http://localhost'));
+        $recordRoute = new \Laminas\Router\Http\Segment(
+            '/Record/[:id[/[:tab]]]',
+            [
+                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+            ],
+            [
+                'controller' => 'Record',
+                'action'     => 'Home',
+            ]
+        );
+
+        $router->addRoute('record', $recordRoute);
+        $url->setRouter($router);
+
+        return $url;
     }
 }
