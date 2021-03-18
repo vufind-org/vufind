b18879dc11c07a8d5dc6c35127e81ff2008d39af
Josef Moravec <josef.moravec@gmail.com>
2021-03-18 11:01:35 +0100

    Create backlink in js instead of manipulating dom

--- themes/bootstrap3/js/covers.js
+++ themes/bootstrap3/js/covers.js
@@ -4,26 +4,21 @@ function loadCoverByElement(data, element) {
   var img = element.find('img');
   var spinner = element.find('div.spinner');
   var container = element.find('div.cover-container');
-  var source = element.find('.cover-source');
+  var source = $('<p class="cover-source">' + VuFind.translate('cover_source_label') + ' </p>');
   function coverCallback(response) {
     if (typeof response.data.url !== 'undefined' && response.data.url !== false) {
       img.attr("src", response.data.url);
-      var inlink = element.parent().children().first().is('a');
+      var inlink = element.parent().is('a');
       var medium = img.parents('.media-left, .media-right, .carousel-item');
       if (typeof response.data.backlink_text !== 'undefined') {
-        var link = element.find('.cover-backlink');
-        var span = element.find('.cover-source-text');
         if (typeof response.data.backlink_url !== 'undefined') {
-          link.html(response.data.backlink_text);
-          link.attr("href", response.data.backlink_url);
-          span.remove();
+          var link = $('<a href="' + response.data.backlink_url + '" class="cover-backlink" target="_blank">' + response.data.backlink_text + '</a>');
+          source.append(link);
         } else {
-          span.html(response.data.backlink_text);
-          link.remove();
+          var span = $('<span class="cover-source-text"' + response.data.backlink_text + '</span>');
+          source.append(span);
         }
         if (inlink === true && response.data.backlink_on_results === true) {
-          var falseLink = source.children('a').not('.cover-backlink');
-          falseLink.replaceWith('<span>' + falseLink.text() + '</span>');
           medium.append(source);
         }
       }
@@ -31,6 +26,8 @@ function loadCoverByElement(data, element) {
         img.detach();
         medium.children('a').prepend(img);
         container.parents('.ajaxcover').remove();
+      } else if ($('.cover-source').length === 0) {
+        container.append(source);
       }
     } else {
       img.remove();
