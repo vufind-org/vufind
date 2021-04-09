fcc49749ea1b45f70e3533a9359e286af26d7509
Demian Katz <demian.katz@villanova.edu>
2021-04-09 11:22:12 -0400

    Remove unnecessary coalesce.

--- module/VuFind/src/VuFind/Autocomplete/Eds.php
+++ module/VuFind/src/VuFind/Autocomplete/Eds.php
@@ -91,7 +91,7 @@ class Eds implements AutocompleteInterface
         } catch (\Exception $e) {
             // Ignore errors -- just return empty results if we must.
         }
-        return is_array($results ?? null) ? array_unique($results) : [];
+        return is_array($results) ? array_unique($results) : [];
     }
 
     /**
