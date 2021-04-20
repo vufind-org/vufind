1716403969bc00d0ad4bdaf9aa3602e790567ec8
Demian Katz <demian.katz@villanova.edu>
2021-04-20 05:49:05 -0500

    Indicate that ContainerExceptions must also be Throwable. (#1923)

--- module/VuFind/src/VuFind/RecordTab/AbstractContentFactory.php
+++ module/VuFind/src/VuFind/RecordTab/AbstractContentFactory.php
@@ -66,7 +66,7 @@ abstract class AbstractContentFactory
      * @throws ServiceNotFoundException if unable to resolve the service.
      * @throws ServiceNotCreatedException if an exception is raised when
      * creating a service.
-     * @throws ContainerException if any other error occurs
+     * @throws ContainerException&\Throwable if any other error occurs
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
