471b911feab6925d5fc34e20f641c21cccd58203
Chris Hallberg <chris.hallberg@hey.com>
2021-04-21 16:41:26 -0400

    Basic implementation with FontAwesome.

---
+++ module/VuFind/src/VuFind/View/Helper/Root/Icon.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Make link view helper
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+namespace VuFind\View\Helper\Root;
+
+use Laminas\Config\Config;
+use Laminas\View\Helper\AbstractHelper;
+use VuFind\Config\PluginManager as ConfigManager;
+
+/**
+ * Make link view helper
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+class Icon extends AbstractHelper
+{
+    /**
+     * AddThis key (false if disabled)
+     *
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * Transforming map
+     *
+     * @var array
+     */
+    protected $iconMap;
+
+    /**
+     * Constructor
+     *
+     * @param string|bool $config AddThis config (false if disabled)
+     */
+    public function __construct(Config $config)
+    {
+        $this->config = $config->Config;
+        $this->iconMap = $config[$config->Config->use];
+    }
+
+    /**
+     * Returns AddThis API key (if AddThis is active) or false if not.
+     *
+     * @return string|bool
+     */
+    public function __invoke($icon, $extra = [])
+    {
+        $mappedIcon = $this->iconMap[$icon] ?? $icon;
+
+        $attrs = '';
+        $escAttr = $this->getView()->plugin('escapeHtmlAttr');
+        foreach ($extra as $key => $val) {
+            $attrs .= ' ' . $key . '="' . $escAttr($val) . '"';
+        }
+
+        return $this->getView()->render(
+            'icons/' . $this->config->use,
+            ['icon' => $mappedIcon, 'attrs' => $attrs]
+        );
+    }
+}
