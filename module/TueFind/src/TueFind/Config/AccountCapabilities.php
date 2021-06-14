23ef4b75fe6e624bfa6937d841ecc085f916c5f9
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-06-14 14:31:11 +0200

    tuefind: One myresearch/menu to replace them all, based on accountCapabilities

--- module/TueFind/src/TueFind/Config/AccountCapabilities.php
+++ module/TueFind/src/TueFind/Config/AccountCapabilities.php
@@ -15,10 +15,43 @@ class AccountCapabilities extends \VuFind\Config\AccountCapabilities
         $this->tuefindConfig = $tuefindConfig;
     }
 
+    /**
+     * Are users allowed to use PDA?
+     */
+    public function getPdaSetting(): string
+    {
+        return isset($this->tuefindConfig->General->pda)
+            && $this->tuefindConfig->General->pda === 'enabled'
+            ? 'enabled' : 'disabled';
+    }
+
+    /**
+     * Are users allowed to request rights on authority datasets?
+     */
     public function getRequestAuthorityRightsSetting(): string
     {
         return isset($this->tuefindConfig->General->request_authority_rights)
             && $this->tuefindConfig->General->request_authority_rights === 'enabled'
+            ? 'enabled' : 'disabled';
+    }
+
+    /**
+     * Are users allowed to subscribe rss feeds?
+     */
+    public function getRssSubscriptionSetting(): string
+    {
+        return isset($this->tuefindConfig->General->rss_subscriptions)
+            && $this->tuefindConfig->General->rss_subscriptions === 'enabled'
+            ? 'enabled' : 'disabled';
+    }
+
+    /**
+     * Are users allowed to subscribe journals?
+     */
+    public function getSubscriptionSetting(): string
+    {
+        return isset($this->tuefindConfig->General->subscriptions)
+            && $this->tuefindConfig->General->subscriptions === 'enabled'
             ? 'enabled' : 'disabled';
     }
 }
