079b367519987f76bac57bf606af97afa16bdcb5
Demian Katz <demian.katz@villanova.edu>
2021-07-01 16:58:02 -0400

    Test improvements.

--- module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/WorldCatTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/WorldCatTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * SolrAuthMarc Record Driver Test Class
+ * WorldCat Record Driver Test Class
  *
  * PHP version 7
  *
@@ -28,7 +28,7 @@
 namespace VuFindTest\RecordDriver;
 
 /**
- * SolrAuthMarc Record Driver Test Class
+ * WorldCat Record Driver Test Class
  *
  * @category VuFind
  * @package  Tests
@@ -36,34 +36,25 @@ namespace VuFindTest\RecordDriver;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
  */
-class SolrAuthMarcTest extends \PHPUnit\Framework\TestCase
+class WorldCatTest extends \PHPUnit\Framework\TestCase
 {
     use \VuFindTest\Feature\FixtureTrait;
 
     /**
-     * Test LCCN support.
+     * Test that we can setRawData() with getRawData() without breaking anything.
      *
      * @return void
      */
-    public function testRawLCCN()
+    public function testSetRawData()
     {
-        // LCCN in 700
-        $marc = $this->getFixture('marc/authlccn1.xml');
-        $config = new \Laminas\Config\Config([]);
-        $record = new \VuFind\RecordDriver\SolrAuthMarc($config);
-        $record->setRawData(['fullrecord' => $marc]);
-        $this->assertEquals('foo', $record->getRawLCCN());
-
-        // LCCN in 010
-        $marc2 = $this->getFixture('marc/authlccn2.xml');
-        $record2 = new \VuFind\RecordDriver\SolrAuthMarc($config);
-        $record2->setRawData(['fullrecord' => $marc2]);
-        $this->assertEquals('92005291', $record2->getRawLCCN());
-
-        // No LCCN
-        $marc3 = $this->getFixture('marc/altscript.xml');
-        $record3 = new \VuFind\RecordDriver\SolrAuthMarc($config);
-        $record3->setRawData(['fullrecord' => $marc3]);
-        $this->assertFalse($record3->getRawLCCN());
+        $marc = $this->getFixture('marc/marctraits.xml');
+        $driver = new \VuFind\RecordDriver\WorldCat();
+        $driver->setRawData($marc);
+        // Test that we can pull data from MARC...
+        $this->assertEquals('The Foo:', $driver->getShortTitle());
+        // Now reassign the raw data...
+        $driver->setRawData($driver->getRawData());
+        // Now confirm that we still get the same result...
+        $this->assertEquals('The Foo:', $driver->getShortTitle());
     }
 }
