3ca79b4782634efe49d375057b1baeab8d599c4b
Demian Katz <demian.katz@villanova.edu>
2021-07-02 07:13:06 -0500

    [VUFIND-1455] Move record driver traits into Feature namespace. (#2008)

--- module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/Feature/MarcBasicTraitTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/Feature/MarcBasicTraitTest.php
@@ -25,7 +25,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
  */
-namespace VuFindTest\RecordDriver;
+namespace VuFindTest\RecordDriver\Feature;
 
 /**
  * Record Driver Marc Traits Test Class
