377ef25fe35f280a2bfd0d589e8082bfbc75188e
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-07-22 16:38:32 +0200

    GetSubscriptionBundleEntries.php: Remove overhead

--- module/TueFind/src/TueFind/AjaxHandler/GetSubscriptionBundleEntries.php
+++ module/TueFind/src/TueFind/AjaxHandler/GetSubscriptionBundleEntries.php
@@ -1,41 +1,9 @@
 <?php
-/**
- * "Get Resolver Links" AJAX handler
- *
- * PHP version 7
- *
- * Copyright (C) Villanova University 2018.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @category VuFind
- * @package  AJAX
- * @author   Demian Katz <demian.katz@villanova.edu>
- * @author   Graham Seaman <Graham.Seaman@rhul.ac.uk>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
- */
+
 namespace TueFind\AjaxHandler;
 
-use Laminas\Config\Config;
 use Laminas\Mvc\Controller\Plugin\Params;
-use Laminas\View\Renderer\RendererInterface;
 use VuFind\I18n\Translator\TranslatorAwareInterface;
-use VuFind\Resolver\Connection;
-use VuFind\Resolver\Driver\PluginManager as ResolverManager;
-use VuFind\Session\Settings as SessionSettings;
-
 
 class GetSubscriptionBundleEntries extends \VuFind\AjaxHandler\AbstractBase implements TranslatorAwareInterface
 {
