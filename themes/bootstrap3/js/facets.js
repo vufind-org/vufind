dadb583fcec45a6c0fdc3711aba363b793c9e655
Demian Katz <demian.katz@villanova.edu>
2021-07-29 10:08:40 -0400

    Work in progress on using new truncation for facets.

--- themes/bootstrap3/js/facets.js
+++ themes/bootstrap3/js/facets.js
@@ -353,20 +353,7 @@ VuFind.register('lightbox_facets', function LightboxFacets() {
 });
 
 function registerMoreLessFacetsEventHandlers() {
-  $('.more-facets, .less-facets').off('click');
-  $('.more-facets').click(function moreFacets() {
-    var id = 'narrowGroupHidden-' + $(this).data('title');
-    $('.' + id).removeClass('hidden');
-    $('#more-' + id).addClass('hidden');
-    return false;
-  });
-
-  $('.less-facets').click(function lessFacets() {
-    var id = 'narrowGroupHidden-' + $(this).data('title');
-    $('.' + id).addClass('hidden');
-    $('#more-' + id).removeClass('hidden');
-    return false;
-  });
+  VuFind.truncate.initTruncate('.truncate-facets');
 }
 
 VuFind.listen('VuFind.sidefacets.loaded', registerMoreLessFacetsEventHandlers);
