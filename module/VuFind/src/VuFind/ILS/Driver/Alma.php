0f66fd796fccef792128adda704f6b92ce935cf5
Ere Maijala <ere.maijala@helsinki.fi>
2021-08-26 13:54:38 +0300

    Update cancelHolds method comments.
    Makes the comments reflect the way the driver in question actually works.

--- module/VuFind/src/VuFind/ILS/Driver/Alma.php
+++ module/VuFind/src/VuFind/ILS/Driver/Alma.php
@@ -932,8 +932,8 @@ class Alma extends AbstractBase implements \VuFindHttp\HttpServiceAwareInterface
      * @param array $cancelDetails An associative array with two keys: patron
      *                             (array returned by the driver's
      *                             patronLogin method) and details (an array
-     *                             of strings eturned by the driver's
-     *                             getCancelHoldDetails method)
+     *                             of strings returned in holds' cancel_details
+     *                             field.
      *
      * @return array                Associative array containing with keys 'count'
      *                                 (number of items successfully cancelled) and
