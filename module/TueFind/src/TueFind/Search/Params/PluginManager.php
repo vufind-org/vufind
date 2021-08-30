8ec70bb202d66eea2fa7be7aa7b904d5b73290dc
Adrii Lysohor <LysogorAnd@gmail.com>
2021-08-30 10:33:18 +0200

    SolrAuthorFacets

--- module/TueFind/src/TueFind/Search/Params/PluginManager.php
+++ module/TueFind/src/TueFind/Search/Params/PluginManager.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace IxTheo\Search\Params;
+namespace TueFind\Search\Params;
 
-use VuFind\Search\Solr\ParamsFactory;
+use TueFind\Search\Solr\ParamsFactory;
 
 class PluginManager extends \VuFind\Search\Params\PluginManager {
     public function __construct($configOrContainerInstance = null,
@@ -10,10 +10,6 @@ class PluginManager extends \VuFind\Search\Params\PluginManager {
     ) {
         $this->aliases['solr'] = \IxTheo\Search\Solr\Params::class;
         $this->aliases['keywordchainsearch'] = \IxTheo\Search\KeywordChainSearch\Params::class;
-
-        $this->factories[\IxTheo\Search\Solr\Params::class] = ParamsFactory::class;
-        $this->factories[\IxTheo\Search\KeywordChainSearch\Params::class] = ParamsFactory::class;
-
         parent::__construct($configOrContainerInstance, $v3config);
     }
 }
