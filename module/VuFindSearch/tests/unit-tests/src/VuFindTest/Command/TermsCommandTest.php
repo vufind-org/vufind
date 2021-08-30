b85a59bc12ab8e423fe54750579a0c9e9a8ae466
Demian Katz <demian.katz@villanova.edu>
2021-08-30 13:17:24 -0400

    Add tests.

---
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Command/TermsCommandTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Unit tests for TermsCommand.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2021.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+namespace VuFindTest\Command;
+
+use PHPUnit\Framework\TestCase;
+use VuFindSearch\Command\TermsCommand;
+
+/**
+ * Unit tests for TermsCommand.
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+class TermsCommandTest extends TestCase
+{
+    /**
+     * Test that the command works as expected
+     *
+     * @return void
+     */
+    public function testCommand(): void
+    {
+        $factory = $this
+            ->getMockBuilder(RecordCollectionFactoryInterface::class)
+            ->getMock();
+        $backendId = 'bar';
+        $backend = $this
+            ->getMockBuilder(\VuFindSearch\Backend\Solr\Backend::class)
+            ->disableOriginalConstructor()->getMock();
+        $backend->expects($this->once())->method('getIdentifier')
+            ->will($this->returnValue($backendId));
+        $backend->expects($this->once())->method('terms')
+            ->with(
+                $this->equalTo('field'),
+                $this->equalTo('from'),
+                $this->equalTo(10)
+            )->will($this->returnValue('result'));  // not a realistic value!
+        $command = new TermsCommand($backendId, 'field', 'from', 10);
+        $command->execute($backend);
+        $this->assertEquals('result', $command->getResult());
+    }
+}
