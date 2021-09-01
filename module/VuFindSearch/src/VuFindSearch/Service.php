3906fbafe2ea70d62ec9a88344daf89b8d042519
Demian Katz <demian.katz@villanova.edu>
2021-09-01 09:10:27 -0400

    Improve search service command object error handling. (#2085)
    - Includes added test case.

--- module/VuFindSearch/src/VuFindSearch/Service.php
+++ module/VuFindSearch/src/VuFindSearch/Service.php
@@ -356,11 +356,19 @@ class Service
                 $args
             );
             if (!$response->stopped()) {
+                // We need to construct our error message differently depending
+                // on whether or not we have a command object...
+                $context = isset($args['command'])
+                    ? $args['command']->getContext()
+                    : ($args['context'] ?? 'null');
+                $backend = isset($args['command'])
+                    ? $args['command']->getTargetBackendName()
+                    : ($args['backend'] ?? $backend);
                 throw new Exception\RuntimeException(
                     sprintf(
                         'Unable to resolve backend: %s, %s',
-                        $args['context'],
-                        $args['backend']
+                        $context,
+                        $backend
                     )
                 );
             }
