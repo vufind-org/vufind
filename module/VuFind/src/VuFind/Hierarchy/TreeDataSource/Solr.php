4e4c42a8f5c1d8772c2c0c8886ec7c74019ae16c
Demian Katz <demian.katz@villanova.edu>
2021-09-01 16:08:51 -0400

    Refactor Solr TreeDataSource to use search service.

--- module/VuFind/src/VuFind/Hierarchy/TreeDataSource/Solr.php
+++ module/VuFind/src/VuFind/Hierarchy/TreeDataSource/Solr.php
@@ -28,9 +28,7 @@
 namespace VuFind\Hierarchy\TreeDataSource;
 
 use VuFind\Hierarchy\TreeDataFormatter\PluginManager as FormatterManager;
-use VuFindSearch\Backend\Solr\Connector;
-use VuFindSearch\ParamBag;
-use VuFindSearch\Query\Query;
+use VuFindSearch\Service;
 
 /**
  * Hierarchy Tree Data Source (Solr)
@@ -48,9 +46,16 @@ class Solr extends AbstractBase
     /**
      * Search service
      *
-     * @var Connector
+     * @var Service
      */
-    protected $solrConnector;
+    protected $searchService;
+
+    /**
+     * Search backend ID used for tree generation.
+     *
+     * @var string
+     */
+    protected $backendId;
 
     /**
      * Formatter manager
@@ -90,20 +95,23 @@ class Solr extends AbstractBase
     /**
      * Constructor.
      *
-     * @param Connector        $connector Solr connector
+     * @param Service          $ss        Search service
+     * @param string           $backendId Search backend ID
      * @param FormatterManager $fm        Formatter manager
      * @param string           $cacheDir  Directory to hold cache results (optional)
      * @param array            $filters   Filters to apply to Solr tree queries
      * @param int              $batchSize Number of records retrieved in a batch
      */
     public function __construct(
-        Connector $connector,
+        Service $ss,
+        string $backendId,
         FormatterManager $fm,
         $cacheDir = null,
         $filters = [],
         $batchSize = 1000
     ) {
-        $this->solrConnector = $connector;
+        $this->searchService = $ss;
+        $this->backendId = $backendId;
         $this->formatterManager = $fm;
         if (null !== $cacheDir) {
             $this->cacheDir = rtrim($cacheDir, '/');
@@ -129,49 +137,6 @@ class Solr extends AbstractBase
     }
 
     /**
-     * Get default search parameters shared by cursorMark and legacy methods.
-     *
-     * @param string $q Search query
-     *
-     * @return array
-     */
-    protected function getDefaultSearchParams(string $q): array
-    {
-        return [
-            'q'  => [$q],
-            'fq' => $this->filters,
-            'hl' => ['false'],
-            'fl' => ['title,id,hierarchy_parent_id,hierarchy_top_id,'
-                . 'is_hierarchy_id,hierarchy_sequence,title_in_hierarchy'],
-            'wt' => ['json'],
-            'json.nl' => ['arrarr'],
-        ];
-    }
-
-    /**
-     * Search Solr using legacy, non-cursorMark method (sometimes needed for
-     * backward compatibility, but usually disabled).
-     *
-     * @param string $q    Search query
-     * @param int    $rows Max rows to retrieve
-     *
-     * @return array
-     */
-    protected function searchSolrLegacy(string $q, int $rows): array
-    {
-        $params = new ParamBag(
-            $this->getDefaultSearchParams($q) +
-            [
-                'rows' => [$rows], // Integer max
-                'start' => [0]
-            ]
-        );
-        $response = $this->solrConnector->search($params);
-        $json = json_decode($response);
-        return $json->response->docs ?? [];
-    }
-
-    /**
      * Search Solr.
      *
      * @param string $q    Search query
@@ -182,41 +147,15 @@ class Solr extends AbstractBase
      */
     protected function searchSolr($q, $rows = 1073741823)
     {
-        // Use legacy method if configured to do so:
-        if ($this->batchSize <= 0) {
-            return $this->searchSolrLegacy($q, $rows);
-        }
-
-        // By default, use cursorMark method:
-        $prevCursorMark = '';
-        $cursorMark = '*';
-        $records = [];
-        while ($cursorMark !== $prevCursorMark) {
-            $params = new ParamBag(
-                $this->getDefaultSearchParams($q) + [
-                    'rows' => [min([$this->batchSize, $rows])],
-                    // Start is always 0 when using cursorMark
-                    'start' => [0],
-                    // Sort is required
-                    'sort' => ['id asc'],
-                    // Override any default timeAllowed since it cannot be used with
-                    // cursorMark
-                    'timeAllowed' => -1,
-                    'cursorMark' => $cursorMark
-                ]
-            );
-            $results = json_decode($this->solrConnector->search($params));
-            if (empty($results->response->docs)) {
-                break;
-            }
-            $records = array_merge($records, $results->response->docs);
-            if (count($records) >= $rows) {
-                break;
-            }
-            $prevCursorMark = $cursorMark;
-            $cursorMark = $results->nextCursorMark;
-        }
-        return $records;
+        $context = compact('q', 'rows') + [
+            'batchSize' => $this->batchSize,
+            'filters' => $this->filters,
+        ];
+        $command = new Command\GetTreeDataCommand(
+            $this->backendId,
+            $context
+        );
+        return $this->searchService->invoke($command)->getResult();
     }
 
     /**
