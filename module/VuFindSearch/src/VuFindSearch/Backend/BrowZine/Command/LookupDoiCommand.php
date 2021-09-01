9320a28e1df1d00e763a793f3c67825bb0729aca
Demian Katz <demian.katz@villanova.edu>
2021-09-01 09:14:09 -0400

    Refactor BrowZine code to use search service.

---
+++ module/VuFindSearch/src/VuFindSearch/Backend/BrowZine/Command/LookupDoiCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Command to look up a DOI in the BrowZine backend.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2021.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+namespace VuFindSearch\Backend\BrowZine\Command;
+
+use VuFindSearch\Backend\BackendInterface;
+use VuFindSearch\Backend\BrowZine\Backend;
+use VuFindSearch\Command\CommandInterface;
+
+/**
+ * Command to look up a DOI in the BrowZine backend.
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org
+ */
+class LookupDoiCommand extends \VuFindSearch\Command\AbstractBase
+{
+    /**
+     * Execute command on backend.
+     *
+     * @param BackendInterface $backend Backend
+     *
+     * @return CommandInterface Command instance for method chaining
+     */
+    public function execute(BackendInterface $backend): CommandInterface
+    {
+        if (!($backend instanceof Backend)) {
+            throw new \Exception('Unexpected backend: ' . get_class($backend));
+        }
+        $this->result = $backend->getConnector()->lookupDoi($this->getContext());
+        return parent::execute($backend);
+    }
+}
