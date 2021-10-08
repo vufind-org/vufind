35582cae7d8bc8714211e0350f512804e6bd74c3
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-10-08 09:42:12 +0200

    Authority: Move subsystem link generation to IxTheo to avoid KrimDok-related problems, see #1546

--- module/TueFind/src/TueFind/RecordDriver/SolrAuthMarc.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrAuthMarc.php
@@ -88,23 +88,8 @@ class SolrAuthMarc extends SolrAuthDefault {
 
     public function getExternalSubsystems(): array
     {
-        $subsystemLinks = [
-            ['title' => 'RelBib', 'url' => 'https://relbib.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'REL'],
-            ['title' => 'BILDI-IxTheo', 'url' => 'https://bible.ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'BIB'],
-            ['title' => 'IxTheo / KALDI / DaKaR', 'url' => 'https://canonlaw.ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'CAN']
-        ];
-
-        $result = [];
-        $result[] = ['title' => 'IxTheo', 'url' => 'https://ixtheo.de/Authority/' . urlencode($this->getUniqueID()), 'label' => 'IXT'];
-        foreach ($this->getSubsystems() as $subsystem) {
-            foreach ($subsystemLinks as $subsystemLink) {
-                if ($subsystemLink['label'] == $subsystem) {
-                    $result[] = $subsystemLink;
-                }
-            }
-        }
-
-        return $result;
+        // This needs to be overridden in IxTheo/KrimDok if subsystems are present
+        return [];
     }
 
     protected function getLifeDates()
