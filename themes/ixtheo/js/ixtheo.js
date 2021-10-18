4de497a5b8ea13749fa9856cd6b956ee5e83884d
Adrii Lysohor <LysogorAnd@gmail.com>
2021-10-18 13:55:56 +0200

    Indentation

--- themes/ixtheo/js/ixtheo.js
+++ themes/ixtheo/js/ixtheo.js
@@ -2,7 +2,7 @@ var IxTheo = {
     ConvertToListEntries: function(bundle_id, items) {
         var bundle_div = $("#" + bundle_id.replace(":", "_")).find("div");
         var entry_list = '<ul class="list-group">';
-        $.each(items, function (index, item) {
+        $.each(items, function(index, item) {
             entry_list += ('<li class="list-group-item">' +
                     '<a href=' + VuFind.path + /Record/ + item.id + '>' + item.title + '</a>' +
                     '<a class="subscribe-record save-record modal-link" data-lightbox  id="' + item.id + '"' +
@@ -31,13 +31,12 @@ var IxTheo = {
                     $("#pda_place_holder").each(function() {
                         if ((received_isbn == isbn) && (pda_status == "OFFER_PDA")) {
                             $(this).replaceWith('<a href="' + pdaSubscribeUrl + '">' + pdaSubscribeText + '</a>');
-                        }
-                        else
+                        } else
                             $("#pda_row").remove();
                     });
                 });
             }, // end success
-            error: function (xhr, ajaxOptions, thrownError) {
+            error: function(xhr, ajaxOptions, thrownError) {
                 $("#pda_place_holder").each(function() {
                     $(this).replaceWith('Invalid server response!!!!!');
                 })
@@ -54,12 +53,12 @@ var IxTheo = {
             },
             dataType: 'json',
             success: function displaySubscriptionItems(items) {
-                $(document).ready(function () {
+                $(document).ready(function() {
                     if (items.length > 0)
                         IxTheo.ConvertToListEntries(bundle_id, items);
                 });
             },
-            error: function (xhr, ajaxOptions, thrownError) {
+            error: function(xhr, ajaxOptions, thrownError) {
                 if (window.console && window.console.log) {
                     console.log("Status: " + xhr.status + ", Error: " + thrownError);
                 }
@@ -68,33 +67,33 @@ var IxTheo = {
     },
 
     ShowMoreButtonFavoriteList: function() {
-      let maxElements = 3;
-      let countListItems = 0;
-      let showMoreButton = false;
-      let mainBlock = $('.savedLists:first');
-      setTimeout(function() {
-	  mainBlock.find('li').each(function() {
-	      countListItems++;
-	      if(countListItems > maxElements) {
-		  $(this).hide();
-		  showMoreButton = true;
-	      }
-	  })
-	  if(showMoreButton === true) {
-	      $('<span class="ix-favoritesListMoreButton">more</span>').insertAfter(mainBlock.find('ul'));
-	  }
-	  mainBlock.removeClass('tf-d-none');
-	  $('.ix-favoritesListMoreButton').click(function() {
-	      $('.ix-favoritesListModal').click();
-	  })
-      }, 500);
+        let maxElements = 3;
+        let countListItems = 0;
+        let showMoreButton = false;
+        let mainBlock = $('.savedLists:first');
+        setTimeout(function() {
+            mainBlock.find('li').each(function() {
+                countListItems++;
+                if (countListItems > maxElements) {
+                    $(this).hide();
+                    showMoreButton = true;
+                }
+            })
+            if (showMoreButton === true) {
+                $('<span class="ix-favoritesListMoreButton">more</span>').insertAfter(mainBlock.find('ul'));
+            }
+            mainBlock.removeClass('tf-d-none');
+            $('.ix-favoritesListMoreButton').click(function() {
+                $('.ix-favoritesListModal').click();
+            })
+        }, 500);
     }
 };
 
 
-$(document).ready(function () {
+$(document).ready(function() {
     var previous_handler;
-    $("#searchForm_type").on('focus', function () {
+    $("#searchForm_type").on('focus', function() {
         previous_handler = this.value;
     }).change(function adjustSearchSort(e) {
         if (previous_handler == 'BibleRangeSearch') {
