4dac174fecb00cdbe08b73627552242561058405
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-10-18 10:11:44 +0200

    Allow NULL values in certain ixtheo_* columns

--- module/IxTheo/sql/mysql.sql
+++ module/IxTheo/sql/mysql.sql
@@ -30,11 +30,11 @@ CREATE TABLE ixtheo_pda_subscriptions (
     PRIMARY KEY (id, book_ppn)
 ) DEFAULT CHARSET=utf8;
 
-ALTER TABLE vufind.user ADD COLUMN ixtheo_user_type ENUM('ixtheo', 'relbib') DEFAULT 'ixtheo';
-ALTER TABLE vufind.user ADD COLUMN ixtheo_appellation VARCHAR(64);
-ALTER TABLE vufind.user ADD COLUMN ixtheo_title VARCHAR(64);
-ALTER TABLE vufind.user ADD COLUMN ixtheo_institution VARCHAR(255);
-ALTER TABLE vufind.user ADD COLUMN ixtheo_country VARCHAR(255);
-ALTER TABLE vufind.user ADD COLUMN ixtheo_language VARCHAR(20);
+ALTER TABLE vufind.user ADD COLUMN ixtheo_user_type_test ENUM('ixtheo', 'relbib') NOT NULL DEFAULT 'ixtheo';
+ALTER TABLE vufind.user ADD COLUMN ixtheo_appellation VARCHAR(64) DEFAULT NULL;
+ALTER TABLE vufind.user ADD COLUMN ixtheo_title VARCHAR(64) DEFAULT NULL;
+ALTER TABLE vufind.user ADD COLUMN ixtheo_institution VARCHAR(255) DEFAULT NULL;
+ALTER TABLE vufind.user ADD COLUMN ixtheo_country VARCHAR(255) DEFAULT NULL;
+ALTER TABLE vufind.user ADD COLUMN ixtheo_language VARCHAR(20) DEFAULT NULL;
 ALTER TABLE vufind.user ADD COLUMN ixtheo_can_use_tad BOOLEAN DEFAULT FALSE;
 ALTER TABLE vufind.user ADD COLUMN ixtheo_journal_subscription_format ENUM ('meistertask') DEFAULT NULL;
