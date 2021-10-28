9b7d56dce7b688773563769d585ccaaf9a15fb3c
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-10-28 09:18:21 +0200

    SolrAuth: Solve problem with missing translation in "Remove Filters" box, see #1789

--- module/TueFind/src/TueFind/Search/SolrAuth/Params.php
+++ module/TueFind/src/TueFind/Search/SolrAuth/Params.php
@@ -4,5 +4,11 @@ namespace TueFind\Search\SolrAuth;
 
 class Params extends \VuFind\Search\SolrAuth\Params
 {
-    
+    protected function formatFilterListEntry($field, $value, $operator, $translate)
+    {
+        $entry = parent::formatFilterListEntry($field, $value, $operator, $translate);
+        if ($field == 'type')
+            $entry['displayText'] = $this->translate('authority_type_' . $value);
+        return $entry;
+    }
 }
