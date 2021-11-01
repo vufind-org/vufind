82fd1e1ac3a30c59b2e233a0bb0b43ad0aa6ff8c
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-11-01 19:05:46 +0100

    [VUFIND-1495] Improve addition of non-tab record routes in RouteGenerator. (#2172)

--- module/VuFind/config/module.config.php
+++ module/VuFind/config/module.config.php
@@ -652,6 +652,13 @@ $recordRoutes = [
     'search2collectionrecord' => 'Search2Record',
 ];
 
+// Define non tab record actions
+$nonTabRecordActions = [
+    'AddComment', 'DeleteComment', 'AddTag', 'DeleteTag', 'Save', 'Email', 'SMS',
+    'Cite', 'Export', 'RDF', 'Hold', 'Home', 'StorageRetrievalRequest',
+    'AjaxTab', 'ILLRequest', 'PDF', 'Epub', 'LinkedText', 'Permalink',
+];
+
 // Define dynamic routes -- controller => [route name => action]
 $dynamicRoutes = [
     'Feedback' => ['feedback-form' => 'Form/[:id]'],
@@ -720,6 +727,7 @@ $staticRoutes = [
 ];
 
 $routeGenerator = new \VuFind\Route\RouteGenerator();
+$routeGenerator->addNonTabRecordActions($config, $nonTabRecordActions);
 $routeGenerator->addRecordRoutes($config, $recordRoutes);
 $routeGenerator->addDynamicRoutes($config, $dynamicRoutes);
 $routeGenerator->addStaticRoutes($config, $staticRoutes);
