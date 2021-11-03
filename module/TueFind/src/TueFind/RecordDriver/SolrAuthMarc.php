a0e74f1290a48a0d6ddf9da22a220e9bd9a66296
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-11-03 16:59:08 +0100

    Add translation for "place of activity"

--- module/TueFind/src/TueFind/RecordDriver/SolrAuthMarc.php
+++ module/TueFind/src/TueFind/RecordDriver/SolrAuthMarc.php
@@ -14,6 +14,7 @@ class SolrAuthMarc extends SolrAuthDefault {
         // Places
         'Geburtsort'    => 'Place of birth',
         'Sterbeort'     => 'Place of death',
+        'Wirkungsort'   => 'Place of activity',
 
         // Personal Relations
         'Ehemann'       => 'Husband',
