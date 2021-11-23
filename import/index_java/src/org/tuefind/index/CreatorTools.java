61091eb03964ab36f901690a2f41a54baeeaa6a4
andreas-ub <andreas.nutz@uni-tuebingen.de>
2021-11-23 09:45:18 +0100

    changes after code review

--- import/index_java/src/org/tuefind/index/CreatorTools.java
+++ import/index_java/src/org/tuefind/index/CreatorTools.java
@@ -36,12 +36,14 @@ public class CreatorTools extends org.vufind.index.CreatorTools
     protected static Map<String, List<String>> normalizedRelatorMap = new ConcurrentHashMap<>();
 
     public List getRelatorsFilteredByRelator(Record record, String tagList,
-            String acceptWithoutRelator, String relatorConfig
-        ) {
-        List<String> authorRoles = getRelatorsFilteredByRelator(record, tagList, acceptWithoutRelator, relatorConfig, acceptWithoutRelator, "false", false);
+            String acceptWithoutRelator, String relatorConfig,
+            String acceptUnknownRelators, String indexRawRelators, Boolean firstOnly)
+    {
+        List<String> authorRoles = super.getRelatorsFilteredByRelator(record, tagList, acceptWithoutRelator, relatorConfig,
+                                                                      acceptUnknownRelators, indexRawRelators, firstOnly);
         List<String> result = new LinkedList<String>();
         for (String elem : authorRoles) {
-            if (elem.length() == 3) {
+            if (elem.length() == 3 && result.contains(elem) == false) {
                 result.add(elem);
             }
         }
