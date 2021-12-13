8a335e092b7b0fa30017bd2c37202c13b569f2e5
Chris Hallberg <chris.hallberg@hey.com>
2021-12-13 16:11:25 -0500

    Theme config example in local_theme_example.

--- themes/local_theme_example/theme.config.php
+++ themes/local_theme_example/theme.config.php
@@ -1,4 +1,16 @@
 <?php
 return [
-    'extends' => 'bootstrap3'
+    'extends' => 'bootstrap3',
+
+    /**
+     * The following is all custom attributes for this theme
+     * in order to demonstrate the themeConfig view helper.
+     */
+    'home-content' => [
+        'links' => [
+            'Setup' => 'install-home',
+            'Dev Tools' => 'devtools-home',
+        ],
+        'contact' => parse_ini_file('contacts-demo.ini'),
+    ],
 ];
