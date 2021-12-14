d2ebca91b1ccfea9550c5f61e20a3192b4bcf1cc
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2021-12-14 11:44:57 +0100

    fixed JavaScript issues

--- themes/bootstrap3/js/resultcount.js
+++ themes/bootstrap3/js/resultcount.js
@@ -2,17 +2,17 @@
 
 VuFind.register('resultcount', function resultCount() {
   function init() {
-    $('ul.nav-tabs li a').each(function(){
+    $('ul.nav-tabs li a').each(function queryResultCount(){
       var $this = $(this);
       if ($this.attr('href') !== undefined) {
         var queryString = $this.attr('href').replace(/^.*\?/, '');
         var source = $this.data('source');
         jQuery.ajax({
-          url:VuFind.path + '/AJAX/JSON?method=getResultCount',
-          dataType:'json',
-          data:{querystring:encodeURIComponent(queryString), source:source},
-          success:function(data, textStatus){
-            $this.append(' (' + data.data.total.toLocaleString() + ')')
+          url: VuFind.path + '/AJAX/JSON?method=getResultCount',
+          dataType: 'json',
+          data: {querystring:encodeURIComponent(queryString), source:source},
+          success: function appendResultCount(response){
+            $this.append(' (' + response.data.total.toLocaleString() + ')')
           }
         });
       }
