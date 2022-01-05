dee9b0e1362f19cfc3cc6714cbe563da0a561433
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2022-01-05 14:15:51 +0100

    using attribute rather than a class

--- themes/bootstrap3/js/resultcount.js
+++ themes/bootstrap3/js/resultcount.js
@@ -2,7 +2,7 @@
 
 VuFind.register('resultcount', function resultCount() {
   function init() {
-    $('ul.nav-tabs li.show-counts a').each(function queryResultCount(){
+    $('ul.nav-tabs [data-show-counts] a').each(function queryResultCount(){
       var $this = $(this);
       if ($this.attr('href') !== undefined) {
         var queryString = $this.attr('href');
