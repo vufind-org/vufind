97c21c34a1762624d3df1022ed3b6b55ad9a67ef
Ere Maijala <ere.maijala@helsinki.fi>
2022-01-07 15:18:45 +0200

    Add streaming load support for MARC records.

--- module/VuFind/src/VuFind/Marc/Serialization/MarcXml.php
+++ module/VuFind/src/VuFind/Marc/Serialization/MarcXml.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) The National Library of Finland 2020.
+ * Copyright (C) The National Library of Finland 2020-2022.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -36,8 +36,29 @@ namespace VuFind\Marc\Serialization;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:plugins:record_drivers Wiki
  */
-class MarcXml implements SerializationInterface
+class MarcXml implements SerializationInterface, SerializationFileInterface
 {
+    /**
+     * Current file
+     *
+     * @var string
+     */
+    protected $fileName = '';
+
+    /**
+     * XML Reader for current file
+     *
+     * @var \XMLReader
+     */
+    protected $xml = null;
+
+    /**
+     * Current XML element path
+     *
+     * @var array
+     */
+    protected $currentXmlPath = [];
+
     /**
      * Check if this class can parse the given MARC string
      *
@@ -62,6 +83,26 @@ class MarcXml implements SerializationInterface
     {
         // A pretty naïve check, but it's enough to tell the different formats apart
         return strncmp(trim($marc), '<', 1) === 0;
+    }
+
+    /**
+     * Check if the serialization class can parse the given MARC collection file
+     *
+     * @param string $file File name
+     *
+     * @return bool
+     */
+    public static function canParseCollectionFile(string $file): bool
+    {
+        if (false === ($f = @fopen($file, 'r'))) {
+            throw new \Exception("Cannot open file '$file' for reading");
+        }
+        do {
+            $s = ltrim(fgets($f, 10));
+        } while (!$s && !feof($f));
+        fclose($f);
+
+        return self::canParseCollection($s);
     }
 
     /**
@@ -220,5 +261,66 @@ class MarcXml implements SerializationInterface
             libxml_use_internal_errors($saveUseErrors);
             throw $e;
         }
+    }
+
+    /**
+     * Open a collection file
+     *
+     * @param string $file File name
+     *
+     * @return void
+     */
+    public function openCollectionFile(string $file): void
+    {
+        $this->fileName = $file;
+        $this->xml = new \XMLReader();
+        $result = $this->xml->open($file);
+        if (false === $result) {
+            throw new \Exception("Cannot open file '$file' for reading");
+        }
+        $this->currentXmlPath = [];
+    }
+
+    /**
+     * Rewind the collection file
+     *
+     * @return void;
+     */
+    public function rewind(): void
+    {
+        if ('' === $this->fileName) {
+            throw new \Exception('Collection file not open');
+        }
+        $this->openCollectionFile($this->fileName);
+    }
+
+    /**
+     * Get next record from the file or an empty string on EOF
+     *
+     * @return string
+     */
+    public function getNextRecord(): string
+    {
+        if (null === $this->xml) {
+            throw new \Exception('Collection file not open');
+        }
+        while ($this->xml->read()) {
+            if ($this->xml->nodeType !== \XMLReader::ELEMENT) {
+                continue;
+            }
+            $this->currentXmlPath
+                = array_slice($this->currentXmlPath, 0, $this->xml->depth);
+            $this->currentXmlPath[] = $this->xml->name;
+            $ns = $this->xml->namespaceURI;
+
+            $currentPathString = '/' . implode('/', $this->currentXmlPath);
+            if ('/collection/record' !== $currentPathString
+                || ($ns && 'http://www.loc.gov/MARC21/slim' !== $ns)
+            ) {
+                continue;
+            }
+            return $this->xml->readOuterXML();
+        }
+        return '';
     }
 }
