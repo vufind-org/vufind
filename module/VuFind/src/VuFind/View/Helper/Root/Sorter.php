db319eda2ec673e6bba334d9a9e652507d2c5cfd
Josef Moravec <josef.moravec@gmail.com>
2022-01-19 17:22:08 +0100

    Add Sorter view helper

---
+++ module/VuFind/src/VuFind/View/Helper/Root/Sorter.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Class Sorter
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2022.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  VuFind\View\Helper\Root
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+namespace VuFind\View\Helper\Root;
+
+/**
+ * Class Sorter
+ *
+ * @category VuFind
+ * @package  VuFind\View\Helper\Root
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+class Sorter implements \VuFind\Service\SorterAwareInterface
+{
+    use \VuFind\Service\SorterAwareTrait;
+
+    /**
+     * Compare function
+     *
+     * @param string $string1 First string to compare
+     * @param string $string2 Second string to compare
+     *
+     * @return int
+     */
+    public function compare(string $string1, string $string2): int
+    {
+        return $this->sorter->compare($string1, $string2);
+    }
+
+    /**
+     * Sort array by values
+     *
+     * @param array $array Array to sort
+     *
+     * @return bool
+     */
+    public function sort(array &$array): bool
+    {
+        return $this->sorter->sort($array);
+    }
+
+    /**
+     * Sort array by values and maintain index association
+     *
+     * @param array $array Array to sort
+     *
+     * @return bool
+     */
+    public function asort(array &$array): bool
+    {
+        return $this->sorter->asort($array);
+    }
+}
