52241b16d6e203f5300514b23f8bd37ffb701852
Jason Savell <jsavell@library.tamu.edu>
2022-01-25 14:53:32 -0600

    move more tamu customizations to tamu module

---
+++ module/TAMU/src/TAMU/RecordDriver/DefaultRecord.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Default model for records
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  RecordDrivers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_drivers Wiki
+ */
+namespace TAMU\RecordDriver;
+
+/**
+ * Default model for records
+ *
+ * @category VuFind
+ * @package  RecordDrivers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_drivers Wiki
+ *
+ * @SuppressWarnings(PHPMD.ExcessivePublicCount)
+ */
+class DefaultRecord extends \VuFind\RecordDriver
+{
+    /**
+     * Get an array of all the language notes associated with the record.
+     *
+     * @return array
+     */
+    public function getLanguageNote()
+    {
+        return (array)($this->fields['language-notes'] ?? []);
+    }
+}
