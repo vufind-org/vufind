1013074d0a1df5d83ae4b55ebfd8cdeff707bbee
Demian Katz <demian.katz@villanova.edu>
2022-02-01 14:30:54 -0500

    Refactor constants/Laminas app setup separate files.

---
+++ config/application.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/constants.config.php';
+
+chdir(APPLICATION_PATH);
+
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    $loader = include 'vendor/autoload.php';
+}
+
+if (!class_exists('Laminas\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load Laminas autoloader.');
+}
+
+// Return the application!
+return Laminas\Mvc\Application::init(require 'config/application.config.php');
