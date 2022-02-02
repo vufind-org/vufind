454f23a5191da53e8f3a6438a14db69ee74e40f8
Demian Katz <demian.katz@villanova.edu>
2022-02-02 11:36:51 -0500

    Improve cover tests. (#2304)
    - Fix incorrect annotations
    - Improve typing
    - Simplify assertions

--- module/VuFind/tests/unit-tests/src/VuFindTest/Content/Covers/LibraryThingTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Content/Covers/LibraryThingTest.php
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @category VuFind
- * @package  Search
+ * @package  Tests
  * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org
@@ -35,7 +35,7 @@ use VuFindCode\ISBN;
  * Unit tests for LibraryThing cover loader.
  *
  * @category VuFind
- * @package  Search
+ * @package  Tests
  * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org
@@ -47,7 +47,7 @@ class LibraryThingTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testValidCoverLoading()
+    public function testValidCoverLoading(): void
     {
         $loader = new LibraryThing();
         $this->assertEquals(
@@ -65,9 +65,9 @@ class LibraryThingTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testMissingIsbn()
+    public function testMissingIsbn(): void
     {
         $loader = new LibraryThing();
-        $this->assertEquals(false, $loader->getUrl('mykey', 'small', []));
+        $this->assertFalse($loader->getUrl('mykey', 'small', []));
     }
 }
