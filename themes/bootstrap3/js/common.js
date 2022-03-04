199718f9d9e1d6933cd813c0bef7b248f3de9b4e
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2022-03-04 10:14:37 +0200

    Update common.js

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -500,7 +500,7 @@ function checkForBrowserSupport() {
     // Object destructuring
     let {c, d} = b;
     // Spread operator
-    let e = [...d, 4, c]; // lgtm[js/useless-assignment-to-local]
+    let e = [...d, 4, c]; // lgtm[js/unused-local-variable]
   } catch (exception) {
     var outdated = document.getElementById('browser-outdated');
     if (outdated) {
