vufind-org/vufind

commit 642b875dc1afe652facd0ac123d86af519c83a6d

Fix duplicate checkbox display when using multiple SideFacets instances. (#2358)

module/VuFind/src/VuFind/Recommend/SideFacets.php
@@ -256,6 +256,10 @@ class SideFacets extends AbstractFacets
      */
     public function getCheckboxFacetSet()
     {
+        // If no checkboxes are configured, return nothing:
+        if (empty($this->checkboxFacets)) {
+            return [];
+        }
         return $this->results->getParams()
             ->getCheckboxFacets(array_keys($this->checkboxFacets));
     }

module/VuFind/tests/unit-tests/src/VuFindTest/Recommend/SideFacetsTest.php
@@ -28,6 +28,9 @@
 namespace VuFindTest\Recommend;
 
 use VuFind\Recommend\SideFacets;
+use VuFind\Search\Solr\HierarchicalFacetHelper;
+use VuFind\Search\Solr\Params;
+use VuFind\Search\Solr\Results;
 
 /**
  * SideFacets recommendation module Test Class
@@ -45,7 +48,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetResults()
+    public function testGetResults(): void
     {
         $results = $this->getMockResults();
         $sf = $this->getSideFacets(null, $results);
@@ -57,7 +60,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testHierarchicalGetters()
+    public function testHierarchicalGetters(): void
     {
         $configLoader = $this->getMockConfigLoader(
             [
@@ -77,10 +80,12 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testMissingHierarchicalFacetHelper()
+    public function testMissingHierarchicalFacetHelper(): void
     {
         $this->expectException(\Exception::class);
-        $this->expectExceptionMessage('VuFind\\Recommend\\SideFacets: hierarchical facet helper unavailable');
+        $this->expectExceptionMessage(
+            'VuFind\\Recommend\\SideFacets: hierarchical facet helper unavailable'
+        );
 
         $configLoader = $this->getMockConfigLoader(
             [
@@ -106,7 +111,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testFacetInit()
+    public function testFacetInit(): void
     {
         $configLoader = $this->getMockConfigLoader(
             [
@@ -123,9 +128,17 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
         );
         $results = $this->getMockResults();
         $params = $results->getParams();
-        $params->expects($this->once())->method('addFacet')->with($this->equalTo('format'), $this->equalTo('Format'), $this->equalTo(true));
-        $params->expects($this->once())->method('addCheckboxFacet')->with($this->equalTo('filter'), $this->equalTo('description'));
-        $this->getSideFacets($configLoader, $results, ':~Checkboxes');  // test ~ checkbox flip function
+        $params->expects($this->once())->method('addFacet')
+            ->with(
+                $this->equalTo('format'),
+                $this->equalTo('Format'),
+                $this->equalTo(true)
+            );
+        $params->expects($this->once())
+            ->method('addCheckboxFacet')
+            ->with($this->equalTo('filter'), $this->equalTo('description'));
+        // test ~ checkbox flip function:
+        $this->getSideFacets($configLoader, $results, ':~Checkboxes');
     }
 
     /**
@@ -133,9 +146,12 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetFacetOperator()
+    public function testGetFacetOperator(): void
     {
-        $this->assertEquals('AND', $this->getSideFacets()->getFacetOperator('format')); // default
+        $this->assertEquals(
+            'AND',
+            $this->getSideFacets()->getFacetOperator('format')
+        ); // default
         $configLoader = $this->getMockConfigLoader(
             [
                 'Results' => [
@@ -155,9 +171,11 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testExcludeAllowed()
+    public function testExcludeAllowed(): void
     {
-        $this->assertFalse($this->getSideFacets()->excludeAllowed('format')); // default
+        $this->assertFalse(
+            $this->getSideFacets()->excludeAllowed('format')
+        ); // default
         $configLoader = $this->getMockConfigLoader(
             [
                 'Results' => [
@@ -177,7 +195,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetAllRangeFacets()
+    public function testGetAllRangeFacets(): void
     {
         $config = [
             'SpecialFacets' => [
@@ -199,7 +217,9 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
         $sf = $this->getSideFacets($this->getMockConfigLoader($config), $results);
         $expected = [
             'date' => ['type' => 'date', 'values' => ['1900', '1905']],
-            'fullDate' => ['type' => 'fulldate', 'values' => ['1900-01-01', '1905-12-31']],
+            'fullDate' => [
+                'type' => 'fulldate', 'values' => ['1900-01-01', '1905-12-31']
+            ],
             'generic' => ['type' => 'generic', 'values' => ['A', 'Z']],
             'numeric' => ['type' => 'numeric', 'values' => ['1', '9']],
         ];
@@ -211,7 +231,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetCollapsedFacetsDefault()
+    public function testGetCollapsedFacetsDefault(): void
     {
         $this->assertEquals([], $this->getSideFacets()->getCollapsedFacets());
     }
@@ -221,7 +241,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetCollapsedFacetsDelimitedList()
+    public function testGetCollapsedFacetsDelimitedList(): void
     {
         $config = [
             'Results_Settings' => ['collapsedFacets' => '   foo, bar,baz   '],
@@ -235,7 +255,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testGetCollapsedFacetsWildcard()
+    public function testGetCollapsedFacetsWildcard(): void
     {
         $config = [
             'Results' => [
@@ -243,30 +263,68 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
             ],
             'Results_Settings' => ['collapsedFacets' => '*'],
         ];
-        $filters = [
-            'format' => [
-                ['value' => 'foo'],
-                ['value' => 'bar'],
-            ],
-        ];
         $results = $this->getMockResults();
         $sf = $this->getSideFacets($this->getMockConfigLoader($config), $results);
         $this->assertEquals(['format'], $sf->getCollapsedFacets());
     }
 
     /**
+     * Test that getCheckboxFacetSet returns an empty array when no checkboxes are
+     * requested.
+     *
+     * @return void
+     */
+    public function testGetCheckboxFacetSetDefaultsToEmptyArray(): void
+    {
+        $results = $this->getMockResults();
+        $params = $results->getParams();
+        $params->expects($this->never())->method('getCheckboxFacets');
+        $params->expects($this->never())->method('addCheckboxFacet');
+        $sf = $this->getSideFacets(null, $results);
+        $this->assertEquals([], $sf->getCheckboxFacetSet());
+    }
+
+    /**
+     * Test that getCheckboxFacetSet returns values when expected.
+     *
+     * @return void
+     */
+    public function testGetCheckboxFacetSetReturnsValueWhenAppropriate(): void
+    {
+        $configLoader = $this->getMockConfigLoader(
+            ['Checkboxes' => ['foo' => 'bar']]
+        );
+        $checkboxData = ['fake result'];
+        $results = $this->getMockResults();
+        $params = $results->getParams();
+        $params->expects($this->once())->method('getCheckboxFacets')
+            ->with($this->equalTo(['foo']))
+            ->will($this->returnValue($checkboxData));
+        $params->expects($this->once())->method('addCheckboxFacet')
+            ->with($this->equalTo('foo'), $this->equalTo('bar'));
+        $sf = $this->getSideFacets($configLoader, $results, ':Checkboxes');
+        $this->assertEquals($checkboxData, $sf->getCheckboxFacetSet());
+    }
+
+    /**
      * Get a fully configured module
      *
-     * @param \VuFind\Config\PluginManager                $configLoader config loader
-     * @param \VuFind\Search\Solr\Results                 $results      results object
-     * @param string                                      $settings     settings
-     * @param \Laminas\Stdlib\Parameters                     $request      request
-     * @param \VuFind\Search\Solr\HierarchicalFacetHelper $facetHelper  hierarchical facet helper (true to build default, null to omit)
+     * @param \VuFind\Config\PluginManager $configLoader config loader
+     * @param Results                      $results      results object
+     * @param string                       $settings     settings
+     * @param \Laminas\Stdlib\Parameters   $request      request
+     * @param HierarchicalFacetHelper|bool $facetHelper  hierarchical facet helper
+     * (true to build default, null to omit)
      *
      * @return SideFacets
      */
-    protected function getSideFacets($configLoader = null, $results = null, $settings = '', $request = null, $facetHelper = true)
-    {
+    protected function getSideFacets(
+        \VuFind\Config\PluginManager $configLoader = null,
+        Results $results = null,
+        string $settings = '',
+        \Laminas\Stdlib\Parameters $request = null,
+        $facetHelper = true
+    ): SideFacets {
         if (null === $configLoader) {
             $configLoader = $this->getMockConfigLoader();
         }
@@ -294,8 +352,10 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @return \VuFind\Config\PluginManager
      */
-    protected function getMockConfigLoader($config = [], $key = 'facets')
-    {
+    protected function getMockConfigLoader(
+        array $config = [],
+        string $key = 'facets'
+    ): \VuFind\Config\PluginManager {
         $loader = $this->getMockBuilder(\VuFind\Config\PluginManager::class)
             ->disableOriginalConstructor()->getMock();
         $loader->expects($this->once())->method('get')->with($this->equalTo($key))
@@ -306,11 +366,11 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
     /**
      * Get a mock results object.
      *
-     * @param \VuFind\Search\Solr\Params $params Params to include in container.
+     * @param Params $params Params to include in container.
      *
-     * @return \VuFind\Search\Solr\Results
+     * @return Results
      */
-    protected function getMockResults($params = null)
+    protected function getMockResults(Params $params = null): Results
     {
         if (null === $params) {
             $params = $this->getMockParams();
@@ -327,9 +387,9 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
      *
      * @param \VuFindSearch\Query\Query $query Query to include in container.
      *
-     * @return \VuFind\Search\Solr\Params
+     * @return Params
      */
-    protected function getMockParams($query = null)
+    protected function getMockParams(\VuFindSearch\Query\Query $query = null): Params
     {
         if (null === $query) {
             $query = new \VuFindSearch\Query\Query('foo', 'bar');

commit 36216f30dd77cdea9c479bb48f1afde53b2036b0

Fix filtering of dynamic checkbox facets. - Fixes EDS bug introduced in #2358.

config/vufind/searches.ini
@@ -301,13 +301,18 @@ WorkKeys = year
 ;       displaying in a small result set. (default = 0)
 ;       [facet-n] A facet to apply to the random selection
 ;       [facetvalue-n] The facet value to apply to the random selection
-; SideFacets:[regular facet section]:[checkbox facet section]:[ini name]
+; SideFacets:[regular facet section]:[checkbox facet section]:[ini name
+;       :[include dynamic checkboxes]
 ;       Display the specified facets, where [ini name] is the name of an ini file
 ;       in your config directory (defaults to "facets" if not supplied),
 ;       [regular facet section] is the name of a section of the ini file containing
 ;       standard facet settings (defaults to "Results" if not specified),
-;       and [checkbox facet section] is the name of a section of the ini file
-;       containing checkbox facet settings (leave blank for no checkbox facets).
+;       [checkbox facet section] is the name of a section of the ini file
+;       containing checkbox facet settings (leave blank for no checkbox facets), and
+;       [include dynamic checkboxes] is a true/false value indicating whether or not
+;       to display dynamically-generated checkbox facets (it defaults to true, but
+;       if you use multiple instances of SideFacets to split up your display, you
+;       should set it to false in all but one of the instances to avoid duplication).
 ;       Checkbox facets are normally in filter => label format; prefix the section
 ;       name with ~ to reverse this and use label => filter format (useful if your
 ;       filters contain values that are illegal in configuration keys -- e.g. []).

module/VuFind/src/VuFind/Recommend/SideFacets.php
@@ -86,6 +86,14 @@ class SideFacets extends AbstractFacets
     protected $checkboxFacets = [];
 
     /**
+     * Should we display dynamically-generated checkbox facets that are not
+     * explicitly configured in $checkboxFacets?
+     *
+     * @var bool
+     */
+    protected $showDynamicCheckboxFacets = true;
+
+    /**
      * Settings controlling how lightbox is used for facet display.
      *
      * @var bool|string
@@ -156,6 +164,7 @@ class SideFacets extends AbstractFacets
         $mainSection = empty($settings[0]) ? 'Results' : $settings[0];
         $checkboxSection = $settings[1] ?? false;
         $iniName = $settings[2] ?? 'facets';
+        $showDynamicCheckboxFacets = $settings[3] ?? true;
 
         // Load the desired facet information...
         $config = $this->configLoader->get($iniName);
@@ -195,6 +204,11 @@ class SideFacets extends AbstractFacets
             ? $config->$checkboxSection->toArray() : [];
         if ($flipCheckboxes) {
             $this->checkboxFacets = array_flip($this->checkboxFacets);
+        }
+        if (!$showDynamicCheckboxFacets
+            || strtolower(trim($showDynamicCheckboxFacets)) === 'false'
+        ) {
+            $this->showDynamicCheckboxFacets = false;
         }
 
         // Show more settings:
@@ -256,12 +270,10 @@ class SideFacets extends AbstractFacets
      */
     public function getCheckboxFacetSet()
     {
-        // If no checkboxes are configured, return nothing:
-        if (empty($this->checkboxFacets)) {
-            return [];
-        }
-        return $this->results->getParams()
-            ->getCheckboxFacets(array_keys($this->checkboxFacets));
+        return $this->results->getParams()->getCheckboxFacets(
+            array_keys($this->checkboxFacets),
+            $this->showDynamicCheckboxFacets
+        );
     }
 
     /**

module/VuFind/src/VuFind/Search/Base/Params.php
@@ -950,18 +950,20 @@ class Params
      * will be applied to the search.  When the checkbox is not checked, no filter
      * will be applied.
      *
-     * @param string $filter [field]:[value] pair to associate with checkbox
-     * @param string $desc   Description to associate with the checkbox
+     * @param string $filter  [field]:[value] pair to associate with checkbox
+     * @param string $desc    Description to associate with the checkbox
+     * @param bool   $dynamic Is this being added dynamically (true) or in response
+     * to a user configuration (false)?
      *
      * @return void
      */
-    public function addCheckboxFacet($filter, $desc)
+    public function addCheckboxFacet($filter, $desc, $dynamic = false)
     {
         // Extract the facet field name from the filter, then add the
         // relevant information to the array.
         [$fieldName] = explode(':', $filter);
         $this->checkboxFacets[$fieldName][$filter]
-            = ['desc' => $desc, 'filter' => $filter];
+            = compact('desc', 'filter', 'dynamic');
     }
 
     /**
@@ -1146,20 +1148,26 @@ class Params
     /**
      * Get information on the current state of the boolean checkbox facets.
      *
-     * @param array $include List of checkbox filters to return (null for all)
+     * @param array $include        List of checkbox filters to return (null for all)
+     * @param bool  $includeDynamic Should we include dynamically-generated
+     * checkboxes that are not part of the include list above?
      *
      * @return array
      */
-    public function getCheckboxFacets(array $include = null)
-    {
+    public function getCheckboxFacets(
+        array $include = null,
+        bool $includeDynamic = true
+    ) {
         // Build up an array of checkbox facets with status booleans and
         // toggle URLs.
         $result = [];
         foreach ($this->checkboxFacets as $facets) {
             foreach ($facets as $facet) {
                 // If the current filter is not on the include list, skip it (but
-                // accept everything if the include list is empty).
-                if (!empty($include) && !in_array($facet['filter'], $include)) {
+                // accept everything if the include list is null).
+                if (($include !== null && !in_array($facet['filter'], $include))
+                    && !($includeDynamic && $facet['dynamic'])
+                ) {
                     continue;
                 }
                 $facet['selected'] = $this->hasFilter($facet['filter']);

module/VuFind/src/VuFind/Search/EDS/Params.php
@@ -301,7 +301,8 @@ class Params extends \VuFind\Search\Base\Params
             foreach ($ssLimiters as $ssLimiter) {
                 $this->addCheckboxFacet(
                     $ssLimiter['selectedvalue'],
-                    $ssLimiter['description']
+                    $ssLimiter['description'],
+                    true
                 );
             }
         }
@@ -321,7 +322,8 @@ class Params extends \VuFind\Search\Base\Params
             foreach ($availableExpanders as $expander) {
                 $this->addCheckboxFacet(
                     $expander['selectedvalue'],
-                    $expander['description']
+                    $expander['description'],
+                    true
                 );
             }
         }

module/VuFind/src/VuFind/Search/Solr/Params.php
@@ -672,14 +672,18 @@ class Params extends \VuFind\Search\Base\Params
     /**
      * Get information on the current state of the boolean checkbox facets.
      *
-     * @param array $include List of checkbox filters to return (null for all)
+     * @param array $include        List of checkbox filters to return (null for all)
+     * @param bool  $includeDynamic Should we include dynamically-generated
+     * checkboxes that are not part of the include list above?
      *
      * @return array
      */
-    public function getCheckboxFacets(array $include = null)
-    {
+    public function getCheckboxFacets(
+        array $include = null,
+        bool $includeDynamic = true
+    ) {
         // Grab checkbox facet details using the standard method:
-        $facets = parent::getCheckboxFacets($include);
+        $facets = parent::getCheckboxFacets($include, $includeDynamic);
 
         $config = $this->configLoader->get($this->getOptions()->getFacetsIni());
         $filterField = $config->CustomFilters->custom_filter_field ?? 'vufind';

module/VuFind/src/VuFind/Search/Summon/Params.php
@@ -168,14 +168,18 @@ class Params extends \VuFind\Search\Base\Params
     /**
      * Get information on the current state of the boolean checkbox facets.
      *
-     * @param array $include List of checkbox filters to return (null for all)
+     * @param array $include        List of checkbox filters to return (null for all)
+     * @param bool  $includeDynamic Should we include dynamically-generated
+     * checkboxes that are not part of the include list above?
      *
      * @return array
      */
-    public function getCheckboxFacets(array $include = null)
-    {
+    public function getCheckboxFacets(
+        array $include = null,
+        bool $includeDynamic = true
+    ) {
         // Grab checkbox facet details using the standard method:
-        $facets = parent::getCheckboxFacets($include);
+        $facets = parent::getCheckboxFacets($include, $includeDynamic);
 
         // Special case -- if we have a "holdings only" or "expand query" facet,
         // we want this to always appear, even on the "no results" screen, since

module/VuFind/tests/unit-tests/src/VuFindTest/Recommend/SideFacetsTest.php
@@ -278,7 +278,9 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
     {
         $results = $this->getMockResults();
         $params = $results->getParams();
-        $params->expects($this->never())->method('getCheckboxFacets');
+        $params->expects($this->once())->method('getCheckboxFacets')
+            ->with($this->equalTo([]), $this->equalTo(true))
+            ->will($this->returnValue([]));
         $params->expects($this->never())->method('addCheckboxFacet');
         $sf = $this->getSideFacets(null, $results);
         $this->assertEquals([], $sf->getCheckboxFacetSet());
@@ -298,7 +300,7 @@ class SideFacetsTest extends \PHPUnit\Framework\TestCase
         $results = $this->getMockResults();
         $params = $results->getParams();
         $params->expects($this->once())->method('getCheckboxFacets')
-            ->with($this->equalTo(['foo']))
+            ->with($this->equalTo(['foo']), $this->equalTo(true))
             ->will($this->returnValue($checkboxData));
         $params->expects($this->once())->method('addCheckboxFacet')
             ->with($this->equalTo('foo'), $this->equalTo('bar'));

module/VuFind/tests/unit-tests/src/VuFindTest/Search/Base/ParamsTest.php
@@ -111,7 +111,8 @@ class ParamsTest extends \PHPUnit\Framework\TestCase
             'desc' => 'checkbox_label',
             'filter' => 'foo:bar',
             'selected' => false,
-            'alwaysVisible' => false
+            'alwaysVisible' => false,
+            'dynamic' => false,
         ];
         $expectedSelected['selected'] = true;
 

module/VuFind/tests/unit-tests/src/VuFindTest/Search/Solr/ParamsTest.php
@@ -147,12 +147,14 @@ class ParamsTest extends \PHPUnit\Framework\TestCase
                     'filter' => 'format:book',
                     'selected' => false,
                     'alwaysVisible' => false,
+                    'dynamic' => false,
                 ],
                 [
                     'desc' => 'Inverted filter',
                     'filter' => 'vufind:inverted',
                     'selected' => false,
                     'alwaysVisible' => true,
+                    'dynamic' => false,
                 ],
             ],
             $params->getCheckboxFacets()