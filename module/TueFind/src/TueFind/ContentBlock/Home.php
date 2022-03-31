6d9b3b568f2312d837140d3b20119ead703b6f2d
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-03-31 13:25:57 +0200

    VuFind 8: Move remaining "static" pages to "content"

--- module/TueFind/src/TueFind/ContentBlock/Home.php
+++ module/TueFind/src/TueFind/ContentBlock/Home.php
@@ -23,6 +23,10 @@ class Home implements \VuFind\ContentBlock\ContentBlockInterface, RequestAwareIn
         // e.g. other Home-like pages with panels only and full-width backgrounds.
         $subpage = $this->request->getQuery('subpage', 'Home');
 
+        if ($subpage != 'Home') {
+            $subpage = 'subpage/' . $subpage;
+        }
+
         // Expose the block object directly by default.
         return ['target' => $this->target,
                 'subpage' => $subpage ];
