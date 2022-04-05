ba6b9beb0f8b14abca126b86480e16c0799d2c31
Demian Katz <demian.katz@villanova.edu>
2022-04-05 15:50:00 -0400

    Add tests for VuFind\Search\Solr\Results.

---
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Search/Solr/ResultsTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Solr Search Object Results Test
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2022.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+namespace VuFindTest\Search\Solr;
+
+use VuFind\Config\PluginManager;
+use VuFind\Record\Loader;
+use VuFind\Search\Solr\Options;
+use VuFind\Search\Solr\Params;
+use VuFind\Search\Solr\Results;
+use VuFind\Search\Solr\SpellingProcessor;
+use VuFindSearch\Service as SearchService;
+
+/**
+ * Solr Search Object Parameters Test
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+class ResultsTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test CursorMark functionality.
+     *
+     * @return void
+     */
+    public function testCursorMark(): void
+    {
+        $results = $this->getResults();
+        $results->setCursorMark('foo');
+        $this->assertEquals('foo', $results->getCursorMark());
+    }
+
+    /**
+     * Test spelling processor support.
+     *
+     * @return void
+     */
+    public function testSpellingProcessor(): void
+    {
+        $results = $this->getResults();
+        $defaultProcessor = $results->getSpellingProcessor();
+        $this->assertTrue(
+            $defaultProcessor instanceof SpellingProcessor,
+            'default spelling processor was created'
+        );
+        $mockProcessor = $this->createMock(SpellingProcessor::class);
+        $results->setSpellingProcessor($mockProcessor);
+        $this->assertEquals($mockProcessor, $results->getSpellingProcessor());
+        $this->assertNotEquals($defaultProcessor, $mockProcessor);
+    }
+
+    /**
+     * Get Results object
+     *
+     * @return Results
+     */
+    protected function getResults(
+        Params $params = null,
+        SearchService $searchService = null,
+        Loader $loader = null
+    ): Results {
+        return new Results(
+            $params ?? $this->getParams(),
+            $searchService ?? $this->createMock(SearchService::class),
+            $loader ?? $this->createMock(Loader::class)
+        );
+    }
+
+    /**
+     * Get Params object
+     *
+     * @param Options       $options    Options object (null to create)
+     * @param PluginManager $mockConfig Mock config plugin manager (null to create)
+     *
+     * @return Params
+     */
+    protected function getParams(
+        Options $options = null,
+        PluginManager $mockConfig = null
+    ): Params {
+        $mockConfig = $mockConfig ?? $this->createMock(PluginManager::class);
+        return new Params(
+            $options ?? new Options($mockConfig),
+            $mockConfig
+        );
+    }
+}
