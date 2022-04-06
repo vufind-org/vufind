f4e98c8198046faccc7d0baad37bcd094b94ccaf
Demian Katz <demian.katz@villanova.edu>
2022-04-06 08:40:30 -0400

    More browser-compatible logic.

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -111,7 +111,8 @@ var VuFind = (function VuFind() {
   };
   var loading = function loading(text = null, extraClass = "") {
     let className = ("loading-spinner " + extraClass).trim();
-    return '<span class="' + className + '">' + icon('spinner') + translate(text ?? 'loading_ellipsis') + '</span>';
+    let string = text === null ? 'loading_ellipsis' : text;
+    return '<span class="' + className + '">' + icon('spinner') + translate(string) + '</span>';
   };
 
   /**
