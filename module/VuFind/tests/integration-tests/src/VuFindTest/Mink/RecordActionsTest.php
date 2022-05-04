5e57d15ccbce4bfca7c0fb2bdae40af5e9c64773
Ere Maijala <ere.maijala@helsinki.fi>
2022-05-04 15:37:38 +0300

    Revert retry setting.

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/RecordActionsTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/RecordActionsTest.php
@@ -37,7 +37,7 @@ namespace VuFindTest\Mink;
  * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Page
- * @retry    0
+ * @retry    4
  */
 final class RecordActionsTest extends \VuFindTest\Integration\MinkTestCase
 {
