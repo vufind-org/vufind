06b182e4c2e963fa3742222f4226b0ca0679b349
Robert Lange <robert.lange@uni-leipzig.de>
2022-05-31 10:32:29 +0200

    advanced search: set focus to first input after adding new search field or group

--- themes/bootstrap3/js/advanced_search.js
+++ themes/bootstrap3/js/advanced_search.js
@@ -3,7 +3,7 @@ var nextGroup = 0;
 var groupLength = [];
 var deleteGroup, deleteSearch;
 
-function addSearch(group, _fieldValues) {
+function addSearch(group, _fieldValues, isUser) {
   var fieldValues = _fieldValues || {};
   // Build the new search
   var inputID = group + '_' + groupLength[group];
@@ -53,6 +53,14 @@ function addSearch(group, _fieldValues) {
     $('#group' + group + ' .adv-term-remove').removeClass('hidden');
   }
   groupLength[group]++;
+
+  // #17985 finc barf
+  if (isUser) {
+    $newSearch.find('input.form-control').focus();
+  } else {
+    $('#group' + group).find('input.form-control').first().focus();
+  }
+
   return false;
 }
 
@@ -67,7 +75,12 @@ deleteSearch = function _deleteSearch(group, sindex) {
   }
   if (groupLength[group] > 1) {
     groupLength[group]--;
-    $('#search' + group + '_' + groupLength[group]).remove();
+    var toRemove = $('#search' + group + '_' + groupLength[group]);
+    var parent = toRemove.parent();
+    toRemove.remove();
+    if (parent.length) {
+      parent.find('.adv-search input.form-control').focus();
+    }
     if (groupLength[group] === 1) {
       $('#group' + group + ' .adv-term-remove').addClass('hidden'); // Hide x
     }
@@ -91,7 +104,7 @@ function addGroup(_firstTerm, _firstField, _join) {
     .attr('id', 'add_search_link_' + nextGroup)
     .data('nextGroup', nextGroup)
     .click(function addSearchHandler() {
-      return addSearch($(this).data('nextGroup'));
+      return addSearch($(this).data('nextGroup'), {}, true);
     })
     .removeClass('hidden');
   $newGroup.find('.adv-group-close')
@@ -111,13 +124,17 @@ function addGroup(_firstTerm, _firstField, _join) {
   $('#groupPlaceHolder').before($newGroup);
   // Populate
   groupLength[nextGroup] = 0;
-  addSearch(nextGroup, {term: firstTerm, field: firstField});
+  addSearch(nextGroup, {term: firstTerm, field: firstField}, false);
   // Show join menu
   if (nextGroup > 0) {
     $('#groupJoin').removeClass('hidden');
     // Show x
     $('.adv-group-close').removeClass('hidden');
   }
+
+  // #17985 finc barf
+  $newGroup.children('input.form-control').first().focus();
+
   return nextGroup++;
 }
 
