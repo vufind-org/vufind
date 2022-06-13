3825ee3ceff6fcea4cf2e8961d09528ad1a03e08
jriedl <johannes.riedl@uni-tuebingen.de>
2022-06-13 17:46:55 +0200

    Some Cleanup

--- module/TueFind/src/TueFind/Search/Search3/FacetCache.php
+++ module/TueFind/src/TueFind/Search/Search3/FacetCache.php
@@ -1,41 +1,6 @@
 <?php
-/**
- * Search2 FacetCache.
- *
- * PHP version 7
- *
- * Copyright (C) Villanova University 2018.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @category VuFind
- * @package  Search_Search2
- * @author   Demian Katz <demian.katz@villanova.edu>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
- */
 namespace TueFind\Search\Search3;
 
-/**
- * Search2 FacetCache.
- *
- * @category VuFind
- * @package  Search_Search2
- * @author   Demian Katz <demian.katz@villanova.edu>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
- */
 class FacetCache extends \VuFind\Search\Base\FacetCache
 {
     /**
