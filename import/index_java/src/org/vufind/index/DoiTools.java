723af179723c956868f477ad24bb3c9cae7658dc
Demian Katz <demian.katz@villanova.edu>
2022-06-21 13:46:45 -0400

    Improve DOI extraction.

--- import/index_java/src/org/vufind/index/DoiTools.java
+++ import/index_java/src/org/vufind/index/DoiTools.java
@@ -19,6 +19,8 @@ package org.vufind.index;
  */
 
 import java.util.LinkedHashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Set;
 import org.marc4j.marc.Record;
 import org.solrmarc.index.SolrIndexer;
@@ -45,6 +47,35 @@ public class DoiTools
             // If the base URL is found in the string, crop it off for our DOI!
             if (current.startsWith(baseUrl)) {
                 result.add(current.substring(baseUrl.length()));
+            }
+        }
+
+        // If we found no matches, return null; otherwise, return our results:
+        return result.isEmpty() ? null : result;
+    }
+
+    /**
+     * Extract DOIs from URLs matching the specified regular expression
+     * @param record MARC record
+     * @param fieldSpec taglist for URL fields
+     * @param regEx Regular expression for matching URLs
+     * @param groupIndex Index of the grouped expression in regEx containing the DOI to extract (passed as a String instead of an Integer due to SolrMarc limitations)
+     * @return Set of DOIs
+     */
+    public Set<String> getDoisFromUrlWithRegEx(final Record record, String fieldSpec, String regEx, String groupIndex) {
+        // Build the regular expression:
+        Pattern pattern = Pattern.compile(regEx);
+
+        // Initialize our return value:
+        Set<String> result = new LinkedHashSet<String>();
+
+        // Loop through the specified MARC fields:
+        Set<String> input = SolrIndexer.instance().getFieldList(record, fieldSpec);
+        for (String current: input) {
+            // If the string matches our pattern, get the extracted DOI.
+            Matcher matcher = pattern.matcher(current);
+            if (matcher.find()) {
+                result.add(matcher.group(Integer.parseInt(groupIndex)));
             }
         }
 
