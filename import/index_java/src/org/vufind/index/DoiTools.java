dbf24e0df644b4dbde2f582835f47e7f16a7edc6
Demian Katz <demian.katz@villanova.edu>
2022-06-21 13:57:25 -0400

    Add URL decoding.

--- import/index_java/src/org/vufind/index/DoiTools.java
+++ import/index_java/src/org/vufind/index/DoiTools.java
@@ -17,11 +17,13 @@ package org.vufind.index;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.LinkedHashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Set;
+import org.apache.log4j.Logger;
 import org.marc4j.marc.Record;
 import org.solrmarc.index.SolrIndexer;
 
@@ -30,6 +32,22 @@ import org.solrmarc.index.SolrIndexer;
  */
 public class DoiTools
 {
+    // Initialize logging category
+    static Logger logger = Logger.getLogger(DoiTools.class.getName());
+
+    /**
+     * URL-decode a DOI that has been extracted from a URL
+     * @param doi DOI to decode
+     * @return Decoded DOI
+     */
+    protected String decodeDoi(String doi) {
+        try {
+            return URLDecoder.decode(doi, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e);
+            return doi;
+        }
+    }
     /**
      * Extract DOIs from URLs with the specified prefix
      * @param record MARC record
@@ -46,7 +64,7 @@ public class DoiTools
         for (String current: input) {
             // If the base URL is found in the string, crop it off for our DOI!
             if (current.startsWith(baseUrl)) {
-                result.add(current.substring(baseUrl.length()));
+                result.add(decodeDoi(current.substring(baseUrl.length())));
             }
         }
 
@@ -75,7 +93,7 @@ public class DoiTools
             // If the string matches our pattern, get the extracted DOI.
             Matcher matcher = pattern.matcher(current);
             if (matcher.find()) {
-                result.add(matcher.group(Integer.parseInt(groupIndex)));
+                result.add(decodeDoi(matcher.group(Integer.parseInt(groupIndex))));
             }
         }
 
