54bfce0e73da162466081cad3059e9497e966b9a
Ere Maijala <ere.maijala@helsinki.fi>
2022-08-05 10:17:53 +0300

    Fix expiration callback.

--- module/VuFind/src/VuFind/Db/Table/AccessToken.php
+++ module/VuFind/src/VuFind/Db/Table/AccessToken.php
@@ -127,14 +127,14 @@ class AccessToken extends Gateway
     /**
      * Update the select statement to find records to delete.
      *
-     * @param Select $select  Select clause
-     * @param int    $daysOld Age in days of an "expired" record.
+     * @param Select $select    Select clause
+     * @param string $dateLimit Date threshold of an "expired" record in format
+     * 'Y-m-d H:i:s'.
      *
      * @return void
      */
-    protected function expirationCallback($select, $daysOld)
+    protected function expirationCallback($select, $dateLimit)
     {
-        $expireDate = time() - $daysOld * 24 * 60 * 60;
-        $select->where->lessThan('created', date('Y-m-d H:i:s', $expireDate));
+        $select->where->lessThan('created', $dateLimit);
     }
 }
