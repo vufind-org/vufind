7d5243dd25440d43549d45aa1d4e45372277e993
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-08-11 10:42:11 +0200

    DSpace 6: Metadata fix

--- module/TueFind/src/TueFind/MetadataVocabulary/DSpace6.php
+++ module/TueFind/src/TueFind/MetadataVocabulary/DSpace6.php
@@ -7,27 +7,15 @@ class DSpace6 extends \VuFind\MetadataVocabulary\AbstractBase
     protected $dspaceMap = [
         [
             'key' => 'dc.contributor.author',
-            'schema' => 'dc',
             'source' => 'author',
-            'element' => 'contributor',
-            'qualifier' => 'author',
-            'language' => '',
         ],
         [
             'key' => 'dc.date.issued',
-            'schema' => 'dc',
             'source' => 'date',
-            'element' => 'date',
-            'qualifier' => 'issued',
-            'language' => '',
         ],
         [
             'key' => 'dc.language.iso',
-            'schema' => 'dc',
             'source' => 'language',
-            'element' => 'language',
-            'qualifier' => 'iso',
-            'language' => '',
             'map' => [
                 'English' => 'en',
                 'German' => 'de',
@@ -35,36 +23,18 @@ class DSpace6 extends \VuFind\MetadataVocabulary\AbstractBase
         ],
         [
             'key' => 'dc.publisher',
-            'schema' => 'dc',
             'source' => 'publisher',
-            'element' => 'publisher',
-            'qualifier' => '',
-            'language' => '',
         ],
         [
             'key' => 'dc.title',
-            'schema' => 'dc',
             'source' => 'title',
-            'element' => 'title',
-            'qualifier' => '',
-            'language' => '',
         ],
     ];
 
 
-    protected function getMetadataTemplateFromMapEntry(array $mapEntry)
-    {
-        $keysToKeep = ['key', 'language', 'schema', 'element', 'qualifier'];
-        $template = [];
-        foreach ($keysToKeep as $key) {
-            if (isset($mapEntry[$key])) {
-                $template[$key] = $mapEntry[$key];
-            }
-        }
-        return $template;
-    }
-
-    // Example, see: https://wiki.lyrasis.org/display/DSDOC6x/REST+API#RESTAPI-ItemObject
+    // Examples, see:
+    // - https://wiki.lyrasis.org/display/DSDOC6x/REST+API#RESTAPI-ItemObject
+    // - https://wiki.lyrasis.org/display/DSDOC6x/REST+API#RESTAPI-MetadataEntryObject
     public function getMappedData(\VuFind\RecordDriver\AbstractBase $driver)
     {
         $rawData = parent::getGenericData($driver);
@@ -82,7 +52,7 @@ class DSpace6 extends \VuFind\MetadataVocabulary\AbstractBase
             }
 
             foreach ($values as $value) {
-                $dspaceMetadata = $this->getMetadataTemplateFromMapEntry($mapEntry);
+                $dspaceMetadata = ['key' => $mapEntry['key']];
 
                 if (isset($mapEntry['map'])) {
                     if (!isset($mapEntry['map'][$value])) {
