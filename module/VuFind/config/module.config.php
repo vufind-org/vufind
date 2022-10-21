5f4366dc40245e986d7997d273cc4f72a75d622d
Demian Katz <demian.katz@villanova.edu>
2022-10-21 13:02:43 -0400

    Better routing.

--- module/VuFind/config/module.config.php
+++ module/VuFind/config/module.config.php
@@ -137,6 +137,7 @@ $config = [
             'VuFind\Controller\AlphabrowseController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\AuthorController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\AuthorityController' => 'VuFind\Controller\AbstractBaseFactory',
+            'VuFind\Controller\AuthorityRecordController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\BrowseController' => 'VuFind\Controller\AbstractBaseWithConfigFactory',
             'VuFind\Controller\BrowZineController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\CartController' => 'VuFind\Controller\CartControllerFactory',
@@ -201,6 +202,8 @@ $config = [
             'author' => 'VuFind\Controller\AuthorController',
             'Authority' => 'VuFind\Controller\AuthorityController',
             'authority' => 'VuFind\Controller\AuthorityController',
+            'AuthorityRecord' => 'VuFind\Controller\AuthorityRecordController',
+            'authorityrecord' => 'VuFind\Controller\AuthorityRecordController',
             'Browse' => 'VuFind\Controller\BrowseController',
             'browse' => 'VuFind\Controller\BrowseController',
             'BrowZine' => 'VuFind\Controller\BrowZineController',
@@ -664,12 +667,14 @@ $recordRoutes = [
     'eitrecord' => 'EITRecord',
     'missingrecord' => 'MissingRecord',
     'primorecord' => 'PrimoRecord',
-    'solrauthrecord' => 'Authority',
+    'solrauthrecord' => 'AuthorityRecord',
     'summonrecord' => 'SummonRecord',
     'worldcatrecord' => 'WorldcatRecord',
     'search2record' => 'Search2Record',
     'search2collection' => 'Search2Collection',
     'search2collectionrecord' => 'Search2Record',
+    // For backward compatibility with pre-9.0 versions:
+    'legacy-solrauthrecord' => 'Authority',
 ];
 
 // Define non tab record actions
