67fa443929435bbd05611d26ffbb5b9c2e1100b3
Chris Hallberg <chris.hallberg@hey.com>
2022-11-07 12:34:51 -0500

    test: add Component helper unit test.

---
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/ComponentTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Class ComponentTest
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  VuFindTest\View\Helper\Root
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+namespace VuFindTest\View\Helper\Root;
+
+use Laminas\View\Renderer\PhpRenderer;
+use VuFind\View\Helper\Root\Component;
+
+/**
+ * Component Test Class
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+class ComponentTest extends \PHPUnit\Framework\TestCase
+{
+    use \VuFindTest\Feature\ViewTrait;
+
+    /**
+     * Get view helper to test.
+     *
+     * @return Component
+     */
+    protected function getHelper()
+    {
+        $helper = new Component();
+
+        $view = $this->getMockBuilder(PhpRenderer::class)->getMock();
+        $helper->setView($view);
+
+        $helper->getView()->method('render')->will($this->returnArgument(0));
+
+        return $helper;
+    }
+
+    /**
+     * Test basic Component conversion
+     *
+     * @return void
+     */
+    public function testComponent()
+    {
+        $helper = $this->getHelper();
+
+        $this->assertEquals($helper('menu'), '_ui/components/menu');
+        $this->assertEquals($helper('menu/sub/sub'), '_ui/components/menu/sub/sub');
+        $this->assertEquals($helper('@atoms/menu'), '_ui/atoms/menu');
+        $this->assertEquals($helper('@atoms/menu/login'), '_ui/atoms/menu/login');
+    }
+}
