70e33c7c60abc4ecdde58449ed182e0f35f49a36
Demian Katz <demian.katz@villanova.edu>
2022-11-11 11:51:53 -0500

    Remove unused use statement.

--- module/VuFindConsole/src/VuFindConsole/Command/Util/IndexReservesCommand.php
+++ module/VuFindConsole/src/VuFindConsole/Command/Util/IndexReservesCommand.php
@@ -27,7 +27,6 @@
  */
 namespace VuFindConsole\Command\Util;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
