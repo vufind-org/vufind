2b159b86b91aded69d38ad4ef813aae88490fec7
Chris Hallberg <chris.hallberg@hey.com>
2022-11-14 13:37:09 -0500

    docs: documentation and traits.

--- module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/ComponentTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/ComponentTest.php
@@ -5,7 +5,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) Moravian Library 2020.
+ * Copyright (C) Villanova University 2022.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,10 +21,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @category VuFind
- * @package  VuFindTest\View\Helper\Root
- * @author   Josef Moravec <moravec@mzk.cz>
- * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://knihovny.cz Main Page
+ * @package  Tests
+ * @author   Chris Hallberg <challber@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
  */
 namespace VuFindTest\View\Helper\Root;
 
@@ -36,14 +36,12 @@ use VuFind\View\Helper\Root\Component;
  *
  * @category VuFind
  * @package  Tests
- * @author   Josef Moravec <moravec@mzk.cz>
+ * @author   Chris Hallberg <challber@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ * @link     https://vufind.org/wiki/development Wiki
  */
 class ComponentTest extends \PHPUnit\Framework\TestCase
 {
-    use \VuFindTest\Feature\ViewTrait;
-
     /**
      * Get view helper to test.
      *
