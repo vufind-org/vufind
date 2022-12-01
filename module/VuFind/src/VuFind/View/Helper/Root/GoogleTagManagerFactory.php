09c289f1bd5aec9e1846f65d1b8ead810ebb0a26
Maccabee Levine <msl321@lehigh.edu>
2022-12-01 16:15:48 -0500

    Fix copyright date

--- module/VuFind/src/VuFind/View/Helper/Root/GoogleTagManagerFactory.php
+++ module/VuFind/src/VuFind/View/Helper/Root/GoogleTagManagerFactory.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) Villanova University 2018.
+ * Copyright (C) Villanova University 2022.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
