6543edd0bf6db2638b213f379897fe1f3af34a13
Maccabee Levine <msl321@lehigh.edu>
2022-12-06 16:19:12 -0500

    Fix phpcs warnings

--- module/VuFind/src/VuFind/View/Helper/Root/GoogleTagManager.php
+++ module/VuFind/src/VuFind/View/Helper/Root/GoogleTagManager.php
@@ -66,6 +66,7 @@ class GoogleTagManager extends \Laminas\View\Helper\AbstractHelper
             return '';
         }
 
+        // phpcs:disable -- line length should be kept for this vendor snippet 
         $js = <<<END
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -74,6 +75,7 @@ class GoogleTagManager extends \Laminas\View\Helper\AbstractHelper
             n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','{$this->gtmContainerId}');                
             END;
+        // phpcs:enable
         $inlineScript = $this->getView()->plugin('inlinescript');
         $js = $inlineScript(\Laminas\View\Helper\HeadScript::SCRIPT, $js, 'SET');
         return $js;
@@ -90,12 +92,14 @@ class GoogleTagManager extends \Laminas\View\Helper\AbstractHelper
             return '';
         }
 
+        // phpcs:disable -- line length should be kept for this vendor snippet 
         return <<<END
             <!-- Google Tag Manager (noscript) -->
             <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={$this->gtmContainerId}"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <!-- End Google Tag Manager (noscript) -->
             END;
+        // phpcs:enable
     }
 
 }
