df53bea5d313167b80c168f234d6a9f107455ad4
Demian Katz <demian.katz@villanova.edu>
2023-01-25 08:34:18 -0500

    Add new entity.

---
+++ module/VuFind/src/VuFind/Db/Entity/AccessToken.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Entity model for access_token table
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+namespace VuFind\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ChangeTracker
+ *
+ * @category VuFind
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ *
+ * @ORM\Table(name="access_token", indexes={@ORM\Index(name="deleted_index", columns={"deleted"})})
+ * @ORM\Entity
+ */
+class AccessToken implements EntityInterface
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="id", type="string", length=255, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=128, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    protected $type;
+
+    /**
+     * @var \VuFind\Db\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="VuFind\Db\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    protected $user;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime", nullable=false, options={"default"="2000-01-01 00:00:00"})
+     */
+    protected $created = '2000-01-01 00:00:00';
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="data", type="text", length=16777215, nullable=true)
+     */
+    protected $data;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="revoked", type="boolean", nullable=false)
+     */
+    protected $revoked = '0';
+}
