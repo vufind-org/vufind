858b5c5dee78b3b7e42fda4a98176873c31deac7
Chris Hallberg <chris.hallberg@hey.com>
2023-02-24 12:12:24 -0500

    fix: half-baked diff removed.

--- themes/bootstrap3/js/record_versions.js
+++ themes/bootstrap3/js/record_versions.js
@@ -8,34 +8,6 @@ VuFind.register("recordVersions", function recordVersions() {
       if (typeof response.data[key] !== "undefined") {
         $(item.el).html(VuFind.updateCspNonce(response.data[key]));
       }
-<<<<<<< HEAD
-=======
-      $elem.addClass('loaded');
-      $elem.removeClass('hidden');
-      $elem.append('<span class="js-load">' + VuFind.translate('loading_ellipsis') + '</span>');
-      var $item = $(this).parents('.result');
-      var id = $item.find('.hiddenId')[0].value;
-      var source = $item.find('.hiddenSource')[0].value;
-      $.getJSON(
-        VuFind.path + '/AJAX/JSON',
-        {
-          method: 'getRecordVersions',
-          id: id,
-          source: source,
-          sid: VuFind.getCurrentSearchId()
-        }
-      )
-        .done(function onGetVersionsDone(response) {
-          if (response.data.length > 0) {
-            $elem.html(VuFind.updateCspNonce(response.data));
-          } else {
-            $elem.text('');
-          }
-        })
-        .fail(function onGetVersionsFail() {
-          $elem.text(VuFind.translate('error_occurred'));
-        });
->>>>>>> origin/dev
     });
   }
 
