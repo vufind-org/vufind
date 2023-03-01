1e5995e973204e31da9ba4ee50d74ec9ab440c2d
Demian Katz <demian.katz@villanova.edu>
2023-03-01 09:40:47 -0500

    Fix date.

--- module/VuFind/src/VuFind/View/Helper/Root/HtmlSafeJsonEncode.php
+++ module/VuFind/src/VuFind/View/Helper/Root/HtmlSafeJsonEncode.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) Villanova University 2020.
+ * Copyright (C) Villanova University 2023.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
