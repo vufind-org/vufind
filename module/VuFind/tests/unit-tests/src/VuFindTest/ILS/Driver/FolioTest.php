83b1bc9ae10309b0cf79cc58386f5d94b75785ab
Demian Katz <demian.katz@villanova.edu>
2023-03-01 13:28:04 -0500

    Rewrite FOLIO test to better check inputs.

--- module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Driver/FolioTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Driver/FolioTest.php
@@ -64,14 +64,21 @@ class FolioTest extends \PHPUnit\Framework\TestCase
      *
      * @var array
      */
-    protected $testResponses = [];
+    protected $fixtureSteps = [];
 
     /**
-     * Log of requests made during test (reset by each test)
+     * Current fixture step
      *
-     * @var array
+     * @var int
      */
-    protected $testRequestLog = [];
+    protected $currentFixtureStep = 0;
+
+    /**
+     * Current fixture name
+     *
+     * @var string
+     */
+    protected $currentFixture = 'none';
 
     /**
      * Driver under test
@@ -100,16 +107,38 @@ class FolioTest extends \PHPUnit\Framework\TestCase
         $httpHeaders = new \Laminas\Http\Headers();
         $httpHeaders->addHeaders($headers);
         [$httpHeaders, $params] = $this->driver->preRequest($httpHeaders, $params);
-        // Log request
-        $this->testRequestLog[] = compact('method', 'path', 'params') + [
-            'headers' => $httpHeaders->toArray()
-        ];
+
+        // Get the next step of the test, and make assertions as necessary
+        // (we'll skip making assertions if the next step is empty):
+        $testData = $this->fixtureSteps[$this->currentFixtureStep] ?? [];
+        $this->currentFixtureStep++;
+        unset($testData['comment']);
+        if (!empty($testData)) {
+            $msg = "Error in step {$this->currentFixtureStep} of fixture: "
+                . $this->currentFixture;
+            $this->assertEquals($testData['expectedMethod'] ?? 'GET', $method, $msg);
+            $this->assertEquals($testData['expectedPath'] ?? '/', $path, $msg);
+            if (isset($testData['expectedParamsRegEx'])) {
+                $this->assertMatchesRegularExpression(
+                    $testData['expectedParamsRegEx'],
+                    $params,
+                    $msg
+                );
+            } else {
+                $this
+                    ->assertEquals($testData['expectedParams'] ?? [], $params, $msg);
+            }
+            $actualHeaders = $httpHeaders->toArray();
+            foreach ($testData['expectedHeaders'] ?? [] as $header => $expected) {
+                $this->assertEquals($expected, $actualHeaders[$header]);
+            }
+        }
+
         // Create response
-        $testResponse = array_shift($this->testResponses);
         $response = new \Laminas\Http\Response();
-        $response->setStatusCode($testResponse['status'] ?? 200);
-        $response->setContent($testResponse['body'] ?? '');
-        $response->getHeaders()->addHeaders($testResponse['headers'] ?? []);
+        $response->setStatusCode($testData['status'] ?? 200);
+        $response->setContent($testData['body'] ?? '');
+        $response->getHeaders()->addHeaders($testData['headers'] ?? []);
         return $response;
     }
 
@@ -127,9 +156,9 @@ class FolioTest extends \PHPUnit\Framework\TestCase
     protected function createConnector(string $test, array $config = null): void
     {
         // Setup test responses
-        $this->testResponses = $this->getJsonFixture("folio/responses/$test.json");
-        // Reset log
-        $this->testRequestLog = [];
+        $this->fixtureSteps = $this->getJsonFixture("folio/responses/$test.json");
+        $this->currentFixture = $test;
+        $this->currentFixtureStep = 0;
         // Session factory
         $factory = function ($namespace) {
             $manager = new \Laminas\Session\SessionManager();
@@ -156,21 +185,7 @@ class FolioTest extends \PHPUnit\Framework\TestCase
     public function testTokens(): void
     {
         $this->createConnector('get-tokens'); // saves to $this->driver
-        $profile = $this->driver->getMyProfile(['id' => 'whatever']);
-        // Get token
-        // - Right URL
-        $this->assertEquals('/authn/login', $this->testRequestLog[0]['path']);
-        // - Right tenant
-        $this->assertEquals(
-            $this->defaultDriverConfig['API']['tenant'],
-            $this->testRequestLog[0]['headers']['X-Okapi-Tenant']
-        );
-        // Profile request
-        // - Passed correct token
-        $this->assertEquals(
-            'x-okapi-token-config-tenant', // from fixtures: get-tokens.json
-            $this->testRequestLog[1]['headers']['X-Okapi-Token']
-        );
+        $this->driver->getMyProfile(['id' => 'whatever']);
     }
 
     /**
@@ -181,16 +196,7 @@ class FolioTest extends \PHPUnit\Framework\TestCase
     public function testCheckValidToken(): void
     {
         $this->createConnector('check-valid-token');
-        $profile = $this->driver->getMyTransactions(['id' => 'whatever']);
-        // Check token
-        $this->assertEquals('/users', $this->testRequestLog[0]['path']);
-        // Move to method call
-        $this->assertEquals('/circulation/loans', $this->testRequestLog[1]['path']);
-        // - Passed correct token
-        $this->assertEquals(
-            'x-okapi-token-config-tenant', // from fixtures: get-tokens.json (cached)
-            $this->testRequestLog[1]['headers']['X-Okapi-Token']
-        );
+        $this->driver->getMyTransactions(['id' => 'whatever']);
     }
 
     /**
@@ -201,18 +207,7 @@ class FolioTest extends \PHPUnit\Framework\TestCase
     public function testCheckInvalidToken(): void
     {
         $this->createConnector('check-invalid-token');
-        $profile = $this->driver->getPickupLocations(['username' => 'whatever']);
-        // Check token
-        $this->assertEquals('/users', $this->testRequestLog[0]['path']);
-        // Request new token
-        $this->assertEquals('/authn/login', $this->testRequestLog[1]['path']);
-        // Move to method call
-        $this->assertEquals('/service-points', $this->testRequestLog[2]['path']);
-        // - Passed correct token
-        $this->assertEquals(
-            'x-okapi-token-after-invalid', // from fixtures: check-invalid-token.json
-            $this->testRequestLog[2]['headers']['X-Okapi-Token']
-        );
+        $this->driver->getPickupLocations(['username' => 'whatever']);
     }
 
     /**
@@ -249,22 +244,6 @@ class FolioTest extends \PHPUnit\Framework\TestCase
             ],
         ];
         $this->assertEquals($expected, $result);
-        $this->assertEquals(
-            '/circulation/requests/request1',
-            $this->testRequestLog[2]['path']
-        );
-        $this->assertEquals(
-            '{"requesterId":"foo","itemId":"item1","status":"Closed - Cancelled","cancellationReasonId":"75187e8d-e25a-47a7-89ad-23ba612338de"}',
-            $this->testRequestLog[2]['params']
-        );
-        $this->assertEquals(
-            '/circulation/requests/request2',
-            $this->testRequestLog[4]['path']
-        );
-        $this->assertEquals(
-            '{"requesterId":"foo","itemId":"item2","status":"Closed - Cancelled","cancellationReasonId":"75187e8d-e25a-47a7-89ad-23ba612338de"}',
-            $this->testRequestLog[4]['params']
-        );
     }
 
     /**
@@ -274,16 +253,8 @@ class FolioTest extends \PHPUnit\Framework\TestCase
      */
     public function testUnsuccessfulPatronLogin(): void
     {
-        $this->createConnector('empty');
+        $this->createConnector('unsuccessful-patron-login');
         $this->assertNull($this->driver->patronLogin('foo', 'bar'));
-        $this->assertEquals(
-            '/users',
-            $this->testRequestLog[1]['path']
-        );
-        $this->assertEquals(
-            ['query' => 'username == "foo"'],
-            $this->testRequestLog[1]['params']
-        );
     }
 
     /**
@@ -308,22 +279,6 @@ class FolioTest extends \PHPUnit\Framework\TestCase
             'email' => 'fake@fake.com',
         ];
         $this->assertEquals($expected, $result);
-        $this->assertEquals(
-            '/authn/login',
-            $this->testRequestLog[1]['path']
-        );
-        $this->assertEquals(
-            '{"tenant":"config_tenant","username":"foo","password":"bar"}',
-            $this->testRequestLog[1]['params']
-        );
-        $this->assertEquals(
-            '/users',
-            $this->testRequestLog[2]['path']
-        );
-        $this->assertEquals(
-            ['query' => 'username == foo'],
-            $this->testRequestLog[2]['params']
-        );
     }
 
     /**
@@ -347,24 +302,6 @@ class FolioTest extends \PHPUnit\Framework\TestCase
             'status' => 'success',
         ];
         $this->assertEquals($expected, $result);
-        $this->assertEquals(
-            '/circulation/requests',
-            $this->testRequestLog[1]['path']
-        );
-        $request = json_decode($this->testRequestLog[1]['params'], true);
-        // Request date changes on every request, so let's not assert about it:
-        unset($request['requestDate']);
-        $this->assertEquals(
-            [
-                'itemId' => 'record1',
-                'requestType' => 'Page',
-                'requesterId' => 'foo',
-                'fulfilmentPreference' => 'Hold Shelf',
-                'requestExpirationDate' => '2022-01-01',
-                'pickupServicePointId' => 'desk1',
-            ],
-            $request
-        );
     }
 
     /**
@@ -392,14 +329,6 @@ class FolioTest extends \PHPUnit\Framework\TestCase
             ]
         ];
         $this->assertEquals($expected, $result);
-        $this->assertEquals(
-            '/circulation/renew-by-id',
-            $this->testRequestLog[1]['path']
-        );
-        $this->assertEquals(
-            '{"itemId":"record1","userId":"foo"}',
-            $this->testRequestLog[1]['params']
-        );
     }
 
     /**
