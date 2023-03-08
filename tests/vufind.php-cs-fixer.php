71be3da8d10917af8939f4c3bd5bd54039e40df8
Demian Katz <demian.katz@villanova.edu>
2023-03-08 12:44:39 -0500

    Remove rule that conflicts with PSR-12.

--- tests/vufind.php-cs-fixer.php
+++ tests/vufind.php-cs-fixer.php
@@ -36,7 +36,6 @@ $rules = [
     'native_function_casing' => true,
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
-    'no_blank_lines_before_namespace' => true,
     'no_closing_tag' => true,
     'no_empty_comment' => true,
     'no_empty_phpdoc' => true,
