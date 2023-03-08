910bfee73826010135035556c12ccd3e0c688e92
Demian Katz <demian.katz@villanova.edu>
2023-03-08 14:44:01 -0500

    Remove form-attr-polyfill.js

--- themes/bootstrap3/theme.config.php
+++ themes/bootstrap3/theme.config.php
@@ -40,7 +40,6 @@ return [
         ['file' => 'vendor/bootstrap.min.js', 'priority' => 120],
         ['file' => 'vendor/bootstrap-accessibility.min.js', 'priority' => 130],
         ['file' => 'vendor/validator.min.js', 'priority' => 140],
-        ['file' => 'lib/form-attr-polyfill.js', 'priority' => 210], // input[form] polyfill, cannot load conditionally, since we need all versions of IE
         ['file' => 'lib/autocomplete.js', 'priority' => 220],
         ['file' => 'common.js', 'priority' => 310],
         ['file' => 'lightbox.js', 'priority' => 320],
