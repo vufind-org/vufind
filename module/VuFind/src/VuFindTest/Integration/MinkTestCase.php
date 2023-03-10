2f859d4dba6b1a9c37106d7574373240260530aa
Ere Maijala <ere.maijala@helsinki.fi>
2023-03-10 20:07:28 +0200

    Ensure cleanup is done before throwing any HTML validation exceptions.

--- module/VuFind/src/VuFindTest/Integration/MinkTestCase.php
+++ module/VuFind/src/VuFindTest/Integration/MinkTestCase.php
@@ -952,12 +952,22 @@ EOS
             }
         }
 
+        $htmlValidationException = null;
         if (!$this->hasFailed()) {
-            $this->validateHtml();
+            try {
+                $this->validateHtml();
+            } catch (\Exception $e) {
+                // Store the exception and throw after cleanup:
+                $htmlValidationException = $e;
+            }
         }
 
         $this->stopMinkSession();
         $this->restoreConfigs();
+
+        if (null !== $htmlValidationException) {
+            throw $htmlValidationException;
+        }
     }
 
     /**
