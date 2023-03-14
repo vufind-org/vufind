5c4a6692df558b1e1dd09acfca07fdae10d1b75d
Ere Maijala <ere.maijala@helsinki.fi>
2023-03-14 18:43:57 +0200

    Adjust copyright.

--- module/VuFind/src/VuFind/Controller/CheckoutsControllerFactory.php
+++ module/VuFind/src/VuFind/Controller/CheckoutsControllerFactory.php
@@ -5,7 +5,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) Villanova University 2018.
+ * Copyright (C) The National Library of Finland 2023.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -22,7 +22,7 @@
  *
  * @category VuFind
  * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
@@ -39,7 +39,7 @@ use Psr\Container\ContainerInterface;
  *
  * @category VuFind
  * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
