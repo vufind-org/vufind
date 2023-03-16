4765a247425572e8312dbe8ba2d821e268d5e7dc
Chris Hallberg <chris.hallberg@hey.com>
2023-03-16 16:47:11 -0400

    refactor: unroll debounce for clearer code.

--- themes/bootstrap3/js/lib/ajax_request_queue.js
+++ themes/bootstrap3/js/lib/ajax_request_queue.js
@@ -1,57 +1,21 @@
 class AjaxRequestQueue {
   constructor({ run, success, failure, delay }) {
+    // Status
     this.isRunning = false;
     this.queue = [];
     this.payload = [];
 
+    // Debounce
+    this.timeout = null;
+    this.delay = delay ?? 300;
+
+    // Function definitions
     const noop = () => {};
     const promiseNoop = () => Promise.reject();
 
     this.runFn = run ? run : promiseNoop;
     this.successFn = success ? success : noop;
     this.failureFn = failure ? failure : noop;
-
-    // TODO: move once Javascript is modularized
-    function debounce(func, delay = 300) {
-      let timeout;
-
-      return function () {
-        clearTimeout(timeout);
-
-        const context = this;
-        const args = arguments;
-
-        timeout = setTimeout(function () {
-          func.apply(context, args);
-        }, delay);
-      };
-    }
-
-    this.runPayload = debounce(function runPayloadCore() {
-      this.isRunning = true;
-
-      this.runFn(this.payload)
-        .then((...res) => this.successFn(this.payload, ...res))
-
-        .catch((...error) => {
-          console.error(...error);
-          this.failureFn(this.payload, ...error);
-        })
-
-        .finally(() => {
-          this.isRunning = false;
-
-          this.payload = [];
-
-          if (this.queue.length > 0) {
-            this.payload = this.queue;
-
-            this.queue = [];
-
-            this.runPayload();
-          }
-        });
-    }, delay);
   }
 
   add(obj) {
@@ -62,5 +26,36 @@ class AjaxRequestQueue {
 
       this.runPayload();
     }
+  }
+
+  runPayload() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => this.#runPayloadImpl(), this.delay);
+  }
+
+  #runPayloadImpl() {
+    this.isRunning = true;
+
+    this.runFn(this.payload)
+      .then((...res) => this.successFn(this.payload, ...res))
+
+      .catch((...error) => {
+        console.error(...error);
+        this.failureFn(this.payload, ...error);
+      })
+
+      .finally(() => {
+        this.isRunning = false;
+
+        this.payload = [];
+
+        if (this.queue.length > 0) {
+          this.payload = this.queue;
+
+          this.queue = [];
+
+          this.runPayload();
+        }
+      });
   }
 }
