22a43b85491e643c48b8a5f40050e32575669e29
Ere Maijala <ere.maijala@helsinki.fi>
2023-04-05 20:35:36 +0300

    Add unit tests.

---
+++ module/VuFindConsole/tests/unit-tests/src/VuFindTest/Command/Util/PurgeCachedRecordCommandTest.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * PurgeCachedRecordCommand test.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2020.
+ * Copyright (C) The National Library of Finland 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+
+namespace VuFindTest\Command\Util;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use VuFindConsole\Command\Util\PurgeCachedRecordCommand;
+
+/**
+ * PurgeCachedRecordCommand test.
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
+ */
+class PurgeCachedRecordCommandTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Data provider for testBasicOperation
+     *
+     * @return array
+     */
+    public function basicOperationProvider(): array
+    {
+        return [
+            ['Solr', '123', false, true, null],
+            ['Solr', '123', false, false, null],
+            ['Solr', '123', true, true, true],
+            ['Solr', '123', true, true, false],
+        ];
+    }
+
+    /**
+     * Test that the purge cached record action is delegated properly.
+     *
+     * @param string $source         Source ID
+     * @param string $id             Record ID
+     * @param bool   $purgeResource  Whether to purge the resource as well
+     * @param bool   $recordRetVal   What the record delete method is expected to return
+     * @param ?bool  $resourceRetVal What, if anything the resource delete method is expected to return
+     *
+     * @return void
+     *
+     * @dataProvider basicOperationProvider
+     */
+    public function testBasicOperation(
+        string $source,
+        string $id,
+        bool $purgeResource,
+        bool $recordRetVal,
+        ?bool $resourceRetVal
+    ): void {
+        $recordTable = $this->getMockBuilder(\VuFind\Db\Table\Record::class)
+            ->disableOriginalConstructor()->getMock();
+        $recordTable->expects($this->once())->method('delete')
+            ->with($this->equalTo(['source' => 'Solr', 'record_id' => 123]))
+            ->willReturn($recordRetVal);
+
+        $resourceTable = $this->getMockBuilder(\VuFind\Db\Table\Resource::class)
+            ->disableOriginalConstructor()->getMock();
+        if (null !== $resourceRetVal) {
+            $resourceTable->expects($this->once())->method('delete')
+                ->willReturn($resourceRetVal);
+        }
+        $params = compact('source', 'id');
+        if ($purgeResource) {
+            $params['--purge-resource'] = true;
+        }
+
+        $command = new PurgeCachedRecordCommand($recordTable, $resourceTable);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute($params);
+        $expected = $recordRetVal ? "Cached record deleted\n" : "No cached record found\n";
+        if (null !== $resourceRetVal) {
+            $expected .= $resourceRetVal ? "Resource deleted\n" : "No resource found\n";
+        }
+        $this->assertEquals($expected, $commandTester->getDisplay());
+        $this->assertEquals(0, $commandTester->getStatusCode());
+    }
+}
