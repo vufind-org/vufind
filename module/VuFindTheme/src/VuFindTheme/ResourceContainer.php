6ed0afddd53460cb59a6b3e3802832a165cddf31
Mathias Maaß <mathias.maass@uni-leipzig.de>
2023-05-04 15:16:34 +0200

    Enable multiple favicons through configuration (#2862)

--- module/VuFindTheme/src/VuFindTheme/ResourceContainer.php
+++ module/VuFindTheme/src/VuFindTheme/ResourceContainer.php
@@ -64,7 +64,7 @@ class ResourceContainer
     /**
      * Favicon
      *
-     * @var string
+     * @var string|array|null
      */
     protected $favicon = null;
 
@@ -347,7 +347,7 @@ class ResourceContainer
     /**
      * Set the favicon.
      *
-     * @param string $favicon New favicon path.
+     * @param string|array $favicon New favicon path.
      *
      * @return void
      */
@@ -359,7 +359,7 @@ class ResourceContainer
     /**
      * Get the favicon (null for none).
      *
-     * @return string
+     * @return string|array|null
      */
     public function getFavicon()
     {
