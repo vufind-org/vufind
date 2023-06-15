0f75435cc754f1583e869eb095c7320e15ab51b9
wagener <wagener@hebis.uni-frankfurt.de>
2023-06-15 16:03:10 +0200

    retriecing lightboxParent before clearing it

--- module/VuFind/src/VuFind/Controller/AbstractBase.php
+++ module/VuFind/src/VuFind/Controller/AbstractBase.php
@@ -757,8 +757,8 @@ class AbstractBase extends AbstractActionController implements TranslatorAwareIn
     protected function getAndClearFollowupUrl($checkRedirect = false)
     {
         if ($url = $this->followup()->retrieve('url', '')) {
-            $this->clearFollowupUrl();
             $lightboxParent = $this->followup()->retrieveAndClear('lightboxParent');
+            $this->clearFollowupUrl();
             // If a user clicks on the "Your Account" link, we want to be sure
             // they get to their account rather than being redirected to an old
             // followup URL. We'll use a redirect=0 GET flag to indicate this:
