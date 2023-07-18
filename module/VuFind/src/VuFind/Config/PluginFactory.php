f29c765ae1b982cb614ab4f413a225415f899732
Thomas Wagener <111493084+ThoWagen@users.noreply.github.com>
2023-07-18 15:31:35 +0200

    Inheritance of local dirs for configuration files (#2947)

--- module/VuFind/src/VuFind/Config/PluginFactory.php
+++ module/VuFind/src/VuFind/Config/PluginFactory.php
@@ -30,9 +30,9 @@
 namespace VuFind\Config;
 
 use Laminas\Config\Config;
-use Laminas\Config\Reader\Ini as IniReader;
 use Laminas\ServiceManager\Factory\AbstractFactoryInterface;
 use Psr\Container\ContainerInterface;
+use VuFind\Config\Feature\IniReaderTrait;
 
 /**
  * VuFind Config Plugin Factory
@@ -45,24 +45,7 @@ use Psr\Container\ContainerInterface;
  */
 class PluginFactory implements AbstractFactoryInterface
 {
-    /**
-     * INI file reader
-     *
-     * @var IniReader
-     */
-    protected $iniReader;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        // Use ASCII 0 as a nest separator; otherwise some of the unusual key names
-        // we have (i.e. in WorldCat.ini search options) will get parsed in
-        // unexpected ways.
-        $this->iniReader = new IniReader();
-        $this->iniReader->setNestSeparator(chr(0));
-    }
+    use IniReaderTrait;
 
     /**
      * Load the specified configuration file.
@@ -84,7 +67,7 @@ class PluginFactory implements AbstractFactoryInterface
         // chain of them if the Parent_Config setting is used:
         do {
             $configs[]
-                = new Config($this->iniReader->fromFile($filename), true);
+                = new Config($this->getIniReader()->fromFile($filename), true);
 
             $i = count($configs) - 1;
             if (isset($configs[$i]->Parent_Config->path)) {
