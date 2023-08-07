d75bdef79af741e1b6ad3b92e70b7c6d03e7263a
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-08-07 16:45:07 +0200

    Improve facet-list lightbox compatibility

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -649,45 +649,6 @@ function setupQRCodeLinks(_container) {
   });
 }
 
-// Useful function to delay callbacks, e.g. when using a keyup event
-// to detect when the user stops typing.
-// See also: https://stackoverflow.com/questions/1909441/how-to-delay-the-keyup-handler-until-the-user-stops-typing
-function getFacetListContentKeyupCallback() {
-  var timeout = null;
-  $('.ajax_param[data-name="contains"]').on('keyup', function onKeyupChangeFacetList() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function onKeyupTimeout() {
-      getFacetListContent();
-    }, 500);
-  });
-}
-
-function getFacetListContent() {
-  let url = VuFind.path + "/AJAX/JSON?q=sta&method=getFacetListContent";
-
-  $('.ajax_param').each(function ajaxParamEach(param) {
-    url += '&' + encodeURI($(this).data('name')) + '=' + encodeURI($(this).val());
-  });
-
-  $.ajax({
-    type: "GET",
-    url: url,
-    dataType: "json",
-    success: function (json) {
-      $('#facet-info-result').html(json.data.html);
-    }
-  });
-
-  // This needs to be registered here as well so it works in a lightbox
-  getFacetListContentKeyupCallback();
-}
-
-function setupFacetList() {
-  $('.ajax_param[data-name="contains"]').on('keyup', function onKeyupChangeFacetList() {
-    getFacetListContentKeyupCallback();
-  });
-}
-
 $(function commonDocReady() {
   // Start up all of our submodules
   VuFind.init();
@@ -703,9 +664,6 @@ $(function commonDocReady() {
 
   // handle QR code links
   setupQRCodeLinks();
-
-  // Setup facet list autocomplete
-  setupFacetList();
 
   // Checkbox select all
   $('.checkbox-select-all').on('change', function selectAllCheckboxes() {
