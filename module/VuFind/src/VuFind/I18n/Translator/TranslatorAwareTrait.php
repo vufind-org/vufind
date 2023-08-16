8ce1cabe5dc0a667010e5dd064fa905d45f265ea
wagener <wagener@hebis.uni-frankfurt.de>
2023-08-16 17:42:57 +0200

    fixed when  is array and added space

--- module/VuFind/src/VuFind/I18n/Translator/TranslatorAwareTrait.php
+++ module/VuFind/src/VuFind/I18n/Translator/TranslatorAwareTrait.php
@@ -101,18 +101,21 @@ trait TranslatorAwareTrait
      */
     public function translate($target, $tokens = [], $default = null)
     {
+        // Figure out the text domain for the string:
+        [$domain, $str] = $this->extractTextDomain($target);
+
         if ($this->getTranslatorLocale() == "keys") {
+            $targetString = $domain !== "default" ? "$domain::$str" : "$str";
             $keyValueToString = function ($key, $val) {
                 return "$key = $val";
             };
             $tokenDetails = empty($tokens)
                 ? ''
-                : '| [' . implode(', ', array_map($keyValueToString, array_keys($tokens), array_values($tokens))) . ']';
-            return "*$target$tokenDetails*";
+                : ' | [' .
+                implode(', ', array_map($keyValueToString, array_keys($tokens), array_values($tokens))) .
+                ']';
+            return "*$targetString$tokenDetails*";
         }
-
-        // Figure out the text domain for the string:
-        [$domain, $str] = $this->extractTextDomain($target);
 
         // Special case: deal with objects with a designated display value:
         if ($str instanceof \VuFind\I18n\TranslatableStringInterface) {
