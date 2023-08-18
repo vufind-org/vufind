b2e92f55e48c4ecfeeca71555e9ddfcbd767f7cc
Damien <damien-git@users.noreply.github.com>
2023-08-18 15:11:36 -0400

    [VUFIND-1630] Added check for single solrmarc jar

--- index-alphabetic-browse.sh
+++ index-alphabetic-browse.sh
@@ -42,7 +42,13 @@ set -x
 
 cd "`dirname $0`/import"
 LOG4J_CLASSPATH=$(find "${SOLR_JAR_PATH}/server/lib/ext" -name 'log4j*.jar' -printf '%p:' | sed 's/:$//')
-CLASSPATH="browse-indexing.jar:$(echo solrmarc_core*.jar):${VUFIND_HOME}/import/lib/*:${SOLR_HOME}/jars/*:${SOLR_JAR_PATH}/modules/analysis-extras/lib/*:${SOLR_JAR_PATH}/server/solr-webapp/webapp/WEB-INF/lib/*:${LOG4J_CLASSPATH}"
+SOLRMARC_CLASSPATH=$(echo solrmarc_core*.jar)
+if [[ `wc -w <<<"$SOLRMARC_CLASSPATH"` -gt 1 ]]
+then
+  echo "Error: more than one solrmarc_core*.jar in import/; exiting."
+  exit 1
+fi
+CLASSPATH="browse-indexing.jar:${SOLRMARC_CLASSPATH}:${VUFIND_HOME}/import/lib/*:${SOLR_HOME}/jars/*:${SOLR_JAR_PATH}/modules/analysis-extras/lib/*:${SOLR_JAR_PATH}/server/solr-webapp/webapp/WEB-INF/lib/*:${LOG4J_CLASSPATH}"
 
 # make index work with replicated index
 # current index is stored in the last line of index.properties
