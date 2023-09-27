79fa0ffba6a3351d0d167113ada424eef204e9fa
Ere Maijala <ere.maijala@helsinki.fi>
2023-09-27 15:55:55 +0300

    SierraRest: Trim whitespace from call number and volume.
    Also cleans up the call number extraction code.

--- module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
+++ module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
@@ -2271,6 +2271,9 @@ class SierraRest extends AbstractBase implements
                     );
                 }
             }
+            $callNumber = isset($item['callNumber'])
+                ? $this->extractCallNumber($item['callNumber'])
+                : $bibCallNumber;
             $volume = isset($item['varFields']) ? $this->extractVolume($item) : '';
 
             $entry = [
@@ -2280,11 +2283,9 @@ class SierraRest extends AbstractBase implements
                 'availability' => $available,
                 'status' => $status,
                 'reserve' => 'N',
-                'callnumber' => isset($item['callNumber'])
-                    ? preg_replace('/^\|a/', '', $item['callNumber'])
-                    : $bibCallNumber,
+                'callnumber' => trim($callNumber),
                 'duedate' => $duedate,
-                'number' => $volume,
+                'number' => trim($volume),
                 'barcode' => $item['barcode'] ?? '',
                 'sort' => $sort--,
             ];
@@ -2363,6 +2364,18 @@ class SierraRest extends AbstractBase implements
 
         usort($statuses, [$this, 'statusSortFunction']);
         return $statuses;
+    }
+
+    /**
+     * Extract the actual call number from item's call number field
+     *
+     * @param string $callNumber Call number field
+     *
+     * @return string
+     */
+    protected function extractCallNumber(string $callNumber): string
+    {
+        return str_starts_with($callNumber, '|a') ? substr($callNumber, 2) : $callNumber;
     }
 
     /**
