6df05b3b11a158ad497b7c1e28de7f7c28095ef4
Maccabee Levine <31278545+maccabeelevine@users.noreply.github.com>
2023-10-09 09:39:34 -0400

    Logging: Add ReferenceId support for username (#3156)

--- module/VuFind/src/VuFind/Log/LoggerFactory.php
+++ module/VuFind/src/VuFind/Log/LoggerFactory.php
@@ -305,6 +305,18 @@ class LoggerFactory implements FactoryInterface
         if (!$hasWriter) {
             $logger->addWriter(new \Laminas\Log\Writer\Noop());
         }
+
+        // Add ReferenceId processor, if applicable:
+        if ($referenceId = $config->Logging->reference_id ?? false) {
+            if ('username' === $referenceId) {
+                $authManager = $container->get(\VuFind\Auth\Manager::class);
+                if ($user = $authManager->isLoggedIn()) {
+                    $processor = new \Laminas\Log\Processor\ReferenceId();
+                    $processor->setReferenceId($user->username);
+                    $logger->addProcessor($processor);
+                }
+            }
+        }
     }
 
     /**
