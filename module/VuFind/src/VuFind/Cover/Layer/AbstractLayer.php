2701c7166217184f3757b7896749c6f549384bf5
Ere Maijala <ere.maijala@helsinki.fi>
2023-11-13 09:46:57 +0200

    Add explicit int casts to cover generator layers.
    Fixes some PHP 8.1 deprecation notices.

--- module/VuFind/src/VuFind/Cover/Layer/AbstractLayer.php
+++ module/VuFind/src/VuFind/Cover/Layer/AbstractLayer.php
@@ -72,7 +72,7 @@ abstract class AbstractLayer implements LayerInterface
      * @param resource $im    Image resource being updated
      * @param string   $color Legal color name from HTML4
      *
-     * @return allocated color
+     * @return int allocated color
      */
     protected function getColor($im, $color)
     {
