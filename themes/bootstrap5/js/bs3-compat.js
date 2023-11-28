a490965810558eabbdbedd8c3179fc8120ab9816
Ere Maijala <ere.maijala@helsinki.fi>
2023-11-28 11:01:21 +0200

    Add compatibility layer for old data attributes.

--- themes/bootstrap5/js/bs3-compat.js
+++ themes/bootstrap5/js/bs3-compat.js
@@ -15,7 +15,9 @@ VuFind.register('bs3-compat', function cookie() {
       el.classList.add('nav-item');
       const aEl = el.querySelector('a');
       if (aEl) {
-        aEl.classList.add('nav-link');
+        if (!aEl.classList.contains('btn')) {
+          aEl.classList.add('nav-link');
+        }
         if (el.classList.contains('active')) {
           aEl.classList.add('active');
         }
@@ -53,6 +55,33 @@ VuFind.register('bs3-compat', function cookie() {
     });
   }
 
+  function convertDataAttributes(el) {
+    if (typeof el.getAttribute === 'undefined') {
+      return;
+    }
+    const attrs = ['dismiss', 'target', 'toggle'];
+    attrs.forEach((attr) => {
+      const val = el.getAttribute('data-' + attr);
+      if (null !== val) {
+        el.setAttribute('data-bs-' + attr, val);
+      }
+    });
+  }
+
+  function initDataAttributeMappings() {
+    document.querySelectorAll('[data-dismiss],[data-target],[data-toggle]').forEach((el) => {
+      convertDataAttributes(el);
+    });
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((el) => {
+          convertDataAttributes(el);
+        });
+      });
+    });
+    observer.observe(document, { subtree: true, childList: true });
+  }
+
   function init() {
     initNav();
     initNavbar();
@@ -60,6 +89,7 @@ VuFind.register('bs3-compat', function cookie() {
     initBreadcrumbs();
     initCollapse();
     initPagination();
+    initDataAttributeMappings();
   }
 
   return {
