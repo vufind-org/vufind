54af7e4a121cce95efbe057ab7ae9391577e0696
Chris Hallberg <chris.hallberg@hey.com>
2023-12-04 13:07:37 -0500

    fix: use global variables in setup.

--- themes/bootstrap3/js/searchbox_controls.js
+++ themes/bootstrap3/js/searchbox_controls.js
@@ -244,26 +244,26 @@ VuFind.register('searchbox_controls', function SearchboxControls() {
   }
 
   function setupSearchResetButton() {
-    const queryInput = document.getElementById("searchForm_lookfor");
-    const resetButton = document.getElementById("searchForm-reset");
+    _textInput = document.getElementById("searchForm_lookfor");
+    _resetButton = document.getElementById("searchForm-reset");
 
-    if (queryInput.value !== "") {
-      resetButton.classList.remove("hidden");
+    if (_textInput.value !== "") {
+      _resetButton.classList.remove("hidden");
     }
 
-    queryInput.addEventListener("input", function resetOnInput() {
-      if (queryInput.value === "") {
-        resetButton.classList.add("hidden");
+    _textInput.addEventListener("input", function resetOnInput() {
+      if (_textInput.value === "") {
+        _resetButton.classList.add("hidden");
       } else {
-        resetButton.classList.remove("hidden");
+        _resetButton.classList.remove("hidden");
       }
     });
 
-    resetButton.addEventListener("click", function resetOnClick() {
-      resetButton.classList.add("hidden");
-      queryInput.setAttribute("value", "");
-      queryInput.focus();
-      queryInput.ac.hide();
+    _resetButton.addEventListener("click", function resetOnClick() {
+      _resetButton.classList.add("hidden");
+      _textInput.setAttribute("value", "");
+      _textInput.focus();
+      _textInput.ac.hide();
     });
   }
 
