aac145c7794c125eee9ed488d6c55e8c6c37ece5
Maccabee Levine <msl321@lehigh.edu>
2024-01-02 16:43:42 +0000

    Refactor deferred recommendation modules into view helper

---
+++ module/VuFind/src/VuFind/View/Helper/Root/DeferredRecommend.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Deferred recommendation module view helper
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Villanova University 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Maccabee Levine <msl321@lehigh.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+
+namespace VuFind\View\Helper\Root;
+
+use VuFind\Recommend\RecommendInterface;
+
+/**
+ * Deferred recommendation module view helper
+ *
+ * @category VuFind
+ * @package  View_Helpers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Maccabee Levine <msl321@lehigh.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+class DeferredRecommend extends \Laminas\View\Helper\AbstractHelper
+{
+    use ClassBasedTemplateRendererTrait;
+
+    /**
+     * Render code to load a recommendation module via AJAX.
+     *
+     * @param RecommendInterface $recommend The recommendation object to render
+     * @param string             $location  Recommendation location (side, top)
+     * @param int                $index     Index of the recommendation configuration
+     *
+     * @return string
+     */
+    public function __invoke(
+        $urlParams,
+        $containerId,
+    ) {
+        $template = 'Recommend/Deferred.phtml';
+        $className = $this::class;
+        $context = [
+            'containerId' => $containerId,
+        ];
+        return $this->renderClassTemplate($template, $className, $context);
+    }
+}
