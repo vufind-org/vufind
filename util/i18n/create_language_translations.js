e705de0aa0a985837af011ce091acb46669a933d
Demian Katz <demian.katz@villanova.edu>
2024-01-04 12:53:34 -0500

    Add missing comment.

--- util/i18n/create_language_translations.js
+++ util/i18n/create_language_translations.js
@@ -1,3 +1,31 @@
+/**
+ * Command-line tool to create language translation filess using the
+ * all-iso-language-codes NPM package.
+ *
+ * Javascript
+ *
+ * Copyright (C) Villanova University 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Utilities
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/automation Wiki
+ */
+
 const childProcess = require("child_process");
 const codes = require("all-iso-language-codes");
 const fs = require("fs");
