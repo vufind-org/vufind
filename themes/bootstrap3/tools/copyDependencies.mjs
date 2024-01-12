dd8ce3ca9243035aab52206813b02c4afe890a8b
Chris Hallberg <chris.hallberg@hey.com>
2024-01-12 15:15:19 -0500

    fix: copy splide dependencies.

--- themes/bootstrap3/tools/copyDependencies.mjs
+++ themes/bootstrap3/tools/copyDependencies.mjs
@@ -8,6 +8,10 @@ await copyFile('node_modules/autocomplete.js/autocomplete.js', 'js/vendor/autoco
 // jstree
 await copyFile('node_modules/jstree/dist/jstree.min.js', 'js/vendor/jsTree/jstree.min.js');
 
+// splide
+await copyFile('node_modules/@splidejs/splide/dist/js/splide.min.js', 'js/vendor/splide.min.js');
+await copyFile('node_modules/@splidejs/splide/dist/css/splide.min.css', 'css/vendor/splide.min.css');
+
 // simple-keyboard
 await copyFile('node_modules/simple-keyboard/build/index.js', 'js/vendor/simple-keyboard/index.js');
 await copyFile('node_modules/simple-keyboard/build/css/index.css', 'css/vendor/simple-keyboard/index.css');
