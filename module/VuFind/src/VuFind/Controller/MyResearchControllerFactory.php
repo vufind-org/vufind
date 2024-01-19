906da771f28ad705e8726e18a76a1ce8c334ce9a
wagener <wagener@hebis.uni-frankfurt.de>
2024-01-19 11:40:16 +0100

    updated copyright year

--- module/VuFind/src/VuFind/Controller/MyResearchControllerFactory.php
+++ module/VuFind/src/VuFind/Controller/MyResearchControllerFactory.php
@@ -5,7 +5,7 @@
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2023.
+ * Copyright (C) Villanova University 2024.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
