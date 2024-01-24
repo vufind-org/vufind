38a8201e08bee0b932123bf6e36a180128ef62db
Demian Katz <demian.katz@villanova.edu>
2024-01-24 14:10:55 -0500

    Improve browscap.ini error handling.

--- module/VuFind/src/VuFind/Auth/LoginTokenManager.php
+++ module/VuFind/src/VuFind/Auth/LoginTokenManager.php
@@ -174,12 +174,14 @@ class LoginTokenManager implements \VuFind\I18n\Translator\TranslatorAwareInterf
         $browser = '';
         $platform = '';
         try {
-            $userInfo = get_browser(null, true) ?? [];
-            $browser = $userInfo['browser'];
-            $platform = $userInfo['platform'];
+            $userInfo = get_browser(null, true);
         } catch (\Exception $e) {
+        }
+        if (!is_array($userInfo ?? null)) {
             throw new AuthException('Problem with browscap.ini');
         }
+        $browser = $userInfo['browser'] ?? '';
+        $platform = $userInfo['platform'] ?? '';
         if ($expires === 0) {
             $lifetime = $this->config->Authentication->persistent_login_lifetime ?? 14;
             $expires = time() + $lifetime * 60 * 60 * 24;
