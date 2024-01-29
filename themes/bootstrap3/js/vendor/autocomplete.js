67f8422cd0c3668444efc848e9c22c99e75d9818
Demian Katz <demian.katz@villanova.edu>
2024-01-29 12:53:44 -0500

    Update js file.

--- themes/bootstrap3/js/vendor/autocomplete.js
+++ themes/bootstrap3/js/vendor/autocomplete.js
@@ -1,4 +1,4 @@
-/* https://github.com/vufind-org/autocomplete.js (v2.1.8) (2023-08-31) */
+/* https://github.com/vufind-org/autocomplete.js (v2.1.8) (2024-01-29) */
 function Autocomplete(_settings) {
   const _DEFAULTS = {
     delay: 250,
