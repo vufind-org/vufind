f43682617ef4a827237ec680d477d6fcaf06c2f6
Demian Katz <demian.katz@villanova.edu>
2024-02-05 06:08:10 -0500

    Update CoverController.php

--- module/VuFind/src/VuFind/Controller/CoverController.php
+++ module/VuFind/src/VuFind/Controller/CoverController.php
@@ -130,7 +130,7 @@ class CoverController extends \Laminas\Mvc\Controller\AbstractActionController
             try {
                 $image = $this->proxy->fetch($url);
                 $contentType = $image?->getHeaders()?->get('content-type')?->getFieldValue() ?? '';
-                if (str_starts_with($contentType, 'image/')) {
+                if (str_starts_with(strtolower($contentType), 'image/')) {
                     return $this->displayImage(
                         $contentType,
                         $image->getContent()
