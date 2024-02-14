68d6411929cee66f569ffaa7c3bd78b400daef59
Demian Katz <demian.katz@villanova.edu>
2024-02-14 07:15:16 -0500

    phpcbf

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/MarkdownTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/MarkdownTest.php
@@ -26,6 +26,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Page
  */
+
 declare(strict_types=1);
 
 namespace VuFindTest\Mink;
