8c67fb50b4e4bc68b391a95478fbd27a9b4b9241
Demian Katz <demian.katz@villanova.edu>
2024-03-01 09:43:31 -0500

    Improve key sanitization; add normalization.

--- module/VuFind/src/VuFind/I18n/Translator/TranslatorAwareTrait.php
+++ module/VuFind/src/VuFind/I18n/Translator/TranslatorAwareTrait.php
@@ -280,6 +280,10 @@ trait TranslatorAwareTrait
         // platform, so they should not be allowed in our code. We'll replace them
         // with underscores so that translations can still be provided if the input
         // cannot be changed (e.g. if it comes from a third-party system).
-        return str_replace(['(', ')', '!', '?'], '_', $key);
+        return str_replace(
+            ['(', ')', '!', '?'],
+            ['_28', '_29', '_21', '_3F'],
+            $key
+        );
     }
 }
