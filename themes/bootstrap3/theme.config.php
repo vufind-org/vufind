37c9cd8c5713a85d1a543136edda2834434d5132
Chris Hallberg <chris.hallberg@hey.com>
2024-03-01 16:24:20 -0500

    fix: missed merge.

--- themes/bootstrap3/theme.config.php
+++ themes/bootstrap3/theme.config.php
@@ -41,22 +41,14 @@ return [
         ['file' => 'vendor/autocomplete.js', 'priority' => 220],
         ['file' => 'lib/ajax_request_queue.js', 'priority' => 230],
         ['file' => 'common.js', 'priority' => 310],
-<<<<<<< HEAD
         ['file' => 'components.js', 'priority' => 320],
         ['file' => 'lightbox.js', 'priority' => 330],
         ['file' => 'searchbox_controls.js', 'priority' => 340],
         ['file' => 'truncate.js', 'priority' => 350],
         ['file' => 'trigger_print.js', 'priority' => 360],
         ['file' => 'observer_manager.js', 'priority' => 370],
-=======
-        ['file' => 'lightbox.js', 'priority' => 320],
-        ['file' => 'searchbox_controls.js', 'priority' => 330],
-        ['file' => 'truncate.js', 'priority' => 340],
-        ['file' => 'trigger_print.js', 'priority' => 350],
-        ['file' => 'observer_manager.js', 'priority' => 360],
-        ['file' => 'openurl.js', 'priority' => 370],
-        ['file' => 'list_item_selection.js', 'priority' => 380],
->>>>>>> origin/dev
+        ['file' => 'openurl.js', 'priority' => 380],
+        ['file' => 'list_item_selection.js', 'priority' => 390],
     ],
     /**
      * Configuration for a single or multiple favicons.
