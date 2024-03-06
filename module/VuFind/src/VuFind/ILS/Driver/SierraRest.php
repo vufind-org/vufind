cba52168575ac7c9f9c4bf6f38abf86a551d02e4
Ere Maijala <ere.maijala@helsinki.fi>
2024-03-06 12:35:26 +0200

    Sierra: Display all fines.
    By default Sierra only returns first 50 records, so use a high limit to include all.

--- module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
+++ module/VuFind/src/VuFind/ILS/Driver/SierraRest.php
@@ -1478,6 +1478,7 @@ class SierraRest extends AbstractBase implements
         $result = $this->makeRequest(
             [$this->apiBase, 'patrons', $patron['id'], 'fines'],
             [
+                'limit' => 10000,
                 'fields' => 'item,assessedDate,description,chargeType,itemCharge'
                     . ',processingFee,billingFee,paidAmount',
             ],
