4a5f0b7bf0f7a69e9a1b47f6e83f0dfe6409b494
Demian Katz <demian.katz@villanova.edu>
2024-04-08 11:41:22 -0400

    Add ExternalSessionEntityInterface.

--- module/VuFind/src/VuFind/Db/Entity/ExternalSessionEntityInterface.php
+++ module/VuFind/src/VuFind/Db/Entity/ExternalSessionEntityInterface.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * Row Definition for external_session
+ * Entity model interface for external_session table
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2010.
- * Copyright (C) The National Library of Finland 2016.
+ * Copyright (C) Villanova University 2024.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -22,34 +21,23 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @category VuFind
- * @package  Db_Row
+ * @package  Database
  * @author   Demian Katz <demian.katz@villanova.edu>
- * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org Main Site
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
  */
 
-namespace VuFind\Db\Row;
+namespace VuFind\Db\Entity;
 
 /**
- * Row Definition for external_session
+ * Entity model interface for external_session table
  *
  * @category VuFind
- * @package  Db_Row
+ * @package  Database
  * @author   Demian Katz <demian.katz@villanova.edu>
- * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org Main Site
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
  */
-class ExternalSession extends RowGateway implements \VuFind\Db\Entity\ExternalSessionEntityInterface
+interface ExternalSessionEntityInterface extends EntityInterface
 {
-    /**
-     * Constructor
-     *
-     * @param \Laminas\Db\Adapter\Adapter $adapter Database adapter
-     */
-    public function __construct($adapter)
-    {
-        parent::__construct('id', 'external_session', $adapter);
-    }
 }
