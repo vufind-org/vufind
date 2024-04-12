b031c2f96db41fb1d3a044fcdc43886022699861
Demian Katz <demian.katz@villanova.edu>
2024-04-12 10:03:26 -0400

    Add database services for comments and resource tables (#3558)

---
+++ module/VuFind/src/VuFind/Db/Service/CommentsService.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Database service for Comments.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Villanova University 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Database
+ * @author   Sudharma Kellampalli <skellamp@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+
+namespace VuFind\Db\Service;
+
+use VuFind\Db\Entity\ResourceEntityInterface;
+use VuFind\Db\Entity\UserEntityInterface;
+
+use function is_int;
+
+/**
+ * Database service for Comments.
+ *
+ * @category VuFind
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+class CommentsService extends AbstractDbService implements CommentsServiceInterface, DbServiceAwareInterface
+{
+    use DbServiceAwareTrait;
+
+    /**
+     * Add a comment to the current resource. Returns comment ID on success, null on failure.
+     *
+     * @param string                      $comment  The comment to save.
+     * @param int|UserEntityInterface     $user     User object or identifier
+     * @param int|ResourceEntityInterface $resource Resource object or identifier
+     *
+     * @return ?int
+     */
+    public function addComment(
+        string $comment,
+        int|UserEntityInterface $user,
+        int|ResourceEntityInterface $resource
+    ): ?int {
+        $userVal = is_int($user)
+            ? $this->getDbService(UserServiceInterface::class)->getUserById($user)
+            : $user;
+        $resourceVal = is_int($resource)
+            ? $this->getDbService(ResourceServiceInterface::class)->getResourceById($resource)
+            : $resource;
+        return $resourceVal->addComment($comment, $userVal);
+    }
+}
--- module/VuFind/src/VuFind/Db/Service/ResourceServiceFactory.php
+++ module/VuFind/src/VuFind/Db/Service/ResourceServiceFactory.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Factory for CommentRecord AJAX handler.
+ * Database resource service factory
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2018.
+ * Copyright (C) Villanova University 2023.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,29 +21,29 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @category VuFind
- * @package  AJAX
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Database
+ * @author   Sudharma Kellampalli <skellamp@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
  */
 
-namespace VuFind\AjaxHandler;
+namespace VuFind\Db\Service;
 
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
 use Laminas\ServiceManager\Exception\ServiceNotCreatedException;
 use Laminas\ServiceManager\Exception\ServiceNotFoundException;
-use Psr\Container\ContainerExceptionInterface as ContainerException;
-use Psr\Container\ContainerInterface;
 
 /**
- * Factory for CommentRecord AJAX handler.
+ * Database resource service factory
  *
  * @category VuFind
- * @package  AJAX
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Database
+ * @author   Sudharma Kellampalli <skellamp@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development Wiki
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
  */
-class CommentRecordFactory implements \Laminas\ServiceManager\Factory\FactoryInterface
+class ResourceServiceFactory extends AbstractDbServiceFactory
 {
     /**
      * Create an object
@@ -58,8 +58,6 @@ class CommentRecordFactory implements \Laminas\ServiceManager\Factory\FactoryInt
      * @throws ServiceNotCreatedException if an exception is raised when
      * creating a service.
      * @throws ContainerException&\Throwable if any other error occurs
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function __invoke(
         ContainerInterface $container,
@@ -67,23 +65,9 @@ class CommentRecordFactory implements \Laminas\ServiceManager\Factory\FactoryInt
         array $options = null
     ) {
         if (!empty($options)) {
-            throw new \Exception('Unexpected options passed to factory.');
+            throw new \Exception('Unexpected options sent to factory!');
         }
-        $tablePluginManager = $container->get(\VuFind\Db\Table\PluginManager::class);
-        $servicePluginManager = $container->get(
-            \VuFind\Db\Service\PluginManager::class
-        );
-        $controllerPluginManager = $container->get('ControllerPluginManager');
-        $capabilities = $container->get(\VuFind\Config\AccountCapabilities::class);
-        return new $requestedName(
-            $tablePluginManager->get(\VuFind\Db\Table\Resource::class),
-            $servicePluginManager->get(\VuFind\Db\Service\CommentsServiceInterface::class),
-            $controllerPluginManager
-                ->get(\VuFind\Controller\Plugin\Captcha::class),
-            $container->get(\VuFind\Auth\Manager::class)->getUserObject(),
-            $capabilities->getCommentSetting() !== 'disabled',
-            $container->get(\VuFind\Record\Loader::class),
-            $container->get(\VuFind\Config\AccountCapabilities::class)
-        );
+        $table = $container->get(\VuFind\Db\Table\PluginManager::class)->get('resource');
+        return parent::__invoke($container, $requestedName, [$table]);
     }
 }
