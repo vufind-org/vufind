dd107ef238438e1aff7922449fc0239765b8fdee
Demian Katz <demian.katz@villanova.edu>
2024-04-19 09:56:25 -0400

    Improve return type of UserEntityInterface::getId(). (#3599)

--- module/VuFind/src/VuFind/Db/Row/User.php
+++ module/VuFind/src/VuFind/Db/Row/User.php
@@ -789,11 +789,11 @@ class User extends RowGateway implements
     }
 
     /**
-     * Get identifier.
+     * Get identifier (returns null for an uninitialized or non-persisted object).
      *
-     * @return int
+     * @return ?int
      */
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
