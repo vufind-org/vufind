96d5cdc0dc66031ca86226aff070a3c81c056bb8
Maccabee Levine <msl321@lehigh.edu>
2024-04-19 20:22:52 +0000

    Fix whitespace

--- module/VuFind/src/VuFind/Cover/LoaderFactory.php
+++ module/VuFind/src/VuFind/Cover/LoaderFactory.php
@@ -70,7 +70,7 @@ class LoaderFactory implements FactoryInterface
         }
         $cacheOptions = $container->get(\VuFind\Cache\Manager::class)
             ->getCache('cover')->getOptions();
-        $cacheDir = method_exists($cacheOptions, 'getOptions') 
+        $cacheDir = method_exists($cacheOptions, 'getOptions')
             ? $cacheOptions->getOptions : null;
         $config = $container->get(\VuFind\Config\PluginManager::class)
             ->get('config');
