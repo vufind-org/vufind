19be38f000dcd5dd628ac49834318c08b71196c1
Demian Katz <demian.katz@villanova.edu>
2024-05-17 10:22:59 -0400

    Remove obsolete reference.

--- module/VuFind/src/VuFind/Db/Table/PluginManager.php
+++ module/VuFind/src/VuFind/Db/Table/PluginManager.php
@@ -48,7 +48,6 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     protected $aliases = [
         'accesstoken' => AccessToken::class,
         'externalsession' => ExternalSession::class,
-        'feedback' => Feedback::class,
         'logintoken' => LoginToken::class,
         'ratings' => Ratings::class,
         'search' => Search::class,
