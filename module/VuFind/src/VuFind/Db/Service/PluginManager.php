057d588b478f2b141e8bc9a639398ef469bfe0c8
Demian Katz <demian.katz@villanova.edu>
2024-05-17 09:39:49 -0400

    Expand Feedback entity interface; add Feedback service (#3650)

--- module/VuFind/src/VuFind/Db/Service/PluginManager.php
+++ module/VuFind/src/VuFind/Db/Service/PluginManager.php
@@ -48,6 +48,7 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     protected $aliases = [
         AccessTokenServiceInterface::class => AccessTokenService::class,
         CommentsServiceInterface::class => CommentsService::class,
+        FeedbackServiceInterface::class => FeedbackService::class,
         OaiResumptionServiceInterface::class => OaiResumptionService::class,
         RatingsServiceInterface::class => RatingsService::class,
         ResourceServiceInterface::class => ResourceService::class,
@@ -65,6 +66,7 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     protected $factories = [
         AccessTokenService::class => AccessTokenServiceFactory::class,
         CommentsService::class => AbstractDbServiceFactory::class,
+        FeedbackService::class => AbstractDbServiceFactory::class,
         OaiResumptionService::class => AbstractDbServiceFactory::class,
         RatingsService::class => AbstractDbServiceFactory::class,
         ResourceService::class => ResourceServiceFactory::class,
