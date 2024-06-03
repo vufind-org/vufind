7aa35dc08af3d6692d44a5c274c828306a18aa4e
padmsreegade <pgade@villanova.edu>
2024-06-03 14:21:07 -0400

    intial creation of getter and setters in the interface

--- module/VuFind/src/VuFind/Db/Entity/SearchEntityInterface.php
+++ module/VuFind/src/VuFind/Db/Entity/SearchEntityInterface.php
@@ -29,6 +29,9 @@
 
 namespace VuFind\Db\Entity;
 
+use DateTime;
+use \VuFind\Search\Minified;
+
 /**
  * Entity model interface for search table
  *
@@ -40,4 +43,170 @@ namespace VuFind\Db\Entity;
  */
 interface SearchEntityInterface extends EntityInterface
 {
+    /**
+     * Get identifier (returns null for an uninitialized or non-persisted object).
+     *
+     * @return ?int
+     */
+    public function getId(): ?int;
+
+    /**
+     * Get user.
+     * 
+     * @return int
+     */
+    public function getUserId(): int;
+
+    /**
+     * Set user.
+     * 
+     * @param int $user_id User id
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setUserId(int $user_id): SearchEntityInterface;
+
+    /**
+     * Get session.
+     * 
+     * @return ?string
+     */
+    public function getSessionId(): ?string;
+
+    /**
+     * Set session.
+     * 
+     * @param ?string $session_id Session id
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setSessionId(?string $session_id): SearchEntityInterface;
+
+    /**
+     * Get created.
+     * 
+     * @return DateTime
+     */
+    public function getCreated(): DateTime;
+
+    /**
+     * Set created date.
+     *
+     * @param DateTime $dateTime Created date
+     *
+     * @return SearchEntityInterface
+     */
+    public function setCreated(DateTime $dateTime): SearchEntityInterface;
+
+    /**
+     * Get title.
+     *
+     * @return ?string
+     */
+    public function getTitle(): ?string;
+
+    /**
+     * Set title.
+     *
+     * @param ?string $title Title
+     *
+     * @return SearchEntityInterface
+     */
+    public function setTitle(?string $title): SearchEntityInterface;
+
+    /**
+     * Get saved.
+     * 
+     * @return int
+     */
+    public function getSaved(): int;
+
+    /**
+     * Set saved.
+     * 
+     * @param int $saved Saved
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setSaved(int $saved): SearchEntityInterface;
+
+    /**
+     * Get search object.
+     * 
+     * @return \VuFind\Search\Minified
+     */
+    public function getSearchObject(): \VuFind\Search\Minified;
+
+    /**
+     * Set search object.
+     * 
+     * @param \VuFind\Search\Minified $search_object Search object
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setSearchObject(\VuFind\Search\Minified $search_object): SearchEntityInterface;
+
+    /**
+     * Get checksum.
+     * 
+     * @return ?int
+     */
+    public function getChecksum(): ?int;
+
+    /**
+     * Set checksum.
+     * 
+     * @param ?int $checksum Checksum
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setChecksum(?int $checksum): SearchEntityInterface;
+
+    /**
+     * Get notification frequency.
+     * 
+     * @return int
+     */
+    public function getNotificationFrequency(): int;
+
+    /**
+     * Set notification frequency.
+     * 
+     * @param int $notification_frequency Notification frequency
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setNotificationFrequency(int $notification_frequency): SearchEntityInterface;
+
+    /**
+     * When was the last notification sent?
+     * 
+     * @return DateTime
+     */
+    public function getLastNotificationSent(): DateTime;
+
+    /**
+     * Set when last notification was sent.
+     * 
+     * @param DateTime $last_notification_sent Time when last notification was sent
+     * 
+     * @return SearchEntityInterface
+     */
+    public function setLastNotificationSent(Datetime $last_notification_sent): SearchEntityInterface;
+
+    /**
+     * Get notification base URL.
+     *
+     * @return string
+     */
+    public function getNotificationBaseUrl(): string;
+
+    /**
+     * Set notification base URL.
+     *
+     * @param string $notification_base_url Notification base URL
+     *
+     * @return SearchEntityInterface
+     */
+    public function setNotificationBaseUrl(string $notification_base_url): SearchEntityInterface;
 }
