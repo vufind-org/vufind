8bb40173aa5aca9189ac75abf465a72e35b5e73d
Demian Katz <demian.katz@villanova.edu>
2024-06-05 13:14:57 -0400

    Deprecate record driver getContainingLists() method. (#3730)

--- module/VuFind/src/VuFind/RecordDriver/AbstractBase.php
+++ module/VuFind/src/VuFind/RecordDriver/AbstractBase.php
@@ -30,6 +30,7 @@
 namespace VuFind\RecordDriver;
 
 use VuFind\Db\Service\TagServiceInterface;
+use VuFind\Db\Service\UserListServiceInterface;
 use VuFind\XSLT\Import\VuFind as ArticleStripper;
 
 use function is_callable;
@@ -359,11 +360,12 @@ abstract class AbstractBase implements
      * @param int $user_id ID of user to load tags from (null for all users)
      *
      * @return array
+     *
+     * @deprecated Use UserListServiceInterface::getListsContainingRecord()
      */
     public function getContainingLists($user_id = null)
     {
-        $table = $this->getDbTable('UserList');
-        return $table->getListsContainingResource(
+        return $this->getDbService(UserListServiceInterface::class)->getListsContainingRecord(
             $this->getUniqueId(),
             $this->getSourceIdentifier(),
             $user_id
