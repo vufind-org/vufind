1bb5c75eeb6a1258277289bf061b47779672cda3
Demian Katz <demian.katz@villanova.edu>
2024-06-10 09:02:53 -0400

    Refactor User and UserList tag methods to FavoritesService (#3772)

--- module/VuFind/tests/unit-tests/src/VuFindTest/Favorites/FavoritesServiceTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Favorites/FavoritesServiceTest.php
@@ -33,7 +33,10 @@ use PHPUnit\Framework\MockObject\MockObject;
 use VuFind\Db\Entity\UserEntityInterface;
 use VuFind\Db\Entity\UserListEntityInterface;
 use VuFind\Db\Service\ResourceServiceInterface;
+use VuFind\Db\Service\ResourceTagsServiceInterface;
+use VuFind\Db\Service\TagServiceInterface;
 use VuFind\Db\Service\UserListServiceInterface;
+use VuFind\Db\Service\UserServiceInterface;
 use VuFind\Favorites\FavoritesService;
 use VuFind\Record\Loader;
 use VuFind\Record\ResourcePopulator;
@@ -61,7 +64,10 @@ class FavoritesServiceTest extends \PHPUnit\Framework\TestCase
     {
         return new FavoritesService(
             $this->createMock(ResourceServiceInterface::class),
+            $this->createMock(ResourceTagsServiceInterface::class),
+            $this->createMock(TagServiceInterface::class),
             $listService ?? $this->createMock(UserListServiceInterface::class),
+            $this->createMock(UserServiceInterface::class),
             $this->createMock(ResourcePopulator::class),
             $this->createMock(Tags::class),
             $this->createMock(Loader::class)
