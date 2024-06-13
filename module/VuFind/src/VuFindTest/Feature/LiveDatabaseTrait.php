783ea1311e4ba9d546d761baf7d8fe5c6e0951ca
Demian Katz <demian.katz@villanova.edu>
2024-06-13 07:47:35 -0400

    Fix test bug.

--- module/VuFind/src/VuFindTest/Feature/LiveDatabaseTrait.php
+++ module/VuFind/src/VuFindTest/Feature/LiveDatabaseTrait.php
@@ -35,6 +35,8 @@ use Throwable;
 use VuFind\Account\UserAccountService;
 use VuFind\Db\Service\DbServiceInterface;
 use VuFind\Db\Service\PluginManager as ServiceManager;
+use VuFind\Db\Service\ResourceTagsServiceInterface;
+use VuFind\Db\Service\TagServiceInterface;
 use VuFind\Db\Table\Gateway;
 use VuFind\Db\Table\PluginManager as TableManager;
 use VuFind\Favorites\FavoritesService;
@@ -96,10 +98,6 @@ trait LiveDatabaseTrait
                 \Laminas\Db\Adapter\Adapter::class,
                 $adapterFactory->getAdapter()
             );
-            $container->set(
-                \VuFind\Tags\TagsService::class,
-                new \VuFind\Tags\TagsService($container->get(ResourcePopulator::class))
-            );
             $container->set('config', $config);
             $container->set(\VuFind\Log\Logger::class, $this->createMock(\Laminas\Log\LoggerInterface::class));
             $container->set(
@@ -110,6 +108,14 @@ trait LiveDatabaseTrait
             $container->set(TableManager::class, $liveTableManager);
             $liveServiceManager = new ServiceManager($container, []);
             $container->set(ServiceManager::class, $liveServiceManager);
+            $container->set(
+                \VuFind\Tags\TagsService::class,
+                new \VuFind\Tags\TagsService(
+                    $liveServiceManager->get(TagServiceInterface::class),
+                    $liveServiceManager->get(ResourceTagsServiceInterface::class),
+                    $container->get(ResourcePopulator::class)
+                )
+            );
             $favoritesFactory = new FavoritesServiceFactory();
             $favoritesService = $favoritesFactory($container, FavoritesService::class);
             $favoritesService->setDbTableManager($liveTableManager);
