8bdfc25272ba0fe034936aea566f0598f8f81b97
Demian Katz <demian.katz@villanova.edu>
2024-06-19 08:12:24 -0400

    Add change email test.

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/EmailVerificationTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/EmailVerificationTest.php
@@ -117,6 +117,88 @@ final class EmailVerificationTest extends \VuFindTest\Integration\MinkTestCase
     }
 
     /**
+     * Test changing email address.
+     *
+     * @return void
+     *
+     * @depends testEmailVerification
+     */
+    public function testEmailAddressChange(): void
+    {
+        // Set up configs, session and message logging:
+        $this->changeConfigs(
+            [
+                'config' => [
+                    'Authentication' => [
+                        'change_email' => true,
+                        'verify_email' => true,
+                    ],
+                    'Mail' => [
+                        'testOnly' => true,
+                        'message_log' => $this->getEmailLogPath(),
+                    ],
+                ],
+            ]
+        );
+
+        $this->resetEmailLog();
+        $session = $this->getMinkSession();
+        $session->visit($this->getVuFindUrl('/MyResearch/Profile'));
+        $page = $session->getPage();
+
+        // Log back in
+        $this->clickCss($page, '#loginOptions a');
+        $this->fillInLoginForm($page, 'username1', 'test');
+        $this->clickCss($page, '.modal-body .btn.btn-primary');
+        $this->waitForPageLoad($page);
+
+        // Request the email change:
+        $this->clickCss($page, '.fa-envelope');
+        $this->assertEquals(
+            'Submitting this form will send an email to the new address; '
+            . 'you will have to click on a link in the email before the change will take effect.',
+            $this->findCssAndGetText($page, '.alert-info')
+        );
+        $this->findCssAndSetValue($page, 'input[name="email"]', 'changed@example.com');
+        $this->clickCss($page, '#newemail .btn-primary');
+
+        // Confirm that the email hasn't changed yet (not yet verified):
+        $session->visit($this->getVuFindUrl('/MyResearch/Profile'));
+        $this->assertStringContainsString(
+            'username1@ignore.com',
+            $this->findCssAndGetText($page, 'table.table-striped')
+        );
+
+        // Confirm that messages went to both new and old email addresses, and extract the verify link:
+        $email = file_get_contents($this->getEmailLogPath());
+        $this->assertStringContainsString('To: changed@example.com', $email);
+        $this->assertStringContainsString('To: username1@ignore.com', $email);
+        $this->assertStringContainsString(
+            'A request was just made to change your email address at Library Catalog.',
+            $email
+        );
+        preg_match('/You can verify your email address with this link: <(http.*)>/', $email, $matches);
+        $verifyLink = $matches[1];
+
+        // Follow the verification link:
+        $session->visit($verifyLink);
+        $this->assertEquals(
+            'Your email address has been verified successfully.',
+            $this->findCssAndGetText($page, '.alert-info')
+        );
+
+        // Confirm that the email has now changed:
+        $session->visit($this->getVuFindUrl('/MyResearch/Profile'));
+        $this->assertStringContainsString(
+            'changed@example.com',
+            $this->findCssAndGetText($page, 'table.table-striped')
+        );
+
+        // Clean up the email log:
+        $this->resetEmailLog();
+    }
+
+    /**
      * Standard teardown method.
      *
      * @return void
