2182f80874a4858dfa8baf38d0579c887d2a3ee5
wagener <wagener@hebis.uni-frankfurt.de>
2024-07-29 09:26:05 +0200

    changed container for record-tab-init in recordDocReady

--- themes/bootstrap5/js/record.js
+++ themes/bootstrap5/js/record.js
@@ -393,7 +393,7 @@ function recordDocReady() {
   VuFind.truncate.initTruncate('.truncate-subjects', '.subject-line');
   VuFind.truncate.initTruncate('table.truncate-field', 'tr.holding-row', function createTd(m) { return '<td colspan="2">' + m + '</td>'; });
   registerTabEvents();
-  VuFind.emit('record-tab-init', {container: document});
+  VuFind.emit('record-tab-init', {container: document.querySelector( '.record-tabs')});
   applyRecordTabHash(false);
 }
 
