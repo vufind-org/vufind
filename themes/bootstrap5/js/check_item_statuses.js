92b50223495a4b98cfa13b4f712dc9c18eb2cc67
Tobias Groth <tobias.groth@uni-jena.de>
2024-08-12 15:32:55 +0200

    Fixed availability display

--- themes/bootstrap5/js/check_item_statuses.js
+++ themes/bootstrap5/js/check_item_statuses.js
@@ -58,15 +58,15 @@ VuFind.register('itemStatuses', function ItemStatuses() {
       el.querySelectorAll('.callnumber,.hideIfDetailed,.location').forEach((e) => e.classList.add('hidden'));
       var locationListHTML = "";
       for (var x = 0; x < result.locationList.length; x++) {
-        var status = result.locationList[x].status;
-        var locationClass =
-          availabilityClasses[status] === 'true' ? 'available'
-            : availabilityClasses[status] === 'false' ? 'unavailable'
-              : availabilityClasses[status];
+        var availability = result.locationList[x].availability;
+        var statusIcon =
+          availability === 'true' ? 'available'
+            : availability === 'false' ? 'unavailable'
+              : availability;
 
         locationListHTML += '<div class="groupLocation">';
-        locationListHTML += '<span class="' + locationClass + '">'
-          + VuFind.icon('status-' + status)
+        locationListHTML += '<span class="' + availabilityClasses[availability] + '">'
+          + VuFind.icon('status-' + statusIcon)
           + result.locationList[x].location
           + '</span> ';
         locationListHTML += '</div>';
