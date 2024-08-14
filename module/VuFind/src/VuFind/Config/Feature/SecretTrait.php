3d9533bc02f9cf9fc1700e6ee56074a159b0bc6b
Robby <roudonro@msu.edu>
2024-08-14 11:02:07 -0400

    Fix pipelines

--- module/VuFind/src/VuFind/Config/Feature/SecretTrait.php
+++ module/VuFind/src/VuFind/Config/Feature/SecretTrait.php
@@ -1,9 +1,46 @@
 <?php
 
+
+/**
+ * Trait to import secret from file rather than a hardcoded config
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Michigan State University 2024.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Config
+ * @author   Robby ROUDON <roudonro@msu.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Page
+ */
+
 namespace VuFind\Config\Feature;
 
 use Laminas\Config\Config;
 
+/**
+ * Trait to import secret from file rather than a hardcoded config
+ *
+ * @category VuFind
+ * @package  Config
+ * @author   Robby ROUDON <roudonro@msu.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Page
+ */
 trait SecretTrait
 {
     /**
@@ -13,7 +50,7 @@ trait SecretTrait
      *
      * @return string|null
      */
-    function getSecretFromConfigOrSecretFile(Config|array|null $config, string $key, bool $trim = false): ?string
+    protected function getSecretFromConfigOrSecretFile(Config|array|null $config, string $key, bool $trim = false): ?string
     {
         if (!$config) {
             return null;
