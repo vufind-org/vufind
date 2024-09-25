4459472cb9266d2182c37ca8142e1c569c6cac4a
Ere Maijala <ere.maijala@helsinki.fi>
2024-09-25 16:10:21 +0300

    Avoid error with MLT when record does not exist

--- module/VuFindSearch/src/VuFindSearch/Backend/Solr/Connector.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/Connector.php
@@ -227,7 +227,15 @@ class Connector implements \Laminas\Log\LoggerAwareInterface
     {
         $handler = $this->map->getHandler(__FUNCTION__);
         $this->map->prepare(__FUNCTION__, $params);
-        return $this->query($handler, $params, true);
+
+        try {
+            return $this->query($handler, $params, true);
+        } catch (RequestErrorException $e) {
+            // If Solr was unable to fetch the record, just act like we have no similar records:
+            if (str_contains($e->getMessage(), 'Could not fetch document with id')) {
+                return '{}';
+            }
+        }
     }
 
     /**
@@ -494,6 +502,15 @@ class Connector implements \Laminas\Log\LoggerAwareInterface
         );
 
         if (!$response->isSuccess()) {
+            // Return a more detailed error message for a 400 error:
+            if ($response->getStatusCode() === 400) {
+                $json = json_decode($response->getBody(), true);
+                throw new RequestErrorException(
+                    '400 ' . $response->getReasonPhrase() . ' ' . $json['error']['msg'],
+                    400,
+                    $response
+                );
+            }
             throw HttpErrorException::createFromResponse($response);
         }
         return $response->getBody();
