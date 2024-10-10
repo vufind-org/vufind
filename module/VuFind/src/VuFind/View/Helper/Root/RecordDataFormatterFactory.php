3a9d196ac3203e07397f4ef0a7a6f12b64743789
Megan Schanz <schanzme@msu.edu>
2024-10-10 12:37:15 -0400

    Change Review Notes to Review

--- module/VuFind/src/VuFind/View/Helper/Root/RecordDataFormatterFactory.php
+++ module/VuFind/src/VuFind/View/Helper/Root/RecordDataFormatterFactory.php
@@ -372,7 +372,7 @@ class RecordDataFormatterFactory implements FactoryInterface
         $spec = new RecordDataFormatter\SpecBuilder();
         $spec->setTemplateLine('Summary', true, 'data-summary.phtml');
         $spec->setLine('Abstract', 'getAbstractNotes');
-        $spec->setLine('Review Notes', 'getReviewNotes');
+        $spec->setLine('Review', 'getReviewNotes');
         $spec->setLine('Content Advice', 'getContentAdviceNotes');
         $spec->setLine('Published', 'getDateSpan');
         $spec->setLine('Item Description', 'getGeneralNotes');
