d6143777441de0606ea2abbd5c42589705b60e3f
Robert Lange <robert.lange@uni-leipzig.de>
2024-10-18 13:58:45 +0200

    refs vufind-org#2999 do not use inheritdoc

--- module/VuFindSearch/src/VuFindSearch/Response/AbstractRecordCollection.php
+++ module/VuFindSearch/src/VuFindSearch/Response/AbstractRecordCollection.php
@@ -190,7 +190,14 @@ abstract class AbstractRecordCollection implements RecordCollectionInterface
     }
 
     /**
-     * @inheritdoc
+     * Sets the result set identifier for all records in the collection.
+     *
+     * This method assigns a given UUID to each record in the collection by calling
+     * the `setResultSetIdentifier` method on each record.
+     *
+     * @param string $uuid A valid UUID to be assigned to each record in the collection.
+     *
+     * @return void
      */
     public function setResultSetIdentifier(string $uuid)
     {
