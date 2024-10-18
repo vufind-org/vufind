d31c99200d03ffb92310a640983c314ddeb16e1f
Robert Lange <robert.lange@uni-leipzig.de>
2024-10-18 11:48:16 +0200

    refs vufind-org#2999 cleanup AbstractRecordCollection - order setResultSetIdentifier in methods like interface and use inheritdoc

--- module/VuFindSearch/src/VuFindSearch/Response/AbstractRecordCollection.php
+++ module/VuFindSearch/src/VuFindSearch/Response/AbstractRecordCollection.php
@@ -190,6 +190,16 @@ abstract class AbstractRecordCollection implements RecordCollectionInterface
     }
 
     /**
+     * @inheritdoc
+     */
+    public function setResultSetIdentifier(string $uuid)
+    {
+        foreach ($this->records as $record) {
+            $record->setResultSetIdentifier($uuid);
+        }
+    }
+
+    /**
      * Add a record to the collection.
      *
      * @param RecordInterface $record        Record to add
@@ -296,22 +306,5 @@ abstract class AbstractRecordCollection implements RecordCollectionInterface
     public function count(): int
     {
         return count($this->records);
-    }
-
-    /**
-     * Sets the result set identifier for all records in the collection.
-     *
-     * This method assigns a given UUID to each record in the collection by calling
-     * the `setResultSetIdentifier` method on each record.
-     *
-     * @param string $uuid A valid UUID to be assigned to each record in the collection.
-     *
-     * @return void
-     */
-    public function setResultSetIdentifier(string $uuid)
-    {
-        foreach ($this->records as $record) {
-            $record->setResultSetIdentifier($uuid);
-        }
     }
 }
