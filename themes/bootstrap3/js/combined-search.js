f6058e795957c63877de0026fa24353aeae44a26
Demian Katz <demian.katz@villanova.edu>
2024-12-04 12:27:29 -0500

    Reduce warnings from combined-search.js.

--- themes/bootstrap3/js/combined-search.js
+++ themes/bootstrap3/js/combined-search.js
@@ -3,11 +3,15 @@ VuFind.combinedSearch = (function CombinedSearch() {
 
   function init(container, url) {
     VuFind.loadHtml(container, url, '', function containerLoad(responseText) {
-      if (responseText.length === 0) {
-        container.style.display = "none";
+      if (!responseText || responseText.length === 0) {
+        if (container.style) {
+          container.style.display = "none";
+        }
         let parent = container.parentNode;
         while (parent && parent.classList.contains('js-hide-if-empty')) {
-          parent.style.display = "none";
+          if (parent.style) {
+            parent.style.display = "none";
+          }
           parent = parent.parentNode;
         }
       } else {
