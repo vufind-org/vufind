ce4a52d3780a2e6973c59a935c24079df85b749e
Demian Katz <demian.katz@villanova.edu>
2024-12-05 14:33:49 -0500

    Fix actual problem with hiding combined search elements.

--- themes/bootstrap3/js/combined-search.js
+++ themes/bootstrap3/js/combined-search.js
@@ -4,14 +4,11 @@ VuFind.combinedSearch = (function CombinedSearch() {
   function init(container, url) {
     VuFind.loadHtml(container, url, '', function containerLoad(responseText) {
       if (!responseText || responseText.length === 0) {
-        if (container.style) {
-          container.style.display = "none";
-        }
-        let parent = container.parentNode;
+        var element = typeof container === 'string' ? document.querySelector(container) : container;
+        element.style.display = "none";
+        let parent = element.parentNode;
         while (parent && parent.classList.contains('js-hide-if-empty')) {
-          if (parent.style) {
-            parent.style.display = "none";
-          }
+          parent.style.display = "none";
           parent = parent.parentNode;
         }
       } else {
