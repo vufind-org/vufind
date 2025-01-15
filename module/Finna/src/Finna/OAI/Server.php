829da11f6c5f0c8aeb5c5ee3d93fc3c5f4a1ef0f
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2025-01-15 16:29:19 +0200

    [FINNA-2838] Add Finna specific OAI metadata format (#3097)

--- module/Finna/src/Finna/OAI/Server.php
+++ module/Finna/src/Finna/OAI/Server.php
@@ -29,6 +29,8 @@
 
 namespace Finna\OAI;
 
+use VuFind\RecordDriver\AbstractBase as AbstractRecordDriver;
+
 /**
  * OAI Server class
  *
@@ -42,6 +44,31 @@ namespace Finna\OAI;
  */
 class Server extends \VuFind\OAI\Server
 {
+    /**
+     * Finna specific api fields from [OAI] section
+     *
+     * @var array
+     */
+    protected array $finnaApiFields = [];
+
+    /**
+     * Finna metadata prefix
+     *
+     * @var string
+     */
+    protected const OAI_FINNA_JSON = 'oai_finna_json';
+
+    /**
+     * Does the current configuration support the Finna metadata format (using
+     * the API's record formatter.
+     *
+     * @return bool
+     */
+    protected function supportsFinnaMetadata()
+    {
+        return !empty($this->finnaApiFields) && null !== $this->recordFormatter;
+    }
+
     /**
      * Initialize data about metadata formats. (This is called on demand and is
      * defined as a separate method to allow easy override by child classes).
@@ -69,5 +96,126 @@ class Server extends \VuFind\OAI\Server
         $this->metadataFormats['oai_qdc'] = [
             'schema' => $qdc,
             'namespace' => 'urn:dc:qdc:container'];
+        if ($this->supportsFinnaMetadata()) {
+            $this->metadataFormats[self::OAI_FINNA_JSON] = [
+                'schema' => 'https://vufind.org/xsd/oai_vufind_json-1.0.xsd',
+                'namespace' => 'http://vufind.org/oai_vufind_json-1.0',
+            ];
+        }
+    }
+
+    /**
+     * Get record as a metadata presentation
+     *
+     * @param AbstractRecordDriver $record A record driver object
+     * @param string               $format Metadata format to obtain
+     *
+     * @return string|bool String on success or false if error occurs
+     */
+    protected function getRecordAsXML(AbstractRecordDriver $record, string $format): string|false
+    {
+        if (self::OAI_FINNA_JSON === $format && $this->supportsFinnaMetadata()) {
+            return $this->getFinnaMetadata($record);
+        }
+        return parent::getRecordAsXml($record, $format);
+    }
+
+    /**
+     * Respond to a ListMetadataFormats request.
+     *
+     * @return string|false
+     */
+    protected function listMetadataFormats()
+    {
+        // If a specific ID was provided, try to load the related record; otherwise,
+        // set $record to false so we know it is a generic request.
+        if (isset($this->params['identifier'])) {
+            if (!($record = $this->loadRecord($this->params['identifier']))) {
+                return $this->showError('idDoesNotExist', 'Unknown Record');
+            }
+        } else {
+            $record = false;
+        }
+
+        // Loop through all available metadata formats and see if they apply in
+        // the current context (all apply if $record is false, since that
+        // means that no specific record ID was requested; otherwise, they only
+        // apply if the current record driver supports them):
+        $response = $this->createResponse();
+        $xml = $response->addChild('ListMetadataFormats');
+        foreach ($this->getMetadataFormats() as $prefix => $details) {
+            if (
+                $record === false
+                || $record->getXML($prefix) !== false
+                || ('oai_vufind_json' === $prefix && $this->supportsVuFindMetadata())
+                || (self::OAI_FINNA_JSON === $prefix && $this->supportsFinnaMetadata())
+            ) {
+                $node = $xml->addChild('metadataFormat');
+                $node->metadataPrefix = $prefix;
+                if (isset($details['schema'])) {
+                    $node->schema = $details['schema'];
+                }
+                if (isset($details['namespace'])) {
+                    $node->metadataNamespace = $details['namespace'];
+                }
+            }
+        }
+
+        // Display the response:
+        return $response->asXML();
+    }
+
+    /**
+     * Load data from the OAI section of config.ini. (This is called by the
+     * constructor and is only a separate method to allow easy override by child
+     * classes).
+     *
+     * @param \Laminas\Config\Config $config VuFind configuration
+     *
+     * @return void
+     */
+    protected function initializeSettings(\Laminas\Config\Config $config)
+    {
+        parent::initializeSettings($config);
+        // Initialize Finna API format fields:
+        $this->finnaApiFields = array_filter(
+            explode(
+                ',',
+                $config->OAI->finna_api_format_fields ?? ''
+            )
+        );
+    }
+
+    /**
+     * Support method for attachNonDeleted() to build the Finna metadata for
+     * a record driver.
+     *
+     * @param object $record A record driver object
+     *
+     * @return string
+     */
+    protected function getFinnaMetadata($record)
+    {
+        // Root node
+        $recordDoc = new \DOMDocument();
+        $finnaFormat = $this->getMetadataFormats()[self::OAI_FINNA_JSON];
+        $rootNode = $recordDoc->createElementNS($finnaFormat['namespace'], self::OAI_FINNA_JSON . ':record');
+        $rootNode->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $rootNode->setAttribute('xsi:schemaLocation', $finnaFormat['namespace'] . ' ' . $finnaFormat['schema']);
+        $recordDoc->appendChild($rootNode);
+
+        // Add oai_dc part
+        $oaiDc = new \DOMDocument();
+        $oaiDc->loadXML($record->getXML('oai_dc', $this->baseHostURL, $this->recordLinkerHelper));
+        $rootNode->appendChild($recordDoc->importNode($oaiDc->documentElement, true));
+
+        // Add Finna specific metadata
+        $records = $this->recordFormatter->format([$record], $this->finnaApiFields);
+        $metadataNode = $recordDoc->createElementNS($finnaFormat['namespace'], self::OAI_FINNA_JSON . ':metadata');
+        $metadataNode->setAttribute('type', 'application/json');
+        $metadataNode->appendChild($recordDoc->createCDATASection(json_encode($records[0])));
+        $rootNode->appendChild($metadataNode);
+
+        return $recordDoc->saveXML();
     }
 }
--- module/Finna/src/Finna/RecordDriver/Feature/SolrFinnaTrait.php
+++ module/Finna/src/Finna/RecordDriver/Feature/SolrFinnaTrait.php
@@ -764,6 +764,49 @@ trait SolrFinnaTrait
     }
 
     /**
+     * Get an array containing media types as strings.
+     *
+     * @return array
+     */
+    public function getMediaTypesAsStrings(): array
+    {
+        return array_map(
+            fn ($entry) => (string)$entry,
+            $this->fields['media_type_str_mv'] ?? []
+        );
+    }
+
+    /**
+     * Get array containing ctrlnum.
+     *
+     * @return array
+     */
+    public function getControlNumbers(): array
+    {
+        return $this->fields['ctrlnum'] ?? [];
+    }
+
+    /**
+     * Get array containing major genres
+     *
+     * @return array
+     */
+    public function getMajorGenres(): array
+    {
+        return $this->fields['major_genre_str_mv'] ?? [];
+    }
+
+    /**
+     * Get array containing Usage rights extended
+     *
+     * @return array
+     */
+    public function getUsageRightsExt(): array
+    {
+        return $this->fields['usage_rights_ext_str_mv'] ?? [];
+    }
+
+    /**
      * Is rating allowed.
      *
      * @return bool
