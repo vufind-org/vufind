b8a278711f14c4729d3cb3680e41e7338e06b5d8
Ere Maijala <ere.maijala@helsinki.fi>
2025-01-30 14:37:21 +0200

    Drop bs3-compat from navs and dropdowns.

--- themes/bootstrap5/js/bs3-compat.js
+++ themes/bootstrap5/js/bs3-compat.js
@@ -2,66 +2,6 @@
 VuFind.register('bootstrap3CompatibilityLayer', function bootstrap3CompatibilityLayer() {
   const data_attribute_selector = '[data-dismiss],[data-target],[data-toggle],[data-ride],[data-slide],[data-slide-to]';
 
-  function initNav() {
-    document.querySelectorAll('.nav').forEach((navEl) => {
-      if (navEl.classList.contains('nav-tabs')) {
-        // tablist role for tabs:
-        navEl.setAttribute('role', 'tablist');
-
-        // Apply active class to tab li for back-compatibility:
-        const observer = new MutationObserver((mutations) => {
-          mutations.forEach((mutation) => {
-            if ('attributes' !== mutation.type || mutation.attributeName !== 'class') {
-              return;
-            }
-            const target = mutation.target;
-            if (target.nodeName === 'A' && target.parentNode && target.classList.contains('active')) {
-              target.parentNode.classList.add('active');
-            } else {
-              target.parentNode.classList.remove('active');
-            }
-          });
-        });
-        observer.observe(navEl, { attributes: true, attributeFilter: ['class'], subtree: true });
-      }
-      navEl.querySelectorAll('li').forEach((liEl) => {
-        const aEl = liEl.querySelector(':scope > a');
-        if (liEl.classList.contains('dropdown__item')) {
-          if (aEl && !aEl.classList.contains('btn')) {
-            aEl.classList.add('dropdown-item');
-            if (liEl.classList.contains('active')) {
-              aEl.classList.add('active');
-            }
-          }
-        } else if (aEl) {
-          const oldStateA = VuFind.disableTransitions(aEl);
-          if (!aEl.classList.contains('btn')) {
-            aEl.classList.add('nav-link');
-          }
-          if (liEl.classList.contains('active')) {
-            aEl.classList.add('active');
-            if (null === liEl.closest('.searchForm')) {
-              liEl.classList.remove('active');
-            }
-          }
-          // Use a timeout to allow the transition to complete before restoring the state:
-          setTimeout(() => { VuFind.restoreTransitions(aEl, oldStateA); }, 0);
-        }
-      });
-    });
-
-    // Reverse effects of record.js:
-    setTimeout(
-      () => {
-        $('.record-tabs .nav-tabs a').off('shown.bs.tab');
-        document.querySelectorAll('.record-tabs .nav-tabs li').forEach((liEl) => {
-          liEl.removeAttribute('aria-selected');
-        });
-      },
-      0
-    );
-  }
-
   function initBreadcrumbs() {
     document.querySelectorAll('.breadcrumb li').forEach((el) => {
       el.classList.add('breadcrumb-item');
@@ -121,7 +61,6 @@ VuFind.register('bootstrap3CompatibilityLayer', function bootstrap3Compatibility
   }
 
   function init() {
-    initNav();
     initFormElements();
     initBreadcrumbs();
     initCollapse();
