eae636f1a57ee6d90d2f8c4eaf2c1d92137ef875
Demian Katz <demian.katz@villanova.edu>
2025-01-30 16:04:11 -0500

    Fix test.

--- module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/RecordDataFormatterTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/RecordDataFormatterTest.php
@@ -89,7 +89,7 @@ class RecordDataFormatterTest extends \PHPUnit\Framework\TestCase
         });
         $record->setDbServiceManager($serviceManager);
         return [
-            'assetPipeline' => new \VuFindTheme\View\Helper\AssetPipeline(),
+            'assetPipeline' => $this->createMock(\VuFindTheme\View\Helper\AssetPipeline::class),
             'auth' => new \VuFind\View\Helper\Root\Auth(
                 $this->createMock(\VuFind\Auth\Manager::class),
                 $this->createMock(\VuFind\Auth\ILSAuthenticator::class)
