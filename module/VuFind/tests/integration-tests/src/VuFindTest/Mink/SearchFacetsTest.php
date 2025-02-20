172c5dd201691ce6e1816111a260b3ca054d0678
Demian Katz <demian.katz@villanova.edu>
2025-02-20 08:57:36 -0500

    Add basic test.

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/SearchFacetsTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/SearchFacetsTest.php
@@ -1465,4 +1465,42 @@ class SearchFacetsTest extends \VuFindTest\Integration\MinkTestCase
 
         $this->assertCount(0, $page->findAll('css', '.facet.active'));
     }
+
+    /**
+     * Test that filters applied during search show up on the record page and can be removed there.
+     *
+     * @return void
+     */
+    public function testFilterClearingOnRecordPage(): void
+    {
+        // Start with a search with multiple filters applied:
+        $path = '/Search/Results'
+            . '?filter[]=building%3A"geo.mrc"'
+            . '&filter[]=format%3A"Book"'
+            . '&filter[]=author_facet%3A"Erickson%2C+Joette"&type=AllFields';
+        $session = $this->getMinkSession();
+        $session->visit($this->getVuFindUrl() . $path);
+        $page = $session->getPage();
+        $this->assertCount(3, $page->findAll('css', '.facet.active'));
+        $this->clickCss($page, '#result0 a.title');
+        $this->waitForPageLoad($page);
+
+        // Make sure the active filters show up:
+        $filterArea = $this->findCss($page, '.active-filters');
+        $filters = $filterArea->findAll('css', '.filter-value');
+        $this->assertCount(3, $filters);
+
+        // Remove the first filter:
+        $filters[0]->click();
+        $this->waitForPageLoad($page);
+
+        // There should now be fewer filters:
+        $filters = $filterArea->findAll('css', '.filter-value');
+        $this->assertCount(2, $filters);
+
+        // Now submit a new search:
+        $this->clickCss($page, '#searchForm .btn-primary');
+        $this->waitForPageLoad($page);
+        $this->assertCount(2, $page->findAll('css', '.facet.active'));
+    }
 }
