ebd3ae35d3a486ded75d79e22618d772021e3136
Maccabee Levine <31278545+maccabeelevine@users.noreply.github.com>
2025-04-10 11:52:42 -0400

    Add missing space
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/Search/EDS/AbstractEDSOptions.php
+++ module/VuFind/src/VuFind/Search/EDS/AbstractEDSOptions.php
@@ -53,7 +53,7 @@ abstract class AbstractEDSOptions extends \VuFind\Search\Base\Options
      *
      * The defaultView API property takes the form vufindSetting_ebscoSetting -- the first component
      * of the underscore-delimited string is the view name used by VuFind (e.g. list or grid).
-     * However, for EDSand EPF, only list is suggested to be used. The second component is the format
+     * However, for EDS and EPF, only list is suggested to be used. The second component is the format
      * requested from the Ebsco API (e.g. title, brief or detailed).
      *
      * @param int     $index   Index of part to extract from the property
