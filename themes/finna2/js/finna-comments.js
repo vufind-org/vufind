ce0ad7ed6134e02794bfed3726f6b0f673d4c58c
Ere Maijala <ere.maijala@helsinki.fi>
2025-04-24 21:13:59 +0300

    [FINNA-3416] Fix upstream compatibility of record comment handling.

--- themes/finna2/js/finna-comments.js
+++ themes/finna2/js/finna-comments.js
@@ -1,4 +1,4 @@
-/*global VuFind, finna, registerAjaxCommentRecord:true, refreshCommentList, refreshRecordRating */
+/*global VuFind, finna, refreshCommentList, refreshRecordRating */
 finna.comments = (function finnaComments() {
   /**
    * Refresh comments and ratings
@@ -6,71 +6,11 @@ finna.comments = (function finnaComments() {
   function requestRefreshCommentsAndRating() {
     let recordId = $('input.hiddenId').val();
     let recordSource = $('input.hiddenSource').val();
-    var tab = $('.usercomments-tab');
-    refreshCommentList(tab, recordId, recordSource);
+    const tab = document.querySelector('.usercomments-tab');
+    if (tab) {
+      refreshCommentList(tab, recordId, recordSource);
+    }
     refreshRecordRating(recordId, recordSource);
-  }
-
-  /**
-   * Initialize form for the comments
-   * @param {Function} parentMethod Parent method to call first
-   */
-  function initCommentForm(parentMethod) {
-    $('.comment-list .delete').off('click');
-    parentMethod();
-
-    $('form.comment-form').off('submit').on('submit', function onCommentSubmit(event) {
-      var form = this;
-      var data = {};
-      $(form).find("input,textarea").each(function appendCaptchaData() {
-        var input = $(this);
-        if (input.attr('type') === 'radio' && !input.prop('checked')) {
-          return true;
-        }
-        data[input.attr('name')] = input.val();
-      });
-
-      if (typeof form.checkValidity == 'function') {
-        // This is for Safari, which doesn't validate forms on submit
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          return;
-        }
-      } else {
-        // JS validation for browsers that don't support form validation
-        if (form.comment.value === '') {
-          $(form.comment).addClass('invalid');
-          event.preventDefault();
-          return;
-        }
-        $(form.comment).removeClass('invalid');
-      }
-
-      $(this).find('input.cancel').toggleClass('hide', true);
-      $(this).find('input[type="submit"]').attr('disabled', true).button('loading');
-
-      var url = VuFind.path + '/AJAX/JSON?' + $.param({method: 'commentRecord'});
-      $.ajax({
-        type: 'POST',
-        url: url,
-        data: data,
-        dataType: 'json'
-      })
-        .done(function onCommentSubmitDone() {
-          requestRefreshCommentsAndRating();
-          $(form).find('textarea[name="comment"]').val('');
-        })
-        .fail(function onCommentSubmitFail(response/*, textStatus*/) {
-          if (typeof response.responseJSON !== 'undefined') {
-            alert(response.responseJSON.data);
-          } else {
-            alert(VuFind.translate('error_occurred'));
-          }
-          $(form).find('input.cancel').toggleClass('hide', false);
-          $(form).find('input[type="submit"]').attr('disabled', false).button('reset');
-        });
-      return false;
-    });
   }
 
   /**
@@ -121,12 +61,6 @@ finna.comments = (function finnaComments() {
 
     initEditComment(allowCommenting);
 
-    // Override global method
-    var _registerAjaxCommentRecord = registerAjaxCommentRecord;
-    registerAjaxCommentRecord = function registerAjaxCommentRecord() { // eslint-disable-line no-global-assign
-      initCommentForm(_registerAjaxCommentRecord);
-    };
-    registerAjaxCommentRecord();
     VuFind.lightbox.bind($('.usercomments-tab'));
   }
 
