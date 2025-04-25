30f5a09f095f5db3949a98cadc6f6211736a2cfe
Johannes Schultze <schultze@effective-webwork.de>
2025-04-25 12:19:50 +0200

    Added setters and getters.

--- module/VuFind/src/VuFind/Db/Entity/NotificationsBroadcastsEntityInterface.php
+++ module/VuFind/src/VuFind/Db/Entity/NotificationsBroadcastsEntityInterface.php
@@ -49,6 +49,213 @@ interface NotificationsBroadcastsEntityInterface extends EntityInterface
      *
      * @return int|null
      */
-
     public function getPrimaryKeyId(): ?int;
+
+    /**
+     * Setter for id
+     *
+     * @param int $id ID
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setId(int $id): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for id
+     *
+     * @return int
+     */
+    public function getId(): int;
+
+    /**
+     * Setter for broadcast_id
+     *
+     * @param int|null $broadcastId Broadcast ID
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setBroadcastId(?int $broadcastId): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for broadcast_id
+     *
+     * @return int|null
+     */
+    public function getBroadcastId(): ?int;
+
+    /**
+     * Setter for visibility
+     *
+     * @param bool|null $visibility Visibility
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setVisibility(?bool $visibility): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for visibility
+     *
+     * @return bool|null
+     */
+    public function getVisibility(): ?bool;
+
+    /**
+     * Setter for visibility_global
+     *
+     * @param bool|null $visibilityGlobal Visibility Global
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setVisibilityGlobal(?bool $visibilityGlobal): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for visibility_global
+     *
+     * @return bool|null
+     */
+    public function getVisibilityGlobal(): ?bool;
+
+    /**
+     * Setter for priority
+     *
+     * @param int|null $priority Priority
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setPriority(?int $priority): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for priority
+     *
+     * @return int|null
+     */
+    public function getPriority(): ?int;
+
+    /**
+     * Setter for author_id
+     *
+     * @param int|null $authorId Author ID
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setAuthorId(?int $authorId): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for author_id
+     *
+     * @return int|null
+     */
+    public function getAuthorId(): ?int;
+
+    /**
+     * Setter for content
+     *
+     * @param string|null $content Content
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setContent(?string $content): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for content
+     *
+     * @return string|null
+     */
+    public function getContent(): ?string;
+
+    /**
+     * Setter for color
+     *
+     * @param string|null $color Color
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setColor(?string $color): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for color
+     *
+     * @return string|null
+     */
+    public function getColor(): ?string;
+
+    /**
+     * Setter for startdate
+     *
+     * @param \DateTime|null $startDate Start Date
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setStartDate(?\DateTime $startDate): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for startdate
+     *
+     * @return \DateTime|null
+     */
+    public function getStartDate(): ?\DateTime;
+
+    /**
+     * Setter for enddate
+     *
+     * @param \DateTime|null $endDate End Date
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setEndDate(?\DateTime $endDate): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for enddate
+     *
+     * @return \DateTime|null
+     */
+    public function getEndDate(): ?\DateTime;
+
+    /**
+     * Setter for change_date
+     *
+     * @param \DateTime|null $changeDate Change Date
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setChangeDate(?\DateTime $changeDate): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for change_date
+     *
+     * @return \DateTime|null
+     */
+    public function getChangeDate(): ?\DateTime;
+
+    /**
+     * Setter for create_date
+     *
+     * @param \DateTime|null $createDate Create Date
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setCreateDate(?\DateTime $createDate): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for create_date
+     *
+     * @return \DateTime|null
+     */
+    public function getCreateDate(): ?\DateTime;
+
+    /**
+     * Setter for language
+     *
+     * @param string|null $language Language
+     *
+     * @return NotificationsBroadcastsEntityInterface
+     */
+    public function setLanguage(?string $language): NotificationsBroadcastsEntityInterface;
+
+    /**
+     * Getter for language
+     *
+     * @return string|null
+     */
+    public function getLanguage(): ?string;
 }
--- module/VuFind/src/VuFind/Db/Entity/NotificationsPagesEntityInterface.php
+++ module/VuFind/src/VuFind/Db/Entity/NotificationsPagesEntityInterface.php
@@ -49,6 +49,213 @@ interface NotificationsPagesEntityInterface extends EntityInterface
      *
      * @return int|null
      */
-
     public function getPrimaryKeyId(): ?int;
+
+    /**
+     * Setter for id
+     *
+     * @param int $id ID
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setId(int $id): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for id
+     *
+     * @return int
+     */
+    public function getId(): int;
+
+    /**
+     * Setter for page_id
+     *
+     * @param int|null $pageId Page ID
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setPageId(?int $pageId): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for page_id
+     *
+     * @return int|null
+     */
+    public function getPageId(): ?int;
+
+    /**
+     * Setter for visibility
+     *
+     * @param bool|null $visibility Visibility
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setVisibility(?bool $visibility): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for visibility
+     *
+     * @return bool|null
+     */
+    public function getVisibility(): ?bool;
+
+    /**
+     * Setter for is_external_url
+     *
+     * @param bool|null $isExternalUrl Is External URL
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setIsExternalUrl(?bool $isExternalUrl): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for is_external_url
+     *
+     * @return bool|null
+     */
+    public function getIsExternalUrl(): ?bool;
+
+    /**
+     * Setter for priority
+     *
+     * @param int|null $priority Priority
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setPriority(?int $priority): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for priority
+     *
+     * @return int|null
+     */
+    public function getPriority(): ?int;
+
+    /**
+     * Setter for author_id
+     *
+     * @param int|null $authorId Author ID
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setAuthorId(?int $authorId): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for author_id
+     *
+     * @return int|null
+     */
+    public function getAuthorId(): ?int;
+
+    /**
+     * Setter for headline
+     *
+     * @param string|null $headline Headline
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setHeadline(?string $headline): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for headline
+     *
+     * @return string|null
+     */
+    public function getHeadline(): ?string;
+
+    /**
+     * Setter for nav_title
+     *
+     * @param string|null $navTitle Navigation Title
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setNavTitle(?string $navTitle): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for nav_title
+     *
+     * @return string|null
+     */
+    public function getNavTitle(): ?string;
+
+    /**
+     * Setter for content
+     *
+     * @param string|null $content Content
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setContent(?string $content): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for content
+     *
+     * @return string|null
+     */
+    public function getContent(): ?string;
+
+    /**
+     * Setter for external_url
+     *
+     * @param string|null $externalUrl External URL
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setExternalUrl(?string $externalUrl): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for external_url
+     *
+     * @return string|null
+     */
+    public function getExternalUrl(): ?string;
+
+    /**
+     * Setter for change_date
+     *
+     * @param \DateTime|null $changeDate Change Date
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setChangeDate(?\DateTime $changeDate): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for change_date
+     *
+     * @return \DateTime|null
+     */
+    public function getChangeDate(): ?\DateTime;
+
+    /**
+     * Setter for create_date
+     *
+     * @param \DateTime|null $createDate Create Date
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setCreateDate(?\DateTime $createDate): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for create_date
+     *
+     * @return \DateTime|null
+     */
+    public function getCreateDate(): ?\DateTime;
+
+    /**
+     * Setter for language
+     *
+     * @param string|null $language Language
+     *
+     * @return NotificationsPagesEntityInterface
+     */
+    public function setLanguage(?string $language): NotificationsPagesEntityInterface;
+
+    /**
+     * Getter for language
+     *
+     * @return string|null
+     */
+    public function getLanguage(): ?string;
 }
