9f089f4d2bf75b4058e934b6573b0e2ae4261d5c
Ere Maijala <ere.maijala@helsinki.fi>
2025-04-28 10:23:59 +0300

    [FINNA-3416] Remove non-functional MyResearch menu JS.

--- themes/finna2/js/finna-menu.js
+++ themes/finna2/js/finna-menu.js
@@ -19,61 +19,9 @@ finna.menu = (function finnaMenu() {
   }
 
   /**
-   * Toggle a sub menu
-   * @param {jQuery} a Container to check for toggle
-   */
-  function toggleSubmenu(a) {
-    a.trigger('beforetoggle');
-    a.toggleClass('collapsed');
-    a.parent().find('ul').first().toggleClass('in', !a.hasClass('collapsed'));
-    a.attr("aria-expanded", !a.hasClass("collapsed"));
-
-    if (a.hasClass('sticky-menu')) {
-      $('.nav-tabs-personal').toggleClass('move-list');
-      if (!$('.nav-tabs-personal').hasClass('move-list')) {
-        window.scroll(0, 0);
-      }
-    }
-  }
-
-  /**
-   * Initialize menu lists
-   */
-  function initMenuLists() {
-    $('.menu-parent').on('togglesubmenu.finna', function onToggleSubmenu() {
-      toggleSubmenu($(this));
-    });
-
-    $('.menu-parent > .js-toggle-menu').on('click', function clickLink(e) {
-      e.preventDefault();
-      $(this).parent().trigger('togglesubmenu');
-    });
-
-    if ($('.mylist-bar').children().length === 0) {
-      $('#open-list').one('beforetoggle.finna', function loadList() {
-        var link = $(this);
-        link.data('preload', false);
-        $.ajax({
-          type: 'GET',
-          dataType: 'json',
-          async: false,
-          url: VuFind.path + '/AJAX/JSON?method=getMyLists',
-          data: {'active': null}
-        }).done(function onGetMyListsDone(data) {
-          $('.mylist-bar').append(data.data);
-          $('.add-new-list-holder').hide();
-        });
-      });
-    } else {
-      $('#open-list').removeClass('collapsed').siblings('ul').first().addClass('in');
-    }
-  }
-
-  /**
    * Initialize finna.menu
    */
   function init() {
-    initMenuLists();
     initAccountChecks();
   }
 
