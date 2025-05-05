3b5fb9ba9d16ae43f62c3ec1a35a6ecdd6839d7e
Robby <roudonro@msu.edu>
2025-05-05 15:06:42 -0400

    Add JS comments

--- themes/bootstrap5/js/get_this_dropdown.js
+++ themes/bootstrap5/js/get_this_dropdown.js
@@ -1,25 +1,30 @@
 let KEY_CODE = {
   DOWN_ARROW: 40,
   UP_ARROW: 38,
-}
+};
 $(function pageLoad() {
   let modal = document.querySelector('#modal');
 
+  /**
+   * Whether the dropdown is open
+   * @returns {boolean}
+   */
   function isDropdownOpen() {
     return modal.querySelector(".get-this-dropdown > ul > li > div").classList.contains("active");
   }
 
+  /**
+   * Whether the link for the current holding is the element being clicked
+   * @param e
+   * @returns {*}
+   */
   function isDropdownLinkTargeted(e) {
     return e.target.matches(".get-this-dropdown > ul > li > a");
   }
 
-  function openDropdownListener(e) {
-    if (isDropdownLinkTargeted(e)) {
-      toggleDropdown();
-      e.preventDefault();
-    }
-  }
-
+  /**
+   * Open / close the dropdown
+   */
   function toggleDropdown() {
     const a = modal.querySelector(".get-this-dropdown > ul > li > a");
     const siblingDiv = modal.querySelector(".get-this-dropdown > ul > li > div");
@@ -37,6 +42,20 @@ $(function pageLoad() {
     }
   }
 
+  /**
+   * Listener on events to open the dropdown
+   * @param e
+   */
+  function openDropdownListener(e) {
+    if (isDropdownLinkTargeted(e)) {
+      toggleDropdown();
+      e.preventDefault();
+    }
+  }
+
+  /**
+   * Listener on events to close the dropdown
+   */
   function closeDropdownListener() {
     if (isDropdownOpen()) {
       // Need delay to let new focus to happen
@@ -47,6 +66,10 @@ $(function pageLoad() {
     }
   }
 
+  /**
+   * Listener on events to open the dropdown if bottom arrow is pressed
+   * @param e
+   */
   function openDropdownOnKeyDown(e) {
     if (isDropdownLinkTargeted(e) && !isDropdownOpen() && e.keyCode === KEY_CODE.DOWN_ARROW) {
       toggleDropdown();
@@ -54,6 +77,11 @@ $(function pageLoad() {
     }
   }
 
+  /**
+   * Switch the focus to a sibling element in the dropdown
+   * @param elem
+   * @returns {boolean}
+   */
   function focusSiblingElement(elem) {
     if (elem) {
       const link = elem.querySelector("a");
@@ -63,6 +91,10 @@ $(function pageLoad() {
     return false;
   }
 
+  /**
+   * Listener on keys pressed (up/down) to change the element focused in the dropdown
+   * @param e
+   */
   function arrowKeysListener(e) {
     const dropdown = document.querySelector(".get-this-dropdown div");
     // If dropdown is not defined or the focus is on an element not in the container
