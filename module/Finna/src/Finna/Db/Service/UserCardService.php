3d0a745aca9fb9f9c0942174e18c3fa46248c99a
Ere Maijala <ere.maijala@helsinki.fi>
2025-05-12 15:12:21 +0300

    [FINNA-3521] Fix check for existing library card on login. (#3211)

--- module/Finna/src/Finna/Db/Service/UserCardService.php
+++ module/Finna/src/Finna/Db/Service/UserCardService.php
@@ -78,8 +78,8 @@ class UserCardService extends \VuFind\Db\Service\UserCardService
         ?string $catUsername = null
     ): array {
         $cards = parent::getLibraryCards($userOrId, $id, $catUsername);
-        // Filter cards by active login targets:
-        if ($cards) {
+        // Filter cards by active login targets unless a specific subset of cards was requested:
+        if ($cards && null === $id && null === $catUsername) {
             $prefixes = ($this->getLoginTargetPrefixes)();
             $cards = array_filter(
                 $cards,
