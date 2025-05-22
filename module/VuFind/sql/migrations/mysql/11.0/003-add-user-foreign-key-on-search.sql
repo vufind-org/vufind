8b3317786962906302d1b963808427a648cffcaf
Demian Katz <demian.katz@villanova.edu>
2025-05-22 13:28:05 -0400

    Add foreign key from search to user.

---
+++ module/VuFind/sql/migrations/mysql/11.0/003-add-user-foreign-key-on-search.sql
@@ -0,0 +1,4 @@
+UPDATE `search` SET `user_id`=null WHERE `user_id` NOT IN (SELECT `id` FROM `user`);
+
+ALTER TABLE `search`
+    ADD CONSTRAINT `search_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
