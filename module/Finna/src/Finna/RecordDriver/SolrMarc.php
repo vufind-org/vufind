52716302183a17bac304b2a7d4116fbbba3dd9b1
rajaro <jaro.ravila@gmail.com>
2025-05-28 11:05:44 +0300

    [FINNA-3411] Marc: Check 856 subfield z for image type (#3229)

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -293,6 +293,10 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc implements \Laminas\Log\Log
                 $part = $this->getSubfield($url, '3');
                 // Only take large image. Thumbnail is too small.
                 $image = strcasecmp($part, 'Image') === 0;
+                if (!$image) {
+                    // Some Quria records do not have subfield q, check subfield z
+                    $image = 'Kansikuva' === $this->getSubfield($url, 'z');
+                }
             }
 
             if (
