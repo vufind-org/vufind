a88798f51c611475889951078a4e5292287cf291
Demian Katz <demian.katz@villanova.edu>
2025-06-03 14:48:45 -0400

    Fix CLI configuration.

--- config/cli-config.php
+++ config/cli-config.php
@@ -2,5 +2,5 @@
 
 $app = include __DIR__ . '/application.php';
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    $app->getServiceManager()->get('doctrine.entity_manager.orm_default')
+    $app->getServiceManager()->get(\Doctrine\ORM\EntityManager::class)
 );
