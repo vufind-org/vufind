08922bb9420ee744a02fff213741e82d28565303
Thomas Wagener <wagener@hebis.uni-frankfurt.de>
2025-06-05 17:01:19 +0200

    Update module/VuFind/src/VuFind/Config/Location/ConfigLocationInterface.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/Config/Location/ConfigLocationInterface.php
+++ module/VuFind/src/VuFind/Config/Location/ConfigLocationInterface.php
@@ -151,7 +151,7 @@ interface ConfigLocationInterface
     public function getDirLocationsParent(): ?ConfigLocationInterface;
 
     /**
-     * Set the parent location.
+     * Set the location of the configuration in the parent directory that might be specified in DirLocations.ini.
      *
      * @param ?ConfigLocationInterface $dirLocationsParent Parent location
      *
