8485e3be1e90407d6774b5cee19f2a03e0102e45
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2025-07-01 10:05:49 +0300

    docker-compose: fix sshproxy container (#3252)
    * restart container when ssh exits
    * add -g flag (bind to all interfaces) to allow connections from other containers

--- local/docker/sshproxy-start.sh
+++ local/docker/sshproxy-start.sh
@@ -6,5 +6,5 @@ set -eu
 install -o 0 -g 0 -m 0600 /tmp/ssh_known_hosts /etc/ssh/
 install -o 0 -g 0 -m 0600 /tmp/ssh.conf /etc/ssh/ssh_config.d/
 
-exec /usr/bin/ssh -v -N -D "$SOCKS5_PROXY_PORT" -i /root/identity \
+exec /usr/bin/ssh -v -N -g -D "$SOCKS5_PROXY_PORT" -i /root/identity \
     -J "$SSH_JUMP_SEQUENCE" "$SOCKS5_PROXY_DST"
