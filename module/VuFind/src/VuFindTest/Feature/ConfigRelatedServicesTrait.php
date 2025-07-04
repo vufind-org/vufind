16c5a9b218fef3b222f39cb5ba2305ff857d8b4f
Thomas Wagener <wagener@hebis.uni-frankfurt.de>
2025-07-04 10:49:37 +0200

    Update module/VuFind/src/VuFindTest/Feature/ConfigRelatedServicesTrait.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFindTest/Feature/ConfigRelatedServicesTrait.php
+++ module/VuFind/src/VuFindTest/Feature/ConfigRelatedServicesTrait.php
@@ -5,7 +5,7 @@
  *
  * PHP version 8
  *
- * Copyright (C) The National Library of Finland 2022.
+ * Copyright (C) The National Library of Finland 2022-2025.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
