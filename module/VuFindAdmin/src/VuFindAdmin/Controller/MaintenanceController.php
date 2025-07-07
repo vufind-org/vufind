8f4590640a36239eebfbfaec0fb401a3b5c17726
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-07-07 15:15:34 -0400

    Remove Additional Implementation of Laminas Log

--- module/VuFindAdmin/src/VuFindAdmin/Controller/MaintenanceController.php
+++ module/VuFindAdmin/src/VuFindAdmin/Controller/MaintenanceController.php
@@ -295,7 +295,7 @@ class MaintenanceController extends AbstractAdmin
         $cache = new SimpleCacheDecorator($this->cacheManager->getCache('browscap'));
         $client = $this->guzzleService->createClient();
 
-        $bc = new \BrowscapPHP\BrowscapUpdater($cache, new \Laminas\Log\PsrLoggerAdapter($this->logger), $client);
+        $bc = new \BrowscapPHP\BrowscapUpdater($cache, $this->logger, $client);
         try {
             $bc->checkUpdate();
         } catch (\BrowscapPHP\Exception\NoNewVersionException $e) {
