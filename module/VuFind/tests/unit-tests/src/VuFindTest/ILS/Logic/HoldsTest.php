a338971b9a274b6f8cd688c00588b87f1dcc44b9
Claus Wolf <cwolf@ebsco.com>
2025-07-10 18:21:05 +0200

    Test formatHoldings
    using some Folio raw data to ensure the formatHoldings responds as expected

--- module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Logic/HoldsTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Logic/HoldsTest.php
@@ -46,6 +46,8 @@ use VuFind\ILS\Logic\Holds;
  */
 class HoldsTest extends \PHPUnit\Framework\TestCase
 {
+    use \VuFindTest\Feature\FixtureTrait;
+
     /**
      * Get a Holds object for testing.
      *
@@ -97,5 +99,42 @@ class HoldsTest extends \PHPUnit\Framework\TestCase
     {
         $logic = $this->getHoldsLogic(config: $configArray);
         $this->assertEquals($expectedList, $logic->getSuppressedLocations());
+    }
+
+    /**
+     * Test formatHoldings method
+     *
+     * @return void
+     */
+    public function testFormatHoldings(): void
+    {
+        $catalog = $this->createMock(Connection::class);
+        $catalog->method('getHoldingsTextFieldNames')->willReturn(['notes', 'summary']);
+        
+        $logic = $this->getHoldsLogic(catalog: $catalog);
+        
+        // Use reflection to access the protected formatHoldings method
+        $reflection = new \ReflectionClass($logic);
+        $method = $reflection->getMethod('formatHoldings');
+        $method->setAccessible(true);
+        
+        // Example holdings data
+        $holdings = $this->getJsonFixture('ils/holdings_example.json');
+        
+        $result = $method->invoke($logic, $holdings);
+
+        // assert various properties of the result
+        $this->assertArrayHasKey('location1_id|Main Library', $result);
+        $this->assertEquals('Main Library', $result['location1_id|Main Library']['location']);
+        $this->assertCount(2, $result['location1_id|Main Library']['items']);
+        $this->assertEquals('02.09.2025', $result['location1_id|Main Library']['items'][0]['duedate']);
+        $this->assertEquals('Circulating', $result['location1_id|Main Library']['items'][0]['loan_type_name']);
+        $this->assertEquals(true, $result['location1_id|Main Library']['items'][0]['is_holdable']);
+        $this->assertEquals('ac4cf292-ffca-40e8-a91a-5bb349532fe0', $result['location1_id|Main Library']['items'][0]['item_id']);
+        $this->assertEquals('228bcdc1-4c16-488d-88d4-57374f412c6e', $result['location1_id|Main Library']['items'][0]['holdings_id']);
+        $this->assertEquals(
+            'item_id=ac4cf292-ffca-40e8-a91a-5bb349532fe0&holdings_id=228bcdc1-4c16-488d-88d4-57374f412c6e&status=Checked+out&hashKey=fb00698a17b30097c95d55a79f658d9d',
+            $result['location1_id|Main Library']['items'][0]['link']['query']
+        );
     }
 }
