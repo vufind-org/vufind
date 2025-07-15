6b4939461faf3c415f8c215f0635909b69b9c902
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-07-15 12:25:37 -0400

    Fix error in SlackHandler to access SlackRecord

--- module/VuFind/src/VuFind/Log/Handler/SlackWebhookHandler.php
+++ module/VuFind/src/VuFind/Log/Handler/SlackWebhookHandler.php
@@ -149,7 +149,7 @@ class SlackWebhookHandler extends MonologSlackWebhookHandler
      */
     protected function getSlackData(LogRecord $record): array
     {
-        $data = this->slackRecord->getSlackData($record);
+        $data = parent::getSlackRecord()->getSlackData($record);
 
         $data['channel'] = $this->channel;
         $data['username'] = $this->username;
