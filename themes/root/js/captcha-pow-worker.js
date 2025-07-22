5a6f03385f537d25924a8813caef30b7e6d1b77a
Chris Hallberg <chris.hallberg@hey.com>
2025-07-21 22:14:19 -0400

    chore: qa-js-and-scss

--- themes/root/js/captcha-pow-worker.js
+++ themes/root/js/captcha-pow-worker.js
@@ -1,3 +1,23 @@
+/**
+ * @param {string} algo PHP hash algorithm key
+ * @returns {string} JS Crypto algorithm key
+ */
+function phpAlgoToJS(algo) {
+  const map = {
+    "sha1": "SHA-1",
+    "sha256": "SHA-256",
+    "sha384": "SHA-384",
+    "sha512": "SHA-512",
+  };
+  return algo in map ? map[algo] : algo;
+}
+
+/**
+ * @param {string} hashAlgo SHA family algorithm (for now)
+ * @param {string} challenge Hash from server
+ * @param {number} nonce Incrementing number for concat
+ * @returns {Promise<string>} Hash of challenge + nonce
+ */
 function hashChallenge(hashAlgo, challenge, nonce) {
   return new Promise((resolve, reject) => {
     let buffer = new TextEncoder().encode(`${challenge}${nonce}`);
@@ -7,40 +27,46 @@ function hashChallenge(hashAlgo, challenge, nonce) {
 
       // convert bytes to hex string
       const hex = bytes
-          .map((byte) => byte.toString(16).padStart(2, "0"))
-          .join("")
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join("");
 
       resolve(hex);
     }, reject);
   });
 }
 
-function phpAlgoToJS(algo) {
-  return {
-    "sha1": "SHA-1",
-    "sha256": "SHA-256",
-    "sha384": "SHA-384",
-    "sha512": "SHA-512",
-  }[algo] ?? algo;
-}
-
-
-self.addEventListener("message", async (event) => {
+/**
+ * @param  {MessageEvent} event Contains hash work parameters
+ * @returns {void} Returns via postMessage
+ */
+function powWorkerMessage(event) {
   const { challenge, difficulty, hashAlgo, start } = event.data;
   const algo = phpAlgoToJS(hashAlgo);
 
-  let nonce = start;
   const startTime = Date.now();
   const target = "0".repeat(difficulty);
-  let attempt = await hashChallenge(algo, challenge, nonce);
-  while (!attempt.startsWith(target)) {
-    nonce += 1;
-    attempt = await hashChallenge(algo, challenge, nonce);
+
+  /**
+   * Recursive on Promise resolution
+   * @param {number} nonce Incrementing number for hash concatenation
+   * @returns {void}
+   */
+  function attemptHash(nonce) {
+    hashChallenge(algo, challenge, nonce).then((result) => {
+      if (result.startsWith(target)) {
+        // Return to main thread
+        self.postMessage({
+          nonce,
+          iters: nonce - start,
+          ms: Date.now() - startTime,
+        });
+      } else {
+        attemptHash(nonce + 1);
+      }
+    });
   }
 
-  self.postMessage({
-    nonce,
-    iters: nonce - start,
-    ms: Date.now() - startTime,
-  });
-});
+  attemptHash(start);
+}
+
+self.addEventListener("message", powWorkerMessage);
--- themes/root/js/captcha-pow.js
+++ themes/root/js/captcha-pow.js
@@ -4,26 +4,41 @@ VuFind.register('pow-captcha', function PoWCaptchas() {
   const READY_CLASS = "js-pow-captcha-ready";
   const CHALLENGE_SELECTOR = ".pow-captcha-challenge";
 
-  function powCaptchaInit(form) {
-    if (form.classList.contains(READY_CLASS)) {
-      return;
-    }
-
-    form.classList.add(READY_CLASS);
-
-    // Wait for user interaction to start work
-
-    form.addEventListener("input", () => { powPerformWork(form) }, { once: true });
+  /**
+   * @param {HTMLFormElement} form Container form
+   * @param {MessageEvent} event Web Worker result
+   * @returns {void}
+   */
+  function powResolveWork(form, event) {
+    const { nonce, iters, ms } = event.data;
+    const nonceInput = form.querySelector('[name="pow-captcha-nonce"]');
+    nonceInput.value = nonce;
 
     const statusEl = form.querySelector(".pow-captcha-status");
-    statusEl.textContent = "PoW Captcha waiting for interaction.";
+    statusEl.textContent = `PoW Captcha: nonce (${nonce}) found after ${iters} iterations (${ms}ms).`;
   }
 
+  /**
+   * @param {HTMLFormElement} form Container form
+   * @param {MessageEvent} event Web Worker result
+   * @returns {void}
+   */
+  function powRejectWork(form, event) {
+    // @TODO: click here to request a new challenge
+    console.error(event);
+  }
+
+  /**
+   * @param {HTMLFormElement} form Container form
+   * @returns {void}
+   */
   function powPerformWork(form) {
     const challenge = form.querySelector(CHALLENGE_SELECTOR).value;
     const difficulty = Number(form.querySelector(".pow-captcha-difficulty").value);
     const hashAlgo = form.querySelector('[name="pow-captcha-hash-algo"]').value;
-    const start = Number(form.querySelector('[name="pow-captcha-start"]')?.value ?? 0);
+    const start = form.querySelector('[name="pow-captcha-start"]')
+      ? Number(form.querySelector('[name="pow-captcha-start"]').value)
+      : 0;
 
     const statusEl = form.querySelector(".pow-captcha-status");
     statusEl.textContent = `PoW Captcha doing work (${hashAlgo}, difficulty ${difficulty}).`;
@@ -33,7 +48,7 @@ VuFind.register('pow-captcha', function PoWCaptchas() {
 
     worker.onmessage = (event) => {
       worker.terminate();
-      powResolveWork(form, event.data);
+      powResolveWork(form, event);
     };
 
     worker.onerror = (event) => {
@@ -44,30 +59,39 @@ VuFind.register('pow-captcha', function PoWCaptchas() {
     worker.postMessage({ challenge, difficulty, hashAlgo, start });
   }
 
-  function powResolveWork(form, { nonce, iters, ms }) {
-    const nonceInput = form.querySelector('[name="pow-captcha-nonce"]');
-    nonceInput.value = nonce;
+  /**
+   * @param {HTMLFormElement} form Container form
+   * @returns {void}
+   */
+  function powCaptchaInit(form) {
+    if (form.classList.contains(READY_CLASS)) {
+      return;
+    }
+
+    form.classList.add(READY_CLASS);
+
+    // Wait for user interaction to start work
+    form.addEventListener("input", function powStart() {
+      powPerformWork(form);
+    }, { once: true });
 
     const statusEl = form.querySelector(".pow-captcha-status");
-    statusEl.textContent = `PoW Captcha: nonce (${nonce}) found after ${iters} iterations (${ms}ms).`;
+    statusEl.textContent = "PoW Captcha waiting for interaction.";
   }
 
-  function powRejectWork(form, event) {
-    // @TODO: click here to request a new challenge
-    console.error(event);
-  }
-
+  /**
+   * @returns {void}
+   */
   function init() {
     // Find PoW challenges
     for (const pow of document.querySelectorAll(CHALLENGE_SELECTOR)) {
       powCaptchaInit(pow.closest("form"));
     }
 
-    // Listen for future forms
-    const observer = new MutationObserver(lookForNewPoWCaptchas);
-    observer.observe(document, { childList: true, subtree: true });
-
-    function lookForNewPoWCaptchas(records, observer) {
+    /**
+     * @param {Array<MutationRecord>} records Nodes just added to the document
+     */
+    function lookForNewPoWCaptchas(records) {
       for (const record of records) {
         for (const addedNode of record.addedNodes) {
           if (addedNode instanceof Text) {
@@ -82,6 +106,10 @@ VuFind.register('pow-captcha', function PoWCaptchas() {
         }
       }
     }
+
+    // Listen for future forms
+    const observer = new MutationObserver(lookForNewPoWCaptchas);
+    observer.observe(document, { childList: true, subtree: true });
   }
 
   return { init };
