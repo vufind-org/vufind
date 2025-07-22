5746da2d06b97a96766647fa0ca82db398017121
Chris Hallberg <chris.hallberg@hey.com>
2025-07-22 15:59:51 -0400

    doc: update copyright
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/Captcha/PoW.php
+++ module/VuFind/src/VuFind/Captcha/PoW.php
@@ -5,7 +5,7 @@
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2020.
+ * Copyright (C) Villanova University 2025.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
