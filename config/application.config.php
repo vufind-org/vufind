14c5013f2dbef76b31c07f1e7cfc2938724fefd9
Demian Katz <demian.katz@villanova.edu>
2025-07-24 12:48:42 -0400

    Enable additional required module.

--- config/application.config.php
+++ config/application.config.php
@@ -11,6 +11,7 @@ $modules = [
     'Laminas\Cache\Storage\Adapter\Memory',
     'Laminas\Form',
     'Laminas\Router',
+    'Lmc\Rbac',
     'Lmc\Rbac\Mvc',
     'Laminas\I18n',
     'Laminas\Mvc\I18n',
