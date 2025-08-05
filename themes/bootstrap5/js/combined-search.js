e198759598c5e153f7dd5527eec26768a50e6b9b
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-05 13:58:15 -0400

    Update the type of container

--- themes/bootstrap5/js/combined-search.js
+++ themes/bootstrap5/js/combined-search.js
@@ -2,7 +2,7 @@
 VuFind.combinedSearch = (function CombinedSearch() {
   /**
    * Initialize the combined search functionality
-   * @param {string|HTMLElement} container The container element or a selector for it.
+   * @param {string|Element} container The container element or a selector for it.
    * @param {string}             url       The URL to fetch the combined search results from.
    */
   function init(container, url) {
