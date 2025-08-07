0405faf4fe5b6996e09e32780802cbc6007d63d6
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-07 12:41:28 -0400

    Add JSDoc to preview.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -17,7 +17,6 @@ module.exports = {
     "themes/bootstrap5/js/record.js",
     "themes/bootstrap5/js/lib/ajax_request_queue.js",
     "themes/bootstrap5/js/visual_facets.js",
-    "themes/bootstrap5/js/preview.js",
     "themes/bootstrap5/js/facets.js",
     "themes/bootstrap5/js/searchbox_controls.js",
     "themes/bootstrap5/js/lightbox.js",
