26c013b5c1740a8e8a7416f84cb30edb26b2de5e
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-11 11:18:01 -0400

    Add JSDoc to check_save_statuses.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -23,7 +23,6 @@ module.exports = {
     "themes/bootstrap5/js/cookie.js",
     "themes/bootstrap5/js/checkouts.js",
     "themes/bootstrap5/js/advanced_search.js",
-    "themes/bootstrap5/js/check_save_statuses.js",
   ],
   extends: [],
   env: {
