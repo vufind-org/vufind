b683d012fb72f34d49f5bdfef14437f408c13396
Demian Katz <demian.katz@villanova.edu>
2025-08-12 08:12:45 -0400

    Update .eslintrc.jsdoc.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -20,7 +20,7 @@ module.exports = {
     "themes/bootstrap5/js/searchbox_controls.js",
     "themes/bootstrap5/js/lightbox.js",
     "themes/bootstrap5/js/hierarchy_tree.js",
-    "themes/bootstrap5/js/checkouts.js",
+    "themes/bootstrap5/js/checkouts.js"
   ],
   extends: [],
   env: {
