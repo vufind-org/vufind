8ce462d941c0c855855c9f65d3e8a6813f8666ec
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-12 15:29:20 -0400

    Add JSDoc for hierarchy_tree.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -14,8 +14,7 @@ module.exports = {
     "themes/bootstrap5/js/lib/ajax_request_queue.js",
     "themes/bootstrap5/js/facets.js",
     "themes/bootstrap5/js/searchbox_controls.js",
-    "themes/bootstrap5/js/lightbox.js",
-    "themes/bootstrap5/js/hierarchy_tree.js"
+    "themes/bootstrap5/js/lightbox.js"
   ],
   extends: [],
   env: {
