63f061123745264e085c27e0d3f1d205f24c3234
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-13 10:05:34 -0400

    Add JSDoc for ajax_request_queue.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -11,7 +11,6 @@ module.exports = {
     "themes/bootstrap5/js/common.js",
     "themes/bootstrap5/js/list_item_selection.js",
     "themes/bootstrap5/js/record.js",
-    "themes/bootstrap5/js/lib/ajax_request_queue.js",
     "themes/bootstrap5/js/facets.js",
     "themes/bootstrap5/js/searchbox_controls.js",
     "themes/bootstrap5/js/lightbox.js"
