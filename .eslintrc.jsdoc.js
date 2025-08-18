c944af060fa2b53e6ffebfc31e9745eac2705388
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-18 11:31:24 -0400

    Add JSDoc for list_item_selection.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -8,7 +8,6 @@ module.exports = {
     "themes/**/vendor/**",
     "themes/**/node_modules/**",
     "themes/bootstrap5/js/common.js",
-    "themes/bootstrap5/js/list_item_selection.js",
     "themes/bootstrap5/js/record.js",
     "themes/bootstrap5/js/facets.js",
     "themes/bootstrap5/js/lightbox.js"
