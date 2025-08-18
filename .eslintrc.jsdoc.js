04d143fa75dad6bfaca2c2e1a4dfca6b2cf65901
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-08-18 14:50:51 -0400

    Add JSDoc for lightbox.js

--- .eslintrc.jsdoc.js
+++ .eslintrc.jsdoc.js
@@ -10,8 +10,7 @@ module.exports = {
     "themes/bootstrap5/js/common.js",
     "themes/bootstrap5/js/list_item_selection.js",
     "themes/bootstrap5/js/record.js",
-    "themes/bootstrap5/js/facets.js",
-    "themes/bootstrap5/js/lightbox.js"
+    "themes/bootstrap5/js/facets.js"
   ],
   extends: [],
   env: {
