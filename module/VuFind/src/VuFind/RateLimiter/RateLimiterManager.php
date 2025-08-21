6c640184c33d537c3cd7599392329ee196e99559
Maccabee Levine <msl321@lehigh.edu>
2025-08-21 15:14:47 +0000

    RateLimiter: Limit the number of times a Turnstile challenge can be ignored

--- module/VuFind/src/VuFind/RateLimiter/RateLimiterManager.php
+++ module/VuFind/src/VuFind/RateLimiter/RateLimiterManager.php
@@ -211,6 +211,26 @@ class RateLimiterManager implements LoggerAwareInterface, TranslatorAwareInterfa
                     $result['allow'] = false;
                     $result['message'] = $this->getTooManyRequestsResponseMessage($event, $result);
                     $result['presentTurnstileChallenge'] = ($priorTurnstileResult === null);
+                    if (
+                        $result['presentTurnstileChallenge'] && 
+                        ($this->config['Policies'][$policyId]['turnstileIgnoredRateLimiterSettings'] ?? false)
+                    ) {
+                        $turnstileIgnoredLimiter = ($this->rateLimiterFactoryCallback)(
+                            $this->config,
+                            $policyId,
+                            $this->clientIp,
+                            $this->userId,
+                            'turnstileIgnoredRateLimiterSettings'
+                        );
+                        $turnstileIgnoredLimit = $turnstileIgnoredLimiter->consume(1);
+                        if (!$turnstileIgnoredLimit->isAccepted()) {
+                            $this->verboseDebug('No more ignoring the challenge.');
+                            $this->turnstile->setResult($policyId, $this->clientIp, false);
+                            $result['presentTurnstileChallenge'] = false;
+                        } else {
+                            $this->verboseDebug('Turnstile ignored limit down to ' . $turnstileIgnoredLimit->getRemainingTokens());
+                        }
+                    }
                     return $result;
                 }
             }
