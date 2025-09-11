5bcc70fa7d935dfa8886266d689cd761fa64946b
Demian Katz <demian.katz@villanova.edu>
2025-09-11 15:08:05 -0400

    Add applyMigrations test.

--- module/VuFind/tests/unit-tests/src/VuFindTest/Db/Migration/MigrationManagerTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Db/Migration/MigrationManagerTest.php
@@ -29,6 +29,7 @@
 
 namespace VuFindTest\Db\Migration;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use VuFind\Db\Connection;
 use VuFind\Db\Migration\MigrationLoader;
 use VuFind\Db\Migration\MigrationManager;
@@ -45,14 +46,51 @@ use VuFind\Db\Migration\MigrationManager;
 class MigrationManagerTest extends \PHPUnit\Framework\TestCase
 {
     /**
+     * Get a default mock Connection for use in getMockMigrationManager.
+     *
+     * @return MockObject&Connection
+     */
+    protected function getMockConnection(): MockObject&Connection
+    {
+        $mockConnection = $this->createMock(Connection::class);
+        $mockConnection->expects($this->once())->method('getDatabasePlatform')->willReturn(new \stdClass());
+        return $mockConnection;
+    }
+
+    /**
+     * Create a mock MigrationManager.
+     *
+     * @param string[]         $methods    Array of methods to mock
+     * @param ?Connection      $connection Database connection (null for default mock)
+     * @param ?MigrationLoader $loader     Migration loader (null for default mock)
+     * @param string           $version    Target version number
+     *
+     * @return MockObject&MigrationManager
+     */
+    protected function getMockMigrationManager(
+        array $methods,
+        ?Connection $connection = null,
+        ?MigrationLoader $loader = null,
+        string $version = '11.0'
+    ): MockObject&MigrationManager {
+        $constructorArgs = [
+            $connection ?? $this->getMockConnection(),
+            $loader ?? $this->createMock(MigrationLoader::class),
+            $version,
+        ];
+        return $this->getMockBuilder(MigrationManager::class)
+            ->setConstructorArgs($constructorArgs)
+            ->onlyMethods($methods)
+            ->getMock();
+    }
+
+    /**
      * Test getMigrations() sort behavior.
      *
      * @return void
      */
     public function testGetMigrationsSorting(): void
     {
-        $mockConnection = $this->createMock(Connection::class);
-        $mockConnection->expects($this->once())->method('getDatabasePlatform')->willReturn(new \stdClass());
         $basePath = '/fake/path';
         // Test data is intentionally out of order, so we can test that sorting behaves as intended.
         $testData = [
@@ -68,10 +106,7 @@ class MigrationManagerTest extends \PHPUnit\Framework\TestCase
         $loader->method('getMigrationsFromDir')->willReturnCallback(
             fn ($version) => array_map(fn ($file) => "$version/$file.sql", $testData[$version])
         );
-        $manager = $this->getMockBuilder(MigrationManager::class)
-            ->setConstructorArgs([$mockConnection, $loader, '11.0'])
-            ->onlyMethods(['getAppliedMigrations'])
-            ->getMock();
+        $manager = $this->getMockMigrationManager(['getAppliedMigrations'], loader: $loader);
         $manager->expects($this->any())->method('getAppliedMigrations')->willReturn([]);
         $this->assertEquals(
             [
@@ -84,5 +119,23 @@ class MigrationManagerTest extends \PHPUnit\Framework\TestCase
             ],
             $manager->getMigrations('9.0')
         );
+    }
+
+    /**
+     * Test that applyMigrations calls applyMigration appropriately.
+     *
+     * @return void
+     */
+    public function testApplyMigrations(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $manager = $this->getMockMigrationManager(['applyMigration']);
+        $manager->expects($this->exactly(3))->method('applyMigration')->willReturnCallback(
+            function (string $migration, ?Connection $incomingConnection) use ($connection) {
+                $this->assertEquals($connection, $incomingConnection);
+                return $migration;
+            }
+        );
+        $this->assertEquals('123', $manager->applyMigrations(['1', '2', '3'], $connection));
     }
 }
