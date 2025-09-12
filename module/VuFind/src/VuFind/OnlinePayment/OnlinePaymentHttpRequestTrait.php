ea978d8c3efd39a3ec698e285c32ee31b9feeca7
Ere Maijala <ere.maijala@helsinki.fi>
2025-09-12 14:24:34 +0300

    Update module/VuFind/src/VuFind/OnlinePayment/OnlinePaymentHttpRequestTrait.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/OnlinePayment/OnlinePaymentHttpRequestTrait.php
+++ module/VuFind/src/VuFind/OnlinePayment/OnlinePaymentHttpRequestTrait.php
@@ -5,7 +5,7 @@
  *
  * PHP version 8
  *
- * Copyright (C) The National Library of Finland 2016-2023.
+ * Copyright (C) The National Library of Finland 2016-2025.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
