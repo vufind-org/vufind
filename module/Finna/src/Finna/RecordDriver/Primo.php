1214788ee10f8c23400b237c2fc0b8b68c76f367
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2025-09-12 16:23:01 +0300

    [FINNA-3291] - Trim Primo article reference to show publishing date as a year (#3282)

--- module/Finna/src/Finna/RecordDriver/Primo.php
+++ module/Finna/src/Finna/RecordDriver/Primo.php
@@ -111,10 +111,13 @@ class Primo extends \VuFind\RecordDriver\Primo
         // Try to take the part after the title. Account for any 'The' etc. in the
         // beginning.
         if ($containerTitle && ($p = strpos($partOf, $containerTitle)) !== false) {
-            return trim(
-                substr($partOf, $p + strlen($containerTitle) + 1),
-                " \t\n\r,"
+            $arrRef = explode(
+                ',',
+                trim(substr($partOf, $p + strlen($containerTitle) + 1), " \t\n\r,")
             );
+            // Remove month & day from date
+            $arrRef[0] = preg_replace('/\b(\d{4})(?:-\d{2}){0,2}\b/', '$1', $arrRef[0]);
+            return implode(',', $arrRef);
         }
         return $partOf;
     }
@@ -272,7 +275,12 @@ class Primo extends \VuFind\RecordDriver\Primo
      */
     public function getPublicationDates()
     {
-        return $this->fields['date'] ?? [];
+        $result = [];
+        $dates = (array)($this->fields['date'] ?? []);
+        foreach ($dates as $date) {
+            $result[] = preg_replace('/\b(\d{4})(?:-\d{2}){0,2}\b/', '$1', $date);
+        }
+        return $result;
     }
 
     /**
