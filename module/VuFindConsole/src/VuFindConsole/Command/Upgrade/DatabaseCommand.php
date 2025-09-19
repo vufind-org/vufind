be07cc83f1f7b5b6a8e7aba13a1b243161a40ed5
Demian Katz <demian.katz@villanova.edu>
2025-09-19 18:04:51 -0400

    Update DatabaseCommand.php

--- module/VuFindConsole/src/VuFindConsole/Command/Upgrade/DatabaseCommand.php
+++ module/VuFindConsole/src/VuFindConsole/Command/Upgrade/DatabaseCommand.php
@@ -29,13 +29,13 @@
 
 namespace VuFindConsole\Command\Upgrade;
 
-use Doctrine\DBAL\Connection;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use VuFind\Db\Connection;
 use VuFind\Db\ConnectionFactory;
 use VuFind\Db\Migration\MigrationManager;
 
