d2401e593e84cc46e95e32f701bdafdb7ab1925e
Douglas Hahn <dhahn@tamu.edu>
2025-09-23 10:39:11 -0500

    Bring theme into alighnemt

--- themes/TAMU/theme.config.php
+++ themes/TAMU/theme.config.php
@@ -2,9 +2,6 @@
 return [
     'extends' => 'bootstrap3',
     'favicon' => 'favicon.ico',
-    'css' => [
-        'StackMap.min.css'
-    ],
     'helpers' => [
         'factories' => [
             'TAMU\View\Helper\Root\Record' => 'VuFind\View\Helper\Root\RecordFactory',
