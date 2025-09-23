f269eff5c9584ae5d44f0aa5669b318d2c413652
Demian Katz <demian.katz@villanova.edu>
2025-09-23 09:01:00 -0400

    Improve test.

--- module/VuFind/tests/unit-tests/src/VuFindTest/Autocomplete/SolrTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Autocomplete/SolrTest.php
@@ -58,21 +58,44 @@ class SolrTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(['title'], $this->getProperty($solr, 'displayField'));
         $this->assertEquals(null, $this->getProperty($solr, 'sortField'));
         $this->assertEquals([], $this->getProperty($solr, 'filters'));
+        $this->assertNull($this->getProperty($solr, 'limit'));
+    }
+
+    /**
+     * Data provider for testSetConfig().
+     *
+     * @return array[]
+     */
+    public static function setConfigProvider(): array
+    {
+        $filters = ['FF1:FV1', 'FF2:FV2'];
+        return [
+            'default limit, filters' => ['Handler:Display:Sort:FF1:FV1:FF2:FV2', $filters, null],
+            'non-default limit, filters' => ['Handler:Display:Sort:20:FF1:FV1:FF2:FV2', $filters, 20],
+            'default limit, no filters' => ['Handler:Display:Sort', [], null],
+            'non-default limit, no filters' => ['Handler:Display:Sort:20', [], 20],
+        ];
     }
 
     /**
      * Test that configuration is parsed correctly.
      *
+     * @param string   $config          Config to test
+     * @param string[] $expectedFilters Filters expected to be parsed from the config
+     * @param ?int     $expectedLimit   Limit expected to be parsed from the config
+     *
      * @return void
+     *
+     * @dataProvider setConfigProvider
      */
-    public function testSetConfig()
+    public function testSetConfig(string $config, array $expectedFilters, ?int $expectedLimit)
     {
         $solr = new Solr($this->getMockResultsPluginManager(allowDefaultFallback: true));
-        $solr->setConfig('Handler:Display:Sort:FF1:FV1:FF2:FV2');
+        $solr->setConfig($config);
         $this->assertEquals('Handler', $this->getProperty($solr, 'handler'));
         $this->assertEquals(['Display'], $this->getProperty($solr, 'displayField'));
         $this->assertEquals('Sort', $this->getProperty($solr, 'sortField'));
-        $expected = ['FF1:FV1', 'FF2:FV2'];
-        $this->assertEquals($expected, $this->getProperty($solr, 'filters'));
+        $this->assertEquals($expectedFilters, $this->getProperty($solr, 'filters'));
+        $this->assertEquals($expectedLimit, $this->getProperty($solr, 'limit'));
     }
 }
