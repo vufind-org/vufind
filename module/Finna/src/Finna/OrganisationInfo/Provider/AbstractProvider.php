bab0317ecaae32e8a513f53677850d9d2a1b033a
Ere Maijala <ere.maijala@helsinki.fi>
2025-09-25 12:45:58 +0300

    Ensure that the result from getConsortiumInfo contains the list element.

--- module/Finna/src/Finna/OrganisationInfo/Provider/AbstractProvider.php
+++ module/Finna/src/Finna/OrganisationInfo/Provider/AbstractProvider.php
@@ -193,7 +193,9 @@ abstract class AbstractProvider implements
         if (!empty($result['consortium']['logo']['small'])) {
             $result['consortium']['logo']['small'] = $this->proxifyImageUrl($result['consortium']['logo']['small']);
         }
-        foreach ($result['list'] ?? [] as &$item) {
+        // Ensure that the result to be returned contains the 'list' element:
+        $result['list'] ??= [];
+        foreach ($result['list'] as &$item) {
             $item = $this->processDetails($item);
         }
         unset($item);
