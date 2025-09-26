eba8f586c990b05da9944b62e3e126c120202a91
Juha Luoma <juhaluoma5@gmail.com>
2025-09-26 15:00:37 +0300

    Added possibility for cat_password to be null

--- module/VuFind/src/VuFind/ILS/Driver/AbstractBase.php
+++ module/VuFind/src/VuFind/ILS/Driver/AbstractBase.php
@@ -94,7 +94,7 @@ abstract class AbstractBase implements DriverInterface
     protected function createPatronArray(
         string $id,
         string $cat_username = '',
-        string $cat_password = '',
+        ?string $cat_password = null,
         Stringable|string|null $email = null,
         string $firstname = '',
         string $lastname = '',
