5b5d9bb668862480069f1355901e590854307f24
Demian Katz <demian.katz@villanova.edu>
2025-10-01 09:52:45 -0400

    php-cs-fixer

--- module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/RecordTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/View/Helper/Root/RecordTest.php
@@ -237,7 +237,7 @@ class RecordTest extends \PHPUnit\Framework\TestCase
         $serviceManager->expects($this->once())->method('get')->with(UserListServiceInterface::class)
             ->willReturn($listService);
         $expected = [
-            'driver' => $driver, 'list' => null, 'user' => $user, 'lists' => [1, 2, 3], 'recordNumber' => $recordNumber
+            'driver' => $driver, 'list' => null, 'user' => $user, 'lists' => [1, 2, 3], 'recordNumber' => $recordNumber,
         ];
         $context = $this->getMockContext();
         $context->expects($this->once())->method('apply')
