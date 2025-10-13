a0e04dc259560ba99ebcfc81d4d8f7c18f96bc7f
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2025-10-13 11:24:17 -0400

    Change response header type in HttpDownloadExceptionTest

--- module/VuFind/tests/unit-tests/src/VuFindTest/Exception/HttpDownloadExceptionTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Exception/HttpDownloadExceptionTest.php
@@ -53,9 +53,9 @@ class HttpDownloadExceptionTest extends \PHPUnit\Framework\TestCase
         $message = 'Not Found';
         $url = 'https://mock.codes/404';
         $statusCode = 404;
-        $responseHeaders = Headers::fromString(
-            'content-type: application/json'
-        );
+        $responseHeaders = [
+            'content-type' => 'application/json'
+        ];
         $responseBody = '{"statusCode" : 404, "description": "Not Found"}';
         $previous = null;
 
