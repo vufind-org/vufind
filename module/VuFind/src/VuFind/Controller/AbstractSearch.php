614b42fde1300fb909bdf20dc9db84609a09ed2e
Demian Katz <demian.katz@villanova.edu>
2025-10-29 10:16:06 -0400

    Improve formatting.

--- module/VuFind/src/VuFind/Controller/AbstractSearch.php
+++ module/VuFind/src/VuFind/Controller/AbstractSearch.php
@@ -195,13 +195,9 @@ class AbstractSearch extends AbstractBase
         // Enable recommendations unless explicitly told to disable them:
         $all = ['top', 'side', 'noresults', 'bottom'];
         $noRecommend = $this->params()->fromQuery('noRecommend', false);
-        if (
-            in_array($noRecommend, [1, '1', 'true', true], true)
-        ) {
+        if (in_array($noRecommend, [1, '1', 'true', true], true)) {
             return [];
-        } elseif (
-            in_array($noRecommend, [0, '0', 'false', false], true)
-        ) {
+        } elseif (in_array($noRecommend, [0, '0', 'false', false], true)) {
             return $all;
         }
         return array_diff(
