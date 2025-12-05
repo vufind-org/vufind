6e17e1e2b6331a806d559501b3a8c6f606dc9599
Maccabee Levine <msl321@lehigh.edu>
2025-12-05 00:37:08 +0000

    Fix a test

--- module/VuFind/src/VuFindTest/Container/MockContainerTrait.php
+++ module/VuFind/src/VuFindTest/Container/MockContainerTrait.php
@@ -140,7 +140,7 @@ trait MockContainerTrait
      *
      * @return bool
      */
-    public function has($rawId)
+    public function has($rawId): bool
     {
         $id = $this->mockAliases[$rawId] ?? $rawId;
         // Assume every service exists unless explicitly disabled
