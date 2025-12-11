44af5ae1bc8755795bcfb40a69b9e9d5d830a01d
Maccabee Levine <msl321@lehigh.edu>
2025-12-11 20:32:04 +0000

    Add a test

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/BlendedSearchTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/BlendedSearchTest.php
@@ -129,18 +129,27 @@ class BlendedSearchTest extends \VuFindTest\Integration\MinkTestCase
             [
                 ['page' => 1],
                 'Blender/Results',
+                'Blended',
             ],
             [
                 ['page' => 2],
                 'Blender/Results',
+                'Blended',
             ],
             [
                 ['page' => 1],
                 'Search/Blended', // legacy path
+                'Blended',
             ],
             [
                 ['page' => 2],
                 'Search/Blended', // legacy path
+                'Blended',
+            ],
+            [
+                ['page' => 1],
+                'Blender2/Results',
+                'Also Blended',
             ],
         ];
     }
@@ -148,13 +157,14 @@ class BlendedSearchTest extends \VuFindTest\Integration\MinkTestCase
     /**
      * Test blended search
      *
-     * @param array  $queryParams Query parameters
-     * @param string $path        URL path
+     * @param array  $queryParams    Query parameters
+     * @param string $path           URL path
+     * @param string $searchBoxLabel Label of the active search box
      *
      * @return void
      */
     #[\PHPUnit\Framework\Attributes\DataProvider('getSearchData')]
-    public function testSearch(array $queryParams, string $path): void
+    public function testSearch(array $queryParams, string $path, string $searchBoxLabel): void
     {
         $expectedLabels = $this->getExpectedLabels($queryParams['page']);
         $this->changeConfigs(
@@ -163,6 +173,7 @@ class BlendedSearchTest extends \VuFindTest\Integration\MinkTestCase
                     'SearchTabs' => [
                         'Solr' => 'Catalog',
                         'Blender' => 'Blended',
+                        'Blender2' => 'Also Blended',
                     ],
                 ],
                 'Blender' => $this->getBlenderIniOverrides(),
@@ -195,7 +206,7 @@ class BlendedSearchTest extends \VuFindTest\Integration\MinkTestCase
         $this->clickCss($page, '#result0 .title');
         $this->waitForPageLoad($page);
         $this->assertEquals(
-            'Blended',
+            $searchBoxLabel,
             $this->findCssAndGetText($page, '.searchbox li a.active')
         );
 
