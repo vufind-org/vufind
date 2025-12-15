5ae4f4962db511b60c422ce25c6d20e2aa60331c
sambhavp96 <40562292+sambhavp96@users.noreply.github.com>
2025-12-15 11:59:43 -0500

    Refactor AbstractExpireCommandTest as an abstract TestCase (#4965)

--- module/VuFindConsole/tests/unit-tests/src/VuFindTest/Command/Util/AbstractExpireCommandTestCase.php
+++ module/VuFindConsole/tests/unit-tests/src/VuFindTest/Command/Util/AbstractExpireCommandTestCase.php
@@ -44,7 +44,7 @@ use VuFindConsole\Command\Util\AbstractExpireCommand;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:testing:unit_tests Wiki
  */
-class AbstractExpireCommandTest extends \PHPUnit\Framework\TestCase
+abstract class AbstractExpireCommandTestCase extends \PHPUnit\Framework\TestCase
 {
     use \VuFindTest\Feature\WithConsecutiveTrait;
 
