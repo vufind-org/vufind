124bcf007877e96a6cd94850202a4507dd5d471d
Damien Guillaume <damieng@msu.edu>
2025-12-17 14:46:41 -0500

    Support queries where part of the query is using ExactSettings

--- module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Solr/QueryBuilderTest.php
+++ module/VuFindSearch/tests/unit-tests/src/VuFindTest/Backend/Solr/QueryBuilderTest.php
@@ -296,6 +296,74 @@ class QueryBuilderTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * Test queries with mixed exact and non-exact parts.
+     *
+     * @return void
+     */
+    public function testMixedExactQueryHandler()
+    {
+        // Check QueryBuilder without ExactSettings
+        $qb = new QueryBuilder(
+            [
+                'TestHandler' => [
+                    'DismaxFields' => ['a'],
+                    'DismaxHandler' => 'edismax',
+                ],
+            ]
+        );
+        $q = new Query('"t1" AND t2', 'TestHandler');
+        $response = $qb->build($q);
+        $queryString = $response->get('q')[0];
+        $this->assertEquals('"t1" AND t2', $queryString);
+
+        // Expected inputs and outputs with ExactSettings:
+        $tests = [
+            ['"t1"', '"t1"'], // simple exact queries are not affected
+            ['("t1" OR t2) AND t3', '("t1" OR t2) AND t3'], // queries with parenthesis are not supported
+            ['"t1" AND title:t2', '"t1" AND title:t2'], // queries with field are not supported
+            ['"t1" AND "t2"', '"t1" AND "t2"'], // queries with multiple exact parts are not supported
+            ['t1 AND "t2" AND t3', 't1 AND "t2" AND t3'], // queries with an exact part in the middle are not supported
+            ['"t1" t2', '((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\"") AND ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2"))'],
+            ['"t1" AND t2', '((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\"") AND ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2"))'],
+            ['"t1" OR t2', '((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\"") OR ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2"))'],
+            ['t1 AND "t2"', '((_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t1") AND ' .
+                '(_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t2\""))'],
+            ['NOT "t1" AND t2', '((*:* NOT ((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\""))) AND ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2"))'],
+            ['t1 AND NOT "t2"', '((_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t1 AND") AND ' .
+                '(*:* NOT ((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t2\""))))'],
+            ['-"t1" t2', '((*:* NOT ((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\""))) AND ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2"))'],
+            ['"t1" AND t2 AND t3', '((_query_:"{!edismax qf=\"b\" mm=\\\'0%\\\'}\"t1\"") AND ' .
+                '(_query_:"{!edismax qf=\"a\" mm=\\\'0%\\\'}t2 AND t3"))'], // would be different with dismax
+        ];
+
+        $qb = new QueryBuilder(
+            [
+                'TestHandler' => [
+                    'DismaxFields' => ['a'],
+                    'DismaxHandler' => 'edismax',
+                    'ExactSettings' => [
+                        'DismaxFields' => ['b'],
+                        'DismaxHandler' => 'edismax',
+                    ],
+                ],
+            ]
+        );
+
+        foreach ($tests as $test) {
+            [$input, $output] = $test;
+            $q = new Query($input, 'TestHandler');
+            $response = $qb->build($q);
+            $queryString = $response->get('q')[0];
+            $this->assertEquals($output, $queryString);
+        }
+    }
+
+    /**
      * Test generation with a query handler with a filter set and DisMax settings
      *
      * @return void
