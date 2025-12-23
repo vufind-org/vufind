201443682ebacb1c1570a24e89b2484829f0f338
Demian Katz <demian.katz@villanova.edu>
2025-12-23 08:58:29 -0500

    php-cs-fixer

--- module/VuFind/src/VuFind/ILS/Driver/Demo.php
+++ module/VuFind/src/VuFind/ILS/Driver/Demo.php
@@ -51,7 +51,6 @@ use function array_key_exists;
 use function array_slice;
 use function count;
 use function in_array;
-use function is_array;
 use function is_callable;
 use function sprintf;
 use function strlen;
