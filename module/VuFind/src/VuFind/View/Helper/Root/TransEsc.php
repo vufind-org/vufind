ed025548fc5f9851f9616f61e4303471f2093d25
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2026-01-20 10:36:49 -0500

    Add missing parameters on TransEsc helper

--- module/VuFind/src/VuFind/View/Helper/Root/TransEsc.php
+++ module/VuFind/src/VuFind/View/Helper/Root/TransEsc.php
@@ -60,17 +60,26 @@ class TransEsc
     /**
      * Translate and escape a string
      *
-     * @param string|object|array $str     String to translate or an array of text
-     *                                     domain and string to translate
-     * @param array               $tokens  Tokens to inject into the translated string
-     * @param string              $default Default value to use if no translation is
-     *                                     found (null for no default).
+     * @param string|object|array $str             String to translate or an array of text
+     *                                             domain and string to translate
+     * @param array               $tokens          Tokens to inject into the translated string
+     * @param string              $default         Default value to use if no translation is
+     *                                             found (null for no default).
+     * @param bool                $useIcuFormatter Should we use an ICU message formatter instead
+     * of the default behavior?
+     * @param string[]            $fallbackDomains Text domains to check if no match is found in
+     * the domain specified in $target
      *
      * @return string
      */
-    public function __invoke($str, $tokens = [], $default = null)
-    {
-        $translated = ($this->translate)($str, $tokens, $default);
+    public function __invoke(
+        $str,
+        $tokens = [],
+        $default = null,
+        $useIcuFormatter = false,
+        $fallbackDomains = []
+    ) {
+        $translated = ($this->translate)($str, $tokens, $default, $useIcuFormatter, $fallbackDomains);
         return ($this->escapeHtml)($translated);
     }
 }
