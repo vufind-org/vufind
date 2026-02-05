33d149e9c7dc225298f1753355e4bd9c50ec0e84
Demian Katz <demian.katz@villanova.edu>
2026-02-05 12:44:13 -0500

    Reduce Auth plugin configuration.

--- module/VuFind/src/VuFind/Auth/PluginManager.php
+++ module/VuFind/src/VuFind/Auth/PluginManager.php
@@ -66,28 +66,6 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     ];
 
     /**
-     * Default plugin factories.
-     *
-     * @var array
-     */
-    protected $factories = [
-        AlmaDatabase::class => AlmaDatabaseFactory::class,
-        CAS::class => CASFactory::class,
-        ChoiceAuth::class => ChoiceAuthFactory::class,
-        Database::class => DatabaseFactory::class,
-        Email::class => EmailFactory::class,
-        Facebook::class => FacebookFactory::class,
-        ILS::class => ILSFactory::class,
-        LDAP::class => LDAPFactory::class,
-        MultiAuth::class => MultiAuthFactory::class,
-        MultiILS::class => ILSFactory::class,
-        OpenIDConnect::class => OpenIDConnectFactory::class,
-        Shibboleth::class => ShibbolethFactory::class,
-        SimulatedSSO::class => SimulatedSSOFactory::class,
-        SIP2::class => SIP2Factory::class,
-    ];
-
-    /**
      * Constructor
      *
      * Make sure plugins are properly initialized.
