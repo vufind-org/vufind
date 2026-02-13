e1d7eba12aaaf0d8b40fa5d094b230b73dfc1e16
Ere Maijala <ere.maijala@helsinki.fi>
2026-02-13 15:57:29 +0200

    Add simple auto-discovery for actions.

--- module/VuFind/src/VuFind/Action/PluginManager.php
+++ module/VuFind/src/VuFind/Action/PluginManager.php
@@ -29,6 +29,7 @@
 
 namespace VuFind\Action;
 
+use VuFind\Action\ShortLink\RedirectAction;
 use VuFind\ServiceManager\Factory\AutowiringFactory;
 
 /**
@@ -45,25 +46,36 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     /**
      * Default plugin aliases.
      *
+     * Action classes are auto-discovered in autoDiscoveryNamespaces (see below). The names must follow the convention
+     * Category\Classname to be auto-discovered.
+     *
      * @var array
      */
     protected $aliases = [
-        'collection/home' => Collection\HomeAction::class,
-        'content/content' => Content\ContentAction::class,
-        'myresearch/login' => MyResearch\LoginAction::class,
-        'shortlink/redirect' => ShortLink\RedirectAction::class,
+        // ShortLink doesn't follow the auto-discovery naming convention:
+        'shortlink/redirect' => RedirectAction::class,
     ];
 
     /**
      * Default plugin factories.
      *
+     * Autowiring factory is automatically added for any auto-discovered class unless already specified here.
+     *
      * @var array
      */
     protected $factories = [
-        Collection\HomeAction::class => AutowiringFactory::class,
-        Content\ContentAction::class => AutowiringFactory::class,
-        MyResearch\LoginAction::class => AutowiringFactory::class,
         ShortLink\RedirectAction::class => ShortLink\RedirectActionFactory::class,
+    ];
+
+    /**
+     * Namespaces used for auto-discovery (excluding leading backslash).
+     *
+     * The namespaces are checked in order from first to last.
+     *
+     * @var array
+     */
+    protected $autoDiscoveryNamespaces = [
+        __NAMESPACE__,
     ];
 
     /**
@@ -95,6 +107,35 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
     }
 
     /**
+     * Get an action.
+     *
+     * @param class-string<InstanceType>|string $name    Service name of plugin to retrieve.
+     * @param null|array<mixed>                 $options Options to use when creating the instance.
+     *
+     * @return mixed
+     *
+     * @throws Exception\ServiceNotFoundException If the manager does not have a service definition for the instance,
+     * and the service is not auto-invokable.
+     * @throws InvalidServiceException If the plugin created is invalid for the plugin context.
+     */
+    public function get($name, ?array $options = null)
+    {
+        return parent::get($this->resolveAlias($name), $options);
+    }
+
+    /**
+     * Check if an action is available.
+     *
+     * @param string|class-string $name Name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return parent::has($this->resolveAlias($name));
+    }
+
+    /**
      * Given a category and action name, return the most appropriate action handler class name, or null if not
      * available.
      *
@@ -111,11 +152,45 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
         $normalizedCategory = $category ? strtolower($category) : '';
         $normalizedAction = $action ? strtolower($action) : '';
 
+        // If the class is known already, just return it:
         if ($normalizedCategory && $normalizedAction && $this->has("$normalizedCategory/$normalizedAction")) {
             return "$normalizedCategory/$normalizedAction";
         } elseif ($this->has("$normalizedCategory$normalizedAction")) {
             return "$normalizedCategory$normalizedAction";
         }
+
         return null;
+    }
+
+    /**
+     * Resolve alias using auto-discovery as required.
+     *
+     * @param string $alias Alias
+     *
+     * @return string
+     */
+    protected function resolveAlias(string $alias): string
+    {
+        if (null !== ($result = $this->aliases[$alias] ?? null)) {
+            return $result;
+        }
+
+        $nameParts = array_map(
+            fn ($s) => ucfirst(strtolower($s)),
+            explode('/', $alias)
+        );
+        $actionClass = implode('\\', $nameParts) . 'Action';
+
+        foreach ($this->autoDiscoveryNamespaces as $ns) {
+            $unprefixedClassName = $ns . '\\' . $actionClass;
+            $className = '\\' . $unprefixedClassName;
+            if (class_exists($className)) {
+                $this->aliases[$alias] = $className;
+                $this->factories[$unprefixedClassName] ??= AutowiringFactory::class;
+                $this->factories[$className] ??= AutowiringFactory::class;
+                return $unprefixedClassName;
+            }
+        }
+        return $alias;
     }
 }
