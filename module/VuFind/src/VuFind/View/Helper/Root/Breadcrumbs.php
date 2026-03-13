fc4ecfe5b0a77f2b7d63ef22a526be192db1f7b6
wagener <wagener@hebis.uni-frankfurt.de>
2026-03-13 13:26:27 +0100

    Improve search breadcrumb in record view

--- module/VuFind/src/VuFind/View/Helper/Root/Breadcrumbs.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Breadcrumbs.php
@@ -49,12 +49,17 @@ class Breadcrumbs extends AbstractHelper
      * @param string  $text   Text of breadcrumb
      * @param ?string $href   Link of breadcrumb (null for no link)
      * @param bool    $active Is this the active breadcrumb (end of trail)?
+     * @param ?string $title  Title attribute of breadcrumb (null for no title)
      *
      * @return string
      */
-    protected function formatBreadcrumb(string $text, ?string $href = null, bool $active = false): string
-    {
-        return $this->getView()->render('Helpers/breadcrumbs/single', compact('text', 'href', 'active'));
+    protected function formatBreadcrumb(
+        string $text,
+        ?string $href = null,
+        bool $active = false,
+        ?string $title = null,
+    ): string {
+        return $this->getView()->render('Helpers/breadcrumbs/single', compact('text', 'href', 'active', 'title'));
     }
 
     /**
@@ -73,12 +78,13 @@ class Breadcrumbs extends AbstractHelper
      * @param string  $text   Text of breadcrumb
      * @param ?string $href   Link of breadcrumb (null for no link)
      * @param bool    $active Is this the active breadcrumb (end of trail)?
+     * @param ?string $title  Title attribute of breadcrumb (null for no title)
      *
      * @return static
      */
-    public function add(string $text, ?string $href = null, bool $active = false): static
+    public function add(string $text, ?string $href = null, bool $active = false, ?string $title = null): static
     {
-        $this->getLayout()->breadcrumbs .= $this->formatBreadcrumb($text, $href, $active);
+        $this->getLayout()->breadcrumbs .= $this->formatBreadcrumb($text, $href, $active, $title);
         return $this;
     }
 
