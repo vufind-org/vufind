0899492e38ccc450225e092930f9b1d3bbfe52e0
Demian Katz <demian.katz@villanova.edu>
2026-03-24 13:19:02 -0400

    Add abstract plugin factory to AjaxHandler plugin manager.

--- module/VuFind/src/VuFind/AjaxHandler/PluginManager.php
+++ module/VuFind/src/VuFind/AjaxHandler/PluginManager.php
@@ -29,6 +29,8 @@
 
 namespace VuFind\AjaxHandler;
 
+use VuFind\ServiceManager\AbstractPluginFactory;
+
 /**
  * AJAX handler plugin manager.
  *
@@ -126,6 +128,23 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
         SystemStatus::class => SystemStatusFactory::class,
         TagRecord::class => TagRecordFactory::class,
     ];
+
+    /**
+     * Constructor.
+     *
+     * Make sure plugins are properly initialized.
+     *
+     * @param mixed $configOrContainerInstance Configuration or container instance
+     * @param array $v3config                  If $configOrContainerInstance is a
+     * container, this value will be passed to the parent constructor.
+     */
+    public function __construct(
+        $configOrContainerInstance = null,
+        array $v3config = []
+    ) {
+        $this->addAbstractFactory(AbstractPluginFactory::class);
+        parent::__construct($configOrContainerInstance, $v3config);
+    }
 
     /**
      * Return the name of the base class or interface that plug-ins must conform
