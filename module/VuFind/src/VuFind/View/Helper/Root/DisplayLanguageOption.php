e0f5d699f29535cb938f95cd13f151aae088a1c1
Demian Katz <demian.katz@villanova.edu>
2026-04-09 14:12:05 -0400

    Remove obsolete reference.

--- module/VuFind/src/VuFind/View/Helper/Root/DisplayLanguageOption.php
+++ module/VuFind/src/VuFind/View/Helper/Root/DisplayLanguageOption.php
@@ -42,7 +42,7 @@ use VuFind\ServiceManager\Factory\Autowire;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-class DisplayLanguageOption extends \Laminas\View\Helper\AbstractHelper
+class DisplayLanguageOption
 {
     /**
      * Constructor.
