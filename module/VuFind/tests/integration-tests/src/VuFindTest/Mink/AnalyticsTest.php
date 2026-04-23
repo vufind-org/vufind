23ff0f8e6ca7e7aebca55c1a0fe593f31aa64152
Demian Katz <demian.katz@villanova.edu>
2026-04-23 07:25:22 -0400

    Cover more Matomo scenarios.

--- module/VuFind/tests/integration-tests/src/VuFindTest/Mink/AnalyticsTest.php
+++ module/VuFind/tests/integration-tests/src/VuFindTest/Mink/AnalyticsTest.php
@@ -103,6 +103,60 @@ class AnalyticsTest extends \VuFindTest\Integration\MinkTestCase
                 "'setCustomVariable',9,'Context','page','page'",
             ],
         ];
+        yield 'Matomo tracks search on faceted search page (with custom variables enabled)' => [
+            '/Search/Results?lookfor=foo&filter=format:Book',
+            $customVariablesMatomoConfig,
+            [
+                'fakeMatomo',
+                '987654321',
+                "'trackSiteSearch', 'Solr|foo', 'basic'",
+                "'setCustomVariable',1,'Facets','format\\x7CBook','page'",
+                "'setCustomVariable',2,'FacetTypes','Format','page'",
+                "'setCustomVariable',3,'SearchType','basic','page'",
+                "'setCustomVariable',4,'SearchBackend','Solr','page'",
+                "'setCustomVariable',5,'Sort','relevance','page'",
+                "'setCustomVariable',6,'Page','1','page'",
+                "'setCustomVariable',7,'Limit','20','page'",
+                "'setCustomVariable',8,'View','list','page'",
+                "'setCustomVariable',9,'Context','page','page'",
+            ],
+        ];
+        yield 'Matomo works on record page (no custom variables by default)' => [
+            '/Record/testbug2',
+            $basicMatomoConfig,
+            ['fakeMatomo', '987654321'],
+            ['setCustomVariable'],
+        ];
+        yield 'Matomo provides appropriate custom variables on record page (when enabled)' => [
+            '/Record/testbug2',
+            $customVariablesMatomoConfig,
+            [
+                'fakeMatomo',
+                '987654321',
+                "'setCustomVariable',1,'Context','page','page'",
+                "'setCustomVariable',2,'RecordFormat','Book','page'",
+                "'setCustomVariable',3,'RecordData','testbug2",
+                "'setCustomVariable',4,'RecordInstitution','MyInstitution','page'",
+            ],
+        ];
+        yield 'Matomo works on AJAX-loaded record tab (no custom variables by default)' => [
+            '/Record/testbug2/AjaxTab',
+            $basicMatomoConfig,
+            ['fakeMatomo', '987654321'],
+            ['setCustomVariable'],
+        ];
+        yield 'Matomo provides appropriate custom variables on AJAX-loaded record tab (when enabled)' => [
+            '/Record/testbug2/AjaxTab',
+            $customVariablesMatomoConfig,
+            [
+                'fakeMatomo',
+                '987654321',
+                "'setCustomVariable',1,'Context','tabs','page'",
+                "'setCustomVariable',2,'RecordFormat','Book','page'",
+                "'setCustomVariable',3,'RecordData','testbug2",
+                "'setCustomVariable',4,'RecordInstitution','MyInstitution','page'",
+            ],
+        ];
     }
 
     /**
@@ -131,5 +185,17 @@ class AnalyticsTest extends \VuFindTest\Integration\MinkTestCase
         foreach ($unexpectedText as $unexpected) {
             $this->assertStringNotContainsString($unexpected, $body, "Unexpectedly found $unexpected at $path");
         }
+    }
+
+    /**
+     * Standard teardown method.
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        // This test uses tools from the Mink base class but doesn't actually use Mink; thus, we only need to
+        // clean up custom configurations in the tearDown method:
+        $this->restoreConfigs();
     }
 }
