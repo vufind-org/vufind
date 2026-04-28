72b527252d52f11627b8f558dd791d0f9cd240d1
Sambhav Pokharel <sambhav.pokharel@gmail.com>
2026-04-28 13:11:14 -0400

    Remove TransEscAttr & TransEscWithPrefix View Helpers' dependency on AbstractHelper

--- themes/root/theme.config.php
+++ themes/root/theme.config.php
@@ -34,8 +34,6 @@ return [
             'VuFind\View\Helper\Root\Summaries' => 'VuFind\View\Helper\Root\ContentLoaderFactory',
             'VuFind\View\Helper\Root\Summon' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             'VuFind\View\Helper\Root\SystemEmail' => 'VuFind\View\Helper\Root\SystemEmailFactory',
-            'VuFind\View\Helper\Root\TransEscAttr' => 'Laminas\ServiceManager\Factory\InvokableFactory',
-            'VuFind\View\Helper\Root\TransEscWithPrefix' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             'VuFind\View\Helper\Root\Url' => 'VuFind\View\Helper\Root\UrlFactory',
             'VuFind\View\Helper\Root\UserList' => 'VuFind\View\Helper\Root\UserListFactory',
 
