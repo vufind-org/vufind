c5d400a51675930b8307aef179099986f83dcd73
sambhavp96 <40562292+sambhavp96@users.noreply.github.com>
2026-04-28 18:07:55 -0400

    Remove TransEscAttr & TransEscWithPrefix View Helpers' dependency on AbstractHelper (#5241)

--- module/VuFind/src/VuFind/View/Helper/Root/TransEscAttr.php
+++ module/VuFind/src/VuFind/View/Helper/Root/TransEscAttr.php
@@ -31,7 +31,8 @@
 
 namespace VuFind\View\Helper\Root;
 
-use Laminas\View\Helper\AbstractHelper;
+use Laminas\View\Helper\EscapeHtmlAttr;
+use VuFind\ServiceManager\Factory\Autowire;
 
 /**
  * Translate + escape view helper for HTML attributes.
@@ -43,8 +44,22 @@ use Laminas\View\Helper\AbstractHelper;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-class TransEscAttr extends AbstractHelper
+class TransEscAttr
 {
+    /**
+     * Constructor.
+     *
+     * @param Translate      $translate      Translate view helper
+     * @param EscapeHtmlAttr $escapeHtmlAttr EscapeHtml view helper
+     */
+    public function __construct(
+        #[Autowire(container: 'ViewHelperManager')]
+        protected Translate $translate,
+        #[Autowire(container: 'ViewHelperManager')]
+        protected EscapeHtmlAttr $escapeHtmlAttr
+    ) {
+    }
+
     /**
      * Translate and escape a string for an HTML attribute.
      *
@@ -67,8 +82,8 @@ class TransEscAttr extends AbstractHelper
         $useIcuFormatter = false,
         $fallbackDomains = []
     ) {
-        $escaper = $this->getView()->plugin('escapeHtmlAttr');
-        $translator = $this->getView()->plugin('translate');
+        $escaper = $this->escapeHtmlAttr;
+        $translator = $this->translate;
         return $escaper($translator($str, $tokens, $default, $useIcuFormatter, $fallbackDomains));
     }
 }
