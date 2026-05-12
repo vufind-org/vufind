b1d8ec3860c465d0bf2ddcc7a0bfca667bf4388b
Demian Katz <demian.katz@villanova.edu>
2026-05-12 16:18:13 -0400

    Add test.

--- module/VuFind/tests/unit-tests/src/VuFindTest/Connection/WebhookTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Connection/WebhookTest.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Unit tests for LibGuides connector.
+ * Unit tests for Webhook connector.
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2023.
+ * Copyright (C) Villanova University 2026.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -23,114 +23,45 @@
  * @category VuFind
  * @package  Connection
  * @author   Demian Katz <demian.katz@villanova.edu>
- * @author   Maccabee Levine <msl321@lehigh.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org
  */
 
 namespace VuFindTest\Connection;
 
-use Laminas\Http\Client\Adapter\Test as TestAdapter;
-use Laminas\Http\Client as HttpClient;
-use VuFind\Config\Config;
-use VuFind\Connection\LibGuides;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use VuFind\Connection\Webhook;
+use VuFind\Http\GuzzleService;
 
 /**
- * Unit tests for Wikipedia connector.
+ * Unit tests for Webhook connector.
  *
  * @category VuFind
  * @package  Search
  * @author   Demian Katz <demian.katz@villanova.edu>
- * @author   Maccabee Levine <msl321@lehigh.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org
  */
-class LibGuidesTest extends \PHPUnit\Framework\TestCase
+class WebhookTest extends \PHPUnit\Framework\TestCase
 {
-    use \VuFindTest\Feature\FixtureTrait;
-
     /**
-     * Test loading accounts.
+     * Test a successful webhook call.
      *
      * @return void
      */
-    public function testGetAccounts()
+    public function testSuccessfulCall(): void
     {
-        $config = $this->getConfig();
-        $client = $this->getClient('accounts');
-        $libGuides = new LibGuides($config, $client);
-        $response = $libGuides->getAccounts();
-        $this->assertCount(2, $response);
-
-        $dewey = $response[0];
-        $this->assertEquals('Melvil', $dewey->first_name);
-    }
-
-    /**
-     * GetAZ test provider.
-     *
-     * @return \Iterator
-     */
-    public static function getAzProvider(): \Iterator
-    {
-        yield 'exclude hidden (explicitly)' => [ true, 4 ];
-        yield 'do not exclude hidden' => [ false, 5 ];
-        yield 'exclude hidden (by default)' => [ null, 4 ];
-    }
-
-    /**
-     * Test loading AZ.
-     *
-     * @param ?bool $excludeHidden Whether to exclude hidden databases
-     * @param int   $expectedCount Expected result count
-     *
-     * @return void
-     */
-    #[\PHPUnit\Framework\Attributes\DataProvider('getAzProvider')]
-    public function testGetAz(?bool $excludeHidden, int $expectedCount): void
-    {
-        $config = $this->getConfig();
-        $client = $this->getClient('az');
-        $libGuides = new LibGuides($config, $client);
-
-        $params = $excludeHidden === null ? [] : [$excludeHidden];
-        $response = $libGuides->getAz(...$params);
-        $this->assertCount($expectedCount, $response);
-    }
-
-    /**
-     * Create a fake LibGuidesAPI.ini config.
-     *
-     * @return Config The fake config
-     */
-    protected function getConfig()
-    {
-        return new Config(
-            [
-                'General' => [
-                    'api_base_url' => 'https://foo.org/',
-                    'client_id' => 'username',
-                    'client_secret' => 'email',
-                ],
-            ]
-        );
-    }
-
-    /**
-     * Load HTTP client w/ fixture.
-     *
-     * @param string $fixture Fixture name
-     *
-     * @return HttpClient
-     */
-    protected function getClient($fixture)
-    {
-        $adapter = new TestAdapter();
-        $adapter->addResponse($this->getFixture('libguides/api/token'));
-        $adapter->addResponse($this->getFixture("libguides/api/$fixture"));
-        $adapter->setResponseIndex(1);
-        $client = new HttpClient();
-        $client->setAdapter($adapter);
-        return $client;
+        $response = $this->createMock(ResponseInterface::class);
+        $response->method('getStatusCode')->willReturn(200);
+        $guzzle = $this->createMock(GuzzleService::class);
+        $guzzle->method('post')->with('http://foo', null, '', 5)->willReturn($response);
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())->method('log')
+            ->with('debug', 'VuFind\Connection\Webhook: Webhook posted successfully');
+        $connector = new Webhook();
+        $connector->setGuzzleService($guzzle);
+        $connector->setLogger($logger);
+        $connector->post('http://foo', 5);
     }
 }
