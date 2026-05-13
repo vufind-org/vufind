04c1371f87618ebee78e80a142cc4a07aed711d8
Ere Maijala <ere.maijala@helsinki.fi>
2026-05-13 10:32:07 +0300

    Revert unintended change.

--- module/VuFind/src/VuFind/Captcha/Interval.php
+++ module/VuFind/src/VuFind/Captcha/Interval.php
@@ -100,6 +100,8 @@ class Interval extends AbstractBase implements TranslatorAwareInterface
      * @param array $queryParams Query params
      *
      * @return bool
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function verify(array $postParams, array $queryParams): bool
     {
