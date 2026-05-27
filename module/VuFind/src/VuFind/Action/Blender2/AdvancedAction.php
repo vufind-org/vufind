c2497f123860250e0bc624dd3bb3eca1cf378c64
Ere Maijala <ere.maijala@helsinki.fi>
2026-05-27 12:00:40 +0300

    Refactor Blender and Blender2 controllers to actions.
    Also fixes a bug in handling of default filters in advanced search action.

--- module/VuFind/src/VuFind/Action/Blender2/AdvancedAction.php
+++ module/VuFind/src/VuFind/Action/Blender2/AdvancedAction.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Blended Search 2 Controller.
+ * Blender2 advanced search action.
  *
  * PHP version 8
  *
- * Copyright (C) The National Library of Finland 2023.
+ * Copyright (C) The National Library of Finland 2026.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,29 +21,40 @@
  * <https://www.gnu.org/licenses/>.
  *
  * @category VuFind
- * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
 
-namespace VuFind\Controller;
+namespace VuFind\Action\Blender2;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * Blended Search 2 Controller.
+ * Blender2 advanced search action.
  *
  * @category VuFind
- * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-class Blender2Controller extends BlenderController
+class AdvancedAction extends AbstractBlender2SearchAndResultsAction
 {
     /**
-     * Search class family to use.
+     * Display search results.
      *
-     * @var string
+     * @param ServerRequestInterface $request  Server request
+     * @param ResponseInterface      $response Response
+     *
+     * @return ResponseInterface
      */
-    protected $searchClassId = 'Blender2';
+    public function action(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $this->renderAdvancedSearch();
+    }
 }
