97266eb00d08b2a808be9f10158676a40f96dcad
Ere Maijala <ere.maijala@helsinki.fi>
2026-06-04 20:32:34 +0300

    Refactor BrowZineController to actions. (#5340)

--- module/VuFind/src/VuFind/Action/BrowZine/SearchAction.php
+++ module/VuFind/src/VuFind/Action/BrowZine/SearchAction.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * BrowZine Controller.
+ * BrowZine search action.
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2017.
+ * Copyright (C) The National Library of Finland 2026.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,45 +21,40 @@
  * <https://www.gnu.org/licenses/>.
  *
  * @category VuFind
- * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
 
-namespace VuFind\Controller;
+namespace VuFind\Action\BrowZine;
 
-use Laminas\ServiceManager\ServiceLocatorInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * BrowZine Controller.
+ * BrowZine search action.
  *
  * @category VuFind
- * @package  Controller
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-class BrowZineController extends AbstractSearch
+class SearchAction extends AbstractBrowZineSearchAndResultsAction
 {
     /**
-     * Constructor.
+     * Display results.
      *
-     * @param ServiceLocatorInterface $sm Service locator
-     */
-    public function __construct(ServiceLocatorInterface $sm)
-    {
-        $this->searchClassId = 'BrowZine';
-        parent::__construct($sm);
-    }
-
-    /**
-     * Search action -- call standard results action.
+     * @param ServerRequestInterface $request  Server request
+     * @param ResponseInterface      $response Response
      *
-     * @return mixed
+     * @return ResponseInterface
      */
-    public function searchAction()
-    {
-        return $this->resultsAction();
+    public function action(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        return $this->renderSearchResults();
     }
 }
