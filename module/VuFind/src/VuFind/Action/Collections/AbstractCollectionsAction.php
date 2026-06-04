796139ec8b24eb7b7c6c80f133525b9d5af30058
Ere Maijala <ere.maijala@helsinki.fi>
2026-06-04 16:04:39 +0300

    Refactor CollectionsController into actions.

---
+++ module/VuFind/src/VuFind/Action/Collections/AbstractCollectionsAction.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Abstract base class for collections.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) The National Library of Finland 2026.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category VuFind
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Site
+ */
+
+namespace VuFind\Action\Collections;
+
+use VuFind\Action\AbstractTemplateRenderingAction;
+use VuFind\I18n\HasSorterInterface;
+use VuFind\I18n\HasSorterTrait;
+use VuFind\I18n\Sorter;
+use VuFind\Search\Results\PluginManager as SearchResultsPluginManager;
+use VuFind\ServiceManager\Factory\Autowire;
+use VuFindSearch\Service as SearchService;
+
+/**
+ * Abstract base class for collections.
+ *
+ * @category VuFind
+ * @package  Action
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Site
+ */
+abstract class AbstractCollectionsAction extends AbstractTemplateRenderingAction implements HasSorterInterface
+{
+    use HasSorterTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param array                      $config                     VuFind configuration
+     * @param SearchService              $searchService              Search service
+     * @param SearchResultsPluginManager $searchResultsPluginManager Search results plugin manager
+     * @param Sorter                     $sorter                     Sorter
+     */
+    public function __construct(
+        #[Autowire(config: 'config')]
+        protected array $config,
+        protected SearchService $searchService,
+        protected SearchResultsPluginManager $searchResultsPluginManager,
+        Sorter $sorter,
+    ) {
+        parent::__construct();
+        $this->setSorter($sorter);
+    }
+
+    /**
+     * Get the collection browse page size.
+     *
+     * @return int
+     */
+    protected function getBrowseLimit(): int
+    {
+        return (int)($this->config['Collections']['browseLimit'] ?? 20);
+    }
+
+    /**
+     * Get a list of initial letters to display.
+     *
+     * @return array
+     */
+    protected function getAlphabetList(): array
+    {
+        return array_merge(range('0', '9'), range('A', 'Z'));
+    }
+
+    /**
+     * Get the delimiter used to separate title from ID in the browse strings.
+     *
+     * @return string
+     */
+    protected function getBrowseDelimiter(): string
+    {
+        return $this->config->Collections->browseDelimiter ?? '{{{_ID_}}}';
+    }
+}
--- module/VuFind/src/VuFind/Action/Collections/HomeAction.php
+++ module/VuFind/src/VuFind/Action/Collections/HomeAction.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * Collections Controller.
+ * Collections home action.
  *
  * PHP version 8
  *
- * Copyright (C) Villanova University 2010, 2022.
+ * Copyright (C) Villanova University 2010.
+ * Copyright (C) The National Library of Finland 2026.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,166 +22,133 @@
  * <https://www.gnu.org/licenses/>.
  *
  * @category VuFind
- * @package  Controller
+ * @package  Action
  * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
 
-namespace VuFind\Controller;
+namespace VuFind\Action\Collections;
 
-use Laminas\ServiceManager\ServiceLocatorInterface;
-use VuFind\Config\Config;
-use VuFindSearch\Command\SearchCommand;
-use VuFindSearch\Query\Query;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use VuFindSearch\Command\AlphabeticBrowseCommand;
 
 use function array_slice;
 use function count;
 
 /**
- * Collections Controller.
+ * Collections home action.
  *
  * @category VuFind
- * @package  Controller
+ * @package  Action
  * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-class CollectionsController extends AbstractBase implements
-    \VuFind\I18n\HasSorterInterface
+class HomeAction extends AbstractCollectionsAction
 {
-    use Feature\AlphaBrowseTrait;
-    use \VuFind\I18n\HasSorterTrait;
+    /**
+     * The name of the backend providing alphabrowse services.
+     *
+     * @var string
+     */
+    protected string $alphabrowseBackend = DEFAULT_SEARCH_BACKEND;
 
     /**
-     * VuFind configuration.
+     * Display home page.
      *
-     * @var Config
-     */
-    protected $config;
-
-    /**
-     * Constructor.
+     * @param ServerRequestInterface $request  Server request
+     * @param ResponseInterface      $response Response
      *
-     * @param ServiceLocatorInterface $sm     Service manager
-     * @param Config                  $config VuFind configuration
+     * @return ResponseInterface
      */
-    public function __construct(ServiceLocatorInterface $sm, Config $config)
-    {
-        $this->config = $config;
-        $this->setSorter($sm->get(\VuFind\I18n\Sorter::class));
-        parent::__construct($sm);
-    }
-
-    /**
-     * Search by title action.
-     *
-     * @return mixed
-     */
-    public function bytitleAction()
-    {
-        $collections = $this->getCollectionsFromTitle(
-            $this->params()->fromQuery('title')
-        );
-        if (count($collections) != 1) {
-            return $this->createViewModel(['collections' => $collections]);
-        }
-        return $this->redirect()
-            ->toRoute('collection', ['id' => $collections[0]->getUniqueId()]);
-    }
-
-    /**
-     * Browse action.
-     *
-     * @return mixed
-     */
-    public function homeAction()
-    {
-        $browseType = $this->config->Collections->browseType ?? 'Index';
-        return ($browseType == 'Alphabetic')
-            ? $this->showBrowseAlphabetic() : $this->showBrowseIndex();
-    }
-
-    /**
-     * Get the delimiter used to separate title from ID in the browse strings.
-     *
-     * @return string
-     */
-    protected function getBrowseDelimiter()
-    {
-        return $this->config->Collections->browseDelimiter ?? '{{{_ID_}}}';
+    public function action(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        $browseType = $this->config['Collections']['browseType'] ?? 'Index';
+        return 'Alphabetic' === $browseType
+            ? $this->showBrowseAlphabetic()
+            : $this->showBrowseIndex();
     }
 
     /**
      * Show the Browse Menu.
      *
-     * @return mixed
+     * @return ResponseInterface
      */
-    protected function showBrowseAlphabetic()
+    protected function showBrowseAlphabetic(): ResponseInterface
     {
         // Process incoming parameters:
         $source = 'hierarchy';
-        $from = $this->params()->fromQuery('from', '');
-        $page = $this->params()->fromQuery('page', 0);
+        $from = $this->getQueryParam('from', '');
+        $page = $this->getQueryParam('page', 0);
         $limit = $this->getBrowseLimit();
 
         // Load Solr data or die trying:
-        $result = $this->alphabeticBrowse($source, $from, $page, $limit);
+        $command = new AlphabeticBrowseCommand(
+            $this->alphabrowseBackend,
+            $source,
+            $from,
+            $page,
+            $limit
+        );
+        $result = $this->searchService->invoke($command)->getResult();
 
         // No results?  Try the previous page just in case we've gone past the
         // end of the list....
         if ($result['Browse']['totalCount'] == 0) {
             $page--;
-            $result = $this->alphabeticBrowse($source, $from, $page, $limit);
+            $command->setPage($page);
+            $result = $this->searchService->invoke($command)->getResult();
         }
 
-        // Begin building view model:
-        $view = $this->createViewModel();
+        $templateParams = [
+            'from' => $from,
+            'letters' => $this->getAlphabetList(),
+        ];
 
         // Only display next/previous page links when applicable:
         if ($result['Browse']['totalCount'] > $limit) {
-            $view->nextpage = $page + 1;
+            $templateParams['nextpage'] = $page + 1;
         }
         if ($result['Browse']['offset'] + $result['Browse']['startRow'] > 1) {
-            $view->prevpage = $page - 1;
+            $templateParams['prevpage'] = $page - 1;
         }
-
-        // Send other relevant values to the template:
-        $view->from = $from;
-        $view->letters = $this->getAlphabetList();
 
         // Format the results for proper display:
         $finalresult = [];
         $delimiter = $this->getBrowseDelimiter();
         foreach ($result['Browse']['items'] as $rkey => $collection) {
-            $collectionIdNamePair
-                = explode($delimiter, $collection['heading']);
+            $collectionIdNamePair = explode($delimiter, $collection['heading']);
             $finalresult[$rkey]['displayText'] = $collectionIdNamePair[0];
             $finalresult[$rkey]['count'] = $collection['count'];
             $finalresult[$rkey]['value'] = $collectionIdNamePair[1];
         }
-        $view->result = $finalresult;
+        $templateParams['result'] = $finalresult;
 
-        // Display the page:
-        return $view;
+        return $this->renderTemplate($this->request, $this->response, $templateParams);
     }
 
     /**
      * Show the Browse Menu.
      *
-     * @return mixed
+     * @return ResponseInterface
      */
-    protected function showBrowseIndex()
+    protected function showBrowseIndex(): ResponseInterface
     {
         // Process incoming parameters:
-        $from = $this->params()->fromQuery('from', '');
-        $page = $this->params()->fromQuery('page', 0);
-        $appliedFilters = $this->params()->fromQuery('filter', []);
+        $from = $this->getQueryParam('from', '');
+        $page = $this->getQueryParam('page', 0);
+        $appliedFilters = $this->getQueryParam('filter', []);
         $limit = $this->getBrowseLimit();
 
         $browseField = 'hierarchy_browse';
 
-        $searchObject = $this->getService(\VuFind\Search\Results\PluginManager::class)->get('Solr');
+        $searchObject = $this->searchResultsPluginManager->get(DEFAULT_SEARCH_BACKEND);
         foreach ($appliedFilters as $filter) {
             $searchObject->getParams()->addFilter($filter);
         }
@@ -215,32 +183,29 @@ class CollectionsController extends AbstractBase implements
             $key = count($result) - 1;
         }
 
-        // Begin building view model:
-        $view = $this->createViewModel();
+        // Begin building template params:
+        $templateParams = [
+            'from' => $from,
+            'letters' => $this->getAlphabetList(),
+            'filters' => $searchObject->getParams()->getFilterList(true),
+        ];
 
         // Only display next/previous page links when applicable:
         if (count($result) > $key + $limit) {
-            $view->nextpage = $page + 1;
+            $templateParams['nextpage'] = $page + 1;
         }
         if ($key > 0) {
-            $view->prevpage = $page - 1;
+            $templateParams['prevpage'] = $page - 1;
         }
 
         // Select just the records to display
-        $result = array_slice(
+        $templateParams['result'] = array_slice(
             $result,
             $key,
             count($result) > $key + $limit ? $limit : null
         );
 
-        // Send other relevant values to the template:
-        $view->from = $from;
-        $view->result = $result;
-        $view->letters = $this->getAlphabetList();
-        $view->filters = $searchObject->getParams()->getFilterList(true);
-
-        // Display the page:
-        return $view;
+        return $this->renderTemplate($this->request, $this->response, $templateParams);
     }
 
     /**
@@ -252,7 +217,7 @@ class CollectionsController extends AbstractBase implements
      *
      * @return int
      */
-    protected function sortFindKeyLocation(&$result, $from)
+    protected function sortFindKeyLocation(array &$result, string $from): int
     {
         // Normalize the from value so it matches the values we are looking up
         $from = $this->normalizeForBrowse($from);
@@ -280,34 +245,13 @@ class CollectionsController extends AbstractBase implements
     }
 
     /**
-     * Function to normalize the names so they sort properly.
-     *
-     * @param array $result Array to sort (passed by reference to use less
-     * memory)
-     *
-     * @return array $resultOut
-     */
-    protected function normalizeAndSortFacets(&$result)
-    {
-        $valuesSorted = [];
-        foreach ($result as $resKey => $resVal) {
-            $valuesSorted[$resKey]
-                = $this->normalizeForBrowse($resVal['displayText']);
-        }
-        $this->getSorter()->asort($valuesSorted);
-
-        // Now the $valuesSorted is in the right order
-        return $valuesSorted;
-    }
-
-    /**
      * Normalize the value for the browse sort.
      *
      * @param string $val Value to normalize
      *
      * @return string $valNormalized
      */
-    protected function normalizeForBrowse($val)
+    protected function normalizeForBrowse(string $val): string
     {
         $valNormalized = iconv('UTF-8', 'US-ASCII//TRANSLIT//IGNORE', $val);
         $valNormalized = strtolower($valNormalized);
@@ -317,44 +261,21 @@ class CollectionsController extends AbstractBase implements
     }
 
     /**
-     * Get a list of initial letters to display.
+     * Function to normalize the names so they sort properly.
      *
-     * @return array
+     * @param array $result Array to sort (passed by reference to use less memory)
+     *
+     * @return array $resultOut
      */
-    protected function getAlphabetList()
+    protected function normalizeAndSortFacets(array &$result): array
     {
-        return array_merge(range('0', '9'), range('A', 'Z'));
-    }
+        $valuesSorted = [];
+        foreach ($result as $resKey => $resVal) {
+            $valuesSorted[$resKey] = $this->normalizeForBrowse($resVal['displayText']);
+        }
+        $this->getSorter()->asort($valuesSorted);
 
-    /**
-     * Get the collection browse page size.
-     *
-     * @return int
-     */
-    protected function getBrowseLimit()
-    {
-        return $this->config->Collections->browseLimit ?? 20;
-    }
-
-    /**
-     * Get collection information matching a given title:
-     *
-     * @param string $title Title to search for
-     *
-     * @return array
-     */
-    protected function getCollectionsFromTitle($title)
-    {
-        $title = addcslashes($title, '"');
-        $query = new Query("is_hierarchy_title:\"$title\"", 'AllFields');
-        $searchService = $this->getService(\VuFindSearch\Service::class);
-        $command = new SearchCommand(
-            'Solr',
-            $query,
-            0,
-            $this->getBrowseLimit()
-        );
-        $result = $searchService->invoke($command)->getResult();
-        return $result->getRecords();
+        // Now the $valuesSorted is in the right order
+        return $valuesSorted;
     }
 }
