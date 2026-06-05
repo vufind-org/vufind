89291bfa3dff533effdfd740e01b10fc6c455f27
Ere Maijala <ere.maijala@helsinki.fi>
2026-06-05 13:56:49 +0300

    Refactor CombinedController into actions.

--- module/VuFind/src/VuFind/Action/Combined/HomeAction.php
+++ module/VuFind/src/VuFind/Action/Combined/HomeAction.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * BrowZine search action.
+ * Combined search home action.
  *
  * PHP version 8
  *
- * Copyright (C) The National Library of Finland 2026.
+ * Copyright (C) Villanova University 2010.
+ * Copyright (C) The National Library of Finland 2024-2026.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -22,29 +23,31 @@
  *
  * @category VuFind
  * @package  Action
+ * @author   Demian Katz <demian.katz@villanova.edu>
  * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
 
-namespace VuFind\Action\BrowZine;
+namespace VuFind\Action\Combined;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * BrowZine search action.
+ * Combined search home action.
  *
  * @category VuFind
  * @package  Action
+ * @author   Demian Katz <demian.katz@villanova.edu>
  * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-class SearchAction extends AbstractBrowZineSearchAndResultsAction
+class HomeAction extends AbstractCombinedSearchAndResultsAction
 {
     /**
-     * Display results.
+     * Display home page.
      *
      * @param ServerRequestInterface $request  Server request
      * @param ResponseInterface      $response Response
@@ -55,6 +58,13 @@ class SearchAction extends AbstractBrowZineSearchAndResultsAction
         ServerRequestInterface $request,
         ResponseInterface $response,
     ): ResponseInterface {
-        return $this->renderSearchResults($request, $response);
+        // We need to load blocks differently in this controller since it
+        // doesn't follow the usual configuration pattern.
+        $blocks = $this->blockLoader->getFromConfig('combined');
+        return $this->renderTemplate(
+            $this->request,
+            $this->response,
+            $this->createTemplateParams(compact('blocks'))
+        );
     }
 }
