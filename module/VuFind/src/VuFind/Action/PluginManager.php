860734990426b212457367a72b2898144b9d5995
Ere Maijala <ere.maijala@helsinki.fi>
2026-06-13 15:19:34 +0300

    Refactor Comments, Ratings and Tag controllers to actions. (#5365)

--- module/VuFind/src/VuFind/Action/PluginManager.php
+++ module/VuFind/src/VuFind/Action/PluginManager.php
@@ -63,7 +63,13 @@ class PluginManager extends \VuFind\ServiceManager\AbstractPluginManager
         'author/facetlist' => Author\FacetListAction::class,
         'checkouts/purgehistory' => Checkouts\PurgeHistoryAction::class,
         'collections/bytitle' => Collections\ByTitleAction::class,
+        'comments/deletecomments' => Comments\DeleteCommentsAction::class,
+        'comments/userlist' => Comments\UserListAction::class,
         'myresearch/cataloglogin' => MyResearch\CatalogLoginAction::class,
+        'ratings/deleteratings' => Ratings\DeleteRatingsAction::class,
+        'ratings/userlist' => Ratings\UserListAction::class,
+        'tag/deletetags' => Tag\DeleteTagsAction::class,
+        'tag/userlist' => Tag\UserListAction::class,
     ];
 
     /**
