e6e5c8c3da7b9f265f402e241ce53a5ab8fd80c4
Ere Maijala <ere.maijala@helsinki.fi>
2026-06-16 15:36:44 +0300

    Refactor DeveloperSettingsController to actions. (#5369)

--- module/VuFind/config/module.config.php
+++ module/VuFind/config/module.config.php
@@ -167,7 +167,6 @@ $config = [
     ],
     'controllers' => [
         'factories' => [
-            'VuFind\Controller\DeveloperSettingsController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\CartController' => 'VuFind\Controller\CartControllerFactory',
             'VuFind\Controller\CombinedController' => 'VuFind\Controller\AbstractBaseFactory',
             'VuFind\Controller\CoverController' => 'VuFind\Controller\CoverControllerFactory',
@@ -222,8 +221,6 @@ $config = [
             'VuFind\ServiceManager\ServiceInitializer',
         ],
         'aliases' => [
-            'DeveloperSettings' => 'VuFind\Controller\DeveloperSettingsController',
-            'developersettings' => 'VuFind\Controller\DeveloperSettingsController',
             'Cart' => 'VuFind\Controller\CartController',
             'cart' => 'VuFind\Controller\CartController',
             'Combined' => 'VuFind\Controller\CombinedController',
