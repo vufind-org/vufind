9d5af94e7195131c70154e2e06eb79e38e9362fc
Demian Katz <demian.katz@villanova.edu>
2026-07-13 11:53:28 -0400

    Make ESLint happy.

--- util/build-css.js
+++ util/build-css.js
@@ -1,179 +1,7 @@
 /*global Buffer, process, require */
 /*eslint-disable no-console -- console needed for CLI output */
 
-if (
-  !process ||
-  !process.env ||
-  !process.env.VUFIND_HOME
-) {
-  console.error(red("Set VUFIND_HOME before running."));
-  process.exit(1);
-}
-
-const fs = require("node:fs");
-const path = require("node:path");
-
-const CleanCSS = require("clean-css");
-const { program } = require("commander");
-const sass = require("sass");
-
-// Arguments
-
-program
-  .option(
-    "-t, --themes [themes...]",
-    "themes to compile - if excluded all themes will be compiled",
-    getThemeList()
-  )
-  .option(
-    "-e, --entry <string>",
-    "source SCSS entry point in {theme}/scss",
-    "compiled.scss"
-  )
-  .option(
-    "-o, --outname <string>",
-    "CSS file to output into {theme}/css",
-    "compiled.css"
-  )
-  .option(
-    "-c, --check-only",
-    "compile but do not write result",
-    false
-  )
-  .option("-M, --no-minify", "do not minify CSS with clean-css (faster compilation)")
-  .option(
-    "-S, --no-sourcemaps",
-    "do not generate sourcemaps (faster compilation)"
-  )
-  .option("--temp <path>",
-    "Override temporary file location",
-    "/tmp"
-  );
-
-program.parse(process.argv);
-const CLI_OPTIONS = program.opts();
-
-// minify CSS, unless told not to with --no-minify (-M)
-// i.e., npm run build:scss -- --no-minify
-const DO_MINIFY = Boolean(CLI_OPTIONS.minify);
-
-// build source maps, unless told not to with --no-sourcemaps (-S)
-// source maps allow compiled CSS to be mapped to the original SCSS source
-// i.e., npm run build:scss -- --no-sourcemaps
-const DO_SOURCEMAPS = Boolean(CLI_OPTIONS.sourcemaps);
-
-console.log(`CHECK-ONLY: ${String(CLI_OPTIONS.checkOnly)}`);
-console.log(`MINIFY: ${String(DO_MINIFY)}`);
-console.log(`SOURCEMAPS: ${String(DO_SOURCEMAPS)}`);
-
-// Main
-
-for (const theme of CLI_OPTIONS.themes) {
-  try {
-    if (fs.existsSync(themePath(theme, "scss"))) {
-      compileTheme(theme);
-    }
-  } catch (e) {
-    console.error(red(e));
-  }
-}
-
-/**
- * Compile a theme.
- * @param {string} themeName Name of theme to compile
- * @returns {void}
- */
-function compileTheme(themeName) {
-  const start = performance.now();
-  console.log(`\n${heading(themeName)}`);
-
-  console.log("- compiling SCSS to CSS");
-
-  // Inspired by bootstrap5 compilation, but adjusted for project needs.
-  // @link https://github.com/twbs/bootstrap/blob/main/package.json
-  // sass --style expanded --source-map --embed-sources --no-error-css scss/:dist/css/
-  const compiled = sass.compile(
-    themePath(themeName, `scss/${CLI_OPTIONS.entry}`),
-    {
-      style: "expanded",
-      sourceMap: DO_SOURCEMAPS,
-      embedSources: true,
-      // needed due to limitations of Bootstrap 5 as of June, 2026:
-      quietDeps: true,
-      silenceDeprecations: ['import'],
-      // no equivalent for no-error-css
-      loadPaths: getLoadPaths(themeName),
-    }
-  );
-
-  let cssContent = compiled.css;
-  let sourceMapContent = DO_SOURCEMAPS ? JSON.stringify(compiled.sourceMap) : null;
-
-  if (DO_MINIFY) {
-    console.log("- minifying");
-
-    // @link https://github.com/twbs/bootstrap/blob/main/package.json
-    // cleancss -O1 --format breakWith=lf --with-rebase --source-map --source-map-inline-sources
-    const compressor = new CleanCSS({
-      level: 1,
-      format: { breakWith: 'lf' },
-      // rebasing breaks Font Awesome icons
-      sourceMap: DO_SOURCEMAPS,
-      sourceMapInlineSources: true,
-    });
-
-    const output = compressor.minify(compiled.css, compiled.sourceMap || null);
-
-    cssContent = output.styles;
-    sourceMapContent = DO_SOURCEMAPS ? output.sourceMap.toString() : null;
-
-    const reduction = (100 * output.stats.efficiency).toFixed(1);
-    console.log(`  - ${output.stats.timeSpent}ms (${reduction.toLocaleString()}% smaller)`);
-  }
-
-  // Write files
-
-  if (sourceMapContent) {
-    // add sourceMappingURL to CSS
-    const sourceMapName = `${CLI_OPTIONS.outname}.map`;
-    cssContent = `${cssContent}\n/*# sourceMappingURL=${sourceMapName} */`;
-
-    if (!CLI_OPTIONS.checkOnly) {
-      console.log(`- writing source map to theme/${themeName}/css/${sourceMapName}`);
-      const sourceMapPath = themePath(themeName, `css/${sourceMapName}`);
-      fs.writeFileSync(sourceMapPath, sourceMapContent, "utf8");
-    }
-  }
-
-  const outCSSPath = themePath(themeName, `css/${CLI_OPTIONS.outname}`);
-  if (CLI_OPTIONS.checkOnly) {
-    // Check if compiled.css is up-to-date
-    if (fs.existsSync(outCSSPath)) {
-      const tmpCSSPath = `${CLI_OPTIONS.temp}/scss-check-${themeName}.css`;
-      console.log(`- writing ${tmpCSSPath} for comparison`);
-      fs.writeFileSync(tmpCSSPath, cssContent, "utf8");
-
-      try {
-        if (filesAreIdentical(tmpCSSPath, outCSSPath)) {
-          console.log(`  - ${green("SCSS up-to-date")}`);
-        } else {
-          console.error(`  - ${red("SCSS needs to be recompiled")}`);
-          process.exitCode = 1; // CI failure without exiting
-        }
-      } catch (e) {
-        console.error(e);
-        process.exit(1);
-      }
-    }
-  } else {
-    console.log(`- writing css to theme/${themeName}/css/${CLI_OPTIONS.outname}`);
-    fs.writeFileSync(outCSSPath, cssContent, "utf8");
-  }
-
-  console.log(`- done (${Math.ceil(performance.now() - start).toLocaleString()}ms)`);
-}
-
-// Functions
+// Display functions
 
 /**
  * Display a string as red text.
@@ -219,6 +47,26 @@ function underline(str) {
 function heading(str) {
   return yellow(underline(str));
 }
+
+// Dependency check:
+
+if (
+  !process ||
+  !process.env ||
+  !process.env.VUFIND_HOME
+) {
+  console.error(red("Set VUFIND_HOME before running."));
+  process.exit(1);
+}
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const CleanCSS = require("clean-css");
+const { program } = require("commander");
+const sass = require("sass");
+
+// Support functions
 
 /**
  * Get the path to a theme (or location within a theme).
@@ -330,6 +178,56 @@ function getLoadPaths(themeName) {
   return paths.map((filePath) => `${filePath}/`);
 }
 
+// Arguments
+
+program
+  .option(
+    "-t, --themes [themes...]",
+    "themes to compile - if excluded all themes will be compiled",
+    getThemeList()
+  )
+  .option(
+    "-e, --entry <string>",
+    "source SCSS entry point in {theme}/scss",
+    "compiled.scss"
+  )
+  .option(
+    "-o, --outname <string>",
+    "CSS file to output into {theme}/css",
+    "compiled.css"
+  )
+  .option(
+    "-c, --check-only",
+    "compile but do not write result",
+    false
+  )
+  .option("-M, --no-minify", "do not minify CSS with clean-css (faster compilation)")
+  .option(
+    "-S, --no-sourcemaps",
+    "do not generate sourcemaps (faster compilation)"
+  )
+  .option("--temp <path>",
+    "Override temporary file location",
+    "/tmp"
+  );
+
+program.parse(process.argv);
+const CLI_OPTIONS = program.opts();
+
+// minify CSS, unless told not to with --no-minify (-M)
+// i.e., npm run build:scss -- --no-minify
+const DO_MINIFY = Boolean(CLI_OPTIONS.minify);
+
+// build source maps, unless told not to with --no-sourcemaps (-S)
+// source maps allow compiled CSS to be mapped to the original SCSS source
+// i.e., npm run build:scss -- --no-sourcemaps
+const DO_SOURCEMAPS = Boolean(CLI_OPTIONS.sourcemaps);
+
+console.log(`CHECK-ONLY: ${String(CLI_OPTIONS.checkOnly)}`);
+console.log(`MINIFY: ${String(DO_MINIFY)}`);
+console.log(`SOURCEMAPS: ${String(DO_SOURCEMAPS)}`);
+
+
 /**
  * Determine whether two files are identical
  * @param {string} aPath First file to compare
@@ -365,4 +263,111 @@ function filesAreIdentical(aPath, bPath) {
   }
 
   return true;
+}
+
+/**
+ * Compile a theme.
+ * @param {string} themeName Name of theme to compile
+ * @returns {void}
+ */
+function compileTheme(themeName) {
+  const start = performance.now();
+  console.log(`\n${heading(themeName)}`);
+
+  console.log("- compiling SCSS to CSS");
+
+  // Inspired by bootstrap5 compilation, but adjusted for project needs.
+  // @link https://github.com/twbs/bootstrap/blob/main/package.json
+  // sass --style expanded --source-map --embed-sources --no-error-css scss/:dist/css/
+  const compiled = sass.compile(
+    themePath(themeName, `scss/${CLI_OPTIONS.entry}`),
+    {
+      style: "expanded",
+      sourceMap: DO_SOURCEMAPS,
+      embedSources: true,
+      // needed due to limitations of Bootstrap 5 as of June, 2026:
+      quietDeps: true,
+      silenceDeprecations: ['import'],
+      // no equivalent for no-error-css
+      loadPaths: getLoadPaths(themeName),
+    }
+  );
+
+  let cssContent = compiled.css;
+  let sourceMapContent = DO_SOURCEMAPS ? JSON.stringify(compiled.sourceMap) : null;
+
+  if (DO_MINIFY) {
+    console.log("- minifying");
+
+    // @link https://github.com/twbs/bootstrap/blob/main/package.json
+    // cleancss -O1 --format breakWith=lf --with-rebase --source-map --source-map-inline-sources
+    const compressor = new CleanCSS({
+      level: 1,
+      format: { breakWith: 'lf' },
+      // rebasing breaks Font Awesome icons
+      sourceMap: DO_SOURCEMAPS,
+      sourceMapInlineSources: true,
+    });
+
+    const output = compressor.minify(compiled.css, compiled.sourceMap || null);
+
+    cssContent = output.styles;
+    sourceMapContent = DO_SOURCEMAPS ? output.sourceMap.toString() : null;
+
+    const reduction = (100 * output.stats.efficiency).toFixed(1);
+    console.log(`  - ${output.stats.timeSpent}ms (${reduction.toLocaleString()}% smaller)`);
+  }
+
+  // Write files
+
+  if (sourceMapContent) {
+    // add sourceMappingURL to CSS
+    const sourceMapName = `${CLI_OPTIONS.outname}.map`;
+    cssContent = `${cssContent}\n/*# sourceMappingURL=${sourceMapName} */`;
+
+    if (!CLI_OPTIONS.checkOnly) {
+      console.log(`- writing source map to theme/${themeName}/css/${sourceMapName}`);
+      const sourceMapPath = themePath(themeName, `css/${sourceMapName}`);
+      fs.writeFileSync(sourceMapPath, sourceMapContent, "utf8");
+    }
+  }
+
+  const outCSSPath = themePath(themeName, `css/${CLI_OPTIONS.outname}`);
+  if (CLI_OPTIONS.checkOnly) {
+    // Check if compiled.css is up-to-date
+    if (fs.existsSync(outCSSPath)) {
+      const tmpCSSPath = `${CLI_OPTIONS.temp}/scss-check-${themeName}.css`;
+      console.log(`- writing ${tmpCSSPath} for comparison`);
+      fs.writeFileSync(tmpCSSPath, cssContent, "utf8");
+
+      try {
+        if (filesAreIdentical(tmpCSSPath, outCSSPath)) {
+          console.log(`  - ${green("SCSS up-to-date")}`);
+        } else {
+          console.error(`  - ${red("SCSS needs to be recompiled")}`);
+          process.exitCode = 1; // CI failure without exiting
+        }
+      } catch (e) {
+        console.error(e);
+        process.exit(1);
+      }
+    }
+  } else {
+    console.log(`- writing css to theme/${themeName}/css/${CLI_OPTIONS.outname}`);
+    fs.writeFileSync(outCSSPath, cssContent, "utf8");
+  }
+
+  console.log(`- done (${Math.ceil(performance.now() - start).toLocaleString()}ms)`);
+}
+
+// Main
+
+for (const theme of CLI_OPTIONS.themes) {
+  try {
+    if (fs.existsSync(themePath(theme, "scss"))) {
+      compileTheme(theme);
+    }
+  } catch (e) {
+    console.error(red(e));
+  }
 }
