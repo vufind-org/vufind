0957f107169d96da9086cb96302a2d23680e83bf
Ryan <rloftu01@villanova.edu>
2026-08-26 16:01:47 -0400

    Remove unused config property from Preview RecordTab

--- module/VuFind/src/VuFind/RecordTab/Preview.php
+++ module/VuFind/src/VuFind/RecordTab/Preview.php
@@ -41,13 +41,6 @@ namespace VuFind\RecordTab;
 class Preview extends AbstractBase
 {
     /**
-     * Configuration.
-     *
-     * @var array
-     */
-    protected $config = null;
-
-    /**
      * Is this tab active?
      *
      * @var bool
