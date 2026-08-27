5fffc775e4c209f45f6c26e539ee39207a9a3423
Ryan <rloftu01@villanova.edu>
2026-08-27 11:33:27 -0400

    change handling of values

--- module/VuFind/src/VuFind/Feature/BulkActionTrait.php
+++ module/VuFind/src/VuFind/Feature/BulkActionTrait.php
@@ -96,9 +96,9 @@ trait BulkActionTrait
             $formats = $this->export->getActiveFormats('bulk');
             return max(array_map([$this, 'getExportActionLimit'], $formats));
         }
-        $bulkActionConfig = $this->getBulkActionConfig()?->BulkActions;
-        return $bulkActionConfig?->limits?->$action
-            ?? $bulkActionConfig?->limits?->default
+        $bulkActionConfig = $this->getBulkActionConfig()['BulkActions'];
+        return $bulkActionConfig['limits']['?$action']
+            ?? $bulkActionConfig['limits']['default']
             ?? 100;
     }
 
@@ -111,10 +111,10 @@ trait BulkActionTrait
      */
     public function getExportActionLimit($format)
     {
-        $bulkActionConfig = $this->getBulkActionConfig()?->BulkActions;
-        return $this->getBulkActionExportConfig()?->$format?->limit
-            ?? $bulkActionConfig?->limits?->export
-            ?? $bulkActionConfig?->limits?->default
+        $bulkActionConfig = $this->getBulkActionConfig()['BulkActions'];
+        return $this->getBulkActionExportConfig()['$format']['limit']
+            ?? $bulkActionConfig['limits']['export']
+            ?? $bulkActionConfig['limits']['default']
             ?? 100;
     }
 }
