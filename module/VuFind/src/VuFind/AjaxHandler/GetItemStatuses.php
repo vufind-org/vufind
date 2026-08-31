e94304e8cbf86ba96692925b95ce0eef8c41ca78
Ryan <rloftu01@villanova.edu>
2026-08-31 10:36:35 -0400

    Use config array instead of object in GetItemStatuses Ajax handler

--- module/VuFind/src/VuFind/AjaxHandler/GetItemStatuses.php
+++ module/VuFind/src/VuFind/AjaxHandler/GetItemStatuses.php
@@ -34,7 +34,6 @@
 namespace VuFind\AjaxHandler;
 
 use Psr\Http\Message\ServerRequestInterface;
-use VuFind\Config\Config;
 use VuFind\Exception\ILS as ILSException;
 use VuFind\GetThis\GetThisLoader;
 use VuFind\Http\RouteHelper;
@@ -79,7 +78,7 @@ class GetItemStatuses extends AbstractBase implements
      * Constructor.
      *
      * @param SessionSettings           $ss                        Session settings
-     * @param Config                    $config                    Top-level configuration
+     * @param array                    $config                    Top-level configuration
      * @param Connection                $ils                       ILS connection
      * @param TemplateRendererInterface $renderer                  Template renderer
      * @param Holds                     $holdLogic                 Holds logic
@@ -89,7 +88,7 @@ class GetItemStatuses extends AbstractBase implements
      */
     public function __construct(
         SessionSettings $ss,
-        protected Config $config,
+        protected array $config,
         protected Connection $ils,
         protected TemplateRendererInterface $renderer,
         protected Holds $holdLogic,
@@ -186,7 +185,7 @@ class GetItemStatuses extends AbstractBase implements
         if ($displaySetting == 'msg' && count($list) > 1) {
             return false;
         }
-        return $this->config->Item_Status->callnumber_handler ?? false;
+        return $this->config['Item_Status']['callnumber_handler'] ?? false;
     }
 
     /**
@@ -207,8 +206,8 @@ class GetItemStatuses extends AbstractBase implements
         $this->getSorter()->sort($services);
 
         // Do we need to deal with a preferred service?
-        $preferred = isset($this->config->Item_Status->preferred_service)
-            ? $normalize($this->config->Item_Status->preferred_service) : false;
+        $preferred = isset($this->config['Item_Status']['preferred_service'])
+            ? $normalize($this->config['Item_Status']['preferred_service']) : false;
         if (false !== $preferred && in_array($preferred, $services)) {
             $services = [$preferred];
         }
@@ -509,11 +508,11 @@ class GetItemStatuses extends AbstractBase implements
         // Default case: no extra holdings fields are shown
         $holdingsTextFieldsToShow = [];
 
-        if ($this->config->Item_Status->include_holdings_text_fields ?? false) {
+        if ($this->config['Item_Status']['include_holdings_text_fields'] ?? false) {
             // If we are showing additional holdings text fields, the set of fields shown is
             // either config.ini's displayed_holdings_text_fields[] (if set), or the set of
             // all fields reported by the ILS driver otherwise.
-            $holdingsTextFieldsToShow = $this->config?->Item_Status?->displayed_holdings_text_fields?->toArray()
+            $holdingsTextFieldsToShow = $this->config['Item_Status']['displayed_holdings_text_fields']
                 ?? $this->ils->getHoldingsTextFieldNames();
         }
 
@@ -573,9 +572,9 @@ class GetItemStatuses extends AbstractBase implements
         $missingIds = array_flip($ids);
 
         // Load callnumber and location settings:
-        $callnumberSetting = $this->config->Item_Status->multiple_call_nos ?? 'msg';
-        $locationSetting = $this->config->Item_Status->multiple_locations ?? 'msg';
-        $showFullStatus = $this->config->Item_Status->show_full_status ?? false;
+        $callnumberSetting = $this->config['Item_Status']['multiple_call_nos'] ?? 'msg';
+        $locationSetting = $this->config['Item_Status']['multiple_locations'] ?? 'msg';
+        $showFullStatus = $this->config['Item_Status']['show_full_status'] ?? false;
 
         // Loop through all the status information that came back
         $statuses = [];
