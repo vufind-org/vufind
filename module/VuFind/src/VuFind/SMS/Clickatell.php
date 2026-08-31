1f1146bbe8651b055d4c88ae45e48e705118a47c
Ryan <rloftu01@villanova.edu>
2026-08-31 13:29:49 -0400

    composer fix

--- module/VuFind/src/VuFind/SMS/Clickatell.php
+++ module/VuFind/src/VuFind/SMS/Clickatell.php
@@ -55,8 +55,8 @@ class Clickatell extends AbstractBase
      * Constructor.
      *
      * @param array $config  SMS configuration
-     * @param array                 $options Additional options (client may be an
-     *                                       HTTP client object)
+     * @param array $options Additional options (client may be an
+     *                       HTTP client object)
      */
     public function __construct(array $config, $options = [])
     {
