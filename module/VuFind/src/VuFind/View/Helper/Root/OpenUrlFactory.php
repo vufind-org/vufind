100c492255d8d5921a14b2d0bce51fe25901436f
Ryan <rloftu01@villanova.edu>
2026-09-03 11:22:02 -0400

    Use config array instead of object in OpenURL root helper

--- module/VuFind/src/VuFind/View/Helper/Root/OpenUrlFactory.php
+++ module/VuFind/src/VuFind/View/Helper/Root/OpenUrlFactory.php
@@ -68,7 +68,7 @@ class OpenUrlFactory implements FactoryInterface
         if (!empty($options)) {
             throw new \Exception('Unexpected options passed to factory.');
         }
-        $config = $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigObject('config');
+        $config = $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigArray('config');
         $pathResolver = $container->get(\VuFind\Config\PathResolver::class);
         $openUrlRules = json_decode(
             file_get_contents(
