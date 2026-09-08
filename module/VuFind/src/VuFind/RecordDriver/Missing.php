d81b2faefe597ae42b00c34197350fb49a3f5994
Ryan <rloftu01@villanova.edu>
2026-09-08 15:44:47 -0400

    Use config array instead of object for Missing RecordDriver

--- module/VuFind/src/VuFind/RecordDriver/Missing.php
+++ module/VuFind/src/VuFind/RecordDriver/Missing.php
@@ -47,9 +47,9 @@ class Missing extends DefaultRecord
     /**
      * Constructor.
      *
-     * @param \VuFind\Config\Config $mainConfig   VuFind main configuration (omit
+     * @param array $mainConfig   VuFind main configuration (omit
      * for built-in defaults)
-     * @param \VuFind\Config\Config $recordConfig Record-specific configuration file
+     * @param array $recordConfig Record-specific configuration file
      * (omit to use $mainConfig as $recordConfig)
      */
     public function __construct($mainConfig = null, $recordConfig = null)
