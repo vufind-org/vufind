66aa6dfa6ffe0b513c7d9ad7fe30db751dd9801b
Ryan <rloftu01@villanova.edu>
2026-09-09 12:47:51 -0400

    eds backend reversion

--- module/VuFindSearch/src/VuFindSearch/Backend/EDS/Backend.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/EDS/Backend.php
@@ -33,6 +33,7 @@ namespace VuFindSearch\Backend\EDS;
 use Exception;
 use Laminas\Cache\Storage\StorageInterface as CacheAdapter;
 use Laminas\Session\Container as SessionContainer;
+use VuFind\Config\Config;
 use VuFind\Config\Feature\SecretTrait;
 use VuFindSearch\Backend\AbstractBackend;
 use VuFindSearch\Backend\EDS\Response\RecordCollection;
@@ -163,7 +164,7 @@ class Backend extends AbstractBackend
      * @param RecordCollectionFactoryInterface $factory Record collection factory
      * @param CacheAdapter                     $cache   Object cache
      * @param SessionContainer                 $session Session container
-     * @param ?array                           $config  Object representing EDS.ini
+     * @param ?Config                          $config  Object representing EDS.ini
      * @param bool                             $isGuest Is the current user a guest?
      */
     public function __construct(
@@ -171,7 +172,7 @@ class Backend extends AbstractBackend
         RecordCollectionFactoryInterface $factory,
         CacheAdapter $cache,
         SessionContainer $session,
-        ?array $config = null,
+        ?Config $config = null,
         $isGuest = true
     ) {
         // Save dependencies/incoming parameters:
@@ -182,12 +183,12 @@ class Backend extends AbstractBackend
         $this->isGuest = $isGuest;
 
         // Extract key values from configuration:
-        $this->userName = $config['EBSCO_Account']['user_name'] ?? null;
-        $this->password = $this->getSecretFromConfig($config['EBSCO_Account'] ?? [], 'password');
-        $this->ipAuth = $config['EBSCO_Account']['ip_auth'] ?? false;
-        $this->profile = $config['EBSCO_Account']['profile'] ?? null;
-        $this->orgId = $config['EBSCO_Account']['organization_id'] ?? null;
-        $this->validationConfig = $config['Validation'] ?? [];
+        $this->userName = $config->EBSCO_Account->user_name ?? null;
+        $this->password = $this->getSecretFromConfig($config->EBSCO_Account, 'password');
+        $this->ipAuth = $config->EBSCO_Account->ip_auth ?? false;
+        $this->profile = $config->EBSCO_Account->profile ?? null;
+        $this->orgId = $config->EBSCO_Account->organization_id ?? null;
+        $this->validationConfig = $config->Validation?->toArray() ?? [];
 
         // Save default profile value, since profile property may be overridden:
         $this->defaultProfile = $this->profile;
