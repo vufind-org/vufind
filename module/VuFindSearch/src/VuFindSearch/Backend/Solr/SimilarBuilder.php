e14e20ac49123e2124d8b3e697f0acdf84f20206
Ryan <rloftu01@villanova.edu>
2026-09-09 12:23:54 -0400

    similarbuilder reversion

--- module/VuFindSearch/src/VuFindSearch/Backend/Solr/SimilarBuilder.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/Solr/SimilarBuilder.php
@@ -83,26 +83,27 @@ class SimilarBuilder implements SimilarBuilderInterface
     /**
      * Constructor.
      *
-     * @param ?array $searchConfig Search config
-     * @param string $uniqueKey    Solr field used to store unique identifier
+     * @param ?\VuFind\Config\Config $searchConfig Search config
+     * @param string                 $uniqueKey    Solr field used to store unique identifier
      *
      * @return void
      */
     public function __construct(
-        ?array $searchConfig = null,
+        ?\VuFind\Config\Config $searchConfig = null,
         $uniqueKey = 'id'
     ) {
         $this->uniqueKey = $uniqueKey;
-        if (isset($searchConfig['MoreLikeThis'])) {
-            $mlt = $searchConfig['MoreLikeThis'];
+        if (isset($searchConfig->MoreLikeThis)) {
+            $mlt = $searchConfig->MoreLikeThis;
             if (
-                $mlt['useMoreLikeThisHandler'] ?? false
+                isset($mlt->useMoreLikeThisHandler)
+                && $mlt->useMoreLikeThisHandler
             ) {
                 $this->useHandler = true;
-                $this->handlerParams = $mlt['params'] ?? '';
+                $this->handlerParams = $mlt->params ?? '';
             }
-            if (isset($mlt['count'])) {
-                $this->count = $mlt['count'];
+            if (isset($mlt->count)) {
+                $this->count = $mlt->count;
             }
         }
     }
