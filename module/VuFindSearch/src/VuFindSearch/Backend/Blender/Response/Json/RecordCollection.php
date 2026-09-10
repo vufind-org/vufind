9f57ffb2f09d30a092301a02063cab9b9ba2dcbe
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-10 14:02:13 -0400

    Update module/VuFindSearch/src/VuFindSearch/Backend/Blender/Response/Json/RecordCollection.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFindSearch/src/VuFindSearch/Backend/Blender/Response/Json/RecordCollection.php
+++ module/VuFindSearch/src/VuFindSearch/Backend/Blender/Response/Json/RecordCollection.php
@@ -89,7 +89,7 @@ class RecordCollection extends \VuFindSearch\Backend\Solr\Response\Json\RecordCo
         $this->mappings = $mappings;
         $this->response = static::$template;
         $this->initialResultsBackends
-            = $this->config['Blending']['initialResults'] ?? [];
+            = $config['Blending']['initialResults'] ?? [];
     }
 
     /**
