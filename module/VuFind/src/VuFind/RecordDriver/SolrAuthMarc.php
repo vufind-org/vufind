d7cbc51d45c67b44ccb64a8f9595f6ce2b391ce6
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-10 14:38:02 -0400

    Update module/VuFind/src/VuFind/RecordDriver/SolrAuthMarc.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/RecordDriver/SolrAuthMarc.php
+++ module/VuFind/src/VuFind/RecordDriver/SolrAuthMarc.php
@@ -49,12 +49,9 @@ class SolrAuthMarc extends SolrAuthDefault
     /**
      * Constructor.
      *
-     * @param array $mainConfig     VuFind main configuration (omit
-     *                              for built-in defaults)
-     * @param array $recordConfig   Record-specific configuration
-     *                              file (omit to use $mainConfig as $recordConfig)
-     * @param array $searchSettings Search-specific configuration
-     *                              file
+     * @param array $mainConfig     VuFind main configuration (omit for built-in defaults)
+     * @param array $recordConfig   Record-specific configuration file (omit to use $mainConfig as $recordConfig)
+     * @param array $searchSettings Search-specific configuration file
      */
     public function __construct(
         $mainConfig = null,
