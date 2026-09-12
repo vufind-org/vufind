766b700a6301e513b35346506673e5112ce20bb0
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-12 19:32:43 -0400

    Use config array instead of object for Email Handler (#5659)

--- module/VuFind/src/VuFind/Form/Handler/Email.php
+++ module/VuFind/src/VuFind/Form/Handler/Email.php
@@ -56,41 +56,17 @@ class Email implements HandlerInterface, LoggerAwareInterface
     use LoggerAwareTrait;
 
     /**
-     * View renderer.
-     *
-     * @var RendererInterface
-     */
-    protected $viewRenderer;
-
-    /**
-     * Main config.
-     *
-     * @var Config
-     */
-    protected $mainConfig;
-
-    /**
-     * Mailer.
-     *
-     * @var Mailer
-     */
-    protected $mailer;
-
-    /**
      * Constructor.
      *
      * @param RendererInterface $viewRenderer View renderer
-     * @param Config            $config       Main config
+     * @param array             $mainConfig   Main config
      * @param Mailer            $mailer       Mailer
      */
     public function __construct(
-        RendererInterface $viewRenderer,
-        Config $config,
-        Mailer $mailer
+        protected RendererInterface $viewRenderer,
+        protected array $mainConfig,
+        protected Mailer $mailer
     ) {
-        $this->viewRenderer = $viewRenderer;
-        $this->mainConfig = $config;
-        $this->mailer = $mailer;
     }
 
     /**
@@ -155,9 +131,9 @@ class Email implements HandlerInterface, LoggerAwareInterface
     {
         $config = $this->mainConfig;
         $email = $form->getEmailFromAddress()
-            ?: $config->Feedback->sender_email ?? 'noreply@vufind.org';
+            ?: $config['Feedback']['sender_email'] ?? 'noreply@vufind.org';
         $name = $form->getEmailFromName()
-            ?: $config->Feedback->sender_name ?? 'VuFind Feedback';
+            ?: $config['Feedback']['sender_name'] ?? 'VuFind Feedback';
 
         return [$name, $email];
     }
