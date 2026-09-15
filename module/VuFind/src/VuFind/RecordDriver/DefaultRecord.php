98b3a8d5cba2a21f41abcbb90a828a69ecef573b
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-15 11:21:29 -0400

    Update module/VuFind/src/VuFind/RecordDriver/DefaultRecord.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/RecordDriver/DefaultRecord.php
+++ module/VuFind/src/VuFind/RecordDriver/DefaultRecord.php
@@ -62,12 +62,9 @@ class DefaultRecord extends AbstractBase
     /**
      * Constructor.
      *
-     * @param array $mainConfig     VuFind main configuration (omit
-     *                              for built-in defaults)
-     * @param array $recordConfig   Record-specific configuration
-     *                              file (omit to use $mainConfig as $recordConfig)
-     * @param array $searchSettings Search-specific configuration
-     *                              file
+     * @param array $mainConfig     VuFind main configuration (omit for built-in defaults)
+     * @param array $recordConfig   Record-specific configuration file (omit to use $mainConfig as $recordConfig)
+     * @param array $searchSettings Search-specific configuration file
      */
     public function __construct(
         $mainConfig = null,
