bbed7abe7f4b24e18be3bb00f17f968183731c7a
Ryan <rloftu01@villanova.edu>
2026-09-15 16:41:27 -0400

    revert backend factory

--- module/VuFind/src/VuFind/Search/Factory/PrimoBackendFactory.php
+++ module/VuFind/src/VuFind/Search/Factory/PrimoBackendFactory.php
@@ -35,6 +35,7 @@ use Lmc\Rbac\Mvc\Service\AuthorizationService;
 use Psr\Container\ContainerExceptionInterface as ContainerException;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use VuFind\Config\Config;
 use VuFind\Search\Primo\InjectOnCampusListener;
 use VuFind\Search\Primo\PrimoPermissionHandler;
 use VuFindSearch\Backend\Primo\Backend;
@@ -190,7 +191,7 @@ class PrimoBackendFactory extends AbstractBackendFactory
 
         // Attach hide facet value listener:
         $hfvListener = $this
-            ->getHideFacetValueListener($backend, $this->primoConfig);
+            ->getHideFacetValueListener($backend, new Config($this->primoConfig));
         if ($hfvListener) {
             $hfvListener->attach($events);
         }
@@ -235,7 +236,7 @@ class PrimoBackendFactory extends AbstractBackendFactory
             $session
         );
         $connector->setLogger($this->logger);
-        if ($cache = $this->createConnectorCache($this->primoConfig)) {
+        if ($cache = $this->createConnectorCache(new Config($this->primoConfig))) {
             $connector->setCache($cache);
         }
         return $connector;
