2b0e17d027ff4013b6b6d6f4769314eee8b6f890
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-15 11:29:34 -0400

    Update module/VuFind/src/VuFind/Auth/AuthInterface.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/src/VuFind/Auth/AuthInterface.php
+++ module/VuFind/src/VuFind/Auth/AuthInterface.php
@@ -71,7 +71,7 @@ interface AuthInterface
     /**
      * Set configuration.
      *
-     * @param array $config Configuration to set
+     * @param ?array $config Configuration to set
      *
      * @return void
      */
