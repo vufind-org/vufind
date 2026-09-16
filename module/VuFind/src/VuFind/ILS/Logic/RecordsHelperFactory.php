d14e7d1a0bd48edcedf99b8f095669d574a793ff
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-16 16:29:31 -0400

    Use config array instead of object in RecordsHelper ILS logic module (#5685)

--- module/VuFind/src/VuFind/ILS/Logic/RecordsHelperFactory.php
+++ module/VuFind/src/VuFind/ILS/Logic/RecordsHelperFactory.php
@@ -69,7 +69,7 @@ class RecordsHelperFactory implements FactoryInterface
             throw new \Exception('Unexpected options passed to factory.');
         }
         return new $requestedName(
-            $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigObject('config'),
+            $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigArray('config'),
             $container->get(\VuFind\Record\Loader::class)
         );
     }
