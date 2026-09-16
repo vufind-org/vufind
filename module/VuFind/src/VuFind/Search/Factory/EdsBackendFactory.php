ab065773c6a79580e69cf577543d95db965aa228
Ryan <rloftu01@villanova.edu>
2026-09-16 09:57:56 -0400

    revert some calls in edsbackendfactory

--- module/VuFind/src/VuFind/Search/Factory/EdsBackendFactory.php
+++ module/VuFind/src/VuFind/Search/Factory/EdsBackendFactory.php
@@ -34,6 +34,7 @@ use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Psr\Container\ContainerExceptionInterface as ContainerException;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use VuFind\Config\Config;
 use VuFindSearch\Backend\EDS\Backend;
 use VuFindSearch\Backend\EDS\Connector;
 use VuFindSearch\Backend\EDS\QueryBuilder;
@@ -170,7 +171,7 @@ class EdsBackendFactory extends AbstractBackendFactory
             )
         );
         $connector->setLogger($this->logger);
-        if ($cache = $this->createConnectorCache($this->edsConfig)) {
+        if ($cache = $this->createConnectorCache(new Config($this->edsConfig))) {
             $connector->setCache($cache);
         }
         return $connector;
@@ -253,7 +254,7 @@ class EdsBackendFactory extends AbstractBackendFactory
         $events = $this->getService('SharedEventManager');
 
         // Attach hide facet value listener:
-        $hfvListener = $this->getHideFacetValueListener($backend, $this->edsConfig);
+        $hfvListener = $this->getHideFacetValueListener($backend, new Config($this->edsConfig));
         if ($hfvListener) {
             $hfvListener->attach($events);
         }
