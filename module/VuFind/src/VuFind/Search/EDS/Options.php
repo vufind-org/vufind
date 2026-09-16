7da0b678007906d827f9c8efb00b6979cbea3a28
Thomas Wagener <wagener@hebis.uni-frankfurt.de>
2026-09-16 19:28:05 +0200

    Fix loading of view options for EDS (#5676)

--- module/VuFind/src/VuFind/Search/EDS/Options.php
+++ module/VuFind/src/VuFind/Search/EDS/Options.php
@@ -441,9 +441,6 @@ class Options extends AbstractEDSOptions
                 : (bool)$highlighting;
         }
 
-        // View preferences
-        $this->initViewOptions($this->searchSettings);
-
         // Load autocomplete preferences:
         $this->configureAutocomplete($this->searchSettings);
 
@@ -659,10 +656,14 @@ class Options extends AbstractEDSOptions
         // default result Limit
         $this->defaultLimit ??= $settings['ResultsPerPage'] ?? 20;
 
+        $searchSettings = $this->searchSettings ?? [];
+
         // default view
-        if (null === $this->defaultView) {
-            $this->setConfiguredDefaultView('list_' . ($settings['ResultListView'] ?? 'brief'));
+        if (!isset($searchSettings['General']['default_view']) && $defaultView = $settings['ResultListView'] ?? null) {
+            $searchSettings['General']['default_view'] = 'list_' . $defaultView;
         }
+
+        $this->initViewOptions($searchSettings);
     }
 
     /**
@@ -765,5 +766,15 @@ class Options extends AbstractEDSOptions
     {
         // Note that getApiProperty() will retrieve any defaultView value set by setConfiguredDefaultView().
         return $this->getApiProperty('defaultView');
+    }
+
+    /**
+     * Get an array of view options.
+     *
+     * @return array
+     */
+    public function getViewOptions()
+    {
+        return $this->getApiProperty('viewOptions');
     }
 }
