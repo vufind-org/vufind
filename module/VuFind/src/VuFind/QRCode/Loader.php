a920e053476c373347691fae6db7f2a8e181b93c
Ryan <rloftu01@villanova.edu>
2026-09-16 10:39:58 -0400

    Use config array instead of object for QR Code loader module

--- module/VuFind/src/VuFind/QRCode/Loader.php
+++ module/VuFind/src/VuFind/QRCode/Loader.php
@@ -61,14 +61,14 @@ class Loader extends \VuFind\ImageLoader
     /**
      * Constructor.
      *
-     * @param \VuFind\Config\Config  $config VuFind configuration
+     * @param array                  $config VuFind configuration
      * @param \VuFindTheme\ThemeInfo $theme  VuFind theme tools
      */
     public function __construct($config, \VuFindTheme\ThemeInfo $theme)
     {
         $this->setThemeInfo($theme);
         $this->configuredFailImage
-            = $config->QRCode->noQRCodeAvailableImage ?? null;
+            = $config['QRCode']['noQRCodeAvailableImage'] ?? null;
         $this->defaultFailImage = 'images/noQRCode.gif';
     }
 
--- module/VuFind/src/VuFind/QRCode/LoaderFactory.php
+++ module/VuFind/src/VuFind/QRCode/LoaderFactory.php
@@ -69,7 +69,7 @@ class LoaderFactory implements FactoryInterface
             throw new \Exception('Unexpected options passed to factory.');
         }
         return new $requestedName(
-            $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigObject('config'),
+            $container->get(\VuFind\Config\ConfigManagerInterface::class)->getConfigArray('config'),
             $container->get(\VuFindTheme\ThemeInfo::class)
         );
     }
