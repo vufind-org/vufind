2f6d7b83fbf4501d9dbe19a4f2043bd1512c351a
Ryan Loftus <152215889+rtloftus@users.noreply.github.com>
2026-09-17 16:59:14 -0400

    Update module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/WorldCat2Test.php
    Co-authored-by: Demian Katz <demian.katz@villanova.edu>

--- module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/WorldCat2Test.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/RecordDriver/WorldCat2Test.php
@@ -275,8 +275,7 @@ class WorldCat2Test extends \PHPUnit\Framework\TestCase
     #[\PHPUnit\Framework\Attributes\DataProvider('methodTests')]
     public function testMethod(string $method, $expected, ?string $fixture = null, array $config = [])
     {
-        $configArr = $config;
-        $driver = new WorldCat2($configArr, $configArr, $configArr);
+        $driver = new WorldCat2($config, $config, $config);
         if ($fixture) {
             $driver->setRawData(
                 json_decode($this->getFixture($fixture), true)
