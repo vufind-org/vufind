87e9b2f3c409ac2209ee8eb0371e8b5599339c6d
Demian Katz <demian.katz@villanova.edu>
2026-09-17 16:05:00 -0400

    Remove stray brackets.

--- module/VuFind/src/VuFind/Auth/Shibboleth.php
+++ module/VuFind/src/VuFind/Auth/Shibboleth.php
@@ -141,7 +141,7 @@ class Shibboleth extends AbstractBase
     {
         // Throw an exception if the required username setting is missing.
         $shib = $this->config['Shibboleth'] ?? [];
-        if (empty([$shib['username']])) {
+        if (empty($shib['username'])) {
             throw new AuthException(
                 'Shibboleth username is missing in your configuration file.'
             );
