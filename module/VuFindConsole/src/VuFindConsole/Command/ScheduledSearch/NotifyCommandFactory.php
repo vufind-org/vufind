68dea302bf139481b01b545d1233d036286425e5
Ryan <rloftu01@villanova.edu>
2026-09-17 10:16:30 -0400

    edited notifycommandfactory and loggerfactory

--- module/VuFindConsole/src/VuFindConsole/Command/ScheduledSearch/NotifyCommandFactory.php
+++ module/VuFindConsole/src/VuFindConsole/Command/ScheduledSearch/NotifyCommandFactory.php
@@ -34,7 +34,6 @@ use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Psr\Container\ContainerExceptionInterface as ContainerException;
 use Psr\Container\ContainerInterface;
-use VuFind\Config\Config;
 use VuFind\Config\PathResolver;
 use VuFind\Db\Service\SearchServiceInterface;
 
@@ -83,7 +82,7 @@ class NotifyCommandFactory implements FactoryInterface
             $container->get('ViewRenderer'),
             $container->get(\VuFind\Search\Results\PluginManager::class),
             $scheduleOptions,
-            new Config($mainConfig),
+            $mainConfig,
             $container->get(\VuFind\Mailer\Mailer::class),
             $container->get(\VuFind\Db\Service\PluginManager::class)->get(SearchServiceInterface::class),
             $container->get(\VuFind\I18n\Locale\LocaleSettings::class),
