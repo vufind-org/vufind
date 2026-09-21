9cd6fffd7e03c84d89b048bac74a7d92459486b5
Ryan <rloftu01@villanova.edu>
2026-09-21 14:16:53 -0400

    fixed eds backend and primo backend

--- module/VuFind/src/VuFind/Search/Factory/EdsBackendFactory.php
+++ module/VuFind/src/VuFind/Search/Factory/EdsBackendFactory.php
@@ -34,7 +34,6 @@ use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Psr\Container\ContainerExceptionInterface as ContainerException;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
-use VuFind\Config\Config;
 use VuFindSearch\Backend\EDS\Backend;
 use VuFindSearch\Backend\EDS\Connector;
 use VuFindSearch\Backend\EDS\QueryBuilder;
@@ -254,7 +253,7 @@ class EdsBackendFactory extends AbstractBackendFactory
         $events = $this->getService('SharedEventManager');
 
         // Attach hide facet value listener:
-        $hfvListener = $this->getHideFacetValueListener($backend, new Config($this->edsConfig));
+        $hfvListener = $this->getHideFacetValueListener($backend, $this->edsConfig);
         if ($hfvListener) {
             $hfvListener->attach($events);
         }
