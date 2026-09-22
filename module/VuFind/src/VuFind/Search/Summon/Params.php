a38f13c86f370e592345a353eff192130e7deab0
Ryan <rloftu01@villanova.edu>
2026-09-22 12:04:55 -0400

    params edit

--- module/VuFind/src/VuFind/Search/Summon/Params.php
+++ module/VuFind/src/VuFind/Search/Summon/Params.php
@@ -87,9 +87,9 @@ class Params extends \VuFind\Search\Base\Params
     public function __construct(\VuFind\Search\Base\Options $options, ConfigManagerInterface $configManager)
     {
         parent::__construct($options, $configManager);
-        $config = $configManager->getConfigObject($options->getFacetsIni());
-        $facetSettings = isset($config->Facet_Settings)
-            ? $config->Facet_Settings->toArray() : null;
+        $config = $configManager->getConfigArray($options->getFacetsIni());
+        $facetSettings = isset($config['Facet_Settings'])
+            ? $config['Facet_Settings'] : null;
         $this->initFacetLimitsFromConfig($facetSettings);
     }
 
@@ -402,13 +402,13 @@ class Params extends \VuFind\Search\Base\Params
     protected function initFacetList(string $facetList, string $facetSettings, ?string $cfgFile = null): bool
     {
         $facetConfigName = $cfgFile ?? $this->getOptions()->getFacetsIni();
-        $config = ($facetConfigName !== null) ? $this->configManager->getConfigObject($facetConfigName) : [];
+        $config = ($facetConfigName !== null) ? $this->configManager->getConfigArray($facetConfigName) : [];
         // Special case -- when most settings are in Results_Settings, the limits
         // can be found in Facet_Settings.
         $limitSection = ($facetSettings === 'Results_Settings')
             ? 'Facet_Settings' : $facetSettings;
-        $facetLimitConfig = isset($config->$limitSection)
-            ? $config->$limitSection->toArray() : null;
+        $facetLimitConfig = isset($config['$limitSection'])
+            ? $config['$limitSection'] : null;
         $this->initFacetLimitsFromConfig($facetLimitConfig);
         return parent::initFacetList($facetList, $facetSettings, $cfgFile);
     }
--- module/VuFind/tests/unit-tests/src/VuFindTest/Auth/ChoiceAuthTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/Auth/ChoiceAuthTest.php
@@ -34,7 +34,6 @@ use Laminas\Session\Container;
 use PHPUnit\Framework\MockObject\MockObject;
 use VuFind\Auth\ChoiceAuth;
 use VuFind\Auth\PluginManager;
-use VuFind\Config\Config;
 use VuFind\Db\Entity\UserEntityInterface;
 use VuFind\Http\PhpEnvironment\Request as PhpEnvironmentRequest;
 
