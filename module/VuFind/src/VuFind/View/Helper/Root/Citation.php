341a51ab6ee4eef1057f1e72d1b5bf26becca418
Demian Katz <demian.katz@villanova.edu>
2026-09-24 14:03:20 -0400

    Improve accuracy of Citation view helper comments.

--- module/VuFind/src/VuFind/View/Helper/Root/Citation.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Citation.php
@@ -118,9 +118,9 @@ class Citation extends \Laminas\View\Helper\AbstractHelper implements Translator
      * Store a record driver object and return this object so that the appropriate
      * template can be rendered.
      *
-     * @param \VuFind\RecordDriver\Base $driver Record driver object.
+     * @param \VuFind\RecordDriver\AbstractBase $driver Record driver object.
      *
-     * @return Citation
+     * @return static
      */
     public function __invoke($driver)
     {
