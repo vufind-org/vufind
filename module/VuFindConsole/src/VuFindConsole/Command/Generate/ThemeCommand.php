8c34ca9805336fae5a3cbbcdd26b991aeae42752
Ryan <rloftu01@villanova.edu>
2026-09-24 10:57:27 -0400

    removed name parameter from construct giving errors

--- module/VuFindConsole/src/VuFindConsole/Command/Generate/ThemeCommand.php
+++ module/VuFindConsole/src/VuFindConsole/Command/Generate/ThemeCommand.php
@@ -59,16 +59,13 @@ class ThemeCommand extends AbstractThemeCommand
      *
      * @param ThemeGenerator $generator Generator to call
      * @param array          $config    Configuration from config.ini
-     * @param string|null    $name      The name of the command; passing null
-     * means it must be set in configure()
      */
     public function __construct(
         ThemeGenerator $generator,
         #[Autowire(config: 'config')]
         protected array $config,
-        $name = null
     ) {
-        parent::__construct($generator, $name);
+        parent::__construct($generator);
     }
 
     /**
